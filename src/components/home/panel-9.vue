d9b2ddfaa503ac7b7c14e0dcb25dadc57dc4b6e5
Joff Tiquez <jofftiquez@gmail.com>
2018-06-06 21:38:34 +0800

    New panel 9

---
+++ src/components/home/panel-9.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  div.panel-9.pt-5.pb-5
+    div.container
+      div.row.align-items-center
+        div.col-xs-12.col-md-12.text-center
+          center
+            div.hr-blue
+          br
+          h1 Care to join us?
+          br
+          p Tell us why you're here:
+          br
+        div.col-xs-12.col-md-4.offset-md-2
+          b-button(variant="outline-primary" size="sm" style="width: 100%") I wanna learn more about your product
+          br
+          br
+          b-button(variant="outline-primary" size="sm" style="width: 100%") I wanna partner with MYCURE
+        div.col-xs-12.col-md-4
+          b-button(variant="outline-primary" size="sm" style="width: 100%") I wanna join the MYCURE team
+          br
+          br
+          b-button(variant="outline-primary" size="sm" style="width: 100%") I wanna care the extra mile with you
+        div.col-xs-12.col-md-12.text-center.mt-4
+          b-button(variant="success" size="sm") I wanna signup now
+</template>
+
+<script>
+  import mcNavbarHome from '../commons/mc-navbar-home';
+  export default {
+    components: {
+      mcNavbarHome
+    }
+  }
+</script>
+
+<style scoped>
+  .panel-9 {
+    /* height: 100vh; */
+    background-color: white;
+  }
+</style>
