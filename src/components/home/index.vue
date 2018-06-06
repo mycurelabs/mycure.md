c2ff5590aaf127534aa861aea94207a6a266c2e6
Joff Tiquez <jofftiquez@gmail.com>
2018-06-06 21:37:40 +0800

    Added panels 4-9

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -3,6 +3,12 @@
     panel-1
     panel-2
     panel-3
+    panel-4
+    panel-5
+    panel-6
+    panel-7
+    panel-8
+    panel-9
       
 </template>
 
@@ -10,11 +16,24 @@
   import panel1 from './panel-1';
   import panel2 from './panel-2';
   import panel3 from './panel-3';
+  import panel4 from './panel-4';
+  import panel5 from './panel-5';
+  import panel6 from './panel-6';
+  import panel7 from './panel-7';
+  import panel8 from './panel-8';
+  import panel9 from './panel-9';
+
   export default {
     components: {
       'panel-1': panel1,
       'panel-2': panel2,
-      'panel-3': panel3
+      'panel-3': panel3,
+      'panel-4': panel4,
+      'panel-5': panel5,
+      'panel-6': panel6,
+      'panel-7': panel7,
+      'panel-8': panel8,
+      'panel-9': panel9
     }
   }
 </script>
