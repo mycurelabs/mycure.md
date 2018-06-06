0b4811c0c96829dd3593e62ea2fa8cec73f83aee
Joff Tiquez <jofftiquez@gmail.com>
2018-06-06 21:38:31 +0800

    New panel 8

---
+++ src/components/home/panel-8.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  div.panel-8.pt-5.pb-5
+    div.container
+      div.row.align-items-center
+        div.col-xs-12.col-md-12.text-center
+          center
+            div.hr-blue
+          br
+          h1 Caring the extra mile is kinda our thing
+          br
+          p Id quis sunt sit sint nostrud dolore consectetur enim ut.
+            br
+            | Proident exercitation voluptate.
+          br
+          b-button(variant="primary" size="sm") Read our story
+</template>
+
+<script>
+  import mcNavbarHome from '../commons/mc-navbar-home';
+  export default {
+    components: {
+      mcNavbarHome
+    }
+  }
+</script>
+
+<style scoped>
+  .panel-8 {
+    /* height: 100vh; */
+    /* background-color: white; */
+  }
+</style>
