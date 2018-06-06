11fd59ca4756f4e0cf6d8629d8c8d600cd599d28
Joff Tiquez <jofftiquez@gmail.com>
2018-06-06 21:38:26 +0800

    New panel 7

---
+++ src/components/home/panel-7.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div.panel-6
+    img(width="100%" src="../../assets/images/v4/1600x515px.png")
+            
+</template>
+
+<script>
+  import mcNavbarHome from '../commons/mc-navbar-home';
+  export default {
+    components: {
+      mcNavbarHome
+    }
+  }
+</script>
+
+<style scoped>
+  .panel-6 {
+    /* height: 100vh; */
+    background-color: white;
+  }
+</style>
