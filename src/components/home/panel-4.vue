abc288d5e74336ee3510b2522b6ce6439fe18180
Joff Tiquez <jofftiquez@gmail.com>
2018-06-06 21:38:12 +0800

    New panel 4

---
+++ src/components/home/panel-4.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+  div.panel-4
+    div.container
+      div(style="height: 100vh").row.align-items-center
+        div.col-xs-12.col-md-12.text-center
+          center
+            div.hr-blue
+          br
+          h1 Medical Records matter to you
+          br
+          p 
+            strong They matter to us too, 
+            | so we perform industry standard
+            br
+            | encryption to protect your records.
+          br
+          div.row.justify-content-center
+            div.col-xs-12.pl-1.pr-1
+              img(src="../../assets/images/v4/100x130px.png")
+              br
+              strong Foo
+            div.col-xs-12.pl-1.pr-1
+              img(src="../../assets/images/v4/100x130px.png")
+              br
+              strong Foo
+            div.col-xs-12.pl-1.pr-1
+              img(src="../../assets/images/v4/100x130px.png")
+              br
+              strong Foo
+            div.col-xs-12.pl-1.pr-1
+              img(src="../../assets/images/v4/100x130px.png")
+              br
+              strong Foo
+            div.col-xs-12.pl-1.pr-1
+              img(src="../../assets/images/v4/100x130px.png")
+              br
+              strong Foo
+          div.col-xs-12.pt-5
+            b-button(variant="primary" size="sm") Data Privacy for Physicians
+</template>
+
+<script>
+  import mcNavbarHome from '../commons/mc-navbar-home';
+  export default {
+    components: {
+      mcNavbarHome
+    }
+  }
+</script>
+
+<style scoped>
+  .panel-4 {
+    height: 100vh;
+    background-color: white;
+  }
+</style>
