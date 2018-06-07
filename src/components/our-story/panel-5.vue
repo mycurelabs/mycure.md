d049435750b4c2a72cb7667e292c845be1d33cb6
Joff Tiquez <jofftiquez@gmail.com>
2018-06-08 02:55:09 +0800

    New panel5

---
+++ src/components/our-story/panel-5.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+  div.panel-3
+    div.container
+      div.row.row-container.align-items-center.justify-content-md-center
+        div.col-xs-12.col-md-8.text-center
+          center
+            div.hr-blue
+          br
+          h1 About the Founders
+          br
+          p Id incididunt sint anim cupidatat ut est nisi enim est nostrud velit eiusmod minim. Qui aliquip fugiat tempor nulla sint fugiat anim cupidatat minim excepteur eiusmod pariatur aliquip. Lorem non proident nulla exercitation ullamco duis anim eu proident incididunt. Veniam ex irure consectetur labore dolor labore amet laborum duis reprehenderit. Tempor tempor veniam officia pariatur. Dolor sunt do consequat occaecat duis tempor in sint minim cillum.
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .panel-3 {
+    min-height: 100vh;
+    background-color: white;
+  }
+
+  .container {
+    /* padding-top: 50px; */
+    /* padding-bottom: 50px; */
+  }
+
+  @media (min-width: 576px) {
+    .container {
+      padding-top: 50px;
+      padding-bottom: 50px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .panel-2 {
+      min-height: 100vh;
+      background-color: white;
+    }
+
+    .container {
+      padding-top: 0;
+      padding-bottom: 0;
+    }
+
+    .row-container {
+      height: 100vh;
+    }
+  }
+
+  @media (min-width: 992px) {
+
+  }
+
+  @media (min-width: 1200px) {
+
+  }
+</style>
