07ee8b78d385b459675d4b638462646f7f06821f
Joff Tiquez <jofftiquez@gmail.com>
2018-06-08 02:55:19 +0800

    New panel 7

---
+++ src/components/our-story/panel-7.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+  div.panel-3
+    div.container
+      div.row.row-container.align-items-center
+        div.col-xs-12.col-md-8
+          div.hr-blue
+          br
+          h1 Joel
+          br
+          p Laborum cupidatat nulla in nisi ex velit enim do. Cillum enim nostrud aliqua aliqua incididunt amet commodo voluptate nisi. Fugiat sunt ullamco excepteur enim id anim do commodo aliqua aliqua officia et elit.
+        div.col-xs-12.col-md-4
+          img(width="100%" src="../../assets/images/v4/234x234px.png")
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .panel-3 {
+    min-height: 100vh;
+    background-color: white;
+  }
+
+  .container {
+    /* padding-top: 50px; */
+    /* padding-bottom: 50px; */
+  }
+
+  @media (min-width: 576px) {
+    .container {
+      padding-top: 50px;
+      padding-bottom: 50px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .panel-2 {
+      min-height: 100vh;
+      background-color: white;
+    }
+
+    .container {
+      padding-top: 0;
+      padding-bottom: 0;
+    }
+
+    .row-container {
+      height: 100vh;
+    }
+  }
+
+  @media (min-width: 992px) {
+
+  }
+
+  @media (min-width: 1200px) {
+
+  }
+</style>
