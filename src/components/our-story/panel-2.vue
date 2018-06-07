003260424157f4fbda4858aebf5ad11d04bd02c9
Joff Tiquez <jofftiquez@gmail.com>
2018-06-08 02:54:55 +0800

    New panel 2

---
+++ src/components/our-story/panel-2.vue
@@ -0,0 +1,64 @@
+<template lang="pug">
+  div.panel-2.pt-5
+    div.container
+      div.row.row-container
+        div.col-xs-12.col-md-12.text-center
+          center
+            div.hr-blue
+          br
+          h1 MYCURE starts and ends with real people and real stories
+          br
+        div.col-xs-12.col-md-4
+          img(width="100%" src="../../assets/images/v4/234x234px.png")
+        div.col-xs-12.col-md-8
+          p Id incididunt sint anim cupidatat ut est nisi enim est nostrud velit eiusmod minim. Qui aliquip fugiat tempor nulla sint fugiat anim cupidatat minim excepteur eiusmod pariatur aliquip. Lorem non proident nulla exercitation ullamco duis anim eu proident incididunt. Veniam ex irure consectetur labore dolor labore amet laborum duis reprehenderit. Tempor tempor veniam officia pariatur. Dolor sunt do consequat occaecat duis tempor in sint minim cillum.
+          p Dolore cillum ex ipsum exercitation tempor aliquip labore exercitation ipsum elit laboris ea cillum. Sunt ut ea sunt aute sit ea incididunt nisi pariatur labore velit eu dolor voluptate. Aliqua amet fugiat mollit qui id mollit aliqua ullamco do consectetur pariatur nisi voluptate. Proident cillum aliqua dolore fugiat sit nostrud deserunt mollit et. Magna sunt reprehenderit sit pariatur quis reprehenderit mollit velit commodo anim. Aliquip pariatur consequat officia ullamco proident.
+          p Minim magna aliqua veniam officia adipisicing.
+          
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .panel-2 {
+    min-height: 100vh;
+    /* background-color: white; */
+  }
+
+  .container {
+    /* padding-top: 50px; */
+    /* padding-bottom: 50px; */
+  }
+
+  @media (min-width: 576px) {
+    .container {
+      padding-top: 50px;
+      padding-bottom: 50px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .panel-2 {
+      min-height: 100vh;
+      /* background-color: white; */
+    }
+
+    .container {
+      padding-top: 0;
+      padding-bottom: 0;
+    }
+
+    .row-container {
+      height: 100vh;
+    }
+  }
+
+  @media (min-width: 992px) {
+
+  }
+
+  @media (min-width: 1200px) {
+
+  }
+</style>
