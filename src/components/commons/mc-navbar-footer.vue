f02193f692ce954a7d2ae207fe4451dde6312cf8
Joff Tiquez <jofftiquez@gmail.com>
2018-06-07 15:36:07 +0800

    navbar for footer

---
+++ src/components/commons/mc-navbar-footer.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  b-navbar(toggleable="md" type="dark" variant="transparent")
+    b-navbar-toggle(target="nav_collapse")
+    
+    b-navbar-brand(href="#") 
+      img(src="../../assets/images/v4/mycure-logo.svg")
+
+    b-collapse(is-nav id="nav_collapse")
+      b-navbar-nav.ml-auto
+        b-nav-item
+          label Follow us on:
+        b-nav-item(href="") 
+          img(src="../../assets/images/v4/mycure-homepage-wireframe-icon-fb.png")
+        b-nav-item(href="")
+          img(src="../../assets/images/v4/mycure-homepage-wireframe-icon-ytub-e.png")
+        b-nav-item(href="")
+          img(src="../../assets/images/v4/mycure-homepage-wireframe-icon-ig.png")
+        b-nav-item(href="")
+          img(src="../../assets/images/v4/mycure-homepage-wireframe-icon-gplus.png")
+        b-nav-item(href="")
+          img(src="../../assets/images/v4/mycure-homepage-wireframe-icon-linkedin.png")
+        b-nav-item(href="")
+          img(src="../../assets/images/v4/mycure-homepage-wireframe-icon-twitter.png")
+
+      b-navbar-nav.ml-auto
+        b-form(@submit.prevent="onSubmit")
+          b-form-input(v-model="search" class="form-control-dark" type="text" placeholder="Search")
+</template>
