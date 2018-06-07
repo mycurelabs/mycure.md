39e855b0a6fbab5fabe196ecdb8e8c355d323326
Joff Tiquez <jofftiquez@gmail.com>
2018-06-08 02:55:00 +0800

    New panel 3

---
+++ src/components/our-story/panel-3.vue
@@ -0,0 +1,60 @@
+<template lang="pug">
+  div.panel-3
+    div.container
+      div.row.row-container.align-items-center
+        div.col-xs-12.col-md-8
+          div.hr-blue
+          br
+          h1 Our Purpose
+          br
+          p Id incididunt sint anim cupidatat ut est nisi enim est nostrud velit eiusmod minim. Qui aliquip fugiat tempor nulla sint fugiat anim cupidatat minim excepteur eiusmod pariatur aliquip. Lorem non proident nulla exercitation ullamco duis anim eu proident incididunt. Veniam ex irure consectetur labore dolor labore amet laborum duis reprehenderit. Tempor tempor veniam officia pariatur. Dolor sunt do consequat occaecat duis tempor in sint minim cillum.
+          p Id incididunt sint anim cupidatat ut est nisi enim est nostrud velit eiusmod minim. Qui aliquip fugiat tempor nulla sint fugiat anim cupidatat minim excepteur eiusmod pariatur aliquip. Lorem non proident nulla exercitation ullamco duis anim eu proident incididunt. Veniam ex irure consectetur labore dolor labore amet laborum duis reprehenderit. Tempor tempor veniam officia pariatur. Dolor sunt do consequat occaecat duis tempor in sint minim cillum.
+        div.col-xs-12.col-md-4
+          img(width="100%" src="../../assets/images/v4/234x234px.png")
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .panel-3 {
+    min-height: 100vh;
+    background-color: white;
+  }
+
+  .container {
+    /* padding-top: 50px; */
+    /* padding-bottom: 50px; */
+  }
+
+  @media (min-width: 576px) {
+    .container {
+      padding-top: 50px;
+      padding-bottom: 50px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .panel-2 {
+      min-height: 100vh;
+      background-color: white;
+    }
+
+    .container {
+      padding-top: 0;
+      padding-bottom: 0;
+    }
+
+    .row-container {
+      height: 100vh;
+    }
+  }
+
+  @media (min-width: 992px) {
+
+  }
+
+  @media (min-width: 1200px) {
+
+  }
+</style>
