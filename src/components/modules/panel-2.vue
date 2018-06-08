e05403952b13e41cc6739df285621afaf6a0636d
Joff Tiquez <jofftiquez@gmail.com>
2018-06-09 03:29:53 +0800

    Modules page > panel 2

---
+++ src/components/modules/panel-2.vue
@@ -0,0 +1,181 @@
+<template lang="pug">
+  div.panel-2
+    b-container(fluid)
+      b-row.justify-content-md-center
+        b-col(md="2")
+        b-col(xs="12" md="7").paper.pl-5.pr-5
+          img(src="../../assets/images/v4/mycure-modules-pin.png" :class="{'animate': scrollPositionTop > 300}").pin
+          img(src="../../assets/images/v4/mycure-modules-plantg.png" :class="{'animate': scrollPositionTop > 420}").pot
+          img(src="../../assets/images/v4/mycure-modules-pen-and-pad.png" :class="{'animate': scrollPositionTop > 570}").notepad
+          img(src="../../assets/images/v4/mycure-modules-white-envelope.png" :class="{'animate': scrollPositionTop > 750}").envelope
+          img(src="../../assets/images/v4/mycure-modules-plate.png" :class="{'animate': scrollPositionTop > 1100}").plate
+          img(src="../../assets/images/v4/mycure-modules-chickpeas.png" :class="{'animate': scrollPositionTop > 1200}").peas
+          b-row.align-items-center.justify-content-md-center
+            b-col(xs="12" md="12").text-center
+              h2 HOW IT WORKS
+              center
+                div.hr-blue
+                div.hr-blue
+              br
+              h1 Create the perfect blend 
+                br 
+                | for you clinic
+              br
+              center
+                div.hr-blue
+          b-row.mt-5.pt-5.justify-content-md-center
+            b-col(xs="12" md="4")
+              h2.mb-5 Core
+              ul.list
+                li ⛋ Doctor
+                li ⛋ Billing
+                li ⛋ Laboratory
+                li ⛋ Imaging
+                li ⛋ Inventory
+            b-col(xs="12" md="4")
+              h2.mb-5 Premium
+              ul.list
+                li ⛋ Point-of-Sales
+                li ⛋ Staff Chatbox
+                li ⛋ Sync Gateway
+                li ⛋ Reports and Analytics
+                li ⛋ Physical Medical Exam
+            b-col(xs="12" md="4")
+              h2.mb-5 Add-ons
+              ul.list
+                li ⛋ DPA Registration Guide
+                li ⛋ Awesome Care
+                li ⛋ Specialty Features
+          //- b-row.mt-5.pt-5
+            b-col(xs="12" md="4")
+              p More coming soon
+              ul.list
+                li ⛋ Patient
+                li ⛋ Philhealth
+                li ⛋ Insurance
+
+        b-col(md="2")
+</template>
+
+<script>
+  export default {
+    created() {
+      this.$scrollspy(scrollPos => {
+        this.scrollPositionTop = scrollPos;
+      });
+    },
+    data() {
+      return {
+        scrollPositionTop: 0
+      }
+    },
+    destroyed() {
+      this.$removeScrollspy();
+    }
+  }
+</script>
+
+<style scoped>
+  .panel-2 {
+    padding-top: 100px;
+    padding-bottom: 600px;
+    background-image: url('../../assets/images/v4/mycure-homepage-background-wood.png');
+    background-repeat: repeat;
+  }
+
+  .paper {
+    box-shadow: 0px 0px 40px 0px rgba(160,181,191,0.66);
+    -webkit-box-shadow: 0px 0px 40px 0px rgba(160,181,191,0.66);
+    -moz-box-shadow: 0px 0px 40px 0px rgba(160,181,191,0.66);
+    padding-top: 50px;
+    padding-bottom: 50px;
+    background-color: #eff3f5;
+    height: 800px;
+  }
+
+  .square {
+    width: 10px;
+    height: 10px;
+    border: 2px solid black;
+  }
+
+  .pin {
+    width: 200px;
+    position: absolute;
+    right: -1000px;
+    top: -50px;
+    -webkit-transition: right 500ms; /* Safari */
+    transition: right 500ms;
+  }
+
+  .pin.animate {
+    right: -100px;
+  }
+  
+  .pot {
+    width: 700px;
+    position: absolute;
+    left: -2000px;
+    top: -100px;
+    -webkit-transition: left 500ms; /* Safari */
+    transition: left 500ms;
+  }
+
+  .pot.animate {
+    left: -400px;
+  }
+
+  .notepad {
+    width: 300px;
+    position: absolute;
+    right: -1000px;
+    top: 200px;
+    -webkit-transition: right 500ms; /* Safari */
+    transition: right 500ms;
+  }
+
+  .notepad.animate {
+    right: -350px;
+  }
+  
+  .envelope {
+    width: 450px;
+    position: absolute;
+    left: -1000px;
+    bottom: -400px;
+    -webkit-transition: left 500ms; /* Safari */
+    transition: left 500ms;
+  }
+
+  .envelope.animate {
+    left: -520px;
+  }
+
+  .peas {
+    width: 380px;
+    position: absolute;
+    left: -1000px;
+    bottom: -400px;
+    -webkit-transition: left 500ms; /* Safari */
+    transition: left 500ms;
+  }
+
+  .peas.animate {
+    left: -150px;
+  }
+  
+  .plate {
+    width: 800px;
+    position: absolute;
+    right: -2000px;
+    bottom: -550px;
+    z-index: inherit;
+    -webkit-transition: right 500ms; /* Safari */
+    transition: right 500ms;
+  }
+
+  .plate.animate {
+    right: -450px;
+  }
+</style>
+
