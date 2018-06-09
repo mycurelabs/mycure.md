579a0f13f2145ddf4af0cc4a83255a3554ac1b2a
Joff Tiquez <jofftiquez@gmail.com>
2018-06-10 03:01:58 +0800

    Added panels 3-6

--- src/components/modules/index.vue
+++ src/components/modules/index.vue
@@ -2,12 +2,12 @@
   div
     panel-1
     panel-2
-    //- panel-3
-    //- panel-4
-    //- panel-5
-    //- panel-6
+    panel-3
+    panel-4
+    panel-5
+    panel-6
     //- panel-7
-    join-us
+    //- join-us
     mc-footer
       
 </template>
@@ -15,24 +15,24 @@
 <script>
   import panel1 from './panel-1';
   import panel2 from './panel-2';
-  // import panel3 from './panel-3';
-  // import panel4 from './panel-4';
-  // import panel5 from './panel-5';
-  // import panel6 from './panel-6';
+  import panel3 from './panel-3';
+  import panel4 from './panel-4';
+  import panel5 from './panel-5';
+  import panel6 from './panel-6';
   // import panel7 from './panel-7';
-  import joinUs from '../home/panel-9';
+  // import joinUs from '../home/panel-9';
   import mcFooter from '../commons/mc-footer';
 
   export default {
     components: {
       'panel-1': panel1,
       'panel-2': panel2,
-      // 'panel-3': panel3,
-      // 'panel-4': panel4,
-      // 'panel-5': panel5,
-      // 'panel-6': panel6,
+      'panel-3': panel3,
+      'panel-4': panel4,
+      'panel-5': panel5,
+      'panel-6': panel6,
       // 'panel-7': panel7,
-      joinUs,
+      // joinUs,
       mcFooter
     }
   }
