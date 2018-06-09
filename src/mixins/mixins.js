81044d381d567f60023f16f27b6cc995e35a058f
Joff Tiquez <jofftiquez@gmail.com>
2018-06-09 18:16:12 +0800

    Obsearvable for scrollspy mixin

--- src/mixins/mixins.js
+++ src/mixins/mixins.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
+import { Observable } from 'rxjs/Observable';
 
 Vue.mixin({
   methods: {
-    $scrollspy: (cb) => {
-      window.addEventListener('scroll', (e) => {
-        cb(window.pageYOffset || document.documentElement.scrollTop);
-      });
+    $scrollspy: () => {
+      return Observable.create(obs => {
+        window.addEventListener('scroll', (e) => {
+          obs.next(window.pageYOffset || document.documentElement.scrollTop);
+        });
+      })
     },
     $removeScrollspy: () => {
       window.removeEventListener('scroll', (e) => {
