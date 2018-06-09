3665daf4620519ae38e61b0535ad9669bb79a96c
Joff Tiquez <jofftiquez@gmail.com>
2018-06-10 03:02:09 +0800

    New panel 4

---
+++ src/components/modules/panel-4.vue
@@ -0,0 +1,109 @@
+<template lang="pug">
+  div.panel-4.pb-5.pt-5
+    div.container
+      div.row
+        div.col-xs-12.col-md-12.text-center.pb-4
+          center  
+            div.hr-blue
+          br
+          h1 Premium Modules
+          p Add these on top of the core modules to boosts your digital clinic experience.
+        div.col-xs-12.col-md-6
+          img(width="100%" src="../../assets/images/v4/234x234px.png")
+        div.col-xs-12.col-md-6
+          div.row
+            div.col-xs-6.col-md-3
+              div.module-container.shadow-5.text-center
+                img(src="../../assets/images/v4/mycure-homepage-wireframe-icon-emr.png")
+                h6 EMR
+            div.col-xs-6.col-md-3
+              div.module-container.shadow-5.text-center
+                img(src="../../assets/images/v4/mycure-homepage-wireframe-icon-emr.png")
+                h6 EMR
+            div.col-xs-6.col-md-3
+              div.module-container.shadow-5.text-center
+                img(src="../../assets/images/v4/mycure-homepage-wireframe-icon-emr.png")
+                h6 EMR
+            div.col-xs-6.col-md-3
+              div.module-container.shadow-5.text-center
+                img(src="../../assets/images/v4/mycure-homepage-wireframe-icon-emr.png")
+                h6 EMR
+          div.row
+            div.col-xs-6.col-md-3
+              div.module-container.shadow-5.text-center
+                img(src="../../assets/images/v4/mycure-homepage-wireframe-icon-emr.png")
+                h6 EMR
+            div.col-xs-6.col-md-3
+              div.module-container.shadow-5.text-center
+                img(src="../../assets/images/v4/mycure-homepage-wireframe-icon-emr.png")
+                h6 EMR
+            div.col-xs-6.col-md-3
+              div.module-container.shadow-5.text-center
+                img(src="../../assets/images/v4/mycure-homepage-wireframe-icon-emr.png")
+                h6 EMR
+            div.col-xs-6.col-md-3
+              div.module-container.shadow-5.text-center
+                img(src="../../assets/images/v4/mycure-homepage-wireframe-icon-emr.png")
+                h6 EMR
+          div.row.mt-5
+            div.col-xs-12.col-md-12
+              img(src="../../assets/images/v4/mycure-homepage-wireframe-icon-emr.png")
+              span
+                strong EMR
+              br
+              br
+              p Aliqua consequat ipsum fugiat nostrud ea occaecat fugiat do qui est deserunt ullamco. Ullamco tempor id quis laboris culpa.
+              br
+              b-button(variant="primary" size="sm") Learn More
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .panel-4 {
+    /* min-height: 100vh; */
+    background-color: white;
+  }
+
+  .container {
+    /* padding-top: 50px; */
+    /* padding-bottom: 50px; */
+  }
+
+  .module-container {
+    padding: 15px;
+    margin: 5px;
+  }
+
+  @media (min-width: 576px) {
+    .container {
+      padding-top: 50px;
+      padding-bottom: 50px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .panel-4 {
+      /* min-height: 100vh; */
+      background-color: white;
+    }
+
+    .container {
+      padding-top: 0;
+      padding-bottom: 0;
+    }
+
+    .row-container {
+      height: 100vh;
+    }
+  }
+
+  @media (min-width: 992px) {
+
+  }
+
+  @media (min-width: 1200px) {
+
+  }
+</style>
