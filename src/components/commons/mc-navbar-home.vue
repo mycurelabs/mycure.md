536b3486f40789407042e5f2709bcc9f1924c472
Joff Tiquez <jofftiquez@gmail.com>
2018-06-10 16:00:13 +0800

    Updated navbar home

--- src/components/commons/mc-navbar-home.vue
+++ src/components/commons/mc-navbar-home.vue
@@ -6,19 +6,12 @@
     b-collapse(is-nav id="nav_collapse")
 
       b-navbar-nav
-        b-nav-item(href="") Our Products
         b-nav-item(href="" :to="{name: 'modules'}") Modules
-        b-nav-item(href="") Pricing
+        b-nav-item(href="" :to="{name: 'pricing'}") Pricing
         b-nav-item(href="" :to="{name: 'our-story'}") Our Story
         b-nav-item(href="") Contact Us
 
       b-navbar-nav.ml-auto
         b-nav-item(href="") Login
         b-button(variant="primary" size="sm") Signup
-        
-        //- b-nav-item-dropdown(text="" right)
-        //-   b-dropdown-item(href="#") EN
-        //-   b-dropdown-item(href="#") ES
-        //-   b-dropdown-item(href="#") RU
-        //-   b-dropdown-item(href="#") FA
 </template>
