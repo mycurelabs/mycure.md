f8a468d4d48d68676074323dccb17812eebc4a42
Joff Tiquez <jofftiquez@gmail.com>
2018-06-13 13:44:00 +0800

    Home > panel-6 content

--- src/components/home/panel-6.vue
+++ src/components/home/panel-6.vue
@@ -6,33 +6,30 @@
           center
             div.hr-blue
           br
-          h1 You are the center of the universe
+          h1 To care is human
           br
-          p Quis ipsum est incididunt ut qui culpa labore 
-            br
-            | dolore id est aute qui ex.
+          p We guarantee that we will meet your needs; if we cannot, then we will meet you. This is how we care.
           br
           div.row.justify-content-center
             div.col-xs-12.col-md-3.pl-3.pr-3
-              img(width="100%" src="../../assets/images/v4/234x234px.png")
-              br
+              img(width="100%" src="../../assets/images/v4/234x234px.png").mb-3
               div.text-left
-                strong Foo
+                h6 User-friendly interface
                 p Occaecat anim nostrud.
             div.col-xs-12.col-md-3.pl-3.pr-3
-              img(width="100%" src="../../assets/images/v4/234x234px.png")
+              img(width="100%" src="../../assets/images/v4/234x234px.png").mb-3
               br
               div.text-left
                 strong Foo
                 p Culpa amet ut laboris.
             div.col-xs-12.col-md-3.pl-3.pr-3
-              img(width="100%" src="../../assets/images/v4/234x234px.png")
+              img(width="100%" src="../../assets/images/v4/234x234px.png").mb-3
               br
               div.text-left
                 strong Foo
                 p Nisi Lorem labore.
             div.col-xs-12.col-md-3.pl-3.pr-3
-              img(width="100%" src="../../assets/images/v4/234x234px.png")
+              img(width="100%" src="../../assets/images/v4/234x234px.png").mb-3
               br
               div.text-left
                 strong Foo
