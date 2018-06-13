1d544cf50cc8fdc657e353f2b1c4b416c4972eeb
Joff Tiquez <jofftiquez@gmail.com>
2018-06-13 13:43:49 +0800

    Home > panel-5 content

--- src/components/home/panel-5.vue
+++ src/components/home/panel-5.vue
@@ -8,23 +8,21 @@
           br
           h1 Why MYCURE?
           br
-          p Est voluptate ut mollit reprehenderit nulla.
-          br
           div.row
             div.col-xs-12.col-md-4.text-left
               h2 Patient-focused
               p Your patients are our top priority.
               br
               div.mb-5
-                img(width="30" src="../../assets/images/v4/01-mycure-feature-patient-focused-better-patient-care.png")
+                img(width="40" src="../../assets/images/v4/mycure-homepage-benefits-patient-care.png")
                 h5 Better patient care
                 p Retrieve your patients’ medical records anytime you need to.
               div.mb-5
-                img(width="30" src="../../assets/images/v4/02-mycure-feature-patient-focused-secure-medical-data.png")
+                img(width="40" src="../../assets/images/v4/mycure-homepage-benefits-secure.png")
                 h5 Secure medical data
                 p We perform industry standard encryption to protect your patient records.
               div.mb-5
-                img(width="30" src="../../assets/images/v4/03-mycure-feature-patient-focused-up-to-date-info.png")
+                img(width="40" src="../../assets/images/v4/mycure-homepage-benefits-uptodate.png")
                 h5 Up-to-date information
                 p Create updated and accurate medical records for your patients’ safety.
             div.col-xs-12.col-md-4.text-left
@@ -32,33 +30,33 @@
               p Get the best on your investments.
               br
               div.mb-5
-                img(width="30" src="../../assets/images/v4/01-mycure-feature-patient-focused-better-patient-care.png")
-                h5 Better patient care
-                p Retrieve your patients’ medical records anytime you need to.
+                img(width="40" src="../../assets/images/v4/mycure-homepage-benefits-cost-effective.png")
+                h5 Cost-effective system
+                p Get highly reduced operating expenses with automated record-keeping.
               div.mb-5
-                img(width="30" src="../../assets/images/v4/02-mycure-feature-patient-focused-secure-medical-data.png")
-                h5 Secure medical data
-                p We perform industry standard encryption to protect your patient records.
+                img(width="40" src="../../assets/images/v4/mycure-homepage-benefits-productive.png")
+                h5 Increased productivity
+                p Double your clinic efficiency, staff productivity and involvement.
               div.mb-5
-                img(width="30" src="../../assets/images/v4/03-mycure-feature-patient-focused-up-to-date-info.png")
-                h5 Up-to-date information
-                p Create updated and accurate medical records for your patients’ safety.
+                img(width="40" src="../../assets/images/v4/mycure-homepage-benefits-prestige.png")
+                h5 Added Prestige
+                p Be a premium healthcare provider with an advanced EMR system.
             div.col-xs-12.col-md-4.text-left
               h2 Flexible technology
               p MYCURE is engineered for your convenience.
               br
               div.mb-5
-                img(width="30" src="../../assets/images/v4/01-mycure-feature-patient-focused-better-patient-care.png")
-                h5 Better patient care
-                p Retrieve your patients’ medical records anytime you need to.
+                img(width="40" src="../../assets/images/v4/mycure-homepage-benefits-upgrades.png")
+                h5 Painless upgrades
+                p Easily explore new features from regular product upgrades without the technical hassle.
               div.mb-5
-                img(width="30" src="../../assets/images/v4/02-mycure-feature-patient-focused-secure-medical-data.png")
-                h5 Secure medical data
-                p We perform industry standard encryption to protect your patient records.
+                img(width="40" src="../../assets/images/v4/mycure-homepage-benefits-storage.png")
+                h5 Optimized file storage
+                p Store and retrieve records anytime without stacking everything in your physical cabinets.
               div.mb-5
-                img(width="30" src="../../assets/images/v4/03-mycure-feature-patient-focused-up-to-date-info.png")
-                h5 Up-to-date information
-                p Create updated and accurate medical records for your patients’ safety.
+                img(width="40" src="../../assets/images/v4/mycure-homepage-benefits-support.png")
+                h5 Hands-on support
+                p We hire humans (not bots) to provide you with technical assistance and awesome care!
             
 </template>
 
