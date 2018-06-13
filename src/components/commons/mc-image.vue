796c0ade76b89a429891a488d4fefa604de5be91
Joff Tiquez <jofftiquez@gmail.com>
2018-06-13 13:42:53 +0800

    New component mc-image

---
+++ src/components/commons/mc-image.vue
@@ -0,0 +1,45 @@
+<template>
+  <img 
+    :src="currentSrc" 
+    :id="id" 
+    :class="customClass"
+    :alt="alt" 
+    :crossorigin="crossorigin"
+    @mouseover="hover"
+    @mouseout="mouseout"
+  >
+</template>
+
+<script>
+  export default {
+    props: {
+      id: String,
+      customClass: String,
+      width: Number,
+      height: Number,
+      src: String,
+      errorSrc: String,
+      loadingSrc: String,
+      hoverSrc: String,
+      alt: String,
+      crossorigin: String
+    },
+    created() {
+      console.log(this.src)
+    },
+    data() {
+      return {
+        currentSrc: this.src
+      }
+    },
+    methods: {
+      hover() {
+        if(this.hoverSrc)
+          this.currentSrc = this.hoverSrc;
+      },
+      mouseout() {
+        this.currentSrc = this.src;
+      }
+    }
+  }
+</script>
