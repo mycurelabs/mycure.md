680c77b2045eac75d6f4c066c9abb5f28987551f
Joff Tiquez <jofftiquez@gmail.com>
2018-06-18 17:15:14 +0800

    Added scroll handler

--- src/components/modules/index.vue
+++ src/components/modules/index.vue
@@ -1,15 +1,12 @@
 <template lang="pug">
   div
-    panel-1
-    panel-2
-    panel-3
-    panel-4
-    panel-5
-    panel-6
-    //- panel-7
-    //- join-us
+    panel-1#panel-1
+    panel-2#panel-2
+    panel-3#panel-3
+    panel-4#panel-4
+    panel-5#panel-5
+    panel-6#panel-6
     mc-footer
-      
 </template>
 
 <script>
@@ -19,9 +16,8 @@
   import panel4 from './panel-4';
   import panel5 from './panel-5';
   import panel6 from './panel-6';
-  // import panel7 from './panel-7';
-  // import joinUs from '../home/panel-9';
   import mcFooter from '../commons/mc-footer';
+  import VueScrollTo from 'vue-scrollto';
 
   export default {
     components: {
@@ -31,9 +27,23 @@
       'panel-4': panel4,
       'panel-5': panel5,
       'panel-6': panel6,
-      // 'panel-7': panel7,
-      // joinUs,
       mcFooter
+    },
+    created() { 
+      this.scroll(this.$route.query.id, 500, 500);
+    },
+    methods: {
+      scroll(id, timeout = 1000, duration = 500) {
+        setTimeout(() => {
+          VueScrollTo.scrollTo(`#${id}`, duration, { easing: 'ease' } );
+        }, timeout);
+      }
+    },
+    watch: {
+      $route(to, from) {
+        const { query } = to;
+        this.scroll(query.id, 0, 500);
+      }
     }
   }
 </script>
