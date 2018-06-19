5bcd35b4292774abccd4d3a73446457fce262c43
Joff Tiquez <jofftiquez@gmail.com>
2018-06-19 19:03:45 +0800

    Directory page

---
+++ src/components/directory/index.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  div
+    search
+    results
+      
+</template>
+
+<script>
+  import search from './search';
+  import results from './results';
+  export default {
+    components: {
+      search,
+      results
+    },
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
