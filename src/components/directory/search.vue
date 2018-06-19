3e7a6b034dddad1a1fa3963f143363432b5209c7
Joff Tiquez <jofftiquez@gmail.com>
2018-06-19 19:04:00 +0800

    Search header component

---
+++ src/components/directory/search.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  div.container
+    div.row.justify-content-center.pt-4.pb-4
+      div.col-xs-12.col-md-8.text-center
+        h1 MYCURE Directory
+        h2 Find doctors near you, Let's start searching.
+        form()
+          input(label="Search for doctors" placeholder="Search for doctors").form-control
+</template>
