3c921ce4a006deed08580a1ae8c7e52f95fbc78c
Joff Tiquez <jofftiquez@gmail.com>
2018-06-27 15:27:17 +0800

    Sdk actions update

---
+++ src/mycure/init.js
@@ -0,0 +1,7 @@
+import { core } from '@mycure/sdk';
+ 
+const config = {
+  urls: { base: 'http://mcv4.herokuapp.com' }
+};
+
+core.initialize(config);
---
+++ src/mycure/user/index.js
@@ -0,0 +1,31 @@
+import { core } from '@mycure/sdk';
+
+export const searchDoctor = async (query) => {
+  try {
+    return await core.personalDetails().find({
+      doc_PRCLicenseNo: {
+        $exists: true
+      },
+      $or: [
+        {
+          'name.firstName': {
+            $regex: query || '', $options: 'i'
+          }
+        },
+        {
+          'name.middleName': {
+            $regex: query || '', $options: 'i'
+          }
+        },
+        {
+          'name.lastName': {
+            $regex: query || '', $options: 'i'
+          }
+        }
+      ],
+      // $limit: 1
+    });
+  } catch (e) {
+    throw e;
+  }
+}
