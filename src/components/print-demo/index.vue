1d876ad8706f76bcdb5ff02c9af56c6a2001697b
Joff Tiquez <jofftiquez@gmail.com>
2018-06-27 15:26:15 +0800

    Print demo

--- src/components/print-demo/index.vue
+++ src/components/print-demo/index.vue
@@ -1,79 +1,33 @@
 <template lang="pug">
   div.container
     div#print.row.pt-5
-      div.col-xs-12.col-md-12
-        div.media
-          img(width="100" src="https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c_400x400.jpeg").mr-3
-          div.media-body
-            h5 Clinic Name
-            p Address
-            p Phone: Foo
+      div.col-xs-12.col-md-10.offset-md-1.pb-3
+        div
+          
+    
+    div#printClaimStub.row.pt-5
+      div.col-xs-12.col-md-4.offset-md-4.pb-3.text-center
+        h2 MYCURE MEDICAL CENTER
+        h1 CLAIM STUB
+        hr(style="margin:0; padding:0;")
+        label TIQUEZ, M.S.R
         br
-        table.table.table-bordered
-          tr
-            td Name: 
-              strong Jofferson Tiquez
-            td Sex: 
-              strong Male
-            td Date Performed: 
-              strong 8/27/2017
-            td Date Released: 
-              strong 8/28/2017
-          tr
-            td Req. Physician: 
-              strong Jofferson Ramirez Tiquez
-            td Age: 
-              strong 25
-            td(colspan="2") HMO: 
-              strong MAXICARE
+        label 25/M
         br
-        h4(align="center") HEMATOLOGY
-        table.table.table-bordered
-          thead(style="background-color: #DEE2E6;")
-            tr
-              th Test Name
-              th Result
-              th Reference Value
-          tbody
-            tr
-              td(style="padding-top: 2px; padding-bottom: 2px;") Joff
-              td(style="padding-top: 2px; padding-bottom: 2px;") Tiquez
-              td(style="padding-top: 2px; padding-bottom: 2px;") Tiquez
-            tr
-              td(style="padding-top: 2px; padding-bottom: 2px;") Foo
-                ul.pl-2.list
-                  li Bar
-                  li Bar
-                  li Bar
-                  li Bar
-              td(style="padding-top: 2px; padding-bottom: 2px;") 
-                div(style="height: 20px;")
-                ul.pl-2.list
-                  li Bar
-                  li Bar
-                  li Bar
-                  li Bar
-              td(style="padding-top: 2px; padding-bottom: 2px;") Tiquez
-      div.col-xs-12.col-md-12.fixed-bottom
-        div.row
-          div.col-xs-12.col-md-6.text-center
-            strong Jofferson Ramirez Tiquez
-            hr(style="padding:0; margin:0")
-            span Medical Technologist
-          div.col-xs-12.col-md-6.text-center
-            strong Jofferson Ramirez Tiquez
-            hr(style="padding:0; margin:0")
-            span Pathologist
-
-
-
+        label {{new Date().toString()}}
+        barcode(width="1" value="Jofferson Ramirez Tiquez")
+    
     div.row
       div.col-xs-12.col-md-12
         button(@click="print").btn.btn-primary Print
 </template>
 
 <script>
+  import VueBarcode from 'vue-barcode';
   export default {
+    components: {
+      'barcode': VueBarcode
+    },
     methods: {
       print() {
         this.$htmlToPaper('print');
