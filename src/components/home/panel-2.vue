13c5024daa2ce81cf04b7d7bf79ee2d6e2269a51
Centipede <jofftiquez@gmail.com>
2018-06-29 12:08:53 +0800

    HOME QA updates

--- src/components/home/panel-2.vue
+++ src/components/home/panel-2.vue
@@ -13,21 +13,20 @@
         b-col(xs="12" md="6").offset-md-3.text-center
           p MYCURE smoothly integrates your patient records together from registration to billing. It simplifies record-keeping for everyone. 
           br
-        b-col(xs="12" md="6").pl-5.pr-5.text-center
-          h2 Other providers
+        b-col(xs="12" md="6").border-right.pl-5.pr-5.text-right
+          h2.right Other providers
           br
           img(src="../../assets/images/v4/mycure-homepage-compare-other-emr-incomplete-clinic-management-system.png" width="100%").mb-3
-        b-col(xs="12" md="6").pl-5.pr-5.text-center
+        b-col(xs="12" md="6").pl-5.pr-5.text-left
           h2 With MYCURE 
             span.color-primary ✓
           br
           img(src="../../assets/images/v4/mycure-homepage-compare-mycure-complete-clinic-management-system.png" width="100%").mb-3
         b-col(xs="12" md="8").offset-md-2.text-center
           br
-          p With MYCURE, you don’t have to depend on multiple, complicated systems anymore.
-          p Your patients’ medical data are synced together and stored safely in the cloud 
+          p With MYCURE, you don’t have to depend on multiple, complicated systems anymore. Your patients’ medical data are synced together and stored safely
             br
-            | using one powerful system.
+            | in the cloud using one powerful system.
 </template>
 
 <script>
@@ -44,10 +43,19 @@
     padding-bottom: 50px;
   }
 
+  .border-right {
+    border: none !important;
+  }
+  
+
   @media (min-width: 576px) {
     .container {
       padding-top: 50px;
       padding-bottom: 50px;
+    }
+
+    .border-right {
+      border: none !important;
     }
   }
 
@@ -59,6 +67,10 @@
     .container {
       padding-top: 0;
       padding-bottom: 0;
+    }
+
+    .border-right {
+      border-right: 1px solid #cecece !important;
     }
   }
 
