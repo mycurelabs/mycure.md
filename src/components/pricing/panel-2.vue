16894b5ba6b77013679e0ba13b7f1f3232899375
Centipede <jofftiquez@gmail.com>
2018-07-03 12:07:57 +0800

    Updated pricing text

--- src/components/pricing/panel-2.vue
+++ src/components/pricing/panel-2.vue
@@ -14,7 +14,7 @@
               strong powerful EMR 
               | system with complementary Queuing and Billing modules
             br
-            p Enjoy a one-month free trial, no credit card needed. 
+            p Enjoy one-month free trial for Liberica and Robusta. No credit card needed. 
               strong Start FREE!
             img(src="../../assets/images/v4/coffee.png" :class="{'animate': scrollTop > 300}").coffee
           b-col(xs="12" md="10")
@@ -41,7 +41,7 @@
                   div.text-left
                     h2 FREE
                     p ₱0/day
-                  b-button(variant="outline-primary" size="sm" :to="{name: 'arabica-link'}" block).shadow-5 ALWAYS FREE
+                  b-button(variant="outline-primary" size="sm" :to="{name: 'arabica-link'}" block).shadow-5 START NOW
               b-col(xs="12" md="4").pt-5.pl-4.pr-4
                 div(style="height: 400px;")
                   div.pl-2.pr-2
@@ -64,7 +64,7 @@
                     h2 ₱299
                       span(style="font-size: 25px") /clinic/mo
                     p ₱10/day
-                  b-button(variant="outline-primary" size="sm" :to="{name: 'robusta-link'}" block).shadow-5 START FREE
+                  b-button(variant="outline-primary" size="sm" :to="{name: 'robusta-link'}" block).shadow-5 START MY TRIAL
               b-col(xs="12" md="4")
                 div(style="background-color: #f0f0f2;").shadow-4.pt-3.pb-3
                   div(style="background-color: #ffcc33").text-center
@@ -90,7 +90,7 @@
                         h2 ₱999
                           span(style="font-size: 25px") /clinic/mo
                         p ₱30/day
-                      b-button(variant="success" size="sm" :to="{name: 'liberica-link'}" block).shadow-5 START FREE
+                      b-button(variant="success" size="sm" :to="{name: 'liberica-link'}" block).shadow-5 START MY TRIAL
 </template>
 
 <script>
