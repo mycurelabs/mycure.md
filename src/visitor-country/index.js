6f5b2291c8fc5ea4df74ba997d96dadd427c28f3
Joff Tiquez <jofftiquez@gmail.com>
2018-08-11 12:40:40 +0800

    Set country to store

--- src/visitor-country/index.js
+++ src/visitor-country/index.js
@@ -4,5 +4,6 @@ import store from '../vuex';
 (async () => {
   const { ip } = await getIP();
   console.log(await getCountry(ip))
-  
+  const country = await getCountry(ip);
+  store.commit('ui/setVisitorCountry', country);
 })();
