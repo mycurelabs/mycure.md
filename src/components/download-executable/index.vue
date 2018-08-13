939be130a22b9b9300f99e4f7e4213f783688a23
Jofferson R Tiquez <jofftiquez@gmail.com>
2018-08-13 14:43:17 +0800

    New download-executable compnent

---
+++ src/components/download-executable/index.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  b-container
+    b-row(style="height: 100vh;").align-items-center.justify-content-md-center
+      b-col(xs="12" md="6")
+        b-card
+          h2 MYCURE CMS Setup
+          p Version: 
+            code 1.3.4
+          p Date released: Aug 13, 2018
+          a(href="https://firebasestorage.googleapis.com/v0/b/mc-v4-prod.appspot.com/o/cms-setup%2Ffoo.rar?alt=media&token=110a5369-3422-4225-b409-ae4fef12eabb" target="_blank") Download
+</template>
