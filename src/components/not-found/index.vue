f0a0703114f9b7fc1e838964ef7f199ca56cfb62
Joff Tiquez <jofftiquez@gmail.com>
2018-08-17 12:10:40 +0800

    New component 404

---
+++ src/components/not-found/index.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  b-container
+    b-row(style="height: 100vh;").align-items-center.justify-content-md-center
+      b-col(xs="12" md="6")
+        b-card.text-center
+          p Ooops! 
+            br 
+            | Looks like the page your are looking for does not exist.
+          h1 404 PAGE NOT FOUND
+          b-btn(variant="primary" :to="{name: 'home'}") Go back to home
+</template>
