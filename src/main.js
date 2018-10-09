72e10b2bcdd44d20483ec05c0bd9697dc1274fd5
Jofferson R Tiquez <jofftiquez@gmail.com>
2018-10-09 15:39:29 +0800

    Added VueMediaQueryMixin

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ import VImgFallback from 'v-img-fallback';
 import VueYouTubeEmbed from 'vue-youtube-embed';
 import VueMediaQueryMixin from 'vue-media-query-mixin';
 
-Vue.use(VueMediaQueryMixin, {framework:'bootstrap'});
+Vue.use(VueMediaQueryMixin, {framework: 'bootstrap'});
 
 Vue.use(VueYouTubeEmbed);
 Vue.config.productionTip = false;
