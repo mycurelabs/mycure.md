e5231c58244103ffeec38e3c094a24b58c4c92a7
Marc Almond Valleza <majvalleza@gmail.com>
2019-04-23 13:23:39 +0800

    Add Navbar and Footer in 404

--- src/components/not-found/index.vue
+++ src/components/not-found/index.vue
@@ -1,11 +1,32 @@
 <template lang="pug">
-  b-container
-    b-row(style="height: 100vh;").align-items-center.justify-content-md-center
-      b-col(xs="12" md="6")
-        b-card.text-center
-          p Ooops! 
-            br 
-            | Looks like the page your are looking for does not exist.
-          h1 404 PAGE NOT FOUND
-          b-btn(variant="primary" :to="{name: 'home'}") Go back to home
+  div
+    mc-navbar-home
+    mc-cookie-prompt
+    b-container
+      b-row(style="height: 100vh;").align-items-center.justify-content-md-center
+        b-col(xs="12" md="6")
+          b-card.text-center
+            p Ooops! 
+              br 
+              | Looks like the page your are looking for does not exist.
+            h1 404 PAGE NOT FOUND
+            b-btn(variant="primary" :to="{name: 'home'}") Go back to home
+    mc-footer
+  
+    
 </template>
+
+<script>
+import mcNavbarHome from '../commons/mc-navbar-home';
+import mcCookiePrompt from '../commons/mc-cookie-prompt';
+import mcFooter from '../commons/mc-footer';
+
+export default {
+  components: {
+    mcNavbarHome,
+    mcCookiePrompt,
+    mcFooter
+  }
+}
+
+</script>
