b208b3072cbc76ad33f796965cb2f0d243db7c3f
Marc Almond Valleza <majvalleza@gmail.com>
2019-04-23 13:56:42 +0800

    Change 404 page

--- src/components/not-found/index.vue
+++ src/components/not-found/index.vue
@@ -1,15 +1,17 @@
 <template lang="pug">
   div
     mc-navbar-home
-    b-container
-      b-row(style="height: 100vh;").align-items-center.justify-content-md-center
-        b-col(xs="12" md="6")
-          b-card.text-center
-            p Ooops! 
-              br 
-              | Looks like the page your are looking for does not exist.
-            h1 404 PAGE NOT FOUND
-            b-btn(variant="primary" :to="{name: 'home'}") Go back to home
+    b-container.py-5
+      b-row(style="height: 100vh;").align-items-center.justify-content-md-center.text-center
+        b-col(xs="12" md="12")
+          img(width="50%" src="../../assets/images/v4/mycure-online-offline-web-app-cms-emr-error-404-image-blue.png").not-found-pic
+          br
+          br
+          h5 Oh snap!
+          span There isn't any page like that in this site. 
+          br
+          span Please check out our other pages instead.
+          // b-btn(variant="primary" :to="{name: 'home'}") Go back to home
     mc-footer
   
     
