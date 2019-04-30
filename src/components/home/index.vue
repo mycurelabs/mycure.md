37d8b2a0b7cea73aa9fbc19d468b8e3790a0eab8
Marc Almond Valleza <majvalleza@gmail.com>
2019-04-30 18:48:27 +0800

    feat (home): add section 5

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -27,10 +27,10 @@
           v-flex(xs4).text-xs-center.white--text Is your clinic WiFi slow?
 
     //- Section 3 of Home Page
-    div.silver
+    div.silver.px-5
       v-container.px-5
         v-layout(row wrap justify-center)
-          h3.pa-5 Is your current clinic system stopping you and your clinic from progess?
+          h2.pa-5 Is your current clinic system stopping you and your clinic from progress?
           v-layout(row wrap justify-center).mx-5
             v-flex(xs4)
               ul
@@ -49,37 +49,62 @@
                 br
                 li Is your clinic system working perfectly even without WiFi?
                 br
-        v-layout(row wrap justify-center)
-          v-btn(
-          color="info"
-          large
-          ) 
-            strong We got a solution
+          v-layout(row wrap justify-center)
+            v-btn(
+            color="info"
+            large
+            ) 
+              strong We got a solution
+          
         p.text-xs-center.py-4 Have questions? Give us a call: (+63) 699 - 7272
 
     //- Section 4 of Home Page
-    div.white
+    div.white.px-5
       v-container
         v-flex(xs12 md12)
             v-layout(row wrap justify-center)
               h2.pa-5 Does your clinic have more than 10 branches? Talk to us for custom pricing.
 
     //- Section 5 of Home Page
-    div.silver
+    div.silver.px-5
       v-container
-        h2.text-xs-center.pa-5 Is this you right now?
         v-layout(row wrap justify-center)
-          v-btn(
-            color="info"
-            large
-            ) 
+          h2.text-xs-center.pa-5 Is this you right now?
+          v-layout(row wrap justify-center)
+            v-flex(xs12 md2).mx-1
+              div.problem-pic.red 
+              h3.text-center The admin with a headache
+              | Telling the biggest problem of the great clinic admin in 158 characters
+            v-flex(xs12 md2).mx-1
+              div.problem-pic.orange
+              h3.text-center The empty pocket techie doctor
+              | Telling the biggest problem of the great clinic admin in 158 characters
+            v-flex(xs12 md2).mx-1
+              div.problem-pic.lime
+              h3.text-center The admin in a rollercoaster ride
+              | Telling the biggest problem of the great clinic admin in 158 characters
+            v-flex(xs12 md2).mx-1
+              div.problem-pic.purple
+              h3.text-center The bill chasing clinic owner
+              | Telling the biggest problem of the great clinic admin in 158 characters
+            v-flex(xs12 md2).mx-1
+              div.problem-pic.pink
+              h3.text-center The daydreamer doctor
+              | Telling the biggest problem of the great clinic admin in 158 characters
+
+          v-layout(row wrap justify-center).pt-5
+            v-btn(
+              color="info"
+              large
+              ) 
               strong We got a solution
-        
-        p.text-xs-center Have questions? Give us a call: (+63) 699 - 7272
+        p.text-xs-center.py-4 Have questions? Give us a call: (+63) 699 - 7272
 
     //- Section 6 of Home Page
-    div.blue
+    div.blue.px-5
       v-container
+        v-layout(row wrap justify-center)
+          h2.text-center.pa-5.white--text Lower your average monthly expenses and increase clinic productivity and effectiveness
 
 
     mcFooter
@@ -98,11 +123,14 @@
 </script>
 
 <style>
-
 @import url('https://fonts.googleapis.com/css?family=Poppins');
 
 .home {
   font-family: 'Poppins', sans-serif;
+}
+
+.problem-pic {
+  height: 150px;
 }
 
 </style>
