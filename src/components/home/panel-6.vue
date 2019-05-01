30536c322a1301f970d73123635abd59a41075a2
Marc Almond Valleza <majvalleza@gmail.com>
2019-05-01 17:43:55 +0800

    chore: adjust alignment

--- src/components/home/panel-6.vue
+++ src/components/home/panel-6.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
   //- Section 6 of Home Page
-  div.blue.px-5
+  div.blue.pa-5
     v-container
       v-layout(row wrap justify-center)
-        h2.text-center.pa-5.white--text Lower your average monthly expenses and increase clinic productivity and effectiveness
+        v-flex(xs8)
+          h2.text-xs-center.pa-5.white--text Lower your average monthly expenses and increase clinic productivity and effectiveness
 </template>
