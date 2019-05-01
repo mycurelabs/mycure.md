675e7b46e028c2a3b4b487ca52452abdcd018b60
Marc Almond Valleza <majvalleza@gmail.com>
2019-05-01 18:49:47 +0800

    feat: add play button icon

--- src/components/home/panel-6.vue
+++ src/components/home/panel-6.vue
@@ -1,8 +1,22 @@
 <template lang="pug">
   //- Section 6 of Home Page
   div.blue.pa-5
-    v-container
-      v-layout(row wrap justify-center)
+    v-container.pa-5
+      v-layout(row wrap justify-center).pt-5
         v-flex(xs8)
           h2.text-xs-center.pa-5.white--text Lower your average monthly expenses and increase clinic productivity and effectiveness
+      v-layout(row wrap justify-center).mx-5
+        v-icon(size="80" color="white") play_circle_filled
+      v-layout(row wrap justify-center).pt-3.pb-5
+        h5.text-xs-center.yellow--text WATCH VIDEO
 </template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style scoped>
+
+</style>
