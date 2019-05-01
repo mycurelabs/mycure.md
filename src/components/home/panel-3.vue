08b0039092f2a5cf1ea1bfc90b82cb071e480206
Marc Almond Valleza <majvalleza@gmail.com>
2019-05-01 17:44:05 +0800

    feat: add check icons

--- src/components/home/panel-3.vue
+++ src/components/home/panel-3.vue
@@ -4,29 +4,68 @@
     v-container.px-5
       v-layout(row wrap justify-center)
         h2.pa-5 Is your current clinic system stopping you and your clinic from progress?
-        v-layout(row wrap justify-center).mx-5
-          v-flex(xs4)
-            ul
-              li Is your clinic system already outdated and lack reliablity?
-              br
-              li Is your clinic system providing accurate, real-time data?
-              br
-              li Is your clinic system already outdated and lack reliability?
-              br
-          v-flex(xs4)
-            ul
-              li Is your clinic system hard to maintain?
-              br
-              br
-              li Is your clinic system help you in Data Privary Issues?
-              br
-              li Is your clinic system working perfectly even without WiFi?
-              br
-        v-layout(row wrap justify-center)
-          v-btn(
-          color="info"
-          large
-          ) 
-            strong We got a solution
+      v-layout(row wrap justify-center).mx-5
+        v-flex(xs12 md4)
+          v-list(two-line).check-list.silver
+            v-list-tile
+              v-list-tile-action
+                img(src="../../assets/images/mycure-icon-check.png")
+              v-list-tile-content
+                | Is your clinic system already outdated and lack reliablity?
+            v-list-tile
+              v-list-tile-action
+                img(src="../../assets/images/mycure-icon-check.png")
+              v-list-tile-content
+                | Is your clinic system providing accurate, real-time data?
+            v-list-tile
+              v-list-tile-action
+                img(src="../../assets/images/mycure-icon-check.png")
+              v-list-tile-content
+                | Is your clinic system already outdated and lack reliability?
+        v-flex(xs12 md4)
+          v-list(two-line).check-list
+            v-list-tile
+              v-list-tile-action
+                img(src="../../assets/images/mycure-icon-check.png")
+              v-list-tile-content
+                | Is your clinic system hard to maintain?
+            v-list-tile
+              v-list-tile-action
+                img(src="../../assets/images/mycure-icon-check.png")
+              v-list-tile-content
+                | Is your clinic system help you in Data Privary Issues?
+            v-list-tile
+              v-list-tile-action
+                img(src="../../assets/images/mycure-icon-check.png")
+              v-list-tile-content
+                | Is your clinic system working perfectly even without WiFi?
+      v-layout(row wrap justify-center)
+        v-btn(
+        color="info"
+        large
+        ) 
+          strong We got a solution
       p.text-xs-center.py-4 Have questions? Give us a call: (+63) 699 - 7272
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      checkIcon: '../../assets/images/mycure-icon-check.png'
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+ul {
+  list-style-type: none;
+}
+
+.check-list {
+  background-color: #fafafa;
+}
+</style>
+
