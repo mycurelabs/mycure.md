0578b2806b115571d3df3158e9978452af33faff
Francis Marquez <fnmarquez@up.edu.ph>
2019-05-02 18:21:14 +0800

    style: adjust panel 2

--- src/components/home/panel-2.vue
+++ src/components/home/panel-2.vue
@@ -1,10 +1,25 @@
 <template lang="pug">
   //- Section 2 of Home Page
-  div.blue
+  div.mc-blue
     v-container
       v-layout(row wrap justify-center)
-        v-flex(xs4).text-xs-center.white--text Is your system outdated
-        v-flex(xs4).text-xs-center.white--text Is this you?
-        v-flex(xs4).text-xs-center.white--text Is your clinic WiFi slow?
+        v-flex(xs4).text-xs-center.white--text.font-m
+          v-icon(
+            color="white"
+            left
+          ) face
+          span Is your system outdated?
+        v-flex(xs4).text-xs-center.white--text.font-m 
+          v-icon(
+            color="white"
+            left
+          ) face
+          span Is this you?
+        v-flex(xs4).text-xs-center.white--text.font-m 
+          v-icon(
+            color="white"
+            left
+          ) face
+          span Is your clinic WiFi slow?
 
 </template>
