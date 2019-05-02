b22ca594ca81c058aa7a363da42e692652860d95
Francis Marquez <fnmarquez@up.edu.ph>
2019-05-02 18:21:24 +0800

    style: adjust panel 4

--- src/components/home/panel-4.vue
+++ src/components/home/panel-4.vue
@@ -1,8 +1,20 @@
 <template lang="pug">
   //- Section 4 of Home Page
-  div.white.px-5
+  div.px-5
     v-container
       v-flex(xs12 md12)
           v-layout(row wrap justify-center)
-            h2.pa-5 Does your clinic have more than 10 branches? Talk to us for custom pricing.
+            h2.pa-5 Does your clinic have more than 10 branches? 
+              a(
+                href="https://mycure.md"
+              )
+               span Talk to us for custom pricing.
 </template>
+
+<style scoped>
+
+a {
+  color: #2e9fdf
+}
+
+</style>
