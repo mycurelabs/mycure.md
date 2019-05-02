9965d14131c9c478adb70decce1ed89d7dbae579
Francis Marquez <fnmarquez@up.edu.ph>
2019-05-02 18:21:20 +0800

    style: adjust panel 3

--- src/components/home/panel-3.vue
+++ src/components/home/panel-3.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
   //- Section 3 of Home Page
-  div.silver.px-5
+  div.mc-light-grey.px-5
     v-container.px-5
       v-layout(row wrap justify-center)
         h2.pa-5 Is your current clinic system stopping you and your clinic from progress?
       v-layout(row wrap justify-center).mx-5
         v-flex(xs12 md4)
-          v-list(two-line).check-list.silver
+          v-list(two-line).check-list
             v-list-tile
               v-list-tile-action
                 img(src="../../assets/images/mycure-icon-check.png")
@@ -43,9 +43,11 @@
         v-btn(
         color="info"
         large
-        ) 
-          strong We got a solution
-      p.text-xs-center.py-4 Have questions? Give us a call: (+63) 699 - 7272
+        ).mt-5 
+          strong.font-s.text-none We got a solution
+    
+      p.text-xs-center.py-4.font-xs Have questions? Give us a call: 
+        strong (+63) 699 - 7272
 </template>
 
 <script>
@@ -65,7 +67,8 @@ ul {
 }
 
 .check-list {
-  background-color: #fafafa;
+  background-color: #f6f6f6;
 }
+
 </style>
 
