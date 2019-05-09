c43129ad55452c8eccce31e5b1cbff094403cde8
Francis Marquez <fnmarquez@up.edu.ph>
2019-05-10 00:51:11 +0800

    style: make some css changes global in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  v-app
+  v-app#body
     v-content
       router-view
 </template>
@@ -17,3 +17,82 @@ export default {
 };
 </script>
 
+<style>
+
+  @import url('https://fonts.googleapis.com/css?family=Poppins');
+
+  #body {
+    max-width: 100% !important;
+    overflow-x: hidden !important;
+    font-family: 'Poppins', sans-serif !important;
+  };
+
+  .mc-blue {
+    background-color: #2e9fdf
+  }
+
+  .mc-dark-grey {
+    background-color: #343a40
+  }
+
+  .mc-grey {
+    background-color: #616161
+  }
+
+  .mc-light-grey {
+    background-color: #f6f6f6
+  }
+
+  .mc-yellow {
+    background-color: #ffff33
+  }
+
+  .font-mc-blue {
+    color: #2e9fdf
+  }
+  
+  .font-mc-dark-grey{
+    color: #343a40
+  }
+
+  .font-mc-grey {
+    color: #616161
+  }
+
+  .font-mc-light-grey {
+    color: #f6f6f6
+  }
+
+  .font-mc-white {
+    color: #ffffff
+  }
+
+  .font-xl {
+    font-size: 40px
+  }
+
+  .font-l {
+    font-size: 24px
+  }
+
+  .font-m {
+    font-size: 18px
+  }
+
+  .font-s {
+    font-size: 16px;
+  }
+
+  .font-xs {
+    font-size: 12px
+  }
+
+  .hr-blue {
+  height: 2px;
+  width: 150px;
+  background-color: #0099cc;
+  padding: 1px;
+  margin: 0 auto;
+}
+
+</style>
