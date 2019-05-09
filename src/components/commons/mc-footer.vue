a60d9dc90ba3ea7bd48b7416006aa554398e982d
Francis Marquez <fnmarquez@up.edu.ph>
2019-05-10 00:52:40 +0800

    feat: add footer

--- src/components/commons/mc-footer.vue
+++ src/components/commons/mc-footer.vue
@@ -1,14 +1,94 @@
 <template lang="pug">
-  v-footer(dark height="auto")
-    v-card(flat tile).grey.white--text.text-xs-center
-      v-card-text.white--text.pt-2
-        | Water. Earth. Fire. Air.
-        | Long ago, the four nations lived together in harmony. Then, everything changed when the Fire Nation attacked. 
-        | Only the Avatar, master of all four elements, could stop them, but when the world needed him most, he vanished. 
-        | A hundred years passed and my brother and I discovered the new Avatar, an airbender named Aang, 
-        | and although his airbending skills are great, he has a lot to learn before he's ready to save anyone. 
-        | But I believe Aang can save the world.
-      v-divider
-      v-card-text.white--text
-        | &copy;2019 — MYCURE
+  div.mc-dark-grey
+    v-container(justify-center)
+      mc-navbar-footer
+      hr.mc-grey.mb-3
+      v-layout(row wrap justify-center)
+        v-flex(xs12 md3 offset-md1).font-mc-grey
+          ul.no-style-type
+            li CLINIC SOLUTIONS
+            li 
+              router-link(:to="{name: 'home'}").link.white--text
+                span HOME
+            li
+              router-link(:to="{name: 'private'}").link.white--text
+                span For Individual Doctors
+            li.pb-3
+              router-link(:to="{name: 'multi-specialty'}").link.white--text
+                span For Multi-sepcialty-clinics
+            li ABOUT
+            li
+              router-link(:to="{name: 'our-story'}").link.white--text 
+                span Our Story
+            li 
+              a(href="http://blog.mycure.md/").link.white--text Blog 
+            li 
+              a(href="https://culture.mycure.md/").link.white--text Careers
+          
+        v-flex(xs12 md3 offset-md1).font-mc-grey 
+          ul.no-style-type
+            li SUPPORT 
+            li 
+              a(href="http://blog.mycure.md/frequently-asked-questions/").link.white--text FAQs
+            li.pb-3
+              a(href="https://calendly.com/mycure").link.white--text Book A Demo
+            li LEGAL
+            li
+              router-link(:to="{name: 'terms'}").link.white--text 
+                span Terms
+            li
+              router-link(:to="{name: 'privacy-policy'}").link.white--text 
+                span Privacy Policy
+          
+        v-flex(xs12 md3 offset-md1).font-mc-grey
+          ul.no-style-type
+            li 
+              a(href="mailto:hello@mycure.md") hello@mycure.md
+            li (+632) 799 6262
+            li (+63) 917 303 4350
+      hr.mc-grey.my-4  
+      v-layout(row wrap justify-center)  
+        span.white--text &copy; {{new Date().getFullYear()}} 
+          span.font-mc-blue MYCURE
+          |. 
+          span.white--text All Rights Reserved.
 </template>
+
+<script>
+import mcNavbarFooter from "./mc-navbar-footer";
+
+export default {
+  components: {
+    mcNavbarFooter
+  }
+};
+</script>
+
+
+<style scoped>
+
+hr {
+  height: 1px;
+  color: #616161;
+  background-color: #616161;
+  border: none;
+}
+
+.no-style-type {
+    list-style-type: none;
+    padding-left: 0;
+  }
+
+a:link {
+  text-decoration: none;
+  color: white;
+}
+
+.center {
+  padding: auto;
+}
+
+.font-mc-blue {
+	color: #2e9fdf
+}
+</style>
---
+++ src/components/commons/mc-navbar-footer.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  div
+    v-toolbar(
+      flat
+      color="rgba(52, 58, 64, 1)"
+    )
+      a(href="#")    
+        img(src="../../assets/images/mycure-logo.svg" height="25")
+      v-spacer
+      a(href="https://www.facebook.com/mycure.md")
+        img(src="../../assets/images/mycure-homepage-wireframe-icon-fb.png" height="25" href="https://google.com").px-2
+      a(href="https://www.youtube.com/channel/UCfQ5QBdFh6f9Tuj2tWkRmvg")
+        img(src="../../assets/images/mycure-homepage-wireframe-icon-ytub-e.png" height="25").px-2
+      a(href="https://mycure.md/ig-link")
+        img(src="../../assets/images/mycure-homepage-wireframe-icon-ig.png" height="25").px-2
+      a(href="https://mycure.md/linkedin-link")
+        img(src="../../assets/images/mycure-homepage-wireframe-icon-linkedin.png" height="25").px-2
+      a(href="https://mycure.md/twitter-link")
+        img(src="../../assets/images/mycure-homepage-wireframe-icon-twitter.png" height="25").px-2
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+.logo-img {
+  height: 20%;
+}
+
+
+</style>
+
