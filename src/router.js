46bd13065e2d77d791ada8a2e359125646e2e804
Francis Marquez <fnmarquez@up.edu.ph>
2019-05-10 00:51:46 +0800

    chore: add routes

--- src/router.js
+++ src/router.js
@@ -13,13 +13,20 @@ export default new Router({
       name: 'home',
       component: Home
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // }
+    {
+      path: '/privacy-policy',
+      name: 'privacy-policy', 
+      component: () => import('./components/privacy-policy')
+    },
+    {
+      path: '/terms',
+      name: 'terms', 
+      component: () => import('./components/terms')
+    },
+    {
+      path: '/our-story',
+      name: 'our-story', 
+      component: () => import('./components/our-story')
+    }
   ]
 });
