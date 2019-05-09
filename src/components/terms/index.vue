e2ef8bd33da875008ffb7181b38f6610b0ccaa3d
Francis Marquez <fnmarquez@up.edu.ph>
2019-05-10 00:55:54 +0800

    feat: add terms page

---
+++ src/components/terms/index.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  div.white
+    mcNavbarHome
+    panel-1
+    panel-2
+    mcFooter
+</template>
+
+<script>
+document.title = "MYCURE - Terms and Conditions";
+
+import mcNavbarHome from '../commons/mc-navbar-home';
+import mcFooter from '../commons/mc-footer';
+import panel1 from './panel-1';
+import panel2 from './panel-2';
+
+export default {
+  data () {
+    return {
+    };
+  }, 
+  components: {
+    mcNavbarHome,
+    mcFooter,
+    panel1,
+    panel2
+  }
+};
+</script>
+
+<style scoped>
+
+  p {
+    /* font-size: 16px; */
+    text-align: justify;
+  }
+
+  ul li {
+    font-size: 15px;
+  }
+
+</style>
---
+++ src/components/terms/panel-1.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  div.panel-1.mb-5
+    v-container
+      v-layout(row wrap align-center).panel-1-row
+        v-flex(xs12 md12).text-xs-center 
+          h5.font-m.mb-1 Last Updated | February 21, 2018
+          div.hr-blue
+          br
+          br
+          h1.font-xl Terms and Conditions
+    
+      
+  
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style scoped>
+  .panel-1 {
+    background-image: url('../../assets/images/mycure-terms-background-doctor-on-tablet-cropped.jpg');
+    background-position: center center;
+    background-size: cover;
+  }
+
+  .panel-1-row {
+    height: 420px;
+  }
+
+  hr {
+  height: 2px;
+  border-color: blue;
+  border: none;
+  width: 150px;
+  padding: 0;
+  margin: 0 auto;
+}
+</style>
