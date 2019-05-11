9bfd534256cc0e70a3031d688fe572589797758b
Francis Marquez <fnmarquez@up.edu.ph>
2019-05-11 14:32:15 +0800

    style: adjust panel-4 styling

--- src/components/home/panel-4.vue
+++ src/components/home/panel-4.vue
@@ -1,14 +1,13 @@
 <template lang="pug">
   //- Section 4 of Home Page
   div.px-5
-    v-container
-      v-flex(xs12 md12)
-          v-layout(row wrap justify-center)
-            strong(style="font-size: 25px").pa-5 Does your clinic have more than 3 branches? 
-              a(
-                href="https://mycure.md"
-              )
-               span Talk to us for custom pricing.
+    v-flex(xs12 md12)
+        v-layout(row wrap justify-center)
+          strong(style="font-size: 25px").pa-5.text-xs-center Does your clinic have more than 3 branches? 
+            a(
+              href="https://mycure.md"
+            )
+              span Talk to us for custom pricing.
 </template>
 
 <style scoped>
