7ccab7bb3f317ab7fcfc0858b01d8226fe6e0ce7
Jofferson R Tiquez <jofftiquez@gmail.com>
2019-05-14 01:53:07 +0800

    refactor: import mixins

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import './mixins';
 // import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
 
