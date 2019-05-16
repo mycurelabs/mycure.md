1f5d79ccaee0e4437e302014254f86a5816ad88c
Francis Marquez <fnmarquez@up.edu.ph>
2019-05-16 13:27:26 +0800

    feat: add panel-1 in pricing page

--- src/components/pricing/panel-1.vue
+++ src/components/pricing/panel-1.vue
@@ -1,7 +1,14 @@
 <template lang="pug">
   div.panel-1
-    v-container
-      h1 akjsdkjsadnsj
+    v-container 
+      v-flex(xs12 md12).pt-5
+        center 
+          strong.font-xl.text-xs-center.white--text Your best CMS investment yet
+          br
+          strong.font-m.white--text Quality clinic technology made more affordable. Only pay for what you need.
+          br
+          br
+          br
 </template>
 
 <script>
