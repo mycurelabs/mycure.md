d3d3a5206b38c5de0bb9ca8fc95cc5e1278b5a86
Francis Marquez <fnmarquez@up.edu.ph>
2019-05-16 13:08:01 +0800

    feat: add panel-6 cms page

--- src/components/clinic-management-system/panel-6.vue
+++ src/components/clinic-management-system/panel-6.vue
@@ -1,5 +1,45 @@
 <template lang="pug">
-  
+  div
+    v-container
+      v-flex(xs12 md12)
+      v-layout(column wrap justify-center).mx-5
+        font(style="width: auto;").font-l.text-xs-center.mx-5 "It's a great experience using MYCURE app in our rural setup. I've been through many EMR apps, but this one is the best so far."
+        br
+        br
+        font.font-xs.text-xs-center Dr. Gay Cadorna-Toledo
+        b.font-xs.text-xs-center Pediatrician, Misamis Occidental
+        font.font-xs.text-xs-center MYCURE Doctor since 2017
+        br
+        br
+        br
+      v-flex(xs12 md12)
+        v-layout(row justify-center)
+          div(style="width: 27%;")
+            v-img(:src="require('../../assets/images/mycure-web-banner-pricing-panel-multispecialty.png')")
+          div(style="diplay: inline-block;")
+          v-card.price-card.text-xs-left.pa-5
+            strong.pb-2.font-m For Multispecialty Clinics
+            br
+            br
+            font.font-xs An all-around solution for your outpatient, 
+              br
+              | multispecialty or specialized clinics that will 
+              br
+              | manage your full-patient journeys with ease.
+            br
+            br
+            font &#10003 &nbsp Queuing &amp Registration
+            br
+            font &#10003 &nbsp Patient Medical Records
+            br
+            font &#10003 &nbsp Billing &amp Collection
+            br
+            font &#10003 &nbsp Laboratory &amp Imaging
+            br
+            font &#10003 &nbsp Materials Management
+            v-layout(row wrap justify-center).py-4
+              v-btn(style="background-color: #2e9fdf; color: white; width: auto" large) 
+                strong GET MYCURE FREE
 </template>
 
 <script>
@@ -7,3 +47,9 @@ export default {
   
 };
 </script>
+
+<style scoped>
+.price-card{
+  border-radius: 0 10px 10px 0;
+}
+</style>
