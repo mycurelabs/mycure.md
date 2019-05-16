e7747b2803c44d21ec8536ac2dd3606114bd3913
Francis Marquez <fnmarquez@up.edu.ph>
2019-05-16 13:07:33 +0800

    style: adjust styling of panel-1

--- src/components/clinic-management-system/panel-1.vue
+++ src/components/clinic-management-system/panel-1.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
   div.panel-1.pt-5.white
     v-container.pt-5
-      v-layout(row wrap align-center).pt-5
+      v-layout(row justify-center).pt-5
         v-flex(xs12 md5)
-          font.font-l A System That Simplifies Work for Multispecialty Clincs
+          font.font-l A System That Simplifies Work for Multispecialty Clinics
           br
           br
-          em.font-s - Itegrated Medical Records
+          em.font-s - Integrated Medical Records
               br
               | - User-friendly Features
               br 
@@ -27,7 +27,7 @@
             v-icon(color="#2e9fdf") play_circle_filled
             strong.font-xs.px-2.black--text(style="text-decoration: underline") WATCH VIDEO
         v-flex(xs12 md7)
-          img(width="100%" src="../../assets/images/mycure-web-banner-first-panel-doctor-clinic-patients.png")
+          img(width="100%" src="../../assets/images/mycure-cms-web-banner-first-panel-doctor-clinic-patients.png")
 
 </template>
 
