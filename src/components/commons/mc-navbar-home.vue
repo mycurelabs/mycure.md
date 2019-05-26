291606c58cd111aefe9e503d4d09dab8ff139b09
Jofferson R Tiquez <jofftiquez@gmail.com>
2019-05-27 00:17:02 +0800

    refactor: update links

--- src/components/commons/mc-navbar-home.vue
+++ src/components/commons/mc-navbar-home.vue
@@ -8,16 +8,27 @@
         strong.font-xs.tab FOR PRIVATE PRACTICE
       v-btn(href="/clinic-management-system" flat)
         strong.font-xs.tab FOR MULTISPECIALTY CLINICS
-      v-btn(flat)
+      v-btn(
+        flat 
+        :href="`${siginURL}?target=${cmsURL}/authenticate`"
+      )
         strong.font-xs.tab LOGIN
       v-btn(
         color="info"
+        :href="`${siginURL}/signup/private/step-1`"
       )
         strong.font-xs.tab SIGN UP
 </template>
 
 <script>
-  export default {};
+  export default {
+    data () {
+      return {
+        siginURL: process.env.VUE_APP_SIGNIN_URL,
+        cmsURL: process.env.VUE_APP_CMS_URL,
+      };
+    }
+  };
 </script>
 
 <style scoped>
