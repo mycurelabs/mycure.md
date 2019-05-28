e797fbf5c8bc41ba50bc66b01710b78bdcaecd37
Francis Marquez <fnmarquez@up.edu.ph>
2019-05-28 17:39:38 +0800

    refactor: update panel-2 home

--- src/components/home/panel-2.vue
+++ src/components/home/panel-2.vue
@@ -4,15 +4,15 @@
       v-flex(xs12 md4)
         router-link(:to="{name: 'pricing'}").link.white--text 
           div.panel-2-box-a
-            b.font-s.text-center.font-mc-grey Serve more patients
+            strong.font-s.text-center.font-mc-grey Serve more patients
       v-flex(xs12 md4)
         router-link(:to="{name: 'pricing'}").link.white--text 
           div.panel-2-box-b
-            b.font-s.text-center.font-mc-grey Easy record-keeping
+            strong.font-s.text-center.font-mc-grey Comply to Data Privacy Law
       v-flex(xs12 md4)
         router-link(:to="{name: 'pricing'}").link.white--text 
           div.panel-2-box-c
-            b.font-s.text-center.font-mc-grey Real-time daily census
+            strong.font-s.text-center.font-mc-grey Works Offline
 </template>
 
 <script>
@@ -29,10 +29,10 @@
 <style scoped>
   .panel-2-box-a {
   height: 90px;
-  background-color: white;
+  background-color: #f5f5f5;
   border-width: thin;
   border-style: solid;
-  border-color: lightgray lightgray transparent transparent;
+  border-color: lightgray lightgray lightgray transparent;
   text-align: center;
   vertical-align: middle;
   line-height: 90px;  
@@ -40,10 +40,10 @@
   }
   .panel-2-box-b {
   height: 90px;
-  background-color: white;
+  background-color: #f5f5f5;
   border-width: thin;
   border-style: solid;
-  border-color: lightgray lightgray transparent transparent;
+  border-color: lightgray lightgray lightgray transparent;
   text-align: center;
   vertical-align: middle;
   line-height: 90px;  
@@ -51,10 +51,10 @@
 
   .panel-2-box-c {
     height: 90px;
-    background-color: white;
+    background-color: #f5f5f5;
     border-width: thin;
     border-style: solid;
-    border-color: lightgray transparent transparent transparent;
+    border-color: lightgray transparent lightgray transparent;
     text-align: center;
     vertical-align: middle;
     line-height: 90px;  
