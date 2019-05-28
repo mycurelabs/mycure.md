27ec273ba85cc4f98a8c563e03196f07753f74d6
Francis Marquez <fnmarquez@up.edu.ph>
2019-05-28 10:45:34 +0800

    refactor: new toolbar design

--- src/components/commons/mc-navbar-home.vue
+++ src/components/commons/mc-navbar-home.vue
@@ -1,25 +1,25 @@
 <template lang="pug">
   div
     div(v-if="!$isMobile")
-      v-toolbar(app flat fixed).white
-        v-layout(row).px-5
+      v-toolbar(app flat fixed).pt-3.white
+        v-layout(row wrap align-center).px-5
           a(href="/")
-            img(src="../../assets/images/mycure-logo.svg" height="35")
-          v-spacer
-          v-btn(href="/electronic-medical-records" flat)
-            strong.font-xs.tab FOR PRIVATE PRACTICE
+            img(src="../../assets/images/mycure-logo.svg" height="50")
+          v-btn(href="/electronic-medical-records" flat).pl-5
+            strong.font-s.tab FOR PRIVATE PRACTICE
           v-btn(href="/clinic-management-system" flat)
-            strong.font-xs.tab FOR MULTISPECIALTY CLINICS
+            strong.font-s.tab FOR MULTISPECIALTY CLINICS
+          v-spacer
           v-btn(
             flat
             :href="`${siginURL}?target=${cmsURL}/authenticate`"
           )
-            strong.font-xs.tab LOGIN
+            strong.font-s.tab LOGIN
           v-btn(
             color="info"
             :href="`${siginURL}/signup/private/step-1`"
           )
-            strong.font-xs.tab SIGN UP
+            strong.font-s.tab SIGN UP
     div(v-else)
       v-toolbar(app flat fixed).white
         a(href="/")
