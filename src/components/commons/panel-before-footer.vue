967180ddf7db6e259d34a36c25b03d94de788b3f
Francis Marquez <fnmarquez@up.edu.ph>
2019-05-30 01:07:44 +0800

    style: make slots for panel before footer

--- src/components/commons/panel-before-footer.vue
+++ src/components/commons/panel-before-footer.vue
@@ -1,36 +1,29 @@
 <template lang="pug">
-  div.py-5.white
-    div(v-if="!$isMobile")
-      v-container
-        v-layout(row)
-          v-flex(xs12 md6)
-            img(width="100%" src="../../assets/images/mycure-web-banner-last-panel-doctor-clinic.png")
-          v-flex(xs12 md6 offset-xs1 align-self-center)
-            h2.font-l Because we believe your clinic can stand out and be its best everyday.
-            br
-            p.font-s.mt-3 Experience what you can do more with
-              a(href="https://mycure.md")
-                strong &nbsp; MYCURE 
-              br  
-              | technology by your side.
-            v-btn(color="info" large) 
-              strong.text-none Contact Sales Today 
-    div(v-else)
-      v-container.pa-4
-        h2.font-l Because we believe your clinic can stand out and be its best everyday.
-        br
-        p.font-s.mt-3 Experience what you can do more with
-          a(href="https://mycure.md")
-            strong &nbsp; MYCURE 
-            | technology by your side.
-        br
-        v-layout(row justify-center)    
-          v-btn(color="info" large) 
-            strong.text-none Contact Sales Today   
+  div
+    v-container
+      v-layout(row wrap justify-center)
+        v-flex(xs6)
+          slot(name="home-img")
+          slot(name="emr-img")
+          slot(name="cms-img")
+        v-flex(xs6 align-self-center)
+          slot(name="home-txt")
+          slot(name="emr-txt")
+          slot(name="cms-txt")
+          br
+          slot(name="emr-sub-txt")
+          slot(name="cms-sub-txt")
+          br
+          br
+          slot(name="sign-up-button")
 </template>
 
 <script>
+  
   export default {
+    components: {
+      //
+    },
     data () {
       return {
         //
@@ -40,8 +33,5 @@
 </script>
 
 <style scoped>
-  a {
-    text-decoration: none;
-    color: black
-  }
+
 </style>
