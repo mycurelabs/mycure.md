479f27e4baecdb65454a893fe9dc025bf438aaff
Francis Marquez <fnmarquez@up.edu.ph>
2019-05-31 12:13:11 +0800

    style: change spacing between paragraphs in terms page

--- src/components/terms/panel-1.vue
+++ src/components/terms/panel-1.vue
@@ -2,11 +2,7 @@
   div.panel-1.mb-5.red
     v-container
       v-layout(row wrap align-center).panel-1-row
-        v-flex(xs12 md12).text-xs-center 
-          h5.font-small.mb-1 Last Updated | February 21, 2018
-          div.hr-blue
-          br
-          br
+        v-flex(xs12 md12).text-xs-center
           h1(v-if="!$isMobile").font-large Terms and Conditions
           h1(v-else).font-large Terms and Conditions
 </template>
