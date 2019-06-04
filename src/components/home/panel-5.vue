be8694a64ff3d932fcd80f7022fd407a1795fb27
Marc Almond Valleza <majvalleza@gmail.com>
2019-06-04 11:10:30 +0800

    refactor: update testimonial content

--- src/components/home/panel-5.vue
+++ src/components/home/panel-5.vue
@@ -23,14 +23,11 @@
                   span.font-medium.white--text {{ testimonial1 }}
                   br
                   br
-                  span.font-small.white--text Dr. Gay Cadorna-Toledo
+                  span.font-small.white--text Atty. Ivy D. Patdu, M.D
                   br
-                  br
-                  strong.font-small.white--text Pediatrician,
+                  strong.font-small.white--text Deputy Privacy Commissioner,
                     | 
-                  span.font-small.white--text Misamis Occidental
-                    br
-                    | MYCURE Doctor since 2017
+                  span.font-small.white--text NPC
                 slide(data-index="2")
                   span.font-medium.white--text {{ testimonial2 }}
                   br
@@ -68,7 +65,7 @@
   export default {
     data () {
       return {
-        testimonial1: '“It\'s a great experience using MYCURE app in our rural setup. I\'ve been through many EMR apps, but this one is the best so far.”', 
+        testimonial1: '“We should harness the power of technology. We should incorporate in system designs means to obtain information while remaining true to our obligations for data protection.”', 
         testimonial2: '“MYCURE provides a well organized patient data profiling with customized options for personalized encoding and with integration of lab tests, immunizations as well as growth charts that are very important for us as pediatricians for growth monitoring of our little ones. It is very convenient to use, simple, adaptable and user friendly even to my secretary.”',
         testimonial3: '“MYCURE fulfills a long time need of physicians for accurate records of patients which they can carry with them wherever they are. It enables them also to prescribe and countercheck their prescriptions, issue medical certificate, communicate with other doctors for referrals.”'
       };
