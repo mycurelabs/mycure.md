8819d22c7d3ba0d837f530627fed027456a9dd2a
Marc Almond Valleza <majvalleza@gmail.com>
2019-06-04 16:09:20 +0800

    feat (home-m): add testimonial carousel

--- src/components/home/panel-7-mobile.vue
+++ src/components/home/panel-7-mobile.vue
@@ -2,13 +2,51 @@
   div.white.py-5
     div
       v-container
-        v-layout(column justify-center)
-          span.font-m.text-xs-center "It's a great experience using MYCURE app in our rural setup. I've been through many EMR apps, but this one is the best so far." 
-          br
-          br
-          span.font-s.text-xs-center Dr. Gay Cadorna-Toledo
-          b.font-xs.text-xs-center Pediatrician, Misamis Occidental
-          span.font-xs.text-xs-center MYCURE Doctor since 2017
+        v-layout(justify-center).px-3.red
+          div
+            //- https://ssense.github.io/vue-carousel/
+            carousel(
+              :per-page="1" 
+              :autoplay="false"
+              :loop="true"
+              :navigationEnabled="true"
+              :adjustableHeight="true"
+              paginationActiveColor="blue"
+              paginationColor="grey"
+              :navigationClickTargetSize="50"
+              navigationNextLabel=" "
+              navigationPrevLabel=" "
+            )
+              slide(data-index="1").text-xs-center
+                span.font-small {{ testimonial1 }}
+                br
+                br
+                span.font-s Atty. Ivy D. Patdu, M.D
+                br
+                strong.font-s Deputy Privacy Commissioner,
+                  | 
+                span.font-small NPC
+              slide(data-index="2").text-xs-center.blue.px-5
+                span.font-small {{ testimonial2 }}
+                br
+                br
+                span.font-s Dr. Gay Cadorna-Toledo
+                br
+                br
+                strong.font-s Pediatrician,
+                  | 
+                span.font-s Misamis Occidental
+                  br
+                  | MYCURE Doctor since 2017
+              slide(data-index="3").text-xs-center
+                span.font-small {{ testimonial3 }}
+                br
+                br
+                span.font-s Dr. Nelson S. Abelardo
+                br
+                strong.font-s Cardiologist,
+                  | 
+                span.font-s Manila Doctors Hospital
       div
         img(width="100%" src="../../assets/images/mycure-web-banner-fifth-panel-private-clinic.png")
         v-layout(column wrap).pt-4.pb-5.pr-5.pl-5
@@ -45,9 +83,13 @@
   export default {
     data () {
       return {
-          private_title: 'Get a powerful combination of features designed for physicians who are practicing solo or in groups.',
-          multispecialty_title: 'An all-around solution for your outpatient, multi-specialty or specialized clinics that will manage your full-patient journeys with ease.',
-          siginURL: process.env.VUE_APP_SIGNIN_URL
+        private_title: 'Get a powerful combination of features designed for physicians who are practicing solo or in groups.',
+        multispecialty_title: 'An all-around solution for your outpatient, multi-specialty or specialized clinics that will manage your full-patient journeys with ease.',
+        siginURL: process.env.VUE_APP_SIGNIN_URL,
+
+        testimonial1: '“We should harness the power of technology. We should incorporate in system designs means to obtain information while remaining true to our obligations for data protection.”', 
+        testimonial2: '“MYCURE provides a well organized patient data profiling with customized options for personalized encoding and with integration of lab tests, immunizations as well as growth charts that are very important for us as pediatricians for growth monitoring of our little ones. It is very convenient to use, simple, adaptable and user friendly even to my secretary.”',
+        testimonial3: '“MYCURE fulfills a long time need of physicians for accurate records of patients which they can carry with them wherever they are. It enables them also to prescribe and countercheck their prescriptions, issue medical certificate, communicate with other doctors for referrals.”'
       };
     }
   };
