7e1061809a45bb7ad8e38dd03037a2ca89a9d13b
Marc Almond Valleza <majvalleza@gmail.com>
2019-06-04 18:02:20 +0800

    refactor (cms-m): remove filler

--- src/components/clinic-management-system/panel-4.vue
+++ src/components/clinic-management-system/panel-4.vue
@@ -38,11 +38,6 @@
           div(style="margin: 0px 0px 0px -20px; z-index: 1") 
             img(src="../../assets/images/mycure-web-video-cover-decor-doctor-right.png")
     div(v-else)
-      div.py-5
-        v-layout(row wrap justify-center)
-          strong(style="font-size: 25px").text-xs-center Does your clinic have more than 3 branches? 
-            a
-              span(@click="toggleChat") Talk to us for custom pricing.
 </template>
 
 <script>
