8aa82cf74e0606015d8c4060609571a63c248d82
Francis Marquez <fnmarquez@up.edu.ph>
2019-06-04 17:20:05 +0800

    style: change layout of homepage carousel

--- src/components/home/panel-5.vue
+++ src/components/home/panel-5.vue
@@ -2,7 +2,7 @@
   div
     div(v-if="!$isMobile")#panel5
       center
-        v-container
+        v-container(style="width: 65% !important")
           v-layout(justify-center)
             v-icon(size="100px") keyboard_arrow_left
             div    
@@ -20,33 +20,33 @@
                 navigationPrevLabel=" "
               ) 
                 slide(data-index="1")
-                  span.font-medium.white--text {{ testimonial1 }}
-                  br
-                  br
-                  span.font-small.white--text Atty. Ivy D. Patdu, M.D
-                  br
-                  strong.font-small.white--text Deputy Privacy Commissioner,
-                    | 
-                  span.font-small.white--text NPC
-                slide(data-index="2")
-                  span.font-medium.white--text {{ testimonial2 }}
+                  h2.white--text {{ testimonial1 }}
                   br
                   br
                   span.font-small.white--text Dr. Gay Cadorna-Toledo
                   br
-                  br
-                  strong.font-small.white--text Pediatrician,
+                  span.font-small.white--text Pediatrician,
                     | 
                   span.font-small.white--text Misamis Occidental
                     br
                     | MYCURE Doctor since 2017
+                slide(data-index="2")
+                  h2.white--text {{ testimonial2 }}
+                  br
+                  br
+                  span.font-small.white--text Atty. Ivy D. Patdu, M.D 
+                  br
+                  span.font-small.white--text Deputy Privacy Commissioner,
+                    | 
+                  span.font-small.white--text NPC
+                    
                 slide(data-index="3")
-                  span.font-medium.white--text {{ testimonial3 }}
+                  h2.white--text {{ testimonial3 }}
                   br
                   br
                   span.font-small.white--text Dr. Nelson S. Abelardo
                   br
-                  strong.font-small.white--text Cardiologist,
+                  span.font-small.white--text Cardiologist,
                     | 
                   span.font-small.white--text Manila Doctors Hospital
             v-icon(size="100px") keyboard_arrow_right
@@ -65,9 +65,9 @@
   export default {
     data () {
       return {
-        testimonial1: '“We should harness the power of technology. We should incorporate in system designs means to obtain information while remaining true to our obligations for data protection.”', 
-        testimonial2: '“MYCURE provides a well organized patient data profiling with customized options for personalized encoding and with integration of lab tests, immunizations as well as growth charts that are very important for us as pediatricians for growth monitoring of our little ones. It is very convenient to use, simple, adaptable and user friendly even to my secretary.”',
-        testimonial3: '“MYCURE fulfills a long time need of physicians for accurate records of patients which they can carry with them wherever they are. It enables them also to prescribe and countercheck their prescriptions, issue medical certificate, communicate with other doctors for referrals.”'
+        testimonial1: '“It\'s a great experience using MYCURE app in our rural setup. I\'ve been through many EMR apps, but this one is the best so far... It is very convenient to use, simple, adaptable and user friendly even to my secretary.”',
+        testimonial2: '“We should harness the power of technology. We should incorporate in system designs means to obtain information while remaining true to our obligations for data protection.” ', 
+        testimonial3: '“MYCURE fulfills a long time need of physicians for accurate records of patients which they can carry with them wherever they are.”'
       };
     } 
   };
