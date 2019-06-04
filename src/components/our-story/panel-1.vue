6e283afbd7f8666737b6e372458f8d5461eb0750
Francis Marquez <fnmarquez@up.edu.ph>
2019-06-04 14:07:56 +0800

    style: update panel1 mobile responsiveness of our story page

--- src/components/our-story/panel-1.vue
+++ src/components/our-story/panel-1.vue
@@ -1,38 +1,50 @@
 <template lang="pug">
-  div.white.pa-5
-    v-container(justify-center fill-height).px-5.panel-1
-      v-layout(row wrap)
-        v-flex(xs12 md5 align-self-center)
-          h1(style="line-height: 70px;").font-large MYCURE cares for people who care for people.
-          br
-          br
-          p.font-small Doctors, nurses, and clinic professionals have a lot of work and we want to make that work easier.
-          br
-          v-btn(
-            v-if="!$isMobile"
-            color="info" 
-            large
-            id="showVid"
-            @click="showVid = true") 
-              strong.font-small.text-none Watch video
-          center(v-else)
+  div
+    div(v-if="!$isMobile").white.pa-5
+      v-container(justify-center fill-height).px-5.panel1
+        v-layout(row wrap)
+          v-flex(xs12 md5 align-self-center)
+            h1(style="line-height: 70px;").font-large MYCURE cares for people who care for people.
+            br
+            br
+            p.font-small Doctors, nurses, and clinic professionals have a lot of work and we want to make that work easier.
+            br
             v-btn(
+              v-if="!$isMobile"
               color="info" 
               large
               id="showVid"
               @click="showVid = true") 
-                strong WATCH VIDEO
-        v-flex(v-if="!$isMobile" xs12 md7 align-self-center).pl-5
-          img(width="100%" src="../../assets/images/mycure-homepage-doctor-kid-pediatrician-drawing.png")
-
-    v-dialog(v-model="showVid" max-width="700px")
-      iframe(
-        align="center"
-        v-if="showVid"
-        id="ytplayer" 
-        type="text/html" width="100%" height="450"
-        src="https://www.youtube.com/embed/ZR8e-Egav0M?autoplay=0&loop=1&showinfo=0&rel=0"
-        frameborder="0")
+                strong.font-small.text-none Watch video
+            center(v-else)
+              v-btn(
+                color="info" 
+                large
+                id="showVid"
+                @click="showVid = true") 
+                  strong WATCH VIDEO
+          v-flex(v-if="!$isMobile" xs12 md7 align-self-center).pl-5
+            img(width="100%" src="../../assets/images/mycure-homepage-doctor-kid-pediatrician-drawing.png")
+      v-dialog(v-model="showVid" max-width="700px")
+        iframe(
+          align="center"
+          v-if="showVid"
+          id="ytplayer" 
+          type="text/html" width="100%" height="450"
+          src="https://www.youtube.com/embed/ZR8e-Egav0M?autoplay=0&loop=1&showinfo=0&rel=0"
+          frameborder="0")
+    div(v-else)
+      v-container
+        v-flex(xs12 align-self-center)
+          h1(style="font-weight: bold").font-l MYCURE cares for people who care for people.
+        br
+        v-flex(xs12 align-self-center)
+          span.font-s Doctors, nurses, and clinic professionals have a lot of work and we want to make that work easier.
+        v-layout(row justify-center)
+        br
+        br
+        br
+        div.hr-blue
 </template>
 
 <script>
@@ -46,7 +58,7 @@
 </script>
 
 <style scoped>
-  .panel-1{
+  .panel1{
     height: auto;
   }
 
