4dd67d2e4f6c5894b246f58475f5cb4634a1b126
Marc Almond Valleza <majvalleza@gmail.com>
2019-06-05 11:09:49 +0800

    refactor (home-m): remove misplaced video

--- src/components/home/panel-5.vue
+++ src/components/home/panel-5.vue
@@ -46,13 +46,6 @@
             v-icon(size="100px") keyboard_arrow_right
       br
     div(v-else)
-      center 
-        iframe(
-          align="middle"
-          id="ytplayer" 
-          type="text/html" width="100%" height="200"
-          src="https://www.youtube.com/embed/ZR8e-Egav0M?autoplay=0&loop=1&showinfo=0&rel=0"
-          frameborder="0")
 </template>
 
 <script>
