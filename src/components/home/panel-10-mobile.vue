0835f23ce47c8a3fb9cfb280cdb9ef127c92467f
Marc Almond Valleza <majvalleza@gmail.com>
2019-06-05 18:15:10 +0800

    chore (home-m): add syncbase gif as comment

--- src/components/home/panel-10-mobile.vue
+++ src/components/home/panel-10-mobile.vue
@@ -1,12 +1,13 @@
 <template lang="pug">
-  div.white.mt-5
+  div.white.mt-4
     v-container
-      span.font-l What makes MYCURE so different?
+      span.font-l.lh-title What makes MYCURE so different?
       br
       br
       span.font-s {{ different }}
       br
       br
+      //- img(src="../../assets/images/mycure-homepage-syncbase-animated.gif" width="100%")
       v-card.elevation-5
         a
           img(v-if="!videoDialog" @click.stop="videoDialog=!videoDialog" width="100%" src="../../assets/images/mycure-web-video-cover-syncbase-online-offline.png")
@@ -25,7 +26,7 @@
   export default {
     data () {
       return {
-        different: "In Southeast Asian regions where internet coverage is limited, MYCURE made it possible for clinics to use a cloud-based system in their local network even if the internet goes down and just syncs data when back online.",
+        different: "In Southeast Asian regions where internet coverage is limited, MYCURE made it possible for clinics to use a cloud-based system in their local network even if the internet goes down.",
         videoDialog: false
       };
     }
