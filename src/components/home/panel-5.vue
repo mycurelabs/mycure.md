2bc799a39b361ff11e88835b971971ddd3919d81
Marc Almond Valleza <majvalleza@gmail.com>
2019-06-05 15:58:44 +0800

    chore (home-m): move mobile vid contents

--- src/components/home/panel-5.vue
+++ src/components/home/panel-5.vue
@@ -45,7 +45,27 @@
                     | Cardiologist, Manila Doctors Hospital
             v-icon(size="100px") keyboard_arrow_right
       br
-    div(v-else)
+    div(v-else style="margin-top: -15%")
+      div(style="background-color: #f5f5f5")
+        img(src="../../assets/images/mycure-home-web-video-green-background-mobile.png" width="100%")
+        a
+          img(
+            v-if="!videoDialog" 
+            @click.stop="videoDialog=!videoDialog"
+            src="../../assets/images/mycure-home-web-video-cover-most-complete-clinic-management-system-mobile.png" 
+            width="100%"
+            style="margin: -2% 0 -2% 0;"
+          )
+          iframe(
+            v-else
+            style="margin: -2% 0 -2% 0;"
+            align="middle"
+            id="ytplayer" 
+            type="text/html" width="375" height="250"
+            src="https://www.youtube.com/embed/ZR8e-Egav0M?autoplay=1&loop=1&showinfo=0&rel=0"
+            frameborder="0"
+            allowfullscreen
+          )
 </template>
 
 <script>
@@ -54,7 +74,8 @@
       return {
         testimonial1: '“It\'s a great experience using MYCURE app in our rural setup. I\'ve been through many EMR apps, but this one is the best so far... It is very convenient to use, simple, adaptable and user friendly even to my secretary.”',
         testimonial2: '“We should harness the power of technology. We should incorporate in system designs means to obtain information while remaining true to our obligations for data protection.” ', 
-        testimonial3: '“MYCURE fulfills a long time need of physicians for accurate records of patients which they can carry with them wherever they are.”'
+        testimonial3: '“MYCURE fulfills a long time need of physicians for accurate records of patients which they can carry with them wherever they are.”',
+        videoDialog: false
       };
     } 
   };
