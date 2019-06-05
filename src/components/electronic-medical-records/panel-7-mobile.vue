9e58dd9438e2714529fd1013b99ba592bb7c403c
Marc Almond Valleza <majvalleza@gmail.com>
2019-06-05 16:19:13 +0800

    feat (emr-m): add panel 7 contents

--- src/components/electronic-medical-records/panel-7-mobile.vue
+++ src/components/electronic-medical-records/panel-7-mobile.vue
@@ -1,5 +1,13 @@
 <template lang="pug">
-  
+  div#panel7mobile.pt-5.transparent
+    center
+      v-layout(column justify-center style="width: 90%").pt-5
+        img(style="max-width: 80%" src="../../assets/images/mycure-web-banner-last-panel-doctor-clinic-for-private-clinic.png")
+        br 
+        span(style="font-size: 200%").text-xs-center.lh-title Be at your best version everyday.
+        br
+        br
+        span.font-s.text-xs-center Experience what you can do more with MYCURE technology by your side.
 </template>
 
 <script>
