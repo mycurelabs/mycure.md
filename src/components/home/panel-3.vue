867791028dceb5c7bb7bcf91a7c77cb3dea3f9d3
Marc Almond Valleza <majvalleza@gmail.com>
2019-06-05 12:47:21 +0800

    feat (home-m): add panel 3 carousel

--- src/components/home/panel-3.vue
+++ src/components/home/panel-3.vue
@@ -7,7 +7,39 @@
       div.py-5
         center
           img(style="max-width: 90%;" src="../../assets/images/mycure-home-web-complete-clinic-management-system-diagram.png")
-    
+    div(v-else)
+      v-container.pt-5
+        v-layout(row justify-center)
+          span.font-l.lh-title.text-xs-center It's complete - everything your clinic needs.
+        br
+        v-layout(row justify-center)
+          div
+            //- https://ssense.github.io/vue-carousel/
+            carousel(
+              :per-page="1" 
+              :autoplay="true"
+              :loop="true"
+              :navigationEnabled="true"
+              :adjustableHeight="true"
+              paginationActiveColor="grey"
+              paginationColor="lightgrey"
+              :navigationClickTargetSize="50"
+              navigationNextLabel=" "
+              navigationPrevLabel=" "
+            )
+              slide(data-index="1").text-xs-center
+                img(src="../../assets/images/mycure-web-clinic-room-01.png" width="100%")
+              slide(data-index="2").text-xs-center
+                img(src="../../assets/images/mycure-web-clinic-room-02.png" width="100%")
+              slide(data-index="3").text-xs-center
+                img(src="../../assets/images/mycure-web-clinic-room-03.png" width="100%")
+              slide(data-index="4").text-xs-center
+                img(src="../../assets/images/mycure-web-clinic-room-04.png" width="100%")
+              slide(data-index="5").text-xs-center
+                img(src="../../assets/images/mycure-web-clinic-room-05.png" width="100%")
+              slide(data-index="6").text-xs-center
+                img(src="../../assets/images/mycure-web-clinic-room-06.png" width="100%")
+
 </template>
 
 <script>
