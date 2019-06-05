1943adbbd21ae8ad8d3cafd422597b1337371f99
Marc Almond Valleza <majvalleza@gmail.com>
2019-06-05 16:37:13 +0800

    feat (cms-m): add video asset

--- src/components/clinic-management-system/panel-4.vue
+++ src/components/clinic-management-system/panel-4.vue
@@ -37,7 +37,27 @@
                 )
           div(style="margin: 0px 0px 0px -20px; z-index: 1") 
             img(src="../../assets/images/mycure-web-video-cover-decor-doctor-right.png")
-    div(v-else)
+    div(v-else style="margin-top: -15%")
+      div(style="background-color: #f5f5f5")
+        img(src="../../assets/images/mycure-home-web-video-green-background-mobile.png" width="100%")
+        a
+          img(
+            v-if="!videoDialog" 
+            @click.stop="videoDialog=!videoDialog"
+            src="../../assets/images/mycure-home-web-video-cover-most-complete-clinic-management-system-mobile.png" 
+            width="100%"
+            style="margin: -2% 0 -2% 0;"
+          )
+          iframe(
+            v-else
+            style="margin: -2% 0 -2% 0;"
+            align="middle"
+            id="ytplayer" 
+            type="text/html" width="375" height="250"
+            src="https://www.youtube.com/embed/ZR8e-Egav0M?autoplay=1&loop=1&showinfo=0&rel=0"
+            frameborder="0"
+            allowfullscreen
+          )
 </template>
 
 <script>
