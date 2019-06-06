929aa237e0f3606b69dda802ba684c6940d74420
Marc Almond Valleza <majvalleza@gmail.com>
2019-06-06 13:12:30 +0800

    style: make border thinner

--- src/components/home/panel-6.vue
+++ src/components/home/panel-6.vue
@@ -34,11 +34,11 @@
                 img(src="../../assets/images/mycure-web-bullet-check.png").mr-3
                 span.font-small Multi-branch Clinics
           tr
-            td 
+            td(style="border-bottom: 1px solid #bbb;")
               v-layout(row wrap center)
                 img(src="../../assets/images/mycure-web-bullet-check.png").mr-3 
                 span.font-small Industrial Clinics
-            td 
+            td(style="border-bottom: 1px solid #bbb;")
               v-layout(row wrap center)
                 img(src="../../assets/images/mycure-web-bullet-check.png").mr-3
                 span.font-small Dental Clinics
@@ -78,7 +78,7 @@
   table tr td {
     border-right: 1px solid #bbb;
     border-top: 1px solid #bbb;
-    border-bottom: 1px solid #bbb;
+    border-bottom: 0px solid #bbb;
     padding: 15px;
   }
   table tr td:first-child {
