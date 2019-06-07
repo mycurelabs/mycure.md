a60b3daa864e01b63450c71e4921e8a2e81c19f1
Marc Almond Valleza <majvalleza@gmail.com>
2019-06-07 18:35:46 +0800

    chore: create filler component

---
+++ src/components/commons/filler.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  div
+    v-container
+      v-layout(justify-center).text-xs-center
+        slot(name="privateFiller")
+        slot(name="multiFiller")
+      v-layout(justify-center).text-xs-center
+        slot(name="chatWindow")
+</template>
