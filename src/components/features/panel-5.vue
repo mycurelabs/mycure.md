197b59e37f7baca0f023f4d1f2d6d0406820a682
Francis Marquez <fnmarquez@up.edu.ph>
2019-06-11 14:27:55 +0800

    feat(features): add panel 5

--- src/components/features/panel-5.vue
+++ src/components/features/panel-5.vue
@@ -1,17 +1,73 @@
 <template lang="pug">
-  
+  //- TODO: mobile responsiveness
+  div
+    div(v-if="!$isMobile")#panel5.pt-5
+      v-container
+        center
+          strong.font-small.font-mc-blue Billing and Payments
+        br
+        br
+        v-layout(row justify-center)
+          span.font-medium Simplified billing, beautiful reports.
+        br
+        br
+        v-layout(row justify-center)
+          span.font-small.text-xs-center Produce neatly-organized reports in a minute or less.
+        br
+        br
+        v-layout(row justify-center)
+          ul
+            li
+              v-layout(v-for="(item, index) in items" :key="index" row align-left).pb-3 
+                img(src="../../assets/images/mycure-web-bullet-check.png")
+                span.font-small.pl-3 {{ item }} 
+        br
+        br
+        br
+        br
+        br
+        v-layout(row justify-center)  
+          //- https://ssense.github.io/vue-carousel/
+          center
+            carousel(
+              :per-page="1" 
+              :autoplay="false"
+              :loop="true"
+              paginationActiveColor="white"
+              paginationColor="grey"
+              navigationNextLabel=" "
+              navigationPrevLabel=" "
+              style="height: 550px"
+            )
+              slide(v-for="(feature,index) in 6" :key="index" :data-index="feature")
+                img(:src="require(`../../assets/images/mycure-web-features-mockup-0${feature}-A.png`)" width="70%")
+    //- div(v-else)
 </template>
 
 <script>
   export default {
     data () {
       return {
-        //
+        items: [
+          'Payment Management & History',
+          'Invoice & Receipt Printing',
+          'HMO Statements, Collections, & Reports',
+          'Interface with Materials Management'
+        ]
       };
     }
   };
 </script>
 
 <style scoped>
+  #panel5 {
+    background-image: url('../../assets/images/mycure-web-features-gradient-background.png');
+    background-position: center bottom;
+    background-repeat: no-repeat;
+    background-size: 100% auto;
+  }
 
+  ul {
+    list-style-type: none;
+  }
 </style>
