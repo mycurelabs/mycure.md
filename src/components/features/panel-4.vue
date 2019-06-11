a2b63ec86822426e123f23c18b7d2a461b33b93b
Francis Marquez <fnmarquez@up.edu.ph>
2019-06-11 14:27:52 +0800

    feat(features): add panel 4

--- src/components/features/panel-4.vue
+++ src/components/features/panel-4.vue
@@ -1,5 +1,44 @@
 <template lang="pug">
-  
+  //- TODO: mobile responsiveness
+  div
+    div(v-if="!$isMobile")#panel4.pt-5
+      v-container
+        center
+          strong.font-small.font-mc-blue Registration and Queueing
+        br
+        br
+        v-layout(row justify-center)
+          span.font-medium Value everyone's time more effectively.
+        br
+        v-layout(row justify-center)
+          span.font-small.text-xs-center With MYCURE, queueing is easier done than spelled. Patients easily register
+            br
+            | via kiosks. Display multiple queues through auxiliary monitors or smart 
+            br
+            | TVs for waiting patients. Incoming doctors even get notified in their
+            br
+            | devices for every new patient in line.
+        br
+        br
+        br
+        br
+        br
+        v-layout(row justify-center)  
+          //- https://ssense.github.io/vue-carousel/
+          center
+            carousel(
+              :per-page="1" 
+              :autoplay="false"
+              :loop="true"
+              paginationActiveColor="white"
+              paginationColor="grey"
+              navigationNextLabel=" "
+              navigationPrevLabel=" "
+              style="height: 550px"
+            )
+              slide(v-for="(feature,index) in 6" :key="index" :data-index="feature")
+                img(:src="require(`../../assets/images/mycure-web-features-mockup-0${feature}-A.png`)" width="70%")
+    //- div(v-else)
 </template>
 
 <script>
@@ -13,5 +52,10 @@
 </script>
 
 <style scoped>
-
+  #panel4 {
+    background-image: url('../../assets/images/mycure-web-features-gradient-background.png');
+    background-position: center bottom;
+    background-repeat: no-repeat;
+    background-size: 100% auto;
+  }
 </style>
