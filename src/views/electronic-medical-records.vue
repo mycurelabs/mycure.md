bf5f24dec447a09e23b051aa926a7e73291bf9d0
Marc Almond Valleza <majvalleza@gmail.com>
2019-06-12 15:23:08 +0800

    chore: create emr views

---
+++ src/views/electronic-medical-records.vue
@@ -0,0 +1,66 @@
+<template lang="pug">
+  div.white#top
+    mc-toolbar-home
+    panel1
+    panel2
+    panel3
+    panel4
+    panel5(style="margin-top: -20%")
+    panel6.showPanel
+    filler(v-if="$isMobile").showPanel.pt-5
+      span(slot="privateFiller" style="font-size: 30px") Are you part of a group clinic?
+      span(slot="chatWindow" @click="toggleChat" style="font-size: 30px").pointer.font-mc-blue Ask us how it works.
+    panel7mobile(v-if="$isMobile").showPanel
+    mc-footer
+</template>
+
+<script>
+  import mcToolbarHome from '@/components/commons/mc-toolbar-home';
+  import mcFooter from '@/components/commons/mc-footer';
+  import panel1 from '@/components/electronic-medical-records/panel-1';
+  import panel2 from '@/components/electronic-medical-records/panel-2';
+  import panel3 from '@/components/electronic-medical-records/panel-3';
+  import panel4 from '@/components/electronic-medical-records/panel-4';
+  import panel5 from '@/components/electronic-medical-records/panel-5';
+  import panel6 from '@/components/electronic-medical-records/panel-6';
+  import panel7mobile from '@/components/electronic-medical-records/panel-7-mobile';
+  import filler from '@/components/commons/filler';
+  import VueScrollTo from 'vue-scrollto';
+
+  export default {
+    data () {
+      return {
+        //
+      };
+    }, 
+    components: {
+      mcToolbarHome,
+      mcFooter,
+      panel1,
+      panel2,
+      panel3,
+      panel4,
+      panel5,
+      panel6,
+      panel7mobile,
+      filler
+    },
+    methods: {
+      toggleChat () {
+        let message = `Hi, I would like to know more about the Electronic Medical Records for Private Practice.`;
+        window.$crisp.push(['do', 'chat:toggle']);
+        window.$crisp.push(['do', 'message:send', ['text', message]]);
+      }
+    },
+    mounted () {
+      VueScrollTo.scrollTo(`#top`, 500, { easing: 'ease' } );
+    }
+  };
+</script>
+
+<style scoped>
+.pointer:hover {
+    cursor: pointer;
+}
+</style>
+
