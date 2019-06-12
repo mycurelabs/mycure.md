a019c0dbdb4ce8e3bd4459eace2fb96e60c11a70
Jofferson R Tiquez <jofftiquez@gmail.com>
2019-06-12 15:16:49 +0800

    feat: create $mainLinks mixin
    - filters all the routes with meta pageType equal to 'main' from vue router's route

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -4,6 +4,14 @@ Vue.mixin({
   computed: {
     $isMobile () {
       return this.$vuetify.breakpoint.mdAndDown;
+    },
+    $mainLinks () {
+      const links = this.$router.options.routes;
+      return links.filter(link => {
+        if (link.meta && link.meta.pageType === 'main') {
+          return link;
+        }
+      });
     }
   }
 });
