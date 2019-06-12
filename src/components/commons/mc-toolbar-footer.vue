ac0ffa532fd6fbab6e1a0a378a97b0cfdd7169af
Marc Almond Valleza <majvalleza@gmail.com>
2019-06-12 17:41:24 +0800

    chore: change to expansion panels

--- src/components/commons/mc-toolbar-footer.vue
+++ src/components/commons/mc-toolbar-footer.vue
@@ -40,74 +40,70 @@
           img(src="../../assets/images/mycure-homepage-wireframe-icon-twitter.png" height="30").px-3
       br
       br
-      v-layout(row).pb-1
-        span.font-small.font-mc-grey CLINIC SOLUTIONS
-        v-spacer
-        v-icon(v-if="!clinicSol" @click="clinicSol=!clinicSol" dark) arrow_right
-        v-icon(v-else @click="clinicSol=!clinicSol" dark) arrow_drop_down
-      hr(style="border: 1px solid grey").mx-1
-      div(v-if="clinicSol").py-3
-        v-layout(column)
-          router-link(:to="{ name: 'electronic-medical-records' }").link.white--text.py-2
-            span.py-2 For Individual Doctors
-          router-link(:to="{ name: 'clinic-management-system' }").link.white--text.py-2
-            span.py-2 For Multispecialty Clinics
-      br
-      v-layout(row).pb-1
-        span.font-small.font-mc-grey ABOUT
-        v-spacer
-        v-icon(v-if="!about" @click="about=!about" dark) arrow_right
-        v-icon(v-else @click="about=!about" dark) arrow_drop_down
-      hr(style="border: 1px solid grey").mx-1
-      div(v-if="about").py-3
-        v-layout(column)
-          router-link(:to="{ name: 'our-story' }").link.white--text.py-2
-            span.py-2 Our Story
-          a(href="http://blog.mycure.md/" target="_blank").link.white--text.py-2
-            span.py-2 Blog
-          a(href="https://culture.mycure.md/" target="_blank").link.white--text.py-2
-            span.py-2 Careers
-      br
-      v-layout(row).pb-1
-        span.font-small.font-mc-grey SUPPORT
-        v-spacer
-        v-icon(v-if="!support" @click="support=!support" dark) arrow_right
-        v-icon(v-else @click="support=!support" dark) arrow_drop_down
-      hr(style="border: 1px solid grey").mx-1
-      div(v-if="support").py-3
-        v-layout(column)
-          a(href="http://blog.mycure.md/frequently-asked-questions/" target="_blank").link.white--text.py-2
-            span.py-2 FAQs
-          router-link(:to="{ name: 'pricing' }").link.white--text.py-2
-            span.py-2 Pricing
-          a(href="https://calendly.com/mycure" target="_blank").link.white--text.py-2
-            span.py-2 Book a Demo
-      br
-      v-layout(row).pb-1
-        span.font-small.font-mc-grey LEGAL
-        v-spacer
-        v-icon(v-if="!legal" @click="legal=!legal" dark) arrow_right
-        v-icon(v-else @click="legal=!legal" dark) arrow_drop_down
-      hr(style="border: 1px solid grey").mx-1
-      div(v-if="legal").py-3
-        v-layout(column)
-          router-link(:to="{ name: 'terms' }").link.white--text.py-2
-            span.py-2 Terms
-          router-link(:to="{ name: 'privacy-policy' }").link.white--text.py-2
-            span.py-2 Privacy Policy
-      br
-      v-layout(row).pb-1
-        span.font-small.font-mc-grey CONTACT US
-        v-spacer
-        v-icon(v-if="!contact" @click="contact=!contact" dark) arrow_right
-        v-icon(v-else @click="contact=!contact" dark) arrow_drop_down
-      hr(style="border: 1px solid grey").mx-1
-      div(v-if="contact").py-3
-        v-layout(column)
-          a(href="mailto:hello@mycure.md").link.white--text.py-2
-            span.py-2 hello@mycure.md
-          span.py-2.white--text (+632) 799 6262
-          span.py-2.white--text (+63) 917 303 4350
+      v-layout(row)
+        v-expansion-panel.elevation-0
+          v-expansion-panel-content(style="background-color: #343a40")
+            template(v-slot:header)
+              span(style="margin-left: -24px").font-small.font-mc-grey CLINIC SOLUTIONS
+            template(v-slot:actions)
+              v-icon(style="margin-right: -24px" color="white") mdi-chevron-down
+            v-layout(column)
+              router-link(:to="{ name: 'emr' }").link.white--text.py-2
+                span.py-2 For Individual Doctors
+              router-link(:to="{ name: 'cms' }").link.white--text.py-2
+                span.py-2 For Multispecialty Clinics
+      v-layout(row)
+        v-expansion-panel.elevation-0
+          v-expansion-panel-content(style="background-color: #343a40")
+            template(v-slot:header)
+              span(style="margin-left: -24px").font-small.font-mc-grey ABOUT
+            template(v-slot:actions)
+              v-icon(style="margin-right: -24px" color="white") mdi-chevron-down
+            v-layout(column)
+              router-link(:to="{ name: 'our-story' }").link.white--text.py-2
+                span.py-2 Our Story
+              a(href="http://blog.mycure.md/" target="_blank").link.white--text.py-2
+                span.py-2 Blog
+              a(href="https://culture.mycure.md/" target="_blank").link.white--text.py-2
+                span.py-2 Careers
+      v-layout(row)
+        v-expansion-panel.elevation-0
+          v-expansion-panel-content(style="background-color: #343a40")
+            template(v-slot:header)
+              span(style="margin-left: -24px").font-small.font-mc-grey SUPPORT
+            template(v-slot:actions)
+              v-icon(style="margin-right: -24px" color="white") mdi-chevron-down
+            v-layout(column)
+              a(href="http://blog.mycure.md/frequently-asked-questions/" target="_blank").link.white--text.py-2
+                span.py-2 FAQs
+              router-link(:to="{ name: 'pricing' }").link.white--text.py-2
+                span.py-2 Pricing
+              a(href="https://calendly.com/mycure" target="_blank").link.white--text.py-2
+                span.py-2 Book a Demo
+      v-layout(row)
+        v-expansion-panel.elevation-0
+          v-expansion-panel-content(style="background-color: #343a40")
+            template(v-slot:header)
+              span(style="margin-left: -24px").font-small.font-mc-grey LEGAL
+            template(v-slot:actions)
+              v-icon(style="margin-right: -24px" color="white") mdi-chevron-down
+            v-layout(column)
+              router-link(:to="{ name: 'terms' }").link.white--text.py-2
+                span.py-2 Terms
+              router-link(:to="{ name: 'privacy-policy' }").link.white--text.py-2
+                span.py-2 Privacy Policy
+      v-layout(row)
+        v-expansion-panel.elevation-0
+          v-expansion-panel-content(style="background-color: #343a40")
+            template(v-slot:header)
+              span(style="margin-left: -24px").font-small.font-mc-grey CONTACT US
+            template(v-slot:actions)
+              v-icon(style="margin-right: -24px" color="white") mdi-chevron-down
+            v-layout(column)
+              a(href="mailto:hello@mycure.md").link.white--text.py-2
+                span.py-2 hello@mycure.md
+              span.py-2.white--text (+632) 799 6262
+              span.py-2.white--text (+63) 917 303 4350
       br
       br
       br
