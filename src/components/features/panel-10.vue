2b917a913ca1d3b5dc9826a7ccbf96a70357338a
Francis Ryan Marquez <fnmarquez@up.edu.ph>
2019-06-13 18:15:24 +0800

    feat(features-m): add panel10

--- src/components/features/panel-10.vue
+++ src/components/features/panel-10.vue
@@ -1,19 +1,30 @@
 <template lang="pug">
   div
-    panelBeforeFooter
-      img(slot="emr-img" style="max-width: 90%" src="../../assets/images/mycure-web-banner-last-panel-features-embrace-habit.png")
-      span(slot="emr-txt").font-medium.lh-title Embrace a new habit.
-      span(slot="emr-sub-txt").font-small Let your patients experience top-of-the-line service through your streamlined clinic operations with the help of MYCURE.
-      v-btn(
-        slot="sign-up-button"
-        :href="`${siginURL}/signup/choose`" 
-        color="#18c551" 
-        style="padding: 25px 30px 25px 30px"
-        right
-      )
-        strong.text-none.white--text.font-small Sign Up Now
-    v-layout(row justify-center)
-        img(width="100%" height="100%" src="../../assets/images/mycure-web-banner-last-panel-health-community.png")
+    div(v-if="!$isMobile")
+      panelBeforeFooter
+        img(slot="emr-img" style="max-width: 90%" src="../../assets/images/mycure-web-banner-last-panel-features-embrace-habit.png")
+        span(slot="emr-txt").font-medium.lh-title Embrace a new habit.
+        span(slot="emr-sub-txt").font-small Let your patients experience top-of-the-line service through your streamlined clinic operations with the help of MYCURE.
+        v-btn(
+          slot="sign-up-button"
+          :href="`${siginURL}/signup/choose`" 
+          color="#18c551" 
+          style="padding: 25px 30px 25px 30px"
+          right
+        )
+          strong.text-none.white--text.font-small Sign Up Now
+      v-layout(row justify-center)
+          img(width="100%" height="100%" src="../../assets/images/mycure-web-banner-last-panel-health-community.png")
+    div(v-else).transparent
+      center
+        v-layout(column justify-center style="width: 90%").pt-5
+          img(style="max-width: 80%" src="../../assets/images/mycure-web-banner-last-panel-features-embrace-habit.png")
+          br 
+          span(style="font-size: 200%").text-xs-center.lh-title Embrace new habit.
+          br
+          br
+          span.font-s.text-xs-center Experience what you can do more with MYCURE technology by your side.
+
 </template>
 
 <script>
