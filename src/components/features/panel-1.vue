ce10550c59d98a14f2a70108fb8d144964c1ecc4
Francis Ryan Marquez <fnmarquez@up.edu.ph>
2019-06-13 18:14:38 +0800

    feat(features-m): add panel1

--- src/components/features/panel-1.vue
+++ src/components/features/panel-1.vue
@@ -1,5 +1,4 @@
 <template lang="pug">
-  //- TODO: mobile responsiveness
   div
     div(v-if="!$isMobile").panel1.mb-5
       v-container(style="padding-top: 70px")
@@ -19,10 +18,20 @@
                 style="padding: 25px 30px 25px 30px"
               ) 
                 strong.font-small.text-none.white--text Sign Up Now 
-    //- div(v-else).panel1.mb-5
-      v-layout(row wrap justify-center style="height: 100%")
-        v-flex(xs12 md12 align-self-center).my-5
-          h1.font-xl.text-xs-center Terms &amp; Conditions
+    div(v-else).panel1.mb-5
+      v-container  
+        v-layout(row wrap justify-center style="height: 100%")
+          v-flex(xs12 md12 align-self-center).my-5
+            h1.font-l.text-xs-center Human-friendly features,
+              br
+              | patient-centric care.
+              p(style="font-weight: normal; padding-top: 40px").font-m.text-xs-center Focus on your patients and let MYCURE handle the rest.
+                br
+                | We made it very easy to learn and use.
+      div(style="height: 75px; margin-top: -1%").mc-green
+          v-layout(row justify-center)
+            a(:href="`${siginURL}/signup/choose`" style="padding: 20px 0; text-decoration: none")
+              strong(style="font-size: 25px").white--text Sign Up Now
 </template>
 
 <script>
