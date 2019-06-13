a404d119585eb70011a558b440516e3f4a59dd82
Francis Ryan Marquez <fnmarquez@up.edu.ph>
2019-06-13 18:14:55 +0800

    feat(features-m): add panel4

--- src/components/features/panel-4.vue
+++ src/components/features/panel-4.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
-  //- TODO: mobile responsiveness
   div
     div(v-if="!$isMobile")#panel4.pt-5
       v-container
         center
-          strong.font-small.font-mc-blue Registration and Queueing
+          strong.font-small.font-mc-blue REGISTRATION AND QUEUEING
         br
         br
         v-layout(row justify-center)
@@ -38,7 +37,42 @@
             )
               slide(v-for="(feature,index) in features" :key="index" :data-index="index+1")
                 img(:src="require(`../../assets/images/mycure-web-features-mockup-02-${feature}.png`)" width="70%")
-    //- div(v-else)
+    div(v-else)#panel4.pt-5
+      v-container
+        center
+          strong.font-s.font-mc-blue REGISTRATION AND QUEUEING
+        br
+        br
+        v-layout(row justify-center)
+          span.font-l Value everyone's time more effectively.
+        br
+        v-layout(row justify-center)
+          span.font-m With MYCURE, queueing is easier done than spelled. Patients easily register
+            br
+            | via kiosks. Display multiple queues through auxiliary monitors or smart 
+            br
+            | TVs for waiting patients. Incoming doctors even get notified in their
+            br
+            | devices for every new patient in line.
+        br
+        br
+        v-layout(row justify-center)  
+          //- https://ssense.github.io/vue-carousel/
+          center
+            carousel(
+              :per-page="1" 
+              :autoplay="false"
+              :loop="true"
+              paginationActiveColor="white"
+              paginationColor="grey"
+              navigationNextLabel=" "
+              navigationPrevLabel=" "
+              style="height: 225px"
+            )
+              slide(v-for="(feature,index) in features" :key="index" :data-index="index+1")
+                img(:src="require(`../../assets/images/mycure-web-features-mockup-02-${feature}.png`)" width="70%")
+    
+      
 </template>
 
 <script>
