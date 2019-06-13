5d0393892aff196a37c7df6b7f92c861ca9db51b
Francis Ryan Marquez <fnmarquez@up.edu.ph>
2019-06-13 18:15:00 +0800

    feat(features-m): add panel5

--- src/components/features/panel-5.vue
+++ src/components/features/panel-5.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
-  //- TODO: mobile responsiveness
   div
     div(v-if="!$isMobile")#panel5.pt-5
       v-container
         center
-          strong.font-small.font-mc-blue Billing and Payments
+          strong.font-small.font-mc-blue BILLING AND PAYMENTS
         br
         br
         v-layout(row justify-center)
@@ -41,7 +40,44 @@
             )
               slide(v-for="(feature,index) in features" :key="index" :data-index="index+1")
                 img(:src="require(`../../assets/images/mycure-web-features-mockup-03-${feature}.png`)" width="70%")
-    //- div(v-else)
+    div(v-else)#panel5.pt-5
+      v-container
+        center
+          strong.font-s.font-mc-blue BILLING AND PAYMENTS
+        br
+        br
+        v-layout(row justify-center)
+          span.font-l Simplified billing, beautiful reports.
+        br
+        br
+        v-layout(row justify-center)
+          span.font-m.text-xs-center Produce neatly-organized reports in a minute or less.
+        br
+        br
+        v-layout(row justify-center)
+          ul
+            li
+              v-layout(v-for="(item, index) in items" :key="index" row align-left).pb-3 
+                img(src="../../assets/images/mycure-web-bullet-check.png" style="height: 29px;")
+                span.font-s.pl-3 {{ item }} 
+        br
+        br
+        v-layout(row justify-center)  
+          //- https://ssense.github.io/vue-carousel/
+          center
+            carousel(
+              :per-page="1" 
+              :autoplay="false"
+              :loop="true"
+              paginationActiveColor="white"
+              paginationColor="grey"
+              navigationNextLabel=" "
+              navigationPrevLabel=" "
+              style="height: 250px"
+            )
+              slide(v-for="(feature,index) in features" :key="index" :data-index="index+1")
+                img(:src="require(`../../assets/images/mycure-web-features-mockup-03-${feature}.png`)" width="70%")
+    
 </template>
 
 <script>
