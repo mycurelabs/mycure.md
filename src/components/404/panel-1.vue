2db3b0af11be75f5002ed0d7628d3ac12951cadc
Francis Ryan Marquez <fnmarquez@up.edu.ph>
2019-06-14 13:43:00 +0800

    refactor(404): add 404 page

---
+++ src/components/404/panel-1.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  div
+    div(v-if="!$isMobile").white
+      v-container(style="padding-top: 70px; height: 90vh")
+        br
+        div
+          center
+            img(src="../../assets/images/mycure-error-404-image.png")
+        br
+        br
+        div
+          center
+            strong.font-small Oh snap!
+            br
+            span.font-small There isn't any page like that in this site.
+              br
+              | Please check out our other pages instead.
+    div(v-else).showPanel
+      v-container
+        br
+        v-flex(xs12)
+          center
+            img(width="100%" src="../../assets/images/mycure-error-404-image.png")
+        br
+        br
+        v-flex(xs12)
+          center
+            strong.font-s Oh snap!
+            br
+            span.font-s There isn't any page like that in this site.
+              br
+              | Please check out our other pages instead.
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        // 
+      };
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
--- src/views/404.vue
+++ src/views/404.vue
@@ -1,10 +1,21 @@
 <template lang="pug">
-  div
-    h1 this is 404 page
+  div#top
+    mcToolbarHome
+    panel1
+    mcFooter
 </template>
 
 <script>
+  import mcToolbarHome from '@/components/commons/mc-toolbar-home';
+  import mcFooter from '@/components/commons/mc-footer';
+  import panel1 from '@/components/404/panel-1';
+  
   export default {
+    components: {
+      mcToolbarHome,
+      mcFooter,
+      panel1
+    },
     data () {
       return {
         //
