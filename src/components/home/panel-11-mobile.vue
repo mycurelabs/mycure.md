637e8970f9fad646ee686b7f5f45f9f2f9b47b9e
Francis Ryan Marquez <fnmarquez@up.edu.ph>
2019-06-14 12:27:09 +0800

    style(home): add signup btn in panel before footer

--- src/components/home/panel-11-mobile.vue
+++ src/components/home/panel-11-mobile.vue
@@ -4,7 +4,14 @@
       v-layout(column justify-center style="width: 90%").pt-5
         img(style="max-width: 80%" src="../../assets/images/mycure-web-banner-last-panel-home-doctors.png")
         br 
-        span(style="font-size: 200%").text-xs-center.lh-title Join thousands of health professionals who get the best out of technology in their practice
+        span(style="font-size: 200%").text-xs-center.lh-title Join thousands of health professionals who get the best out of technology in their practice.
+      br
+      br
+      v-layout(row justify-center)
+        v-btn(
+          :href="`${siginURL}/signup/choose`"
+          style="background-color: #18c551; width: 80%; height: 60px")  
+          strong.font-m.white--text.text-none Sign Up Now  
 </template>
 
 <script>
