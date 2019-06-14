a94b5bb2b302696acab484e80231cde675f4172f
Francis Ryan Marquez <fnmarquez@up.edu.ph>
2019-06-14 11:10:48 +0800

    feat(404): add 404 view

---
+++ src/views/404.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div
+    h1 this is 404 page
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        //
+      };
+    }  
+  };
+</script>
+
+<style scoped>
+
+</style>
