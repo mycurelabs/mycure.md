027c8858c170a5af04f2915bf22ce8877309e1f2
Francis Ryan Marquez <fnmarquez@up.edu.ph>
2019-06-18 13:03:13 +0800

    style(features): update panel1 latest layout

--- src/components/features/panel-1.vue
+++ src/components/features/panel-1.vue
@@ -21,20 +21,24 @@
               strong.font-small.text-none.white--text Sign Up Now
           v-flex(xs12 md7 align-self-center style="margin-top: 5%")
             img(width="100%" src="../../assets/images/mycure-cms-web-banner-first-panel-features.png" alt="Features")
-    div(v-else).panel1.mb-5
-      v-container  
-        v-layout(row wrap justify-center style="height: 100%")
-          v-flex(xs12 md12 align-self-center).my-5
-            h1.font-l.text-xs-center Human-friendly features,
+    div(v-else).white
+      v-container.pt-5
+        v-layout(row justify-center)
+          v-flex(xs12)
+            h1(style="line-height: 55px;").font-l.text-xs-center Human-friendly
               br
-              | patient-centric care
-              p(style="font-weight: normal; padding-top: 40px").font-m.text-xs-center Focus on your patients and let MYCURE handle the rest.
-                br
-                | We made it very easy to learn and use.
-      div(style="height: 75px; margin-top: -1%").mc-green
-          v-layout(row justify-center)
-            a(:href="`${siginURL}/signup/choose`" style="padding: 20px 0; text-decoration: none")
-              strong(style="font-size: 25px").white--text Sign Up Now
+              | features,
+              br
+              | patient-centric
+              br
+              | care
+        br
+        v-flex(xs12)
+          img(width="100%" src="../../assets/images/mycure-cms-web-banner-first-panel-features.png" alt="Features").pb-5
+      div(style="height: 75px; margin-top: -10%").mc-green
+        v-layout(row justify-center)
+          a(:href="`${siginURL}/signup/choose`" style="padding: 20px 0; text-decoration: none")
+            strong(style="font-size: 25px").white--text Sign Up Now
 </template>
 
 <script>
