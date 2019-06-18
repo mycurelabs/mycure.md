53cd8b487af2637f06b76ed7f65c25fd5d6fac5d
Francis Ryan Marquez <fnmarquez@up.edu.ph>
2019-06-18 13:03:26 +0800

    style(features): update panel4 latest layout

--- src/components/features/panel-4.vue
+++ src/components/features/panel-4.vue
@@ -29,34 +29,36 @@
                 img(:src="require(`../../assets/images/mycure-web-features-mockup-02-${feature}.png`)" alt="Clinic Registration and Queueing" width="70%")
     div(v-else)#panel4.pt-5
       v-container
-        center
+        v-layout(row justify-center)        
           strong.font-s.font-mc-blue REGISTRATION AND QUEUEING
         br
         br
         v-layout(row justify-center)
-          span.font-l Value everyone's time more effectively.
+          p.font-l.text-xs-center Value everyone's time more effectively.
         br
         v-layout(row justify-center)
-          span.font-m With MYCURE, queueing is easier done than spelled. Patients easily register via kiosks.
+          p.font-m.text-xs-center With MYCURE, queueing is easier done than spelled. Patients easily register via kiosks.
+            br
             br
             | Display multiple queues through auxiliary monitors or smart 
             | TVs for waiting patients. Incoming doctors even get notified in their devices for every new patient in line.
+        v-layout(row justify-center).add-spacing
+          img(src="@/assets/images/mycure-cms-web-features-arrow-down.png")
         br
         br
         v-layout(row justify-center)  
           //- https://ssense.github.io/vue-carousel/
-          center
-            carousel(
-              :per-page="1" 
-              :autoplay="false"
-              :loop="true"
-              paginationActiveColor="white"
-              paginationColor="grey"
-              navigationNextLabel=" "
-              navigationPrevLabel=" "
-            )
-              slide(v-for="(feature,index) in features" :key="index" :data-index="index+1")
-                img(:src="require(`../../assets/images/mycure-web-features-mockup-02-${feature}.png`)" alt="Clinic Registration and Queueing" width="100%")
+          carousel(
+            :per-page="1" 
+            autoplay
+            loop
+            adjustableHeight
+            paginationActiveColor="#3498db"
+            paginationColor="#808080"
+          ).pa-1
+            slide(v-for="(feature,index) in features" :key="index" :data-index="index+1")
+              v-layout(row justify-center)
+                img(:src="require(`../../assets/images/mycure-web-features-mockup-02-${feature}.png`)" alt="Clinic Registration and Queueing" height="210px")
 </template>
 
 <script>
