bd6b2c07fa8c3c9405e5a0a7cff43ca8e1e8a346
Marc Almond Valleza <majvalleza@gmail.com>
2019-06-19 16:51:26 +0800

    feat (highlights): create a reusable component
    - reusable component that shows highlights used in home page and emr page

---
+++ src/components/commons/highlights.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  div.pt-5
+    v-container
+      v-layout(v-for="(highlight,index) in highlights" :key="index" row wrap justify-center)
+        v-flex(xs5 v-if="index%2 === 0")
+          img(style="max-width: 90%" :src="require(`@/assets/images/${ highlight.image }`)" :alt="highlight.title")
+        v-flex(xs7 v-else align-self-center).pr-5.text-xs-right
+          span.font-medium.lh-title {{ highlight.title }}
+          br
+          br
+          span.font-small {{ highlight.text }}
+        v-flex(xs7 v-if="index%2 === 0" align-self-center).pl-5
+          span.font-medium.lh-title {{ highlight.title }}
+          br
+          br
+          span.font-small {{ highlight.text }}
+        v-flex(xs5 v-else)
+          img(style="max-width: 90%" :src="require(`@/assets/images/${ highlight.image }`)" :alt="highlight.title")
+</template>
+
+<script>
+  export default {
+    props: {
+      highlights: Array
+    }, 
+  };
+</script>
+
