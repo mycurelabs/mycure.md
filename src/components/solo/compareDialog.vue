fc5e67c52663d67aef6533bd7220472ee8341a88
Marc Almond Valleza <majvalleza@gmail.com>
2019-06-21 00:19:21 +0800

    feat (solo) - create dialog component
    - setup in preparation for Jem's idea of a comparison pop-up

---
+++ src/components/solo/compareDialog.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  v-dialog(v-model="dialog" width="600" persistent)
+    v-card
+      v-card-title This dialog is in preparation for Jem's idea of a comparison pop up.
+      v-divider
+      v-card-actions
+        v-btn(flat color="error" @click="close") Close  
+</template>
+
+<script>
+  export default {
+    props: {
+      dialog: {
+        type: Boolean,
+        default: false
+      }
+    },
+    data () {
+      return {
+        //
+      };
+    },
+    methods: {
+      close () {
+        this.$emit('close', false);
+      }
+    }
+  };
+</script>
+
