5ce9cda9c1c57da6685281eb421bfc4aa8d23d76
Marc Almond Valleza <majvalleza@gmail.com>
2019-06-21 20:18:12 +0800

    refactor (home): change end home panels

--- src/components/home/panel-9.vue
+++ src/components/home/panel-9.vue
@@ -1,22 +1,25 @@
 <template lang="pug">
   div#panel9
     div(v-if="!$isMobile")
-      v-container
+      v-container.text-xs-center
+        b.font-small What brings you here?
+        br
+        br
         v-layout(row justify-center)
           v-card(
             v-for="(card,i) in productCards"
             :to="{ name: card.link }"
             :key="i"
-            style="width: 250px; height: 350px"
-          ).roundedCard.mx-3
+            style="width: 250px; height: 100px"
+          ).roundedCard.mx-3.cardText
             v-layout(column justify-center)
-              v-flex(xs12 align-baseline).pa-3
-                p.font-small {{ card.text }}
-              br
-              br
-              br
+              //- v-flex(xs12 align-baseline).pa-3
+              //-   p.font-small {{ card.text }}
+              //- br
+              //- br
+              //- br
               v-flex(xs12 align-self-end).pa-3
-                strong.font-mc-blue.font-small {{ card.subtxt }}
+                strong.font-small {{ card.subtxt }}
         br
         v-layout(row justify-center).pt-5
           p.font-small.text-xs-center Unsure where you fit? 
@@ -25,20 +28,23 @@
       v-layout(row justify-center)
           img(width="100%" height="100%" src="../../assets/images/mycure-web-banner-last-panel-health-community.png" alt="Health community")
     div(v-else).showPanel
-      v-container
+      v-container.text-xs-center
+        b.font-small What brings you here?
+        br
+        br
         v-layout(column align-center)
           v-card(
             v-for="(card,i) in productCards"
             :key="i"
             :to="{ name: card.link }"
-            style="width: 225px; height: 300px"
-          ).roundedCard.my-3
+            style="width: 225px; height: 100px"
+          ).roundedCard.my-3.cardText
             v-layout(column justify-center)
-              v-flex(xs12 align-baseline).pa-3
-                p.font-s {{ card.text }}
-              br
+              //- v-flex(xs12 align-baseline).pa-3
+              //-   p.font-s {{ card.text }}
+              //- br
               v-flex(xs12 align-self-end).pa-3
-                strong.font-mc-blue.font-s {{ card.subtxt }}
+                strong.font-s {{ card.subtxt }}
         v-layout(row justify-center).pt-5
           p.font-small.text-xs-center Unsure where you fit? 
             router-link(:to="{ name: 'calendly' }")
@@ -52,17 +58,17 @@
         productCards: [
           {
             text: 'Do you run a Multispecialty Clinic with branches around the country?',
-            subtxt: 'Maybe you’re fit to use MYCURE: Multispecialty Clinics',
+            subtxt: 'I manage a multispecialty clinic',
             link: 'cms'
           },
           {
             text: 'Are you a private medical practitioner? Running a clinic on your own or collaborating with others?',
-            subtxt: 'Try MYCURE Private Practice',
+            subtxt: 'I am a private medical practitioner',
             link: 'emr'
           },
           {
             text: 'Do you want to secure your own client’s EMR? Or live remotely with very scarce internet connectivity?',
-            subtxt: 'You should download MYCURE Solo',
+            subtxt: ' I need a free and independent CMS',
             link: 'mycure-solo'          
           }
         ] 
@@ -74,5 +80,18 @@
 <style scoped>
 .roundedCard {
   border-radius: 20px;
+}
+
+.roundedCard:hover {
+  color: white;
+  background-color: #2e9fdf;
+}
+
+.cardText {
+  color: #2e9fdf;
+}
+
+.cardText:hover {
+  color: white;
 }
 </style>
