62e28af4546d7c799096e548b2a7b609775873d8
Francis Ryan Marquez <fnmarquez@up.edu.ph>
2019-06-28 11:19:34 +0800

    refactor(404): update layout and content

--- src/components/404/panel-1.vue
+++ src/components/404/panel-1.vue
@@ -1,35 +1,17 @@
 <template lang="pug">
-  div
-    div(v-if="!$isMobile").white
-      v-container(style="padding-top: 70px; height: 90vh")
-        br
-        div
-          v-layout(row justify-center)
-            img(src="../../assets/images/mycure-error-404-image.png" alt="Error 404")
-        br
-        br
-        div
-          v-layout(row justify-center)
-            strong.font-18 Oh snap!
-            br
-            span.font-18 There isn't any page like that in this site.
-              br
-              | Please check out our other pages instead.
-    div(v-else).showPanel
-      v-container
-        br
-        v-flex(xs12)
-          v-layout(row justify-center)
-            img(width="100%" src="../../assets/images/mycure-error-404-image.png" alt="Error 404")
-        br
-        br
-        v-flex(xs12)
-          v-layout(row justify-center)
-            strong.font-16 Oh snap!
-            br
-            span.font-16 There isn't any page like that in this site.
-              br
-              | Please check out our other pages instead.
+  div.white
+    v-container(fluid fill-height align-start style="height: 90vh; padding-top: 70px;")
+      v-layout(row align-center)
+        v-flex
+          v-layout(row justify-center)  
+            img(v-if="!$isMobile" width="auto" src="../../assets/images/mycure-error-404-image.png" alt="Error 404")
+            img(v-else width="100%" src="../../assets/images/mycure-error-404-image.png" alt="Error 404")
+          br
+          v-layout(column justify-center).text-xs-center
+            strong.pb-2.font-18 Oh snap!
+            p.pb-2.font-16 The page you’re looking for can’t be found.
+            router-link(:to="{ name: 'home' }" title="MYCURE | Clinic Management System | Cloud EMR Philippines") 
+              p.font-16 Back to Home >
 </template>
 
 <script>
