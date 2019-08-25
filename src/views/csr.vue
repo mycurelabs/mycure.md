a107a9d488e0b6724573d3ce3c9d79fd1d72f2d9
Francis Ryan Marquez <francisryanmarquez@gmail.com>
2019-08-25 11:44:34 +0800

    refactor(csr): add csr view

---
+++ src/views/csr.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  div.white#top
+    mc-toolbar-home
+    usp
+    div.skyBg
+      pillars
+      filler
+    mc-footer
+</template>
+
+<script>
+  import mcToolbarHome from '@/components/commons/mc-toolbar-home';
+  import mcFooter from '@/components/commons/mc-footer';
+  import usp from '@/components/corporate-social-responsibility/usp';
+  import pillars from '@/components/corporate-social-responsibility/pillars';
+  import filler from '@/components/commons/filler';
+  import VueScrollTo from 'vue-scrollto';
+
+  export default {
+    components: {
+      mcToolbarHome,
+      mcFooter,
+      usp,
+      pillars,
+      filler
+    },
+    methods: {
+      // toggleChat () {
+      //   let message = `Hi, I would like to know more about  Group Practice.`;
+      //   window.$crisp.push(['do', 'chat:toggle']);
+      //   window.$crisp.push(['do', 'message:send', ['text', message]]);
+      // }
+    },
+    mounted () {
+      VueScrollTo.scrollTo(`#top`, 500, { easing: 'ease' } );
+    }
+  };
+</script>
+
+<style scoped>
+.pointer:hover {
+    cursor: pointer;
+}
+</style>
+
