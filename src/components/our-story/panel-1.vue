9a182ebf3f7a42672ff746ff5d79a15249bc2691
Francis Ryan Marquez <francisryanmarquez@gmail.com>
2019-08-28 11:12:14 +0800

    refactor(our-story): add video in our story usp

--- src/components/our-story/panel-1.vue
+++ src/components/our-story/panel-1.vue
@@ -1,61 +1,115 @@
 <template lang="pug">
-  div
-    div(v-if="!$isMobile").white
-      v-container(align-center fill-height).px-5.panel-1
+div
+  div(v-if="!$isMobile").whole-page
+    v-container(fill-height)
+      v-layout(row align-center justify-center)
+        div.panel-size.usp-panel
+          v-layout(row justify-center)
+            v-flex(xs12 md6)
+              v-layout(column align-start)
+                h1.font-weight-bold.font-18.font-mc-blue.pb-3.uspMetaTitle MYCURE | Our Story
+                h2.title-line-height.uspTitle.font-weight-regular.pb-4 MYCURE starts and ends with real people.
+                v-btn(
+                  @click.stop="popUpVideo"
+                  :color="mcGreen"
+                  target="_blank"
+                  large
+                ).text-none.white--text.mt-4.btnHeight
+                  strong.font-18 Watch Video
+                v-dialog(v-model="videoDialog" max-width="600px")
+                  v-layout(row justify-center)
+                    v-card(width="600")
+                      v-card-text
+                        iframe(
+                          align="middle"
+                          id="ytplayer" 
+                          type="text/html" height="400" width="570"
+                          src="https://www.youtube.com/embed/zvM3LIvq-6k?autoplay=1&loop=1&showinfo=0&rel=0"
+                          frameborder="0"
+                          allowfullscreen
+                      )
+            v-flex(xs12 md6 text-xs-right)
+              img(
+                :src="require(`@/assets/images/mycure-homepage-doctor-kid-pediatrician-drawing.png`)"
+                width="90%"
+                alt="our-story"
+              )
+  div(v-else)
+    v-layout(column wrap justify-center).mt-5
+      h1.font-weight-bold.font-18.font-mc-blue.pb-2.text-xs-center MYCURE | Our Story
+      h2.font-40.font-weight-regular.title-line-height.pb-5.text-xs-center.uspTitle MYCURE starts and ends with real people.
+      v-layout(row justify-center).mb-5
+        img(
+          :src="require(`@/assets/images/mycure-homepage-doctor-kid-pediatrician-drawing.png`)"
+          height="250"
+          alt="our-story"
+        )
+      v-layout(row justify-center)
+        v-btn(
+          @click.stop="popUpVideo"
+          :color="mcGreen"
+          target="_blank"
+          large
+          block
+        ).uspMobileBtn
+          strong.font-25.text-none.white--text Watch Video
+      v-dialog(v-model="videoDialog" max-width="100%")
         v-layout(row justify-center)
-          v-flex(xs12 md6 align-self-center).uspLeft
-            h1.lh-title.font-60 MYCURE starts 
-              br
-              | and ends
-              br
-              | with real people.
-          v-flex(v-if="!$isMobile" xs12 md6 align-self-center).uspRight
-            img(width="100%" src="../../assets/images/mycure-homepage-doctor-kid-pediatrician-drawing.png" alt="Our Story")
-      v-dialog(v-model="showVid" max-width="700px")
-        iframe(
-          align="center"
-          v-if="showVid"
-          id="ytplayer" 
-          type="text/html" width="100%" height="450"
-          src="https://www.youtube.com/embed/ZR8e-Egav0M?autoplay=1&loop=1&showinfo=0&rel=0"
-          frameborder="0")
-    div(v-else).white
-      v-container.pt-5
-        v-flex(xs12)
-          h1.font-weight-bold.font-40.text-xs-center MYCURE starts and ends with real people.
-        br
-        v-flex(xs12)
-          img(width="100%" src="../../assets/images/mycure-homepage-doctor-kid-pediatrician-drawing.png" alt="Our Story").pb-5
+          v-card(width="600")
+            v-card-text
+              iframe(
+                align="middle"
+                id="ytplayer" 
+                type="text/html" height="175" width="300"
+                src="https://www.youtube.com/embed/zvM3LIvq-6k?autoplay=1&loop=1&showinfo=0&rel=0"
+                frameborder="0"
+                allowfullscreen
+            )
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        showVid: false
-      };
-    },
-    computed: {
-      // 
-    },
-    methods: {
-      handleOurStoryWatchVid () {
-        this.$ga.event({
-          eventCategory: 'button',
-          eventAction: 'click-our-story-watch-vid-btn',
-          eventLabel: 'our-story-watch-vid-btn'
-        });
-      }
+export default {
+  data () {
+    return {
+      videoDialog: false,
+      mcGreen: '#18c551',
+      mcBlue: '#2e9fdf'
+    };
+  },
+  methods: {
+    popUpVideo () {
+      this.videoDialog = true;
     }
-  };
+  }
+};
 </script>
 
 <style scoped>
-  .panel1{
-    height: auto;
-  }
-
-  h1 {
-    font-weight: normal
-  }
+.whole-page {
+  height: 100vh;
+}
+.font-xxs {
+  font-size: 11px;
+}
+.uspBtn-padding {
+  padding-bottom: 20px;
+}
+.title-line-height {
+  line-height: 1.25em;
+}
+.uspTitle {
+  font-size: 45px;
+}
+.uspMetaTitle {
+  white-space: pre;
+}
+.panel-size {
+  width: 1100px;
+}
+.btnHeight {
+  height: 50px;
+}
+.uspMobileBtn {
+  height: 75px;
+}
 </style>
