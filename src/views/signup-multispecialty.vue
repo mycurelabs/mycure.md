1ccb653394bfbefe86b536ae346ad52efd7e55cd
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-01-20 21:34:54 +0800

    refactor: update signup multispecialty router-view component

---
+++ src/views/signup-multispecialty.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  v-app
+    v-content
+      v-container(
+        fluid fill-height
+      ).bg.white
+        router-view
+</template>
+
+<style scoped>
+.bg {
+  background-image: url('../assets/images/mycure-signup-062618-footer-BG.png');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: bottom center;
+  /* background-size: 250px, contain; */
+  /* background-position: bottom right 50px, bottom; */
+}
+</style>
