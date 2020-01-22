165bc7cfcd50713d0ce652af119ad9702b8d3944
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-01-22 10:02:19 +0800

    feat: create page for individual parent

---
+++ src/views/signup-individual.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  v-app(:dark="dayOrNight === 'night'")
+    v-content
+      v-container(
+        fluid fill-height
+        :class="dayOrNight === 'day' ? 'white' : ''"
+      ).bg
+        router-view
+</template>
+
+<script>
+import dayOrNight from '../utils/day-or-night';
+export default {
+  data () {
+    this.dayOrNight = dayOrNight();
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.bg {
+  background-image: url('../assets/images/mycure-signup-062618-footer-BG.png');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: bottom center;
+  /* background-size: 250px, contain; */
+  /* background-position: bottom right 50px, bottom; */
+}
+</style>
