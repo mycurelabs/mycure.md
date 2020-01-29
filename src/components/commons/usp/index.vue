64231665b2346bbb6f84428e2be82c067842f321
Marc Almond Valleza <majvalleza@gmail.com>
2020-01-29 14:01:10 +0800

    refactor: update usp layout

--- src/components/commons/usp/index.vue
+++ src/components/commons/usp/index.vue
@@ -1,55 +1,48 @@
 <template lang="pug">
-div
-  div(v-if="!$isMobile").whole-page
-    v-container(fill-height)
-      v-layout(row align-center justify-center)
-        div.panel-size.usp-panel
-          v-layout(row justify-center)
-            v-flex(xs12 md7)
-              v-layout(column align-start)
-                h1.font-weight-bold.font-18.font-mc-blue.pb-3.uspMetaTitle {{ uspContents.metaTitle }}
-                h2.title-line-height.uspTitle.font-weight-regular.pb-4 {{ uspContents.title }}
-                //- :href="uspContents.btn"                
-                v-btn(
-                  v-if="uspContents.btnText"
-                  :href="`${this.signInURL}/signup/${uspContents.btn}`"
-                  :color="mcGreen"
-                  :id="uspContents.btnId"
-                  target="_blank"
-                  large
-                ).text-none.white--text.mt-4.btnHeight
-                  strong.font-18 {{ uspContents.btnText }}
-            v-flex(xs12 md7 text-xs-right)
-              img(
-                :src="require(`@/assets/images/${ uspContents.img }.png`)"
-                width="90%"
-                :alt="uspContents.img"
-              )
-  div(v-else)
-    v-layout(column wrap justify-center).mt-5
-      h1.font-weight-bold.font-18.font-mc-blue.pb-2.text-xs-center {{ uspContents.metaTitle }}
-      h2.font-30.font-weight-regular.title-line-height.pb-5.text-xs-center.uspTitle {{ uspContents.title }}
-      v-layout(row justify-center).mb-5
-        img(
-          :src="require(`@/assets/images/${ uspContents.img }.png`)"
-          height="250"
-          :alt="uspContents.img"
-        )
-      v-layout(row justify-center)
-        v-btn(
-          v-if="uspContents.btn"
-          :href="uspContents.btn"
-          :color="mcGreen"
-          :id="uspContents.btnId"
-          target="_blank"
-          large
-          block
-        ).uspMobileBtn
-          strong.font-25.text-none.white--text {{ uspContents.btnText }}
+  div
+    v-container(v-if="!$isMobile").whole-page
+      v-container(fill-height)
+        v-layout(row align-center)
+          v-flex(xs12 md12).text-xs-center
+            h2.text-xs-center.uspTitle.font-48 {{ uspTitle }}
+            p.text-xs-center.uspDescription.pt-3.font-16 {{ uspDescription }}
+            div.pt-5
+              v-btn(
+                :href="uspContents.btn"
+                :color="$mcColors.mcAltGreen"
+                :id="uspContents.btnId"
+                large
+              ).text-none.white--text
+                strong.font-18 {{ uspContents.btnText }}
+            div.pt-5
+              v-img(:src="require(`@/assets/images/${uspContents.coverImg}.png`)" :alt="uspContents.coverImg")
+        
+    div(v-else)
+      v-layout(column wrap justify-center).mt-5
+        h1.font-weight-bold.font-18.font-mc-blue.pb-2.text-xs-center {{ uspContents.metaTitle }}
+        h2.font-30.font-weight-regular.title-line-height.pb-5.text-xs-center.uspTitle {{ uspContents.title }}
+        v-layout(row justify-center).mb-5
+          img(
+            :src="require(`@/assets/images/${ uspContents.img }.png`)"
+            height="250"
+            :alt="uspContents.img"
+          )
+        v-layout(row justify-center)
+          v-btn(
+            v-if="uspContents.btn"
+            :href="uspContents.btn"
+            :color="mcGreen"
+            :id="uspContents.btnId"
+            target="_blank"
+            large
+            block
+          ).uspMobileBtn
+            strong.font-25.text-none.white--text {{ uspContents.btnText }}
 </template>
 
 <script>
 import { USP_CONTENTS } from './constants';
+import { parseTextWithNewLine } from '@/utils';
 export default {
   data () {
     return {
@@ -63,6 +56,14 @@ export default {
     },
     mcBlue () {
       return this.$mcColors.mcBlue;
+    },
+    uspTitle () {
+      const title = this.uspContents.title; 
+      return parseTextWithNewLine(title, ['workflows']);
+    },
+    uspDescription () {
+      const description = this.uspContents.description;
+      return parseTextWithNewLine(description, ['system']);
     }
   }
 };
@@ -70,7 +71,8 @@ export default {
 
 <style scoped>
 .whole-page {
-  height: 100vh;
+  min-height: 120vh;
+  padding-top: 10vh;
 }
 .title-line-height {
   line-height: 1.25em;
@@ -78,6 +80,11 @@ export default {
 .uspTitle {
   font-size: 45px;
   white-space: pre;
+  font-family: 'Work Sans', 'Poppins', sans-serif !important;
+}
+.uspDescription {
+  white-space: pre;
+  font-family: 'Source Sans Pro', 'Poppins', sans-serif !important;
 }
 .uspMetaTitle {
   white-space: pre;
