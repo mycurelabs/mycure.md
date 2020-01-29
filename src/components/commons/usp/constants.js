f62a133a8e9d3d8c664dd09ff0a689a45e331557
Marc Almond Valleza <majvalleza@gmail.com>
2020-01-29 14:00:57 +0800

    refactor: update usp contents

--- src/components/commons/usp/constants.js
+++ src/components/commons/usp/constants.js
@@ -1,8 +1,11 @@
 export const USP_CONTENTS = {
   metaTitle: 'MYCURE Complete Clinic Practice Management System',
-  title: 'The first complete clinic \nsolution designed for \nsmart clinics to work \nonline and offline.',
+  title: 'Optimize your clinic workflows and maximize its potential.',
+  description: 'Manage and monitor your daily operations using MYCURE, an advanced clinic management\
+  system that gives real-time medical and business insights.',
   btn: `/choose`,
   btnId: 'home-usp-signup-btn',
-  btnText: 'Sign Up Now',
+  btnText: 'Get Demo',
   img: 'mycure-web-banner-first-panel-doctor-clinic-patients',
-}
+  coverImg: 'mycure-homepage-usp-cover'
+};
