8ea8f22319e255700bb7e828b13994f529f291e5
Marc Almond Valleza <majvalleza@gmail.com>
2020-01-30 21:49:27 +0800

    refactor: make health suites mobile responsive

--- src/components/home/health-suites/index.vue
+++ src/components/home/health-suites/index.vue
@@ -1,61 +1,19 @@
 <template lang="pug">
   div
-    //- TODO: Mobile view
     div(v-if="!$isMobile").pt-5.health-suites
-      v-container
-        v-layout(row align-center)
-          v-flex(xs12 md12).text-xs-center
-            h2.text-xs-center.font-work-sans.font-xl.white--text {{ panelTitle }}
-            p.font-s.text-xs-center.white--text {{ panelSubtitle }}
-        v-layout(row fill-height).pt-5
-          v-flex(
-            v-for="(suite, key) in healthSuites"
-            :key="key"
-            xs12
-            md4
-          ).mx-1
-            v-card(height="100%").roundedCard
-              v-card-text
-                strong.font-s {{ suite.header }}
-                p.font-14 {{ suite.subheader }}
-                v-layout(row wrap)
-                  v-flex(xs12 md4)
-                    v-img(
-                      :src="require(`@/assets/images/${suite.image}.png`)"
-                    )
-                  v-flex(xs12 md8).pl-1
-                    ul
-                      li(v-for="(feat, key) in suite.features" :key="key") {{ feat }}
-                      br(v-if="key === 1")
-                v-layout(row wrap)
-                  v-flex(xs12 md4)
-                    v-btn(
-                      :color="$mcColors.mcBlue"
-                      dark
-                    ).text-none.font-weight-bold Start Free
-                  v-flex(xs12 md8).pl-1
-                    v-btn(
-                      left
-                      flat
-                      :color="$mcColors.mcBlue"
-                    ).text-none.font-weight-bold Learn More >
+      health-suites-web
+    div(v-else).pt-5.health-suites
+      health-suites-mobile
+      
 </template>
 
 <script>
-import { HEALTH_SUITES_HEADERS, HEALTH_SUITES_ITEMS } from './constants';
+import HealthSuitesWeb from './web';
+import HealthSuitesMobile from './mobile';
 export default {
-  data () {
-    return {
-      healthSuites: HEALTH_SUITES_ITEMS
-    };
-  },
-  computed: {
-    panelTitle () {
-      return HEALTH_SUITES_HEADERS.title;
-    },
-    panelSubtitle () {
-      return HEALTH_SUITES_HEADERS.subtitle;
-    }
+  components: {
+    HealthSuitesWeb,
+    HealthSuitesMobile
   }
 };
 </script>
@@ -63,8 +21,5 @@ export default {
 <style scoped>
 .health-suites {
   background-color: #075b76;
-}
-.roundedCard {
-  border-radius: 10px;
 }
 </style>
---
+++ src/components/home/health-suites/web.vue
@@ -0,0 +1,64 @@
+<template lang="pug">
+  v-container
+    v-layout(row align-center)
+      v-flex(xs12 md12).text-xs-center
+        h2.text-xs-center.font-work-sans.font-xl.white--text {{ panelTitle }}
+        p.font-s.text-xs-center.white--text {{ panelSubtitle }}
+    v-layout(row fill-height).pt-5
+      v-flex(
+        v-for="(suite, key) in healthSuites"
+        :key="key"
+        xs12
+        md4
+      ).mx-1
+        v-card(height="100%").roundedCard
+          v-card-text
+            strong.font-s {{ suite.header }}
+            p.font-14 {{ suite.subheader }}
+            v-layout(row wrap)
+              v-flex(xs12 md4)
+                v-img(
+                  :src="require(`@/assets/images/${suite.image}.png`)"
+                )
+              v-flex(xs12 md8).pl-1
+                ul
+                  li(v-for="(feat, key) in suite.features" :key="key") {{ feat }}
+                  br(v-if="key === 1")
+            v-layout(row wrap)
+              v-flex(xs12 md4)
+                v-btn(
+                  :color="$mcColors.mcBlue"
+                  dark
+                ).text-none.font-weight-bold Start Free
+              v-flex(xs12 md8).pl-1
+                v-btn(
+                  left
+                  flat
+                  :color="$mcColors.mcBlue"
+                ).text-none.font-weight-bold Learn More >
+</template>
+
+<script>
+import { HEALTH_SUITES_HEADERS, HEALTH_SUITES_ITEMS } from './constants';
+export default {
+  data () {
+    return {
+      healthSuites: HEALTH_SUITES_ITEMS
+    };
+  },
+  computed: {
+    panelTitle () {
+      return HEALTH_SUITES_HEADERS.title;
+    },
+    panelSubtitle () {
+      return HEALTH_SUITES_HEADERS.subtitle;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.roundedCard {
+  border-radius: 10px;
+}
+</style>
