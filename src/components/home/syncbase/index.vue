7dbe6eeea3effa2e8db7517b6b99ed0f082ab467
Marc Almond Valleza <majvalleza@gmail.com>
2020-01-30 18:16:37 +0800

    refactor: update syncbase panel

---
+++ src/components/home/syncbase/index.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+  div#panel4
+    //- WEB
+    div(v-if="!$isMobile").py-5
+      v-container
+        v-layout(row wrap justify center)
+          v-flex(xs6 align-self-center).px-3
+            h2.panelTitle.font-40 {{ webTitle }}
+            br
+            br
+            p.font-18 {{ panelContent }}
+          v-flex(xs6 align-self-center).px-3.text-xs-center
+            img(src="../../../assets/images/mycure-homepage-syncbase.png" width="80%" alt="MYCURE Syncbase")
+    //- MOBILE
+    div(v-else).pt-5
+      v-container
+        p.font-40.lh-title.text-xs-center {{ panelTitle }}
+        br
+        p.font-18.text-xs-center {{ panelContent }}
+        br
+        img(src="../../../assets/images/mycure-homepage-syncbase.png" width="100%" alt="MYCURE Syncbase")
+</template>
+
+<script>
+import { parseTextWithNewLine } from '@/utils';
+export default {
+  data () {
+    return {
+      panelTitle: 'MYCURE Health Suites work online and offline',
+      panelContent: 'Work as if you have an in-house server, with the convenience of the cloud.\
+       Create your medical records locally using multiple devices even if the internet is down!\
+       Once back online, it instantly syncs your data into the cloud.',
+      panelImage: 'mycure-homepage-syncbase'
+    };
+  },
+  computed: {
+    webTitle () {
+      return parseTextWithNewLine(this.panelTitle, ['Health ', 'online ']);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.panelTitle {
+  font-family: 'Work Sans', 'Poppins', sans-serif !important;
+  white-space: pre;
+}
+</style>
+
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,23 +1,31 @@
 <template lang="pug">
   //-TODO: Videos are still playing even when dialogs are closed
   div#top.white
+    //- 1st panel
     usp
+    //- 2nd panel
     health-suites
+    //- 3rd panel
     solutions
+    //- 4th panel
     storyflow(
       :storyflow="storyflowItems"
       :introduction="introText"
       :featuresButton="true"
       horizontal
     ).showPanel
+    //- 5th panel
     vid
     br
-    unique
+    //- 6th panel
+    syncbase
     hr
+    //- 7th panel
     security
     hr
+    //- 8th panel
     testimonial
-    //- targetMarketMobile(v-if="$isMobile").showPanel
+    //- final panel
     final-words.pb-5
     //- mcFooter
 </template>
@@ -29,11 +37,10 @@ import HealthSuites from '@/components/home/health-suites';
 import Solutions from '@/components/home/solutions';
 import Vid from '@/components/home/vid';
 import Product from '@/components/home/product';
-import Unique from '@/components/home/unique';
+import Syncbase from '@/components/home/syncbase';
 import Security from '@/components/home/security';
 import Testimonial from '@/components/home/testimonial';
 import FinalWords from '@/components/home/final-words';
-import TargetMarketMobile from '@/components/home/target-market-mobile';
 import Storyflow from '@/components/commons/storyflow';
 import VueScrollTo from 'vue-scrollto';
 
@@ -44,11 +51,10 @@ export default {
     Solutions,
     Vid,
     Product,
-    Unique,
+    Syncbase,
     Security,
     Testimonial,
     FinalWords,
-    TargetMarketMobile,
     Storyflow
   },
   mounted () {
