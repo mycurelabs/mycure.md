a162e0c98eefcb0796eae537a908a851e1329451
Marc Almond Valleza <majvalleza@gmail.com>
2020-02-03 15:53:54 +0800

    refactor: utilize features diagram component

--- src/components/home/solutions/index.vue
+++ src/components/home/solutions/index.vue
@@ -1,32 +1,33 @@
 <template lang="pug">
-  div
-    div.pt-3
-      v-container
-        v-layout(row align-center)
-          v-flex(xs12 md12).text-xs-center
-            h2(:class="{'pre-white-space': !$isMobile}").text-xs-center.font-work-sans.lh-title.font-xl {{ panelTitle }}
-            p.font-16.text-xs-center.pt-3 {{ panelContent.description }}
-        div.pt-5
-          v-img(
-            :src="require(`@/assets/images/${panelContent.image}.png`)"
-            :alt="panelContent.image"
-          )
+  div.pt-5
+    features-diagram(
+      :title="title"
+      :description="panelContent.description"
+      :image="panelContent.image"
+    )
 </template>
 
 <script>
+// utils
 import { parseTextWithNewLine } from '@/utils';
+// constants
 import { CONTENT } from './constants';
+// components
+import FeaturesDiagram from '@/components/commons/features-diagram';
 export default {
+  components: {
+    FeaturesDiagram
+  },
   data () {
     return {
       panelContent: CONTENT,
     };
   },
   computed: {
-    panelTitle () {
+    title () {
       const title = CONTENT.title;
       return !this.$isMobile ? parseTextWithNewLine(title, ['better']) : title;
-    }
+    },
   }
 };
 </script>
