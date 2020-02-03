9c14405dcda8bfc9a27a71ee2408640adf40f828
Marc Almond Valleza <majvalleza@gmail.com>
2020-02-03 16:54:39 +0800

    feat: create doctors clinics page

---
+++ src/views/doctors-clinics/index.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div#top
+    //- 1st panel
+    usp
+</template>
+
+<script>
+import Usp from '@/components/doctors-clinics/usp';
+import VueScrollTo from 'vue-scrollto';
+export default {
+  components: {
+    Usp
+  },
+  mounted () {
+    VueScrollTo.scrollTo(`#top`, 500, { easing: 'ease' } );
+  }
+};
+</script>
