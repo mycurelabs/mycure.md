e2531e15d1129a928687dea177bc355404a1d8eb
Marc Almond Valleza <majvalleza@gmail.com>
2020-02-03 17:29:04 +0800

    feat: add workflow panel in doctors clinics

---
+++ src/components/doctors-clinics/workflow/index.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+  div
+    div(v-if="!$isMobile").px-5.white
+      v-container.px-5
+        v-layout(row justify-center).text-xs-center
+          h1.font-45.pa-5.text-xs-center.font-work-sans {{ title }}
+        v-layout(row wrap justify-center)
+          v-flex(xs12 md4)
+            v-list(two-line).check-list.white
+              v-list-tile(v-for="(problem,index) in clinicProblems" :key="index" v-if="index<=2")
+                v-list-tile-action
+                  img(:src="require(`@/assets/images/${checkIcon}.png`)" alt="Check icon")
+                v-list-tile-content
+                  span.font-18 {{ problem }}
+          v-flex(xs12 md4)
+            v-list(two-line).check-list.white
+              v-list-tile(v-for="(problem,index) in clinicProblems" :key="index" v-if="index>2")
+                v-list-tile-action
+                  img(:src="require(`@/assets/images/${checkIcon}.png`)" alt="Check icon")
+                v-list-tile-content
+                  span.font-18 {{ problem }}
+    div(v-else).white.pt-5
+      v-container
+        v-layout(row justify-center)
+          h1.font-40.text-xs-center.font-work-sans {{ title }}
+      v-container
+        v-layout(row justify-center)
+          v-list(two-line).white
+            v-list-tile(v-for="(problem,index) in clinicProblems" :key="index")
+              v-list-tile-action
+                img(:src="require(`@/assets/images/${checkIcon}.png`)" alt="Check icon") 
+              v-list-tile-content
+                span.font-18 {{ problem }}
+</template>
+
+<script>
+import CLINIC_PROBLEMS from './constants.json';
+export default {
+  data () {
+    return {
+      title: 'Why do doctors use MYCURE',
+      checkIcon: 'mycure-web-bullet-check',
+      clinicProblems: CLINIC_PROBLEMS
+    };
+  }
+};
+</script>
+
+<style scoped>
+  .check-list {
+    background-color: #f6f6f6;
+  }
+</style>
+
--- src/views/doctors-clinics/index.vue
+++ src/views/doctors-clinics/index.vue
@@ -2,14 +2,18 @@
   div#top
     //- 1st panel
     usp
+    //- 2nd panel
+    workflow
 </template>
 
 <script>
 import Usp from '@/components/doctors-clinics/usp';
+import Workflow from '@/components/doctors-clinics/workflow';
 import VueScrollTo from 'vue-scrollto';
 export default {
   components: {
-    Usp
+    Usp,
+    Workflow
   },
   mounted () {
     VueScrollTo.scrollTo(`#top`, 500, { easing: 'ease' } );
