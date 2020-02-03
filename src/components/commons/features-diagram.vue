71cb36e7492b3da016d9e068dda55609be6ee007
Marc Almond Valleza <majvalleza@gmail.com>
2020-02-03 15:53:44 +0800

    refactor: make features diagram reusable

---
+++ src/components/commons/features-diagram.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  v-container
+    v-layout(row align-center)
+      v-flex(xs12 md12).text-xs-center
+        h1(:class="{'pre-white-space': !$isMobile}").text-xs-center.font-work-sans.lh-title.font-xl {{ title }}
+        p.font-16.text-xs-center.pt-3 {{ description }}
+    div.pt-5
+      v-img(
+        v-if="image !== ''"
+        :src="require(`@/assets/images/${image}.png`)"
+        :alt="image"
+      )
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Title'
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    image: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
+
