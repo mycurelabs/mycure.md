ec283afc810f8d1695a1491b05ab71028d5a5c85
Marc Almond Valleza <majvalleza@gmail.com>
2020-02-04 20:45:21 +0800

    refactor: add slot for features diagram

--- src/components/commons/features-diagram.vue
+++ src/components/commons/features-diagram.vue
@@ -1,15 +1,18 @@
 <template lang="pug">
-  v-container
-    v-layout(row align-center)
-      v-flex(xs12 md12).text-xs-center
-        h1(:class="{'pre-white-space': !$isMobile}").text-xs-center.font-work-sans.lh-title.font-xl {{ title }}
-        p.font-16.text-xs-center.pt-3 {{ description }}
-    div.pt-5
-      v-img(
-        v-if="image !== ''"
-        :src="require(`@/assets/images/${customPath}${image}.png`)"
-        :alt="image"
-      )
+  fragment
+    v-container
+      v-layout(row align-center)
+        v-flex(xs12 md12).text-xs-center
+          h1(:class="{'pre-white-space': !$isMobile}").text-xs-center.font-work-sans.lh-title.font-xl {{ title }}
+          p(:class="{'pre-white-space': !$isMobile}").font-16.text-xs-center.pt-3 {{ description }}
+      div.pt-5
+        v-img(
+          v-if="image !== ''"
+          :src="require(`@/assets/images/${customPath}${image}.png`)"
+          :alt="image"
+        )
+    slot(name="diagram-content")
+
 </template>
 
 <script>
