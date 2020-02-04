25a756e5d03b633e1a28af69bcf71041353139a8
Marc Almond Valleza <majvalleza@gmail.com>
2020-02-04 21:40:36 +0800

    feat: add mobile view of multispecialty features

--- src/components/multispecialty/features/index.vue
+++ src/components/multispecialty/features/index.vue
@@ -1,10 +1,46 @@
 <template lang="pug">
   div.pt-5.white
+    //- WEB
     features-diagram(
+      v-if="!$isMobile"
       :title="title"
       :customPath="customPath"
       :image="image"
     )
+    //- MOBILE
+    div(v-else)
+      v-container
+        v-layout(row justify-center)
+          h1.font-xl.text-xs-center.lh-title {{ title }}
+        br
+        v-layout(row justify-center)
+          div
+            //- https://ssense.github.io/vue-carousel/
+            carousel(
+              :per-page="1" 
+              :autoplay="true"
+              :loop="true"
+              :navigationEnabled="true"
+              :adjustableHeight="true"
+              :paginationEnabled="false"
+              :navigationClickTargetSize="50"
+              navigationNextLabel=" "
+              navigationPrevLabel=" "
+            ) 
+              slide(
+                v-for="(product,index) in 6"
+                :key="index"
+                :data-index="product"
+              ).px-2
+                img(:src="require(`@/assets/images/mycure-web-clinic-type-0${product}.png`)" alt="Multispecialty Clinic" width="100%")
+        br
+        v-layout(row justify-center v-for="feature in multiFeatures" :key="feature.title").pt-3
+          v-flex(xs3).text-xs-center
+            img(v-if="feature.icon" :src="require(`@/assets/images/${feature.icon}`)" :alt="feature.title")
+          v-flex(xs8 offset-xs1)
+            b {{ feature.title }}
+            p {{ feature.description}}
+
     v-layout(row justify-center align-center).pt-4
       v-flex(xs12).text-xs-center
         v-btn(
@@ -26,7 +62,39 @@ export default {
   data () {
     return {
       customPath: 'multispecialty/',
-      image: 'mycure-home-web-complete-for-clinics-of-all-sizes-diagram'
+      image: 'mycure-home-web-complete-for-clinics-of-all-sizes-diagram',
+      multiFeatures: [
+          {
+            title: 'Multispecialty Clinics',
+            description: 'One-stop, multi-staff clinics that provide ambulatory care and ancillary services',
+            icon: 'mycure-web-clinic-type-icon-multispecialty.png'
+          },
+          {
+            title: 'Outpatient Clinics',
+            description: 'Hospital-based clinics that provide outpatient consultation services from various specialists',
+            icon: 'mycure-web-clinic-type-icon-opd.png'
+          },
+          {
+            title: 'Industrial Clinics',
+            description: 'Primary clinics inside companies, schools, BPOs, and other industries covered by OSHS',
+            icon: 'mycure-web-clinic-type-icon-industrial.png'
+          },
+          {
+            title: 'Skin & Aesthetics Clinics',
+            description: 'Specialized for clinics that offer aesthetic procedures and dermatological services',
+            icon: 'mycure-web-clinic-type-icon-skin.png'
+          },
+          {
+            title: 'PME Clinics',
+            description: 'Clinics specializing in Physical Medical Exam (PME) Services',
+            icon: 'mycure-web-clinic-type-icon-pme.png'
+          },
+          {
+            title: 'Family Clinics',
+            description: 'Small to medium clinics that offer family medicine services, laboratory tests, and pharmacy',
+            icon: 'mycure-web-clinic-type-icon-family-clinic.png'
+          }
+        ]
     };
   },
   computed: {
