880ce7f1f134ef57b374f4cee185fd0a18c374d4
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-02-05 11:16:49 +0800

    refactor: fix typo

--- src/components/pricing/multispecialty.vue
+++ src/components/pricing/multispecialty.vue
@@ -70,7 +70,7 @@
               br
               br
               | 
-              span.font-mc-grey We’ll contact you within 24 hours to schedule the best time to meet you. Then during the demo, you will learn more about the features that will be useful for your clinic. You will also get expert recommendations from MYCURE specialists to to ease the transition.
+              span.font-mc-grey We’ll contact you within 24 hours to schedule the best time to meet you. Then during the demo, you will learn more about the features that will be useful for your clinic. You will also get expert recommendations from MYCURE specialists to ease the transition.
             br
             br
             br
@@ -158,7 +158,7 @@
               br
               br
               | 
-              span.font-mc-grey We’ll contact you within 24 hours to schedule the best time to meet you. Then during the demo, you will learn more about the features that will be useful for your clinic. You will also get expert recommendations from MYCURE specialists to to ease the transition.
+              span.font-mc-grey We’ll contact you within 24 hours to schedule the best time to meet you. Then during the demo, you will learn more about the features that will be useful for your clinic. You will also get expert recommendations from MYCURE specialists to ease the transition.
             br
             br
             br
