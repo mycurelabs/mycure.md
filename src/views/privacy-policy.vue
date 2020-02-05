e81cfa97f5494ee199ae3c58f1d6f46ff1745400
Marc Almond Valleza <majvalleza@gmail.com>
2020-02-05 18:42:56 +0800

    fix: remove duplicate footer

--- src/views/privacy-policy.vue
+++ src/views/privacy-policy.vue
@@ -2,12 +2,10 @@
   div#top
     panel-1
     panel-2.showPanel
-    mc-footer(:webSky="false")
 </template>
 
 <script>
   import mcToolbarHome from '@/components/commons/mc-toolbar-home';
-  import mcFooter from '@/components/commons/mc-footer';
   import panel1 from '@/components/privacy-policy/panel-1';
   import panel2 from '@/components/privacy-policy/panel-2';
   import VueScrollTo from 'vue-scrollto';
@@ -19,7 +17,6 @@
     }, 
     components: {
       mcToolbarHome,
-      mcFooter,
       panel1,
       panel2
     },
