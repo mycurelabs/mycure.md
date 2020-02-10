023e9e274ac686fc134f6ae3cce509c841ed7de6
Marc Almond Valleza <majvalleza@gmail.com>
2020-02-10 15:33:25 +0800

    refactor: change csr usp panel

---
+++ src/components/corporate-social-responsibility/usp/index.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  div.white
+    usp-template(
+      :uspTitle="uspTitle"
+      :uspMetaTitle="uspMetaTitle"
+      :btnId="uspContents.btnId"
+      :btnText="uspContents.btnText"
+      :coverImg="uspContents.coverImg"
+    )
+</template>
+
+<script>
+// utils
+import { parseTextWithNewLine } from '@/utils';
+// constants
+import USP_CONTENTS from './constants.json';
+// components
+import UspTemplate from '../../commons/usp';
+
+export default {
+  components: {
+    UspTemplate
+  },
+  data () {
+    return {
+      uspContents: USP_CONTENTS,
+    };
+  },
+  computed: {
+    uspTitle () {
+      const title = this.uspContents.title;
+      return !this.$isMobile ? parseTextWithNewLine(title, ['cares for people']) : title ;
+    },
+    uspMetaTitle () {
+      const metaTitle = this.uspContents.metaTitle;
+      return metaTitle;
+    }
+  }
+};
+</script>
