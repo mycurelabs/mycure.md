c0d784ba94fe2cb18854084799309c106558e406
Marc Almond Valleza <majvalleza@gmail.com>
2020-02-10 16:42:50 +0800

    refactor: change multispecialty step one font size

--- src/components/signup-multispecialty/step-1.vue
+++ src/components/signup-multispecialty/step-1.vue
@@ -9,21 +9,11 @@
           ).link-to-home.mb-3
           h1#step-1-title Make your clinic more efficient in minutes.
           br
-          v-layout(row)
-            v-flex(shrink).pr-2
+          v-layout(row v-for="(item, key) in checkListItems" :key="key")
+            v-flex(shrink).pr-2.pt-2
               img(width="20" src="../../assets/images/mycure-check.png")
             v-flex(shrink)
-              p Better operations
-          v-layout(row)
-            v-flex(shrink).pr-2
-              img(width="20" src="../../assets/images/mycure-check.png")
-            v-flex(shrink)
-              p Beautiful reports
-          v-layout(row)
-            v-flex(shrink).pr-2
-              img(width="20" src="../../assets/images/mycure-check.png")
-            v-flex(shrink)
-              p Bye paperworks
+              p.font-21 {{ item }}
         v-flex(xs12 md5)
           v-card
             v-card-text
@@ -36,27 +26,27 @@
                   outline
                   label="Clinic Name"
                   :rules="[requiredRule]"
-                )
+                ).step-one-field.font-21
                 v-text-field(
                   v-model="clinic.facilityAddress"
                   outline
                   label="Clinic Address"
                   :rules="[requiredRule]"
-                )
+                ).step-one-field.font-21
                 v-text-field(
                   v-model="clinic.numberOfStaff"
                   type="number"
                   outline
                   label="No. of staff (?)"
                   :rules="[requiredRule]"
-                )
+                ).step-one-field.font-21
                 v-text-field(
                   v-model="clinic.numberOfPatients"
                   type="number"
                   outline
                   label="Average patients per day (?)"
                   :rules="[requiredRule]"
-                )
+                ).step-one-field.font-21
                 //- h5(style="margin-bottom: -20px;").grey--text No. of staff (?)
                 //- v-slider(
                 //-   color="primary"
@@ -75,17 +65,17 @@
                 //-   min="1"
                 //-   max="1000"
                 //- )
-                h5(style="margin-bottom: -20px;").grey--text Does your clinic have other branches?
+                h5(style="margin-bottom: -20px;").grey--text.font-21 Does your clinic have other branches?
                 v-radio-group(row v-model="clinic.hasOtherBranches")
-                  v-radio(label="Yes" :value="true")
-                  v-radio(label="No" :value="false")
+                  v-radio(label="Yes" :value="true").step-one-field
+                  v-radio(label="No" :value="false").step-one-field
             v-card-actions
               v-spacer
               v-btn(
                 color="accent"
                 @click="next"
               ) Next
-          div.mt-3
+          div.mt-3.font-18
             b Already have an account? 
               router-link(:to="{ name: 'signin' }") Sign in.
 </template>
@@ -96,7 +86,12 @@ export default {
     return {
       valid: false,
       clinic: {},
-      requiredRule: v => !!v || 'This field is required'
+      requiredRule: v => !!v || 'This field is required',
+      checkListItems: [
+        'Better operations',
+        'Beautiful reports',
+        'Bye paperworks'
+      ]
     };
   },
   watch: {
@@ -129,5 +124,9 @@ h1 {
 
 .link-to-home:hover {
   cursor: pointer;
+}
+
+.step-one-field >>> label {
+  font-size: 21px !important;
 }
 </style>
