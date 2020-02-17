b3147e7b2a2c935ddf89400f221ca901f6b03a76
Marc Almond Valleza <majvalleza@gmail.com>
2020-02-17 13:39:25 +0800

    refactor: use mobile diagram carousel component

--- src/components/multispecialty/features/index.vue
+++ src/components/multispecialty/features/index.vue
@@ -15,34 +15,10 @@
         v-layout(row justify-center)
           h1.font-xl.text-xs-center.lh-title {{ title }}
         br
-        v-layout(row justify-center)
-          div
-            //- https://ssense.github.io/vue-carousel/
-            carousel(
-              :per-page="1" 
-              :autoplay="true"
-              :loop="true"
-              :navigationEnabled="true"
-              :adjustableHeight="true"
-              :paginationEnabled="false"
-              :navigationClickTargetSize="50"
-              navigationNextLabel=" "
-              navigationPrevLabel=" "
-            ) 
-              slide(
-                v-for="(product,index) in 6"
-                :key="index"
-                :data-index="product"
-              ).px-2
-                img(:src="require(`@/assets/images/mycure-web-clinic-type-0${product}.png`)" alt="Multispecialty Clinic" width="100%")
-        br
-        v-layout(row justify-center v-for="feature in multiFeatures" :key="feature.title").pt-3
-          v-flex(xs3).text-xs-center
-            img(v-if="feature.icon" :src="require(`@/assets/images/${feature.icon}`)" :alt="feature.title")
-          v-flex(xs8 offset-xs1)
-            b {{ feature.title }}
-            p {{ feature.description}}
-
+        mobile-diagram-carousel(
+          :slides="slides"
+          :items="multiFeatures"
+        )
     v-layout(row justify-center align-center).pt-4
       v-flex(xs12).text-xs-center
         v-btn(
@@ -58,46 +34,56 @@
 <script>
 import { parseTextWithNewLine } from '@/utils';
 import FeaturesDiagram from '@/components/commons/features-diagram';
+import MobileDiagramCarousel from '@/components/commons/mobile-diagram-carousel';
 
 export default {
   components: {
-    FeaturesDiagram
+    FeaturesDiagram,
+    MobileDiagramCarousel
   },
   data () {
     return {
       customPath: 'multispecialty/',
       exploreFeaturesId: 'multispecialty-explore-features-btn',
       image: 'mycure-home-web-complete-for-clinics-of-all-sizes-diagram',
+      slides: [
+        'mycure-web-clinic-type-01',
+        'mycure-web-clinic-type-02',
+        'mycure-web-clinic-type-03',
+        'mycure-web-clinic-type-04',
+        'mycure-web-clinic-type-05',
+        'mycure-web-clinic-type-06'
+      ],
       multiFeatures: [
           {
             title: 'Multispecialty Clinics',
             description: 'One-stop, multi-staff clinics that provide ambulatory care and ancillary services',
-            icon: 'mycure-web-clinic-type-icon-multispecialty.png'
+            icon: 'mycure-web-clinic-type-icon-multispecialty'
           },
           {
             title: 'Outpatient Clinics',
             description: 'Hospital-based clinics that provide outpatient consultation services from various specialists',
-            icon: 'mycure-web-clinic-type-icon-opd.png'
+            icon: 'mycure-web-clinic-type-icon-opd'
           },
           {
             title: 'Industrial Clinics',
             description: 'Primary clinics inside companies, schools, BPOs, and other industries covered by OSHS',
-            icon: 'mycure-web-clinic-type-icon-industrial.png'
+            icon: 'mycure-web-clinic-type-icon-industrial'
           },
           {
             title: 'Skin & Aesthetics Clinics',
             description: 'Specialized for clinics that offer aesthetic procedures and dermatological services',
-            icon: 'mycure-web-clinic-type-icon-skin.png'
+            icon: 'mycure-web-clinic-type-icon-skin'
           },
           {
             title: 'PME Clinics',
             description: 'Clinics specializing in Physical Medical Exam (PME) Services',
-            icon: 'mycure-web-clinic-type-icon-pme.png'
+            icon: 'mycure-web-clinic-type-icon-pme'
           },
           {
             title: 'Family Clinics',
             description: 'Small to medium clinics that offer family medicine services, laboratory tests, and pharmacy',
-            icon: 'mycure-web-clinic-type-icon-family-clinic.png'
+            icon: 'mycure-web-clinic-type-icon-family-clinic'
           }
         ]
     };
