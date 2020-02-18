d0ae264f02b704184f2ff4d52411a46e61781c4d
Marc Almond Valleza <majvalleza@gmail.com>
2020-02-18 19:48:20 +0800

    refactor: remove pricing from nav

--- src/components/commons/mc-toolbar/index.vue
+++ src/components/commons/mc-toolbar/index.vue
@@ -50,7 +50,6 @@ export default {
       ],
       toolbarLinks: [
         { id: 'features', name: 'Features', route: 'features' },
-        { id: 'pricing', name: 'Pricing', route: 'pricing' }
       ]
     };
   },
