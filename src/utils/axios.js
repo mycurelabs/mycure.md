a66fd3b5352588db4b0a9d42bd50491e0b76ea77
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-03-18 00:10:29 +0800

    refactor: update error handling function

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -8,14 +8,9 @@ function handleError (e) {
     // console.log(e.response.status);
     const { data } = e.response.data;
     return data;
-  } else if (e.request) {
-    // The request was made but no response was received
-    // `e.request` is an instance of XMLHttpRequest in the browser and an instance of
-    // http.ClientRequest in node.js
-    // console.log(e.request);
   } else {
     // Something happened in setting up the request that triggered an Error
-    console.log('Error', e.message);
+    return e;
   }
 }
 
