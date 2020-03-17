9cc984df6dd7db69d6b51c4b911bb15f05c9b8b5
Marc Almond Valleza <majvalleza@gmail.com>
2020-03-17 14:51:31 +0800

    style: bolden text

--- src/components/our-story/panel-2.vue
+++ src/components/our-story/panel-2.vue
@@ -12,7 +12,9 @@
         v-flex(xs12 md8 align-self-center).px-3.font-21
           p Paulette became a young widow at age 30. Her two children were just 4- and 6-year olds then. Despite limited resources, she managed to give her kids a good life.
           p Just when things got a lot better, Paulette encountered a bigger challenge—cancer.
-          p For about a decade and a half, her life had been all about trips to the doctor, regular checkups, and staying healthy. Years of medical files were definitely taking up space in their home and in her everyday life. As she shuttled between Guam (where she is based) and the Philippines, carrying her medical files to different doctors and pharmacies became too cumbersome and costly. Replenishing her medication had become a guessing game. Staying healthy sometimes became detrimental to her own health.
+          p For about a decade and a half, her life had been all about trips to the doctor, regular checkups, and staying healthy.&nbsp;
+            strong Years of medical files&nbsp;
+            | were definitely taking up space in their home and in her everyday life. As she shuttled between Guam (where she is based) and the Philippines, carrying her medical files to different doctors and pharmacies became too cumbersome and costly. Replenishing her medication had become a guessing game. Staying healthy sometimes became detrimental to her own health.
           p Paulette stood strong as a cancer warrior and survivor.
           p Our founder, Dale, had been witness to all of this. Paulette is Dale's mother.
 </template>
