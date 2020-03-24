f2a251602adb357e49d9cf161140f2a372b15f09
Marc Almond Valleza <majvalleza@gmail.com>
2020-03-24 12:37:21 +0800

    chore: configure vuetify colors

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,17 +61,18 @@ module.exports = {
   */
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
+    frameworkOptions: {
+      theme: {
+        themes: {
+          light: {
+            primary: '#2e9fdf',
+            accent: '#7fad33',
+            secondary: '#616161',
+            info: colors.teal.lighten1,
+            warning: colors.amber.base,
+            error: '#ff5252',
+            success: colors.green.accent3,
+          },
         },
       },
     },
