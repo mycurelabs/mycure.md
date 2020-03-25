ac3a0a4b2c762c9750ed4291665e1950dd740cb5
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-03-25 13:49:05 +0800

    feat: add new layout
    - add new page for user layout

---
+++ layouts/user.vue
@@ -0,0 +1,5 @@
+<template lang="pug">
+  v-app#app
+    v-content
+      nuxt
+</template>
---
+++ pages/forgot-password.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  v-container
+    v-row
+      v-col
+</template>
+
+<script>
+export default {
+  layout: 'user',
+};
+</script>
