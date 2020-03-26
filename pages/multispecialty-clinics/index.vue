04f228da4ceaf9f28354be22daa4af99ae6e3117
Marc Almond Valleza <majvalleza@gmail.com>
2020-03-26 10:57:16 +0800

    feat: add ms storyflow

--- pages/multispecialty-clinics/index.vue
+++ pages/multispecialty-clinics/index.vue
@@ -6,18 +6,63 @@
     workflow
     //- 3rd panel
     features
+    //- 4th panel
+    storyflow(
+      :introduction="storyflowIntro"
+      :storyflow="storyflowItems"
+      horizontal
+      :customPath="storyflowCustomPath"
+      white-bg
+    )
 </template>
 
 <script>
+import VueScrollTo from 'vue-scrollto';
+// - constants
+import STORYFLOW_ITEMS from './storyflow-items.json';
+// - components
 import Usp from '~/components/multispecialty-clinics/usp';
 import Workflow from '~/components/multispecialty-clinics/workflow';
 import Features from '~/components/multispecialty-clinics/features';
+import Storyflow from '~/components/commons/storyflow';
+// - utils
 import headMeta from '~/utils/head-meta';
+import { parseTextWithNewLine } from '~/utils/newline';
+
 export default {
   components: {
     Usp,
     Workflow,
     Features,
+    Storyflow,
+  },
+  data () {
+    this.storyflowItems = STORYFLOW_ITEMS;
+    this.storyflowCustomPath = 'multispecialty/';
+    return {
+      ctaContent: {
+        text: 'Premium clinical services work best with premium technology',
+        subtext: this.parseCtaSubtext(),
+        btnText: 'Book A Free Demo',
+        btnLink: 'signup-multispecialty',
+        image: 'mycure-web-footer',
+      },
+    };
+  },
+  computed: {
+    storyflowIntro () {
+      const text = 'Make your patients and your staff become more engaged';
+      return parseTextWithNewLine(text, ['staff']);
+    },
+  },
+  mounted () {
+    VueScrollTo.scrollTo('#app', 500, { easing: 'ease' });
+  },
+  methods: {
+    parseCtaSubtext () {
+      const text = 'Your multispecialty clinic deserves the most advanced clinic management system around. Do more with MYCURE technology by your side.';
+      return parseTextWithNewLine(text, ['around.']);
+    },
   },
   head () {
     return headMeta({
