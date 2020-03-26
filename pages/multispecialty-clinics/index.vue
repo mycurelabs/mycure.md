01fd3455ea51415b5593e75f5de66431a6a6d097
Marc Almond Valleza <majvalleza@gmail.com>
2020-03-26 10:04:53 +0800

    feat: add ms usp panel

---
+++ pages/multispecialty-clinics/index.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  div#top
+    //- 1st panel
+    usp
+</template>
+
+<script>
+import Usp from '~/components/multispecialty-clinics/usp';
+export default {
+  components: {
+    Usp,
+  },
+};
+</script>
