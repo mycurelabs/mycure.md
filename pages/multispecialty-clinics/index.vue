c7d0aaac8ad1ba7665ba3690e00dd514e95b044d
Marc Almond Valleza <majvalleza@gmail.com>
2020-03-26 10:24:15 +0800

    feat: add ms workflow panel

--- pages/multispecialty-clinics/index.vue
+++ pages/multispecialty-clinics/index.vue
@@ -2,13 +2,17 @@
   div#top
     //- 1st panel
     usp
+    //- 2nd panel
+    workflow
 </template>
 
 <script>
 import Usp from '~/components/multispecialty-clinics/usp';
+import Workflow from '~/components/multispecialty-clinics/workflow';
 export default {
   components: {
     Usp,
+    Workflow,
   },
 };
 </script>
