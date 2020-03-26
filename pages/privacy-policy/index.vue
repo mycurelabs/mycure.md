93fdb21ef75c3da18fd4ab5298404711c13b5cb2
Marc Almond Valleza <majvalleza@gmail.com>
2020-03-26 19:04:39 +0800

    feat: create privacy policy page

---
+++ pages/privacy-policy/index.vue
@@ -0,0 +1,161 @@
+<template lang="pug">
+  div#top
+    div.header.mb-5
+      v-container
+        v-row(align="center" :class="{'header-container': !$isMobile}")
+          v-col(cols="12")
+            h1.font-60.text-center Privacy Policy
+    div.content
+      v-container
+        v-row(justify="center")
+          v-col(cols="12" md="10").mb-3
+            strong.font-18 Last Updated | March 6, 2018
+            br
+            br
+            h2 Privacy Statement
+            br
+            p MYCURE, Inc. - USA and Team O.P.S. Inc. - Philippines ("company", "us", "we", or "our") operate the www.MYCURE.md website and the MYCURE mobile applications (the "Service").
+            p Your privacy is very important to us. Accordingly, we have developed this Policy in order for you to understand how we collect, use, communicate, and disclose and make use of Personal Information when you use our Service. MYCURE is very much committed to conducting our business in accordance with these principles in order to ensure that the confidentiality of personal information is protected and maintained.
+            p Any sensitive, privileged and personal information that you provide shall be kept protected under the Republic Act No. 10173, or the Data Privacy Act of 2012, and its Implementing Rules and Regulations (collectively, the "Data Privacy Act").
+            p We will not use or share your Personal Information with anyone except as described in this Privacy Policy and in MYCURE's&nbsp;
+              nuxt-link(:to="{name:'terms'}" title="MYCURE Terms and Conditions").blue--text Terms and Conditions.
+              |&nbspInformation will only be used for its intended purpose (or as may be required by existing national and local laws, rules and regulations).
+            p We use your Personal Information for providing and improving the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our&nbsp;
+              nuxt-link(:to="{name:'terms'}" title="MYCURE Terms and Conditions").blue--text Terms and Conditions.
+          v-col(cols="12" md="10").mb-3
+            h2 Information Collection And Use
+            br
+            p While using our Service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you. Personally identifiable information may include, but is not limited to, your email address, name, phone number, age, date of birth, nationality, civil status, postal address, organization/company information, job title/occupation, and other information ("Personal Information").
+          v-col(cols="12" md="10").mb-3
+            h2 Log Data
+            br
+            p We may also collect information that your browser sends whenever you visit our Service or when you access the Service by or through a mobile device ("Log Data").
+            p This Log Data may include information such as your computer's Internet Protocol ("IP") address, browser type, browser version, the pages of our Service that you visit, the time and date of your visit, the time spent on those pages and other statistics.
+            p When you access the Service by or through a mobile device, this Log Data may include information such as the type of mobile device you use, your mobile device unique ID, the IP address of your mobile device, your mobile operating system, the type of mobile Internet browser you use and other statistics.
+            p In addition, we may use third party services such as Google Analytics that collect, monitor and analyze this type of information in order to increase our Service's functionality. These third party service providers have their own privacy policies addressing how they use such information.
+          v-col(cols="12" md="10").mb-3
+            h2 Location information
+            br
+            p We may use and store information about your location, if you give us permission to do so. We use this information to provide features of our Service, to improve and customize our Service. You can enable or disable location services when you use our Service at anytime, through your mobile device settings.
+          v-col(cols="12" md="10").mb-3
+            h2 Cookies
+            br
+            p Cookies are files with small amount of data, which may include an anonymous unique identifier. Cookies are sent to your browser from a web site and stored on your computer's hard drive.
+            p We use "cookies" to collect information. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Service.
+          v-col(cols="12" md="10").mb-3
+            h2 DoubleClick Cookie
+            br
+            p Google, as a third party vendor, uses cookies to serve ads on our Service. Google's use of the DoubleClick cookie enables it and its partners to serve ads to our users based on their visit to our Service or other web sites on the Internet.
+            p You may opt out of the use of the DoubleClick Cookie for interest-based advertising by visiting the Google Ads Settings web page: http://www.google.com/ads/preferences/.
+          v-col(cols="12" md="10").mb-3
+            h2 Sharing of Personal Information
+            br
+            p We may employ third party companies and individuals to facilitate our Service, to provide the Service on our behalf, to perform Service-related services or to assist us in analyzing how our Service is used.
+            p These third parties have access to your Personal Information only to perform these tasks on our behalf and are obligated not to disclose or use it for any other purpose.
+            p Whenever necessary, a data sharing agreement shall cover any sharing of data between MYCURE and other service providers. MYCURE may also share information in accordance with any requirements from any relevant government agency as provided by law.
+          v-col(cols="12" md="10").mb-3
+            h2 Communications
+            br
+            p We may use your Personal Information to contact you with newsletters, marketing or promotional materials and other information that may be of interest to you. You may opt out of receiving any, or all, of these communications from us by following the unsubscribe link or instructions provided in any email we send.
+          v-col(cols="12" md="10").mb-3
+            h2 Compliance With Laws
+            br
+            p We will disclose your Personal Information where required to do so by law or subpoena or if we believe that such action is necessary to comply with the law and the reasonable requests of law enforcement or to protect the security or integrity of our Service.
+          v-col(cols="12" md="10").mb-3
+            h2 Business Transaction
+            br
+            p If MYCURE, Inc. is involved in a merger, acquisition or asset sale, your Personal Information may be transferred. We will provide notice before your Personal Information is transferred and becomes subject to a different Privacy Policy.
+          v-col(cols="12" md="10").mb-3
+            h2 Storage and Protection of Personal Information
+            br
+            p MYCURE is implementing technological and physical security measures in addition to instituting policies and procedures to protect Personal Information from loss, misuse and unauthorized access or disclosure or destruction.  MYCURE Team undergoes orientation and training on its data privacy policy to ensure that they are knowledgeable about protecting sensitive data. They are likewise  required to execute a non-disclosure agreement among others.
+            p MYCURE's web and mobile applications go through regular scanning for possible security holes and know vulnerabilities. Your Personal Information is contained behind secured servers and is only accessible by a limited, qualified and authorized personnel who hold your information with strict confidentiality. Protocols and operational checks and balances are likewise in place on how to access these servers while deploying encryption and physical security controls for sensitive data.
+            p We highly regard the security of your Personal Information, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security and there will always be a risk of unauthorized disclosure of personal data. However, MYCURE believes that protocols and safeguards in place are sufficient to prevent the occurrence of such risk and it is deeply committed to continuous updates and improvements to protect your Personal Information in compliance with the Data Privacy Act.
+          v-col(cols="12" md="10").mb-3
+            h2 Retention Period
+            br
+            p A part of MYCURE platform is being a storage service provider and as such it will keep your data until your subscription expires or upon your request to have it deleted from its systems/databases.
+          v-col(cols="12" md="10").mb-3
+            h2 International Transfer
+            br
+            p Your information, including Personal Information, may be transferred to - and maintained on - computers located outside of your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from your jurisdiction.
+            p If you are located outside Philippines and choose to provide information to us, please note that we may transfer the information, including Personal Information, to Philippines and process it there.
+            p Your consent to this Privacy Policy followed by your submission of such information represents your agreement to that transfer.
+          v-col(cols="12" md="10").mb-3
+            h2 Links To Other Sites
+            br
+            p Our Service may contain links to other sites that are not operated by us. If you click on a third party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy of every site you visit.
+            p We have no control over, and assume no responsibility for the content, privacy policies or practices of any third party sites or services.
+          v-col(cols="12" md="10").mb-3
+            h2 Children's Privacy
+            br
+            p Our Service does not address anyone under the age of 13 ("Children").
+            p We do not knowingly collect personally identifiable information from children under 13. If you are a parent or guardian and you are aware that your Children has provided us with Personal Information, please contact us. If we become aware that we have collected Personal Information from a child under age 13 without verification of parental consent, we take steps to remove that information from our servers.
+          v-col(cols="12" md="10").mb-3
+            h2 Your Rights under the Data Privacy Act
+            br
+            p Under the Data Privacy Act of 2012, you have the right to be informed, to have reasonable access, to correct any inaccuracy, to obtain copy, to remove and/or object to any processing of Personal Information that you have provided to us. You may do so by contacting our Data Privacy Officer (DPO), see Contact Us below.
+            p Any request for correction, erasure and objection to process of the personal data, however, is subject to any exceptions under applicable laws, rules, and regulations.
+          v-col(cols="12" md="10").mb-3
+            h2 Changes To This Privacy Policy
+            br
+            p We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.
+            p You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.
+          v-col(cols="12" md="10").mb-3
+            h2 Consent
+            br
+            p By agreeing to this policy, you explicitly consent and allow to the collection, processing and storage of your Personal Information by MYCURE for the purpose(s) described in this Data Privacy Policy.
+            p MYCURE requires you to get and secure consent from your parents or legal guardians in order to use the Service if you are below eighteen (18) years old. MYCURE does not specifically cater to persons under eighteen (18) years old.
+          v-col(cols="12" md="10").mb-3
+            h2 Contact Us
+            br
+            p If you have any questions about this Privacy Policy, please contact:
+            ul
+              li Title: Data Protection Officer
+              li Address: #107 TKC Square (Formerly Luisa Bldg. 2) Maginhawa St, Teacher's Village - East, QC 1101
+              li Phone No: (+632) 7799 6262
+              li Email Address: privacy@mycure.md
+</template>
+
+<script>
+import VueScrollTo from 'vue-scrollto';
+import headMeta from '~/utils/head-meta';
+export default {
+  mounted () {
+    VueScrollTo.scrollTo('#app', 500, { easing: 'ease' });
+  },
+  head () {
+    return headMeta({
+      title: 'Privacy Policy',
+      description: 'MYCURE practices data privacy and integrity at its core. Learn more about our Privacy Policy so you will know how we process your data using MYCURE Clinic Management System in your practice. Last updated: March 6, 2018.',
+      // - TODO: Replace with local if applicable
+      socialBanner: 'https://firebasestorage.googleapis.com/v0/b/mc-v4-prod.appspot.com/o/web-main-assets%2FMYCURE_Open_Graph_Images_Privacy_Policy.png?alt=media&token=f68b2956-ed80-421b-bbcd-a7c129f7430c',
+    });
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  background-image: url('../../assets/images/mycure-clinic-background-legal.png');
+  background-position: center center;
+  background-size: cover;
+}
+
+.header-container {
+  height: 300px;
+}
+
+p {
+  font-size: 18px;
+  text-align: justify;
+}
+
+h2 {
+  font-size: 26px;
+}
+
+ul li {
+  font-size: 18px;
+}
+</style>
