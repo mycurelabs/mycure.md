35cc75b3dd49fcf724237f0854499c07f08d36e4
Marc Almond Valleza <majvalleza@gmail.com>
2020-03-26 10:27:30 +0800

    refactor: add ms page meta

--- pages/multispecialty-clinics/index.vue
+++ pages/multispecialty-clinics/index.vue
@@ -9,10 +9,19 @@
 <script>
 import Usp from '~/components/multispecialty-clinics/usp';
 import Workflow from '~/components/multispecialty-clinics/workflow';
+import headMeta from '~/utils/head-meta';
 export default {
   components: {
     Usp,
     Workflow,
+  },
+  head () {
+    return headMeta({
+      title: 'Clinic Management System for Multispecialty Clinics',
+      description: 'MYCURE is the best EMR clinic management system for multispecialty clinics with its simplified workflows and faster performance. Book a free demo now!',
+      // - TODO: Replace with local if applicable
+      socialBanner: 'https://firebasestorage.googleapis.com/v0/b/mc-v4-prod.appspot.com/o/web-main-assets%2FMYCURE-Open-Graph-Images-Multispecialty.png?alt=media&token=771bd2f8-fd7b-47fd-9bb9-0f495f1e94f8',
+    });
   },
 };
 </script>
