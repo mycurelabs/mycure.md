80cbc544fab5f7c03a682a635c3435d31b9a06c0
Marc Almond Valleza <majvalleza@gmail.com>
2020-03-27 19:22:30 +0800

    feat: create ms sign up step 1

---
+++ pages/signup/multispecialty/index.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+</template>
+
+<script>
+export default {
+  layout: 'user',
+  created () {
+    this.$nuxt.$router.push({ name: 'signup-multispecialty-step-1' });
+  },
+};
+</script>
---
+++ pages/signup/multispecialty/step-1.vue
@@ -0,0 +1,137 @@
+<template lang="pug">
+  v-row(justify="center" align="center")
+    v-col(cols="12" md="10")
+      v-row(justify="center")
+        v-col(cols="12" md="5" :class="[{'content-padding': !$isMobile}]")
+          img(
+            src=`~/assets/images/mycure-${dayOrNight === 'night' ? 'footer' : 'header'}-logo.png`
+            @click="$nuxt.$nuxt.push({ name: 'index' })"
+            alt="MYCURE logo"
+          ).link-to-home.mb-3
+          h2.primary--text Multispecialty Clinic: Sign Up (Step 1 of 3)
+          br
+          h1#step-1-title Make your clinic more efficient in minutes.
+          br
+          v-row(v-for="(item, key) in checkListItems" :key="key" align="center" no-gutters)
+            v-col(cols="1")
+              img(width="20" src="~/assets/images/mycure-check.png")
+            v-col(cols="11")
+              p.font-21 {{ item }}
+          v-row.pt-5
+            v-col.mb-3
+              b.font-18 Already have an account?&nbsp;
+                nuxt-link(:to="{ name: 'signin' }") Sign in.
+        v-col(cols="12" md="5")
+          v-card
+            v-card-text
+              h1 Tell us about your facility.
+            v-card-text
+              v-form(ref="formRef" v-model="valid")
+                v-text-field(
+                  v-model="clinic.facilityName"
+                  outlined
+                  label="Clinic Name"
+                  :rules="[requiredRule]"
+                ).step-one-field.font-21
+                v-text-field(
+                  v-model="clinic.facilityAddress"
+                  outlined
+                  label="Clinic Address"
+                  :rules="[requiredRule]"
+                ).step-one-field.font-21
+                v-text-field(
+                  v-model="clinic.numberOfStaff"
+                  outlined
+                  label="No. of staff (?)"
+                  :rules="[requiredRule, ...numberRules]"
+                ).step-one-field.font-21
+                v-text-field(
+                  v-model="clinic.numberOfPatients"
+                  outlined
+                  label="Average patients per day (?)"
+                  :rules="[requiredRule, ...numberRules]"
+                ).step-one-field.font-21
+                h5(style="margin-bottom: -20px;").grey--text.font-21 Does your clinic have other branches?
+                v-radio-group(row v-model="clinic.hasOtherBranches")
+                  v-radio(label="Yes" :value="true").step-one-field
+                  v-radio(label="No" :value="false").step-one-field
+            v-card-actions
+              v-spacer
+              v-btn(
+                color="accent"
+                :disabled="!valid"
+                @click="next"
+                large
+              ).font-weight-bold Next
+</template>
+
+<script>
+// - utils
+import dayOrNight from '~/utils/day-or-night';
+import headMeta from '~/utils/head-meta';
+
+export default {
+  layout: 'user',
+  data () {
+    this.dayOrNight = dayOrNight();
+    return {
+      valid: false,
+      clinic: {},
+      requiredRule: v => !!v || 'This field is required',
+      numberRules: [
+        v => v > 0 || 'Please enter a positive number',
+        v => !(/.\./.test(v)) || 'Whole numbers only',
+      ],
+      checkListItems: [
+        'Better operations',
+        'Beautiful reports',
+        'Bye paperworks',
+      ],
+    };
+  },
+  created () {
+    if (process.browser) {
+      if (localStorage.getItem('multi:step1:model')) {
+        this.clinic = JSON.parse(localStorage.getItem('multi:step1:model'));
+      } else {
+        this.clinic.hasOtherBranches = true;
+      }
+    }
+  },
+  methods: {
+    next () {
+      if (this.$refs.formRef.validate()) {
+        if (process.browser) {
+          localStorage.setItem('multi:step1:model', JSON.stringify(this.clinic));
+        }
+        this.$nuxt.$router.push({ name: 'signup-multispecialty-step-2' });
+      }
+    },
+  },
+  head () {
+    return headMeta({
+      title: 'Book a Free Demo for MYCURE Clinic Management System',
+      description: 'Book a free demo with MYCURE Specialists to learn more how the clinic management system can help boost your clinic performance',
+      socialBanner: 'https://firebasestorage.googleapis.com/v0/b/mc-v4-prod.appspot.com/o/web-main-assets%2FMYCURE-Open-Graph-Images-Home.png?alt=media&token=61978d70-326f-4323-8e6a-17d7b9c9a53b',
+    });
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  line-height: 35px;
+}
+
+.link-to-home:hover {
+  cursor: pointer;
+}
+
+.content-padding {
+  padding-top: 100px;
+}
+
+.step-one-field >>> label {
+  font-size: 18px !important;
+}
+</style>
