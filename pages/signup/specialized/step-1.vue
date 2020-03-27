5ac8127a536a090a0dd3d87025013aebdaa6a87a
Marc Almond Valleza <majvalleza@gmail.com>
2020-03-27 14:52:39 +0800

    feat: update specialized signup meta

--- pages/signup/specialized/step-1.vue
+++ pages/signup/specialized/step-1.vue
@@ -13,8 +13,8 @@ export default {
   },
   head () {
     return headMeta({
-      title: 'Start Free EMR Doctor Practice Management System',
-      description: 'Create a free MYCURE account today and become a techy doctor in minutes! Better operations, beautiful reports, bye paperworks!',
+      title: 'Start Free Trial: MYCURE Clinic Management System for Specialized Clinics',
+      description: 'Customize your clinic system for skin and aesthetics, maternity care, pediatrics, dentistry, or diagnostics. Start your 14-day trial now!',
       // - TODO: Replace with local if applicable
       socialBanner: 'https://firebasestorage.googleapis.com/v0/b/mc-v4-prod.appspot.com/o/web-main-assets%2FMYCURE-Open-Graph-Images-Home.png?alt=media&token=61978d70-326f-4323-8e6a-17d7b9c9a53b',
     });
