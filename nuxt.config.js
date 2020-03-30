9f1d5c3ae44434981a9efa228a10c0cf264e7a31
Marc Almond Valleza <majvalleza@gmail.com>
2020-03-30 10:23:42 +0800

    chore: install nuxt-ssr-cache

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,44 @@ module.exports = {
     // Doc: https://github.com/nuxt-community/dotenv-module
     '@nuxtjs/dotenv',
     'nuxt-webfontloader',
+    'nuxt-ssr-cache',
   ],
+  cache: {
+    // if you're serving multiple host names (with differing
+    // results) from the same server, set this option to true.
+    // (cache keys will be prefixed by your host name)
+    // if your server is behind a reverse-proxy, please use
+    // express or whatever else that uses 'X-Forwarded-Host'
+    // header field to provide req.hostname (actual host name)
+    useHostPrefix: false,
+    pages: [
+      // these are prefixes of pages that need to be cached
+      // if you want to cache all pages, just include '/'
+      '/',
+      '/doctors-clinics',
+      '/specialized-clinics',
+      '/multispecialty-clinics',
+      '/features',
+      '/our-story'
+    ],
+    
+    key(route, context) {
+      // custom function to return cache key, when used previous
+      // properties (useHostPrefix, pages) are ignored. return 
+      // falsy value to bypass the cache
+    },
+
+    store: {
+      type: 'memory',
+
+      // maximum number of pages to store in memory
+      // if limit is reached, least recently used page
+      // is removed.
+      max: 10,
+      // number of seconds to store this page in cache
+      ttl: 60,
+    },
+  },
   webfontloader: {
     google: {
       families: ['Work+Sans:400,700,900', 'Source+Sans+Pro:400,700'],
