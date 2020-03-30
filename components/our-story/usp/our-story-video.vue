fa3b101938a5760ccc254a01c56dcd926c137326
Marc Almond Valleza <majvalleza@gmail.com>
2020-03-30 11:37:28 +0800

    fix: adjust our story video dialog

--- components/our-story/usp/our-story-video.vue
+++ components/our-story/usp/our-story-video.vue
@@ -1,18 +1,17 @@
 <template lang="pug">
-  v-dialog(v-model="videoDialog" max-width="600px")
-    v-row(justify="center")
-      v-card(width="600")
-        v-card-text
-          iframe(
-            align="middle"
-            id="ytplayer"
-            type="text/html"
-            :height="!$isMobile ? '400' : '175'"
-            :width="!$isMobile ? '570' : '300'"
-            src="https://www.youtube.com/embed/zvM3LIvq-6k?autoplay=0&loop=1&showinfo=0rel=0"
-            frameborder="0"
-            allowfullscreen
-          )
+  v-dialog(v-model="videoDialog" max-width="600")
+    v-card(width="600").pt-5
+      v-card-text
+        iframe(
+          align="middle"
+          id="ytplayer"
+          type="text/html"
+          :height="!$isMobile ? '400' : '175'"
+          width="100%"
+          src="https://www.youtube.com/embed/zvM3LIvq-6k?autoplay=0&loop=1&showinfo=0rel=0"
+          frameborder="0"
+          allowfullscreen
+        )
 </template>
 
 <script>
