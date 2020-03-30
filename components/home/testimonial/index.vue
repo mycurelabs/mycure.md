09cc21cb3fa84db09604e113f3673782cf0a3827
Marc Almond Valleza <majvalleza@gmail.com>
2020-03-30 17:01:40 +0800

    refactor: start testimonial carousel on viewport
    Resolves #59

--- components/home/testimonial/index.vue
+++ components/home/testimonial/index.vue
@@ -1,25 +1,27 @@
 <template lang="pug">
   div.gradient-grey
     client-only
-      v-container(v-if="!isMobile")
+      v-container(v-if="!isMobile" @mouseover="playCarousel = true")
         v-row(justify="center")
           v-col(cols="12" md="9").pt-2.pb-1
-              v-carousel(
-                dark
-                hide-delimiters
-                next-icon="mdi-chevron-right"
-                height="350"
-              ).elevation-0.testimonial-carousel
-                v-carousel-item(
-                  v-for="(testimonial,i) in testimonials"
-                  :key="i"
-                ).pt-10
-                  v-row(justify="center" align="center")
-                    v-col(cols="10").text-center.pa-5
-                      h2.black--text.mb-5.font-25 {{ testimonial.message }}
-                      strong.font-s.primary--text {{ testimonial.name }}
-                      br
-                      span.font-s.subheading.grey--text {{ testimonial.position }}
+            v-carousel(
+              dark
+              hide-delimiters
+              :cycle="playCarousel"
+              :interval="4000"
+              next-icon="mdi-chevron-right"
+              height="350"
+            ).elevation-0.testimonial-carousel
+              v-carousel-item(
+                v-for="(testimonial,i) in testimonials"
+                :key="i"
+              ).pt-10
+                v-row(justify="center" align="center")
+                  v-col(cols="10").text-center.pa-5
+                    h2.black--text.mb-5.font-25 {{ testimonial.message }}
+                    strong.font-s.primary--text {{ testimonial.name }}
+                    br
+                    span.font-s.subheading.grey--text {{ testimonial.position }}
       div(v-else).transparent.pt-5
         v-container
           v-row(justify="center").px-3
@@ -54,14 +56,14 @@ export default {
         message: '“It\'s a great experience using MYCURE app in our rural setup. I\'ve been through many EMR apps, but this one is the best so far... It is very convenient to use, simple, adaptable and user-friendly even to my secretary.”',
       },
       {
-        name: 'Atty. Ivy D. Patdu, M.D.',
-        position: 'Deputy Privacy Commissioner, NPC',
-        message: '“We should harness the power of technology. We should incorporate in system designs means to obtain information while remaining true to our obligations for data protection.”',
-      },
-      {
         name: 'Nelson S. Abelardo, M.D.',
         position: 'Cardiologist',
         message: '“MYCURE fulfills a long time need of physicians for accurate records of patients which they can carry with them wherever they are.”',
+      },
+      {
+        name: 'Atty. Ivy D. Patdu, M.D.',
+        position: 'Deputy Privacy Commissioner, NPC',
+        message: '“We should harness the power of technology. We should incorporate in system designs means to obtain information while remaining true to our obligations for data protection.”',
       },
       {
         name: 'Greg Burke, M.D.',
@@ -76,6 +78,7 @@ export default {
     ];
     return {
       isMobile: true,
+      playCarousel: false,
     };
   },
   watch: {
