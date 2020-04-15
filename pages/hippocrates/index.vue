338c5993ec3cdfae999611d2df0cb517d6fa5cb3
Marc Almond Valleza <majvalleza@gmail.com>
2020-04-15 13:51:52 +0800

    refactor: add OG graph for hippocrates page

--- pages/hippocrates/index.vue
+++ pages/hippocrates/index.vue
@@ -82,7 +82,7 @@ export default {
       title: 'Hippocrates by MYCURE',
       description: 'A Complete Clinic Management Solution for your Medical Arts Center',
       // - TODO: Replace with local if applicable
-      socialBanner: 'https://firebasestorage.googleapis.com/v0/b/mc-v4-prod.appspot.com/o/web-main-assets%2FMYCURE-Open-Graph-Images-Home.png?alt=media&token=61978d70-326f-4323-8e6a-17d7b9c9a53b',
+      socialBanner: require('~/assets/images/hippocrates/MYCURE-Open-Graph-Images-Hippocrates.png'),
     });
   },
 };
