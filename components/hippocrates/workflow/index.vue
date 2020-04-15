eec861a6475d5d4d9f4c8ae2fc928e9f84c0c43f
Marc Almond Valleza <majvalleza@gmail.com>
2020-04-15 14:48:08 +0800

    feat: add hippocrates workflow mobile

--- components/hippocrates/workflow/index.vue
+++ components/hippocrates/workflow/index.vue
@@ -9,18 +9,34 @@
       :customPath="customPath"
       :image-width="$isMobile ? '100%' : '80%'"
     )
-    //- //- MOBILE
-    //- div(v-else)
-    //-   v-container
-    //-     v-row(justify="center")
-    //-       h1.font-40.pre-white-space.text-center.lh-title.font-work-sans {{ title }}
-    //-     br
-    //-     mobile-diagram-carousel(
-    //-       :slides="slides"
-    //-       :customSlidePath="customPath"
-    //-       :customItemPath="customPath"
-    //-       :items="doctorsFeatures"
-    //-     )
+    //- MOBILE
+    div(v-else)
+      v-container
+        v-row(justify="center")
+          h1.font-36.text-center.lh-title.font-work-sans {{ title }}
+        br
+        v-row(
+          v-for="(step, key) in steps"
+          :key="key"
+          justify="center"
+          dense
+        )
+          v-col(cols="12")
+            v-btn(
+              fab
+              small
+              depressed
+              :color="key === 1 ? '#f0f0f0' : '#372A82'"
+              :class="{'white--text' : key !== 1}"
+            ).font-s {{ step.number }}
+            div.ml-5.mt-5
+              span.font.s {{ step.text }}
+              br
+              img(
+                :src="require(`~/assets/images/hippocrates/${step.image}`)"
+                alt="Hippocrates Workflow"
+                width="50%"
+              )
 </template>
 
 <script>
@@ -35,43 +51,49 @@ export default {
   data () {
     this.image = 'mycure-hippocrates-diagram-steps';
     this.customPath = 'hippocrates/';
-    // this.doctorsFeatures = [
-    //   {
-    //     title: 'SOAP Charting',
-    //     description: 'Quickly store and retrieve your charts anytime, anywhere.',
-    //     icon: 'mycure-group-clinic-diagram-mobile-icon-soap-charting',
-    //   },
-    //   {
-    //     title: 'Secretary Account',
-    //     description: 'Queuing and billing can be managed by your secretary.',
-    //     icon: 'mycure-group-clinic-diagram-mobile-icon-secretary',
-    //   },
-    //   {
-    //     title: 'Syncbase Technology',
-    //     description: 'Work offline and sync data online!',
-    //     icon: 'mycure-group-clinic-diagram-mobile-icon-syncbase',
-    //   },
-    //   {
-    //     title: 'Diagnostic Orders',
-    //     description: 'Laboratory and imaging test requests in a few clicks',
-    //     icon: 'mycure-group-clinic-diagram-mobile-icon-diagx',
-    //   },
-    //   {
-    //     title: 'Custom Forms',
-    //     description: 'Create medical certificates, questionnaires, and waivers.',
-    //     icon: 'mycure-group-clinic-diagram-mobile-icon-forms',
-    //   },
-    //   {
-    //     title: 'Daily Census',
-    //     description: 'Manage your group\'s daily performance & income with ease.',
-    //     icon: 'mycure-group-clinic-diagram-mobile-icon-census',
-    //   },
-    //   {
-    //     title: 'Print Records Anytime',
-    //     description: 'Printable medical records for you and your patients',
-    //     icon: 'mycure-group-clinic-diagram-mobile-icon-print',
-    //   },
-    // ];
+    this.steps = [
+      {
+        number: 1,
+        text: 'Patient goes to your virtual MAC website, and will then be asked a few questions.',
+        image: 'mycure-hippocrates-virtual-medical-center-steps-01.png',
+      },
+      {
+        number: 2,
+        text: 'If positive...',
+        image: 'mycure-hippocrates-virtual-medical-center-steps-02B.png',
+      },
+      {
+        number: 2,
+        text: 'Patient searches for or selects a doctor.',
+        image: 'mycure-hippocrates-virtual-medical-center-steps-02A.png',
+      },
+      {
+        number: 3,
+        text: 'Patient sets an appointment with selected doctor.',
+        image: 'mycure-hippocrates-virtual-medical-center-steps-03.png',
+      },
+      {
+        number: 4,
+        text: 'When Doctor is ready, a video screen will open & online consultation begins.',
+        image: 'mycure-hippocrates-virtual-medical-center-steps-04.png',
+      },
+      {
+        number: 5,
+        text: 'Doctor takes diagnosis notes in provided EMR. Doctor issues ePrescription.',
+        image: 'mycure-hippocrates-virtual-medical-center-steps-05.png',
+      },
+      {
+        number: 6,
+        text: 'Doctor can order Diagnostic Tests (connected to Hospital existing LIS or RIS)',
+        note: 'Note: If hospital does not have an LIS or RIS, Hippocrates also has existing modules for this.',
+        image: 'mycure-hippocrates-virtual-medical-center-steps-06.png',
+      },
+      {
+        number: 7,
+        text: 'Once done, patient pays online (via credit card). ePrescription is shown in patient dashboard.',
+        image: 'mycure-hippocrates-virtual-medical-center-steps-07.png',
+      },
+    ];
     return {};
   },
   computed: {
@@ -79,7 +101,7 @@ export default {
       const title = 'Imagine your Medical Arts Center Going Online';
       return !this.$isMobile
         ? parseTextWithNewLine(title, ['Arts'])
-        : parseTextWithNewLine(title, ['your', 'Arts']);
+        : title;
     },
     description () {
       const description = 'Continue your outpatient services with online video chat consultations bundled with an advanced EMR system that your physicians can access anytime.';
