d68f3eae3ba9e1cc4ce125373349db7d86834b16
Marc Almond Valleza <majvalleza@gmail.com>
2020-04-22 08:43:45 +0800

    refactor: remove hippocrates in nav

--- components/commons/mc-navbar/index.vue
+++ components/commons/mc-navbar/index.vue
@@ -43,7 +43,7 @@ export default {
       { name: 'Doctors Clinics', route: 'doctors-clinics' },
       { name: 'Specialized Clinics', route: 'specialized-clinics' },
       { name: 'Multispecialty Clinics', route: 'multispecialty-clinics' },
-      { name: 'Hippocrates by MYCURE', route: 'hippocrates' },
+      // { name: 'Hippocrates by MYCURE', route: 'hippocrates' },
     ];
     this.toolbarLinks = [
       { name: 'Features', route: 'features' },
