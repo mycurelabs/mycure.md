084c80cf41e4e5a19340dc6f9aecce3fbf0fe4d0
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-04-25 10:48:06 +0800

    feat: create layout and page for doctors

---
+++ pages/doctors/_id.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  v-container
+    v-row
+      v-col
+        img(width="100" :src="picURL")
+        pre {{name}}
+</template>
+
+<script>
+import { getDoctorWebsite } from '../../utils/axios';
+export default {
+  layout: 'doctor-website',
+  async asyncData ({ app, router, params }) {
+    const doctor = await getDoctorWebsite({ username: params.id });
+    return {
+      picURL: doctor.picURL,
+      name: doctor.name,
+    };
+  },
+  head () {
+    return {
+      title: `Dr. ${this.name?.firstName} ${this.name?.lastName} | MYCURE`,
+    };
+  },
+};
+</script>
