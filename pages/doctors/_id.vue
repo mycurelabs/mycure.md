40ec336acdfcce3a11edd46542a80ebc9e8f2901
Jofferson R. Tiquez <jofftiquez@gmail.com>
2020-05-05 22:35:50 +0800

    refactor: add new sections
    - tabs
    - social
    - footer

--- pages/doctors/_id.vue
+++ pages/doctors/_id.vue
@@ -9,7 +9,16 @@
     services(
       :services="services"
     )
-    pre {{doctor}}
+    tabs
+    social
+    v-footer(
+      height="auto"
+      color="primary"
+    )
+      v-row
+        v-col.text-center
+          span.white--text Copyright {{new Date().getFullYear()}} | All Rights Reserved | Powered by #[a(href="https://mycure.md" target="_blank").mycure-link MYCURE]
+    //- pre {{doctor}}
 </template>
 
 <script>
@@ -18,18 +27,26 @@ import { formatName } from '../../utils/formats';
 import AppBar from '~/components/doctor-website/app-bar';
 import Panel1 from '~/components/doctor-website/panel-1';
 import Services from '~/components/doctor-website/services';
+import Tabs from '~/components/doctor-website/tabs';
+import Social from '~/components/doctor-website/social';
 export default {
   layout: 'doctor-website',
   components: {
     AppBar,
     Panel1,
     Services,
+    Tabs,
+    Social,
   },
   async asyncData ({ app, router, params }) {
     const doctor = await getDoctorWebsite({ username: params.id });
-    console.warn(doctor);
     return {
       doctor: doctor || {},
+    };
+  },
+  data () {
+    return {
+      selectedTab: 'clinics',
     };
   },
   computed: {
@@ -64,3 +81,10 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.mycure-link {
+  color: white;
+  text-decoration: none;
+}
+</style>
