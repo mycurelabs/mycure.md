c6c69226f488aeca02bf71f3b76b83b1ab0ad81b
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-05-05 08:52:54 +0800

    feat: create app bar for doctor website

---
+++ components/doctor-website/app-bar.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  v-app-bar(flat color="white")
+    img(
+      height="45"
+      src="../../assets/images/mycure-header-logo.png"
+      to="/"
+    )
+    v-spacer
+    book-appointment-btn.mr-2
+    online-consult-btn
+</template>
+
+<script>
+import BookAppointmentBtn from '~/components/commons/book-appointment-btn';
+import OnlineConsultBtn from '~/components/commons/online-consult-btn';
+export default {
+  components: {
+    BookAppointmentBtn,
+    OnlineConsultBtn,
+  },
+};
+</script>
