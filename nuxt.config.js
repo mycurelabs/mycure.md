2b35554b4f838350af51fe81422b6f6399f3fd04
Marc Almond Valleza <majvalleza@gmail.com>
2020-05-06 09:51:07 +0800

    fix: add additional head meta hid
    Resolves #254

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,11 +19,10 @@ module.exports = {
   },
   head: {
     titleTemplate: '%s',
-    title: process.env.npm_package_name || '',
+    title: 'MYCURE Advanced Complete Clinic Management System',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
