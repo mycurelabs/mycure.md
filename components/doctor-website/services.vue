6976a2c931dae08620d99589f16aa441f6420804
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-05-06 19:22:50 +0800

    refactor: update services

--- components/doctor-website/services.vue
+++ components/doctor-website/services.vue
@@ -4,19 +4,22 @@
       v-col(cols="12").text-center
         h1 Services Offered
     v-row(justify="center")
-      v-col(cols="12" md="10")
-        v-row
-          v-col(
-            v-for="(service, index) in services" :key="index"
-            cols="12"
-            sm="12"
-            md="4"
-          ).pa-1
-            media(align="left" image-position="left")
-              template(slot="media-image")
-                img(src="../../assets/images/mycure-check.png")
-              template(slot="media-content")
-                span {{service}}
+      v-col(cols="12" md="8")
+        v-row(justify="center")
+          template(v-if="services.length === 0")
+            div.text-center.pa-5
+              h3.grey--text #[i No services to show]
+          template(v-else v-for="(service, index) in services")
+            v-col(
+              cols="12"
+              sm="12"
+              md="4"
+            ).pa-1
+              media(image-position="left")
+                template(slot="media-image")
+                  img(src="../../assets/images/mycure-check.png")
+                template(slot="media-content")
+                  span {{service}}
 </template>
 
 <script>
