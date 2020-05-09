a02b8526b383b2dd9e9f2bcad0ae903931ca8b99
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-05-09 15:37:15 +0800

    refactor: update formatName util

--- utils/formats.js
+++ utils/formats.js
@@ -1,9 +1,11 @@
 export const formatName = (name, format) => {
-  const { firstName, middleName, lastName } = name;
+  const { firstName, middleName, lastName, generationalSuffix } = name;
   return format.replace(/firstName/gi, firstName)
     .replace(/middleName/gi, middleName || '')
     .replace(/middleInitial/gi, middleName ? `${middleName.substr(0, 1).toUpperCase()}.` : '')
-    .replace(/lastName/gi, lastName);
+    .replace(/lastName/gi, lastName)
+    .replace(/generationalSuffix/gi, generationalSuffix || '')
+    .trim();
 };
 
 export const formatCurrency = (num = 0, currency = 'USD') => {
