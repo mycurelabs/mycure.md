5b16b9b208fab1ea530d5ff13d32ac1c636a5fef
Marc Almond Valleza <majvalleza@gmail.com>
2020-05-29 16:45:18 +0800

    refactor: create covid banner component

---
+++ components/commons/mc-covid-banner.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+  div.main-container.font-work-sans.blue.white--text
+    v-container
+      v-row(v-if="!isMobile").px-4
+        v-col(cols="11").pt-2.text-center
+          | {{ bannerText }}
+          u.font-weight-bold impacts of COVID.19
+      v-row(v-else).pl-3
+        v-col(cols="10").text-center
+          | {{ bannerText }}
+          u.font-weight-bold impacts of COVID.19
+</template>
+
+<script>
+// components
+import CookieLaw from 'vue-cookie-law';
+export default {
+  components: {
+    CookieLaw,
+  },
+  data () {
+    this.bannerText = 'The world needs independent businesses. Learn about the actions we\'re taking to address the ';
+    return {
+      isMobile: true,
+    };
+  },
+  computed: {
+    position () {
+      return 'top';
+    },
+    bannerClasses () {
+      const webClasses = ['font-16', 'py-3'];
+      return this.isMobile
+        ? ['py-1']
+        : webClasses;
+    },
+  },
+  watch: {
+    $isMobile (val) {
+      this.isMobile = val;
+    },
+  },
+  mounted () {
+    this.isMobile = this.$isMobile;
+  },
+};
+</script>
+
+<style scoped>
+.main-container {
+  width: 100%
+}
+</style>
