70560eb9b13b0023d4838bb90f408831c0413fbd
Gerald Canonizado <gerald35canonizado@gmail.com>
2020-06-03 09:51:08 -0800

    refactor: use $isMobile instead of isMobile

--- components/doctors-clinics/cta/index.vue
+++ components/doctors-clinics/cta/index.vue
@@ -5,27 +5,27 @@
       :background-image-mobile="backgroundImageMobile"
       :background-image-mobile-configs="backgroundImageMobileConfigs"
       :customPath="customPath"
-      :class="{'pt-12': isMobile}"
+      :class="{'pt-12': $isMobile}"
     )
       v-row(slot="content").row-content
-        v-col(cols="12" md="5" :class="[{'web-content-margin': !isMobile}]" v-if="!isMobile")
+        v-col(cols="12" md="5" :class="[{'web-content-margin': !$isMobile}]" v-if="!$isMobile")
           h1(:class="titleClasses").font-poppins.font-40.lh-title {{ uspTitle }}
           p(:class="[centerText]").font-italic.font-18.px-1 {{ uspSubtitle }}
-          div(v-if="!isMobile").text-field-container.white
+          div(v-if="!$isMobile").text-field-container.white
             v-text-field(
               v-model="email"
               outlined
               placeholder="myname@email.com"
             )
           v-btn(
-            v-if="!isMobile"
+            v-if="!$isMobile"
             block
             color="accent"
             large
             @click="onGetStarted"
           ).text-none.font-weight-bold.font-18.mt-5 Get Started
           p(:class="[centerText]").white--text.font-16.px-1.text-center.py-2 {{ uspAgreement }}
-        v-col(cols="12" md="5" v-if="isMobile" one-line).cta-form
+        v-col(cols="12" md="5" v-if="$isMobile" one-line).cta-form
           h1.font-poppins.font-30.lh-title {{ uspTitle }}
           p.font-italic.font-18.font-weight-light.px-1.pt-1 {{ uspSubtitle }}
           div.text-field-container.white
