73d2d0e2a980ef11f855614c64640a8f008f1c14
Marc Almond Valleza <majvalleza@gmail.com>
2020-06-04 14:37:09 +0800

    refactor: add medical-records bottom images

--- components/features/medical-records.vue
+++ components/features/medical-records.vue
@@ -6,10 +6,29 @@
         align-left-column="start"
         :header="header"
         :descriptions="descriptions"
-        :web-image="panelImage"
+        :web-image="mediaImage"
         :custom-image-path="customPath"
         hide-image-mobile
       )
+        div(slot="additional-content")
+          v-btn(text @click="onGetStarted").text-none.get-started-btn
+            strong.primary--text Get Started
+            v-icon.primary--text mdi-arrow-right
+      //- Bottom images
+      v-container(v-if="!$isMobile")
+        v-row
+          v-col(cols="12" md="5")
+            img(
+              v-lazy="leftBottomImage"
+              alt="Print prescription"
+              width="100%"
+            )
+          v-col(cols="12" md="7")
+            img(
+              v-lazy="rightBottomImage"
+              alt="Charting"
+              width="100%"
+            )
       //- div(v-else).panel-container.pt-5
       //-     v-row(justify="center").mx-2
       //-       p.font-18.text-center Record full medical history, vitals, and physical exam records according
@@ -45,27 +64,39 @@ export default {
     GenericMediaPanel,
   },
   data () {
-    this.features = ['A', 'B', 'C'];
     this.header = 'Designed for doctors, by doctors.';
     this.descriptions = [
       'Record full medical history, vitals, and physical exam records according to SOAP charting. Eliminate the haste of scribbling with Rx printing, ICD-10 database, multiple image uploads, and more.',
     ];
-    this.panelImage = 'MYCURE-virtual-clinic-healthcare-practice-online-features-B-01-emr.png';
+    this.panelImages = [
+      'MYCURE-virtual-clinic-healthcare-practice-online-features-B-01-emr.png',
+      'MYCURE-virtual-clinic-healthcare-practice-online-features-B-02-print-prescription.png',
+      'MYCURE-virtual-clinic-healthcare-practice-online-features-B-03-charting.png',
+    ];
     this.customPath = 'features/';
     return {};
+  },
+  computed: {
+    mediaImage () {
+      return this.panelImages[0];
+    },
+    leftBottomImage () {
+      return require(`~/assets/images/features/${this.panelImages[1]}`);
+    },
+    rightBottomImage () {
+      return require(`~/assets/images/features/${this.panelImages[2]}`);
+    },
+  },
+  methods: {
+    onGetStarted () {
+      // TODO: Go to signup
+    },
   },
 };
 </script>
 
 <style scoped>
-.panel-container {
-  background-image: url('../../assets/images/features/mycure-web-features-gradient-background.png');
-  background-position: center bottom;
-  background-repeat: no-repeat;
-  background-size: 100% auto;
-}
-
-.add-spacing {
-  padding-top: 32px;
+.get-started-btn {
+  margin-left: -3%;
 }
 </style>
