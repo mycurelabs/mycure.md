c75b5a3725025c86782d0d3fd2b0c02cc8f2461f
Mar Jayson San Agustin <sanagustinjayson@gmail.com>
2020-06-04 16:21:30 +0800

    feat: add module item component

---
+++ components/features/module-item.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  div
+    div
+        img(v-lazy="require(`~/assets/images/module-icons/${ moduleItem.icon }`)" width="40%" height="40%" :alt="moduleItem.title")
+        br
+        span.font-21 &nbsp; {{ moduleItem.title }}
+        br
+        p.font-15.module-item-subTitle &nbsp; {{ moduleItem.subTitle }}
+    br
+    div.module-item-description {{ moduleItem.description }}
+</template>
+
+<script>
+export default {
+  name: 'ModuleItem',
+  props: {
+    moduleItem: {
+      type: Object,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.module-item-subTitle {
+  text-align: center;
+  width: 85%;
+  color: #04b0e7;
+  margin: 0 auto;
+}
+
+.module-item-description {
+  text-align: center;
+  color: #a7a7a7;
+}
+</style>
