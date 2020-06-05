cfdec0990159b5877b398e833343c4eb17e10b3e
Gerald Canonizado <gerald35canonizado@gmail.com>
2020-06-05 15:50:35 -0800

    refactor: align get started button in enterprise

--- components/enterprise/multiple-branches/index.vue
+++ components/enterprise/multiple-branches/index.vue
@@ -6,13 +6,13 @@
       v-col(cols="12" md="6" align-self="center" v-if="!$isMobile").pl-10
         h1.font-30.lh-title.pb-3.font-weight-light {{branches.header}}
         p.font-18.mt-3 {{branches.description}}
-        v-btn(text).mt-5
+        v-btn(text).mt-5.ml-n4
           strong.text-capitalize.primary--text {{branches.btnTxt}}
           v-icon.primary--text {{branches.btnIcon}}
       v-col(cols="12" md="6" align-self="center" v-if="$isMobile")
         h1.font-30.lh-title.pb-3.font-weight-light {{branches.header}}
         p.font-18.mt-3 {{branches.description}}
-        v-btn(text).mt-5
+        v-btn(text).mt-5.ml-n4
           strong.text-capitalize.primary--text {{branches.btnTxt}}
           v-icon.primary--text {{branches.btnIcon}}
       v-col(cols="12" md="6" align="center" v-if="$isMobile")
