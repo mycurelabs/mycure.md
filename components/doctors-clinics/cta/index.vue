370ad013688360e002e23cab4cfa2aca0f87bc7f
Gerald Canonizado <gerald35canonizado@gmail.com>
2020-06-09 13:39:20 -0800

    refactor: push routes in cta button

--- components/doctors-clinics/cta/index.vue
+++ components/doctors-clinics/cta/index.vue
@@ -96,7 +96,10 @@ export default {
   },
   methods: {
     onGetStarted () {
-      this.$emit('getStarted');
+      if (!this.email) {
+        return;
+      }
+      this.$emit('getStarted', this.email);
     },
   },
 };
