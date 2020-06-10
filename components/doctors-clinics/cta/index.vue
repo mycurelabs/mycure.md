b8793b42297fe109dfafe91df07c60a873b36225
Gerald Canonizado <gerald35canonizado@gmail.com>
2020-06-09 20:18:50 -0800

    refactor: change footer background image
    Resolves #323

--- components/doctors-clinics/cta/index.vue
+++ components/doctors-clinics/cta/index.vue
@@ -68,7 +68,7 @@ export default {
     GenericBackgroundPanel,
   },
   data () {
-    this.backgroundImage = 'MYCURE-virtual-clinic-healthcare-practice-online-doctors-clinic-final-cta-cover.png';
+    this.backgroundImage = 'MYCURE-virtual-clinic-healthcare-practice-online-doctors-clinic-final-cta-cover.webp';
     this.backgroundImageMobile = 'MYCURE-virtual-clinic-healthcare-practice-online-doctors-clinic-final-cta-cover-mobile.png';
     this.panelTitle = 'Build your virtual clinic today.';
     this.ctaSubtitle = 'For Modern Doctors, Virtual is the new normal.';
