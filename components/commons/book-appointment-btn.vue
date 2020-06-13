1e14c28544674c1104b33ebef6b80ab249baf97f
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-06-13 23:07:39 +0800

    refactor: add url to buttons

--- components/commons/book-appointment-btn.vue
+++ components/commons/book-appointment-btn.vue
@@ -3,5 +3,18 @@
     outlined
     large
     color="primary"
+    :href="url"
   ).text-none Book Appointment
 </template>
+
+<script>
+export default {
+  computed: {
+    url () {
+      const pxPortalUrl = process.env.PX_PORTAL_URL;
+      const { id } = this.$route.params;
+      return `${pxPortalUrl}/appointment/step-1?doctor=${id}`;
+    },
+  },
+};
+</script>
--- components/commons/online-consult-btn.vue
+++ components/commons/online-consult-btn.vue
@@ -3,5 +3,18 @@
     large
     color="primary"
     depressed
+    :href="url"
   ).text-none Online Consult
 </template>
+
+<script>
+export default {
+  computed: {
+    url () {
+      const pxPortalUrl = process.env.PX_PORTAL_URL;
+      const { id } = this.$route.params;
+      return `${pxPortalUrl}/appointment/step-1?doctor=${id}`;
+    },
+  },
+};
+</script>
