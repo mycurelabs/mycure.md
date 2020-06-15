747d96d162aed0083e05f456ec5db0b6a267ccf4
Gerald Canonizado <gerald35canonizado@gmail.com>
2020-06-15 14:27:17 -0800

    refactor: update features page social banner
    Resolves #475

--- pages/features/index.vue
+++ pages/features/index.vue
@@ -87,8 +87,7 @@ export default {
     return headMeta({
       title: 'MYCURE Clinic Management System and EMR Features',
       description: 'MYCURE Clinic Management System includes modularized features that are customizable, user-friendly and efficient for a more patient-centric care.',
-      // - TODO: Replace with local if applicable
-      socialBanner: 'https://firebasestorage.googleapis.com/v0/b/mc-v4-prod.appspot.com/o/web-main-assets%2FMYCURE-Open-Graph-Images-Features.png?alt=media&token=6bac1cce-cd24-4af2-b3d8-b0cf3005ec91',
+      socialBanner: '~/assets/images/banners/MYCURE Open Graph Images - Features.png',
     });
   },
 };
