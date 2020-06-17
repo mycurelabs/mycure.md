85128c67fbdc81e19d316821b150f14cdae9c6c1
Marc Almond Valleza <majvalleza@gmail.com>
2020-06-17 22:15:16 +0800

    refactor: add guard for source set

--- utils/image-src-set.js
+++ utils/image-src-set.js
@@ -1,10 +1,12 @@
 /**
  * Sets srcset values for source tags
  * @param {String} source - file extension
- *  @example webp, png
  * @param {String} image - image name (without extension)
  * @param {String} customPath - custompath if not in root images folder
  */
 export default (source, image, customPath = '') => {
-  return require(`~/assets/images/${customPath}${image}${source}`);
+  const fileExtension = source.charAt(0) === '.'
+    ? source
+    : `.${source}`;
+  return require(`~/assets/images/${customPath}${image}${fileExtension}`);
 };
