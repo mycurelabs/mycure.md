7bf5f0a5a9ba9e4d700597e3f9d32d4f8d269437
Marc Almond Valleza <majvalleza@gmail.com>
2020-06-17 23:37:26 +0800

    docs: add props to description to reusable components
    - generic-media-panel
    - PictureSource

--- components/commons/PictureSource.vue
+++ components/commons/PictureSource.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   picture
     source(
-      v-for="(source, key) in imageSources"
+      v-for="(source, key) in sourceTypes"
       :key="key"
       :srcset="getSrcSetValue(source, image)"
       :type="`image/${source}`"
@@ -20,33 +20,79 @@ import parseSrcSet from '~/utils/image-src-set';
 
 export default {
   props: {
-    imageSources: {
-      type: Array,
-      default: () => (['webp', 'png']),
-    },
+    /**
+     * Path from root images folder
+     * @type {String}
+     * @example features/  (~/assets/images/features/)
+     */
     customPath: {
       type: String,
       default: '',
     },
+    /**
+     * If the passed file extension is the only source type to include
+     * @type {Boolean}
+     */
+    extensionExclusive: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * List of source types
+     * @type {Array}
+     */
+    imageSources: {
+      type: Array,
+      default: () => (['webp', 'png']),
+    },
+    /**
+     * Image file name (without extension)
+     * @type {String}
+     */
     image: {
       type: String,
       required: true,
     },
+    /**
+     * Width of image
+     * @type {String}
+     */
     imageWidth: {
       type: String,
       default: '100%',
     },
+    /**
+     * Alt value of image
+     * @type {String}
+     */
     imageAlt: {
       type: String,
       default: 'Content image',
     },
+    /**
+     * File extension of image
+     * @type {String}
+     * @example .webp
+     */
     imageFileExtension: {
       type: String,
       required: true,
     },
+    /**
+     * List of classes to apply to image
+     * @type {Array}
+     * @example ['ma-1', 'pt-2']
+     */
     imageClasses: {
       type: Array,
       default: () => ([]),
+    },
+  },
+  computed: {
+    sourceTypes () {
+      return this.extensionExclusive
+        ? this.imageSources.filter(source => source === this.imageFileExtension)
+        : this.imageSources;
     },
   },
   methods: {
--- components/commons/generic-media-panel.vue
+++ components/commons/generic-media-panel.vue
@@ -4,26 +4,15 @@
     v-row(v-if="!centerMedia" justify="center").py-10
       //- Left column
       v-col(:align-self="alignLeftColumn" cols="12" :md="colsLeft" :offset-md="offsetColsLeft")
-        picture(v-if="!$isMobile && contentAlignRight")
-          source(
-            v-if="fileExtension !== '.png'"
-            :srcset="getSrcsetValue('.webp')"
-            :alt="header || 'media-image'"
-            :width="webImageWidth"
-            type="image/webp"
-          )
-          source(
-            v-if="fileExtension !== '.webp'"
-            :srcset="getSrcsetValue('.png')"
-            :alt="header || 'media-image'"
-            :width="webImageWidth"
-            type="image/png"
-          )
-          img(
-            v-lazy="webImagePath"
-            :alt="header || 'media-image'"
-            :width="webImageWidth"
-          )
+        picture-source(
+          v-if="!$isMobile && contentAlignRight"
+          :customPath="customImagePath"
+          :extensionExclusive="extensionExclusive"
+          :image="webImage"
+          :imageAlt="header || 'media-image'"
+          :imageFileExtension="fileExtension"
+          :imageWidth="webImageWidth"
+        )
         template(v-if="contentAlignLeft")
           h1.font-30.lh-title.pb-3.font-weight-light {{header}}
           br
@@ -33,26 +22,14 @@
           slot(name="additional-content")
       //- Right Column
       v-col(:align-self="alignRightColumn" cols="12" :md="colsRight" :offset-md="offsetColsRight")
-        picture(v-if="!$isMobile && contentAlignLeft")
-          source(
-            v-if="fileExtension !== '.png'"
-            :srcset="getSrcsetValue('.webp')"
-            :alt="header || 'media-image'"
-            :width="webImageWidth"
-            type="image/webp"
-          )
-          source(
-            v-if="fileExtension !== '.webp'"
-            :srcset="getSrcsetValue('.png')"
-            :alt="header || 'media-image'"
-            :width="webImageWidth"
-            type="image/png"
-          )
-          img(
-            v-lazy="webImagePath"
-            :alt="header || 'media-image'"
-            :width="webImageWidth"
-          )
+        picture-source(
+          v-if="!$isMobile && contentAlignLeft"
+          :customPath="customImagePath"
+          :image="webImage"
+          :imageAlt="header || 'media-image'"
+          :imageFileExtension="fileExtension"
+          :imageWidth="webImageWidth"
+        )
         template(v-if="contentAlignRight")
           h1.font-30.lh-title.pb-3.font-weight-light {{header}}
           br
@@ -61,165 +38,204 @@
             br
           slot(name="additional-content")
       //- Mobile Image
-      picture(v-if="$isMobile && !hideImageMobile")
-        source(
-          v-if="fileExtension !== '.png'"
-          :srcset="getSrcsetValue('.webp')"
-          :alt="header || 'media-image'"
-          :width="webImageWidth"
-          type="image/webp"
-        )
-        source(
-          v-if="fileExtension !== '.webp'"
-          :srcset="getSrcsetValue('.png')"
-          :alt="header || 'media-image'"
-          :width="mobileImageWidth"
-          type="image/png"
-        )
-        img(
-          v-lazy="mobileImagePath"
-          :alt="header || 'media-image'"
-          :width="mobileImageWidth"
-        )
+      picture-source(
+        v-if="$isMobile && !hideImageMobile"
+        :customPath="customImagePath"
+        :extensionExclusive="extensionExclusive"
+        :image="mobileImage || webImage"
+        :imageAlt="header || 'media-image'"
+        :imageFileExtension="fileExtension"
+        :imageWidth="mobileImageWidth"
+      )
     //- CENTER VIEW
     v-row(v-else justify="center").py-10
       v-col(cols="12" md="10" :class="{'text-center': !$isMobile}")
         h1.font-30.lh-title.pb-3.font-weight-light {{header}}
-        picture(v-if="!$isMobile")
-          source(
-            v-if="fileExtension !== '.png'"
-            :srcset="getSrcsetValue('.webp')"
-            :alt="header || 'media-image'"
-            :width="webImageWidth"
-            type="image/webp"
-          )
-          source(
-            v-if="fileExtension !== '.webp'"
-            :srcset="getSrcsetValue('.png')"
-            :alt="header || 'media-image'"
-            :width="webImageWidth"
-            type="image/png"
-          )
-          img(
-            v-lazy="webImagePath"
-            :alt="header || 'media-image'"
-            :width="webImageWidth"
-          )
+        picture-source(
+          v-if="$isMobile"
+          :customPath="customImagePath"
+          :extensionExclusive="extensionExclusive"
+          :image="webImage"
+          :imageAlt="header || 'media-image'"
+          :imageFileExtension="fileExtension"
+          :imageWidth="webImageWidth"
+        )
         br
         template(v-if="descriptions.length")
           p(v-for="(description, key) in descriptions" :key="key").font-16.mt-3.font-gray {{description}}
           br
         slot(name="additional-content")
-        picture(v-if="$isMobile && !hideImageMobile")
-          source(
-            v-if="fileExtension !== '.png'"
-            :srcset="getSrcsetValue('.webp')"
-            :alt="header || 'media-image'"
-            :width="mobileImageWidth"
-            type="image/webp"
-          )
-          source(
-            v-if="fileExtension !== '.webp'"
-            :srcset="getSrcsetValue('.png')"
-            :alt="header || 'media-image'"
-            :width="mobileImageWidth"
-            type="image/png"
-          )
-          img(
-            v-lazy="mobileImagePath"
-            :alt="header || 'media-image'"
-            :width="mobileImageWidth"
-          )
+        picture-source(
+          v-if="!$isMobile && !hideImageMobile"
+          :customPath="customImagePath"
+          :extensionExclusive="extensionExclusive"
+          :image="mobileImage || webImage"
+          :imageAlt="header || 'media-image'"
+          :imageFileExtension="fileExtension"
+          :imageWidth="mobileImageWidth"
+        )
 </template>
 
 <script>
+// components
+import PictureSource from './PictureSource';
+
 export default {
+  components: { PictureSource },
   props: {
+    /**
+     * Alignment of left column
+     * @type {String}
+     */
     alignLeftColumn: {
       type: String,
       default: 'center',
     },
+    /**
+     * Alignment of right column
+     * @type {String}
+     */
     alignRightColumn: {
       type: String,
       default: 'center',
     },
+    /**
+     * If passed file extension is the only source to use
+     * @type {Boolean}
+     */
+    extensionExclusive: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * Panel will have media centered instead of two columns
+     * @type {Boolean}
+     */
     centerMedia: {
       type: Boolean,
       default: false,
     },
+    /**
+     * Number of cols of the left column
+     * @type {String}
+     */
     colsLeft: {
       type: String,
       default: '5',
     },
+    /**
+     * Number of cols of the right column
+     * @type {String}
+     */
     colsRight: {
       type: String,
       default: '6',
     },
+    /**
+     * Number of offset-cols of the left column
+     * @type {String}
+     */
     offsetColsLeft: {
       type: String,
       default: '0',
     },
+    /**
+     * Number of offset-cols of the right column
+     */
     offsetColsRight: {
       type: String,
       default: '0',
     },
+    /**
+     * Text will be at the left side
+     * @type {Boolean}
+     */
     contentAlignLeft: {
       type: Boolean,
       default: false,
     },
+    /**
+     * Text will be at the right side
+     * @type {Boolean}
+     */
     contentAlignRight: {
       type: Boolean,
       default: false,
     },
+    /**
+     * Custom path from the root images folder
+     * @type {String}
+     * @example features/
+     *  - will become ~/assets/images/features/
+     */
     customImagePath: {
       type: String,
       default: '',
     },
+    /**
+     * Content paragraphs
+     * @type {string []}
+     */
     descriptions: {
       type: Array,
       default: () => ([]),
     },
+    /**
+     * Panel header
+     * @type {String}
+     */
     header: {
       type: String,
       default: '',
     },
+    /**
+     * File name of image for web view (without file extension)
+     * @type {String}
+     */
     webImage: {
       type: String,
       required: true,
     },
+    /**
+     * Width of web image
+     * @type {String}
+     */
     webImageWidth: {
       type: String,
       default: '100%',
     },
+    /**
+     * File name of image for mobile view (without file extension)
+     * @type {String}
+     */
     mobileImage: {
       type: String,
       default: undefined,
     },
+    /**
+     * Width of mobile image
+     * @type {String}
+     */
     mobileImageWidth: {
       type: String,
       default: '100%',
     },
+    /**
+     * Hides image in mobile view
+     * @type {Boolean}
+     */
     hideImageMobile: {
       type: Boolean,
       default: false,
     },
+    /**
+     * File extension of the image file
+     * @type {String}
+     * @example .webp
+     */
     fileExtension: {
       type: String,
       default: '.webp',
-    },
-  },
-  computed: {
-    webImagePath () {
-      return require(`~/assets/images/${this.customImagePath}${this.webImage}${this.fileExtension}`);
-    },
-    mobileImagePath () {
-      const image = this.mobileImage || this.webImage;
-      return require(`~/assets/images/${this.customImagePath}${image}${this.fileExtension}`);
-    },
-  },
-  methods: {
-    getSrcsetValue (fileExtension) {
-      return require(`~/assets/images/${this.customImagePath}${this.webImage}${this.fileExtension}`);
     },
   },
 };
