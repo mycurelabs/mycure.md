f9818ad89c4687f0269cc259219782076904fe0f
Jason Jamora <jamorajj@gmail.com>
2020-06-23 17:34:54 +0800

    refactor: remove unneeded code on index

--- pages/clinics/index.vue
+++ pages/clinics/index.vue
@@ -34,28 +34,6 @@
     doctors(
       :doctors="doctors"
     )
-    //- panel-1(
-    //-   :pic-url="picURL"
-    //-   :full-name="fullNameWithSuffixes"
-    //-   :bio="bio"
-    //-   :specialties="specialties"
-    //-   :professions="professions"
-    //-   :practicing-since="practicingSince"
-    //- )
-    //- services(
-    //-   :services="services"
-    //- )
-    //- tabs(
-    //-   :clinics="clinics"
-    //-   :bio="bio"
-    //-   :specialties="specialties"
-    //-   :professions="professions"
-    //-   :practicing-since="practicingSince"
-    //-   :education="education"
-    //- )
-    //- social(
-    //-   :name="name"
-    //- )
     v-divider
     v-footer(
       height="auto"
@@ -68,48 +46,21 @@
 </template>
 
 <script>
-import PictureSource from '~/components/commons/PictureSource';
-// import _ from 'lodash';
-// import { getDoctorWebsite, getDoctorClinics, recordWebsiteVisit } from '~/utils/axios';
-// import { formatName } from '~/utils/formats';
 import headMeta from '~/utils/head-meta';
 import AppBar from '~/components/clinic-website/app-bar';
 import Panel1 from '~/components/clinic-website/panel-1';
 import Services from '~/components/clinic-website/services';
 import ClinicInfo from '~/components/clinic-website/clinic-info';
 import Doctors from '~/components/clinic-website/doctors';
-// import Services from '~/components/doctor-website/services';
-import Tabs from '~/components/doctor-website/tabs';
-import Social from '~/components/doctor-website/social';
 export default {
   layout: 'clinic-website',
   components: {
-    PictureSource,
     AppBar,
     Panel1,
     Services,
     ClinicInfo,
     Doctors,
-    Tabs,
-    Social,
   },
-  // async asyncData ({ app, router, params, error }) {
-  //   try {
-  //     const doctor = await getDoctorWebsite({ username: params.id });
-  //     console.warn('doctor', doctor);
-  //     if (_.isEmpty(doctor)) {
-  //       error({ statusCode: 404, message: 'doctor-not-found' });
-  //     }
-  //     const clinics = await getDoctorClinics({ uid: doctor.id });
-  //     console.warn('clinics', clinics);
-  //     return {
-  //       doctor,
-  //       clinics: clinics || [],
-  //     };
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-  // },
   data () {
     return {
       selectedTab: 'clinics',
@@ -202,53 +153,6 @@ export default {
         },
       ],
     };
-  },
-  computed: {
-    // picURL () {
-    //   const sex = this.doctor?.sex;
-    //   if (sex === 'female') {
-    //     return this.doctor?.picURL || require('~/assets/images/doctor-website/doctor-website-profile-female.png');
-    //   }
-    //   return this.doctor?.picURL || require('~/assets/images/doctor-website/doctor-website-profile-male.png');
-    // },
-    // name () {
-    //   return this.doctor?.name;
-    // },
-    // fullName () {
-    //   return formatName(this.doctor?.name || {}, 'firstName middleInitial lastName generationalSuffix');
-    // },
-    // fullNameWithSuffixes () {
-    //   return [
-    //     this.fullName,
-    //     ...this.professions,
-    //     this.doctor?.name.academicSuffix,
-    //     this.doctor?.name.professionalSuffix,
-    //   ].filter(Boolean).join(', ');
-    // },
-    // bio () {
-    //   return this.doctor?.doc_bio || ''; // eslint-disable-line
-    // },
-    // specialties () {
-    //   return this.doctor?.doc_specialties || []; // eslint-disable-line
-    // },
-    // professions () {
-    //   return this.doctor?.doc_professions || []; // eslint-disable-line
-    // },
-    // practicingSince () {
-    //   return this.doctor?.doc_practicingSince; // eslint-disable-line
-    // },
-    // services () {
-    //   return this.doctor?.doc_services; // eslint-disable-line
-    // },
-    // education () {
-    //   return this.doctor?.educations;
-    // },
-  },
-  async mounted () {
-    // if (this.$route.query.audience === 'self') {
-    //   return;
-    // };
-    // await recordWebsiteVisit({ uid: this.doctor.id });
   },
   head () {
     // TODO: update meta tags
