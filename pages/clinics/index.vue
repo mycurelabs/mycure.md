b625bafd48630d36cbfed7e40ec0b077557ac144
Jason Jamora <jamorajj@gmail.com>
2020-06-23 13:46:19 +0800

    feat: clinic website services initial

--- pages/clinics/index.vue
+++ pages/clinics/index.vue
@@ -6,6 +6,7 @@
     // TODO: clinic info
     // TODO: doctors list
     // TODO: about clinic
+    // TODO: social panel
     // TODO: final cta
     // TODO: footer -- done
     app-bar
@@ -18,6 +19,52 @@
       clinic-copy="Copy of the clinic. Ex. The best clinic since 1945"
       clinic-tagline="Up to 50 characters plus a tagline up to well 60 characters."
     )
+    v-container(fluid).my-10
+      v-row(justify="center" align="center" :class="{ 'mb-10': !$isMobile }")
+        h1 We offer this healthcare services for you.
+      v-row(v-if="!$isMobile" justify="center" align="center").px-10
+        v-col(cols="12" md="4" align-self="start")
+          v-list(two-line).text-left
+            v-list-item(
+              v-for="(item, key) in servicesColumn1"
+              :key="key"
+            )
+              v-list-item-action
+                img(src="~/assets/images/mycure-web-bullet-check.png" alt="Check icon")
+              v-list-item-content
+                span {{ item }}
+        v-col(v-if="showTwoColumn || showThreeColumn" cols="12" md="4" align-self="start")
+          v-list(two-line).text-left
+            v-list-item(
+              v-for="(item, key) in servicesColumn2"
+              :key="key"
+            )
+              v-list-item-action
+                img(src="~/assets/images/mycure-web-bullet-check.png" alt="Check icon")
+              v-list-item-content
+                span {{ item }}
+        v-col(v-if="showThreeColumn" cols="12" md="4" align-self="start")
+          v-list(two-line).text-left
+            v-list-item(
+              v-for="(item, key) in servicesColumn3"
+              :key="key"
+            )
+              v-list-item-action
+                img(src="~/assets/images/mycure-web-bullet-check.png" alt="Check icon")
+              v-list-item-content
+                span {{ item }}
+      v-row(v-if="$isMobile" justify="center" align="center").px-10
+        v-col(cols="12")
+          v-list(two-line).text-left
+            v-list-item(
+              v-for="(item, key) in services"
+              :key="key"
+            )
+              v-list-item-action
+                img(src="~/assets/images/mycure-web-bullet-check.png" alt="Check icon")
+              v-list-item-content
+                span {{ item }}
+    v-divider
     //- panel-1(
     //-   :pic-url="picURL"
     //-   :full-name="fullNameWithSuffixes"
@@ -92,9 +139,64 @@ export default {
   data () {
     return {
       selectedTab: 'clinics',
+      services: [
+        'Obstetrics and Gynecology',
+        'Pediatrics and Neonatology',
+        'Internal Medicine',
+        'Cardiology',
+        'Pulmonology',
+        'General Surgery',
+        'Pathology, Radiology, and Sonology',
+        'Endocrinology',
+        'Gastroenterology',
+      ],
     };
   },
   computed: {
+    showOneColumn () {
+      return this.services.length < 4;
+    },
+    showTwoColumn () {
+      return this.services.length < 7 && this.services.length > 3;
+    },
+    showThreeColumn () {
+      return this.services.length >= 7;
+    },
+    columnCount () {
+      return this.showOneColumn ? '4' : this.showTwoColumn ? '4' : '4';
+    },
+    columnOffsetCount () {
+      return this.showOneColumn ? '4' : this.showTwoColumn ? '2' : '';
+    },
+    servicesColumn1 () {
+      if (this.showOneColumn) {
+        return this.services;
+      }
+
+      if (this.showTwoColumn) {
+        return this.services.filter((v, i) => !(i % 2));
+      }
+
+      return this.services.filter((v, i) => !(i % 3));
+    },
+    servicesColumn2 () {
+      if (this.showOneColumn) {
+        return false;
+      }
+
+      if (this.showTwoColumn) {
+        return this.services.filter((v, i) => (i % 2));
+      }
+
+      return this.services.filter((v, i) => (i % 3 === 1));
+    },
+    servicesColumn3 () {
+      if (this.showThreeColumn) {
+        return this.services.filter((v, i) => (i % 3 > 1));
+      }
+
+      return false;
+    },
     // picURL () {
     //   const sex = this.doctor?.sex;
     //   if (sex === 'female') {
