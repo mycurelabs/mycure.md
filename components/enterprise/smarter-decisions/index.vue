1052d7eac52160567a50eddf447971681278f23c
Gerald Canonizado <gerald35canonizado@gmail.com>
2020-06-25 11:11:17 -0800

    refactor: change images to checklist style
    resolves #551

--- components/enterprise/smarter-decisions/index.vue
+++ components/enterprise/smarter-decisions/index.vue
@@ -13,16 +13,16 @@
         v-for="(data, key) in fifthPanelContent"
         :key="key"
       ).item-container
-        v-col(
-          cols="6"
-          md="4"
-        )
-          img(v-lazy="require(`~/assets/images/enterprise/${data.image}`)" :alt="data.imageTitle" width="100%")
-        v-col(
-          cols="6"
-          md="8"
-        ).item-description
-          p.text-center.font-16 {{data.imageTitle}}
+        img(:src="require(`~/assets/images/${data.image}`)" :alt="data.imageTitle")
+        span.pl-3.font-16 {{data.imageTitle}}
+        //- v-col(
+        //-   cols="6"
+        //-   md="4"
+        //- )
+        //- v-col(
+        //-   cols="6"
+        //-   md="8"
+        //- ).item-description
 </template>
 
 <script>
@@ -45,97 +45,15 @@ export default {
 }
 .item-container {
   position: relative;
-  min-height: 175px;
+  min-height: 85px;
+  background-color: red;
 }
-.item-description {
+/* .item-description {
   position: absolute;
   margin-left: auto;
   margin-right: auto;
   left: 0;
   right: 0;
   top: 60%;
-}
-@media screen and (device-width: 360px) {
-  .item-container {
-    min-height: 250px;
-  }
-  .item-description {
-    top: 30%;
-  }
-}
-@media screen and (device-width: 375px) {
-  .item-container {
-    min-height: 250px;
-  }
-  .item-description {
-    top: 30%;
-  }
-}
-/* Pixel 2/XL */
-@media only screen
-  and (device-width: 411px)
-  and (device-height: 731px)
-  and (-webkit-min-device-pixel-ratio: 2) {
-  .item-container {
-    min-height: 250px;
-  }
-  .item-description {
-    top: 35%;
-  }
-}
-@media only screen
-  and (device-width: 411px)
-  and (device-height: 823px)
-  and (-webkit-min-device-pixel-ratio: 2) {
-  .item-container {
-    min-height: 260px;
-  }
-  .item-description {
-    top: 35%;
-  }
-}
-/* iphone 5/SE */
-@media only screen
-  and (min-device-width: 320px)
-  and (max-device-width: 568px)
-  and (-webkit-min-device-pixel-ratio: 2) {
-  .item-container {
-    min-height: 225px;
-  }
-  .item-description {
-    top: 30%;
-  }
-}
-/* iphone 6/7/8 */
-@media only screen
-  and (min-device-width: 375px)
-  and (max-device-width: 667px)
-  and (-webkit-min-device-pixel-ratio: 2) {
-  .item-container {
-    min-height: 240px;
-  }
-  .item-description {
-    top: 35%;
-  }
-}
-@media screen and (device-width: 768px) {
-  .item-container {
-    min-height: 300px;
-  }
-}
-@media screen and (device-width: 1920px) {
-  .item-container {
-    min-height: 300px;
-  }
-}
-@media screen and (device-width: 2304px) {
-  .item-container {
-    min-height: 300px;
-  }
-}
-@media screen and (device-width: 2560px) {
-  .item-container {
-    min-height: 300px;
-  }
-}
+} */
 </style>
