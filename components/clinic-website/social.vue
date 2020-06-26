ccec58fefd142cdfb2b3563f5bc0b99ec60608bb
Gerald Canonizado <gerald35canonizado@gmail.com>
2020-06-26 15:16:32 -0800

    refactor: arrange social icons in mobile view

--- components/clinic-website/social.vue
+++ components/clinic-website/social.vue
@@ -1,11 +1,14 @@
 <template lang="pug">
   v-container.pt-6
     v-row(justify="center" align="center" v-for="(data, key) in social" :key="key")
-      v-col(cols="12" md="6" justify="center" align="center").panel-content
+      v-col(cols="12" md="7" justify="center" align="center").panel-content
         p.pt-1 {{data.title}}
-        template(v-for="(data, key) in data.icons" align-self="center")
+        template(v-if="!$isMobile" v-for="(data, key) in data.icons")
           a(:href="data.link" target="_blank" rel="noopener noreferrer")
             v-icon(:class="paddingClasses").font-30.primary--text.pl-4 {{data.icon}}
+      v-col(v-if="$isMobile" v-for="(icon, key) in data.icons" :key="key" align="center")
+        a(:href="icon.link" target="_blank" rel="noopener noreferrer")
+          v-icon(:class="paddingClasses").font-30.primary--text.mt-n12 {{icon.icon}}
 </template>
 
 <script>
