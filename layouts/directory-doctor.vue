a9b16a515e79c4b13320514909b6f3df7b239d99
Jason Jamora <jamorajj@gmail.com>
2020-06-27 08:57:38 +0800

    feat: create starting point for doctor directory

---
+++ layouts/directory-doctor.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  v-app
+    nuxt
+</template>
+
+<style>
+.font-64 {
+  font-size: 64px !important;
+}
+.font-32 {
+  font-size: 32px !important;
+}
+.font-26 {
+  font-size: 26px !important;
+}
+.font-20 {
+  font-size: 20px !important;
+}
+
+.letter-spacing-normal {
+  letter-spacing: normal !important;
+}
+</style>
