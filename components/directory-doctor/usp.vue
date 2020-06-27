83d8fa24893fd6a301ebe3082f3a92026e260bfd
Gerald Canonizado <gerald35canonizado@gmail.com>
2020-06-27 11:20:40 -0800

    feat: add usp search bar

--- components/directory-doctor/usp.vue
+++ components/directory-doctor/usp.vue
@@ -1,8 +1,20 @@
 <template lang="pug">
-  v-container(fluid)
+  v-container(fluid).usp-panel
     v-row(justify="center" align="center")
       v-col(cols="12" md="10" v-for="(data, key) in uspInfo" :key="key")
-        h1.text-center.font-weight-bold.font-50 {{data.title}}
+        h1(:class="titleSizeClass").text-center.font-weight-bold {{data.title}}
+      v-col(cols="12" md="6")
+        v-text-field(
+          label="Search Doctor, Specialization"
+          append-icon="mdi-magnify"
+          v-model="searchItem"
+          @click:append="searchDoctorSpecialization()"
+          @keydown.enter="searchDoctorSpecialization()"
+          @click:clear="isSearching = false"
+          :loading="isLoading"
+          clearable
+          outlined
+        )
 </template>
 
 <script>
@@ -11,6 +23,26 @@ export default {
     uspInfo: {
       type: Array,
       default: () => ([]),
+    },
+  },
+  data () {
+    return {
+      searchItem: '',
+      isSearching: false,
+      isLoading: false,
+    };
+  },
+  computed: {
+    titleSizeClass () {
+      return [this.$isMobile ? 'font-34' : 'font-50'];
+    },
+  },
+  methods: {
+    searchDoctorSpecialization () {
+      this.isLoading = true;
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 2000);
     },
   },
 };
