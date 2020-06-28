8f462aeec7ad52b5f8c24a226c43607ef13141d3
Gerald Canonizado <gerald35canonizado@gmail.com>
2020-06-27 19:09:00 -0800

    feat: add category carousel

---
+++ components/directory-doctor/category.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  v-container(fluid).py-12
+    v-row(justify="center" align="center")
+      v-cols(cols="12" md="4")
+        h1 I need a doctor...
+        v-carousel(hide-delimiters continuous).py-10
+          v-carousel-item(cols="12" v-for="(data, key) in cardItems" :key="key")
+            v-card(width="300" color="indigo lighten-3").text-center
+              div.py-5
+                img(:src="require(`~/assets/images/directory-doctor/${data.image}`)")
+                h2 {{data.title}}
+                p {{data.subTitle}}
+</template>
+
+<script>
+export default {
+  props: {
+    /**
+     * Array of doctor objects
+     * @type {Array}
+     */
+    cardItems: {
+      type: Array,
+      default: () => ([]),
+    },
+  },
+  data () {
+    return {
+      key: 0,
+    };
+  },
+//   computed: {
+//     cardColor () {
+//       return [this.key === 0 ? 'indigo lighten-3' : 'teal lighten-3'];
+//     },
+//   },
+};
+</script>
+
+<style scoped>
+</style>
--- pages/directory/doctors/index.vue
+++ pages/directory/doctors/index.vue
@@ -1,36 +1,14 @@
 <template lang="pug">
   v-container(fluid)
     app-bar
-    panel-1(
-      pic-url="MYCURE-virtual-clinic-healthcare-practice-online-enterprise-F-multi-specialty"
-      pic-extension=".webp"
-      pic-alt="Clinic Image"
-      pic-custom-path="enterprise/"
-      clinic-copy="Copy of the clinic. Ex. The best clinic since 1945"
-      clinic-tagline="Up to 50 characters plus a tagline up to well 60 characters."
-    )
-    services(
-      header-text="We offer this healthcare services for you."
-      :services="services"
-    )
-    v-divider
-    clinic-info(
-      :schedules="schedules"
-      :rates="rates"
-      clinic-name="MYCURE Virtual Clinic"
-      clinic-address="1036, Delos Santos STI Medical Center 201 E. Rodriguez Avenue Quezon City"
-    )
-    v-divider
-    doctors(
-      :doctors="doctors"
-      :specializations="sortItems"
-      :sort-by="filterItems"
-    )
     //- Usp
     usp(:uspInfo="usp" :specialization-items="specializations")
 
     //- Sign Up
     sign-me-up(:signUpInfo="signMeUp")
+
+    //- Category
+    category(:cardItems="categoryItems")
 
     //- About panel
     about-clinic(:about="aboutInfo")
@@ -56,26 +34,17 @@
 
 <script>
 import {
-  SERVICES_LIST,
-  SCHEDULES_LIST,
-  RATES,
-  DOCTORS_LIST,
   USP,
   SPECIALIZATIONS,
   SIGN_ME_UP,
+  CATEGORY,
   ABOUT_INFO,
   SOCIAL_ITEM,
-  FILTER_ITEMS,
-  SORT_ITEMS,
 } from './directory-content';
 
 import headMeta from '~/utils/head-meta';
-import AppBar from '~/components/clinic-website/app-bar';
-import Panel1 from '~/components/clinic-website/panel-1';
-import Services from '~/components/clinic-website/services';
-import ClinicInfo from '~/components/clinic-website/clinic-info';
-import Doctors from '~/components/clinic-website/doctors';
 import Usp from '~/components/directory-doctor/usp';
+import Category from '~/components/directory-doctor/category';
 import SignMeUp from '~/components/directory-doctor/sign-me-up';
 import AboutClinic from '~/components/directory-doctor/about-clinic';
 import Social from '~/components/directory-doctor/social';
@@ -83,29 +52,20 @@ import Cta from '~/components/directory-doctor/final-cta';
 export default {
   layout: 'directory-doctor',
   components: {
-    AppBar,
-    Panel1,
-    Services,
-    ClinicInfo,
-    Doctors,
     Usp,
     SignMeUp,
+    Category,
     AboutClinic,
     Social,
     Cta,
   },
   data () {
-    this.services = SERVICES_LIST;
-    this.schedules = SCHEDULES_LIST;
-    this.rates = RATES;
-    this.doctors = DOCTORS_LIST;
     this.usp = USP;
     this.specializations = SPECIALIZATIONS;
     this.signMeUp = SIGN_ME_UP;
+    this.categoryItems = CATEGORY;
     this.aboutInfo = ABOUT_INFO;
     this.socialItem = SOCIAL_ITEM;
-    this.filterItems = FILTER_ITEMS;
-    this.sortItems = SORT_ITEMS;
     return {};
   },
   mounted () {
