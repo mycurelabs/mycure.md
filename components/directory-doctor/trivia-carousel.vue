10a5afd5b1ae3e1feb070af6aa1afdff832d424d
Jason Jamora <jamorajj@gmail.com>
2020-06-29 15:46:03 +0800

    refactor: add mobile classes for carousel

--- components/directory-doctor/trivia-carousel.vue
+++ components/directory-doctor/trivia-carousel.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
-  v-container(fluid).primary.py-12
-    v-carousel(height="300" show-arrows-on-hover hide-delimiters)
+  v-container(fluid :class="{ 'py-12': !$isMobile }").primary
+    v-carousel(:height="$isMobile ? '250' : '300'" show-arrows-on-hover hide-delimiters)
       v-carousel-item(v-for="(slide, i) in slides" :key="i")
         v-row(align="center" justify="center" class="fill-height").text-center
-          div(style="width: 50%;")
-            h3.mb-4.font-32 {{ slide[0] }}
-            h1.mb-4.font-56.white--text {{ slide[1] }}
-            p.font-20.white--text {{ slide[2] }}
+          div(:class="containerClass")
+            h3(:class="headerClasses").mb-2 {{ slide[0] }}
+            h1(:class="contentClasses").mb-2.white--text {{ slide[1] }}
+            p(:class="sourceClasses").white--text {{ slide[2] }}
 </template>
 
 <script>
@@ -42,5 +42,37 @@ export default {
       ],
     };
   },
+  computed: {
+    containerClass () {
+      // return !this.$isMobile ? ['web-container'] : ['mobile-container'];
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs': return ['mobile-container'];
+        case 'sm': return ['mobile-container'];
+        case 'md': return ['tablet-container'];
+      }
+      return ['web-container'];
+    },
+    headerClasses () {
+      return !this.$isMobile ? ['font-32'] : ['font-24'];
+    },
+    contentClasses () {
+      return !this.$isMobile ? ['font-56'] : ['font-30'];
+    },
+    sourceClasses () {
+      return !this.$isMobile ? ['font-20'] : ['font-16'];
+    },
+  },
 };
 </script>
+
+<style scoped>
+.web-container {
+  width: 60%;
+}
+.tablet-container {
+  width: 75%;
+}
+.mobile-container {
+  width: 90%;
+}
+</style>
