eb75fe33bf5d080f315af7defce71eb1403c56a9
Jason Jamora <jamorajj@gmail.com>
2020-06-29 15:45:46 +0800

    style: add font sizes for mobile

--- layouts/directory-doctor.vue
+++ layouts/directory-doctor.vue
@@ -13,11 +13,20 @@
 .font-32 {
   font-size: 32px !important;
 }
+.font-30 {
+  font-size: 30px !important;
+}
 .font-26 {
   font-size: 26px !important;
 }
+.font-24 {
+  font-size: 24px !important;
+}
 .font-20 {
   font-size: 20px !important;
+}
+.font-16 {
+  font-size: 16px !important;
 }
 
 .letter-spacing-normal {
