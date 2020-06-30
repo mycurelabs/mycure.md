b67f1ede03068cdde4c4642789a6a8e7bef8348f
Gerald Canonizado <gerald35canonizado@gmail.com>
2020-06-29 19:42:29 -0800

    refactor: delete unused classes

--- components/directory-doctor/sign-me-up.vue
+++ components/directory-doctor/sign-me-up.vue
@@ -8,7 +8,7 @@
               h2.white--text.py-5 {{data.title}}
               span(v-html="data.subTitle")
               v-card-actions.pt-5
-                v-btn(color="accent" :width="$isMobile ? '' : '135'").ml-n2
+                v-btn(color="accent" width="135").ml-n2
                   strong {{data.btnTxt}}
       v-col(v-if="!$isMobile" cols="12" md="6")
         v-card(v-for="(data, key) in data.consult" :key="key")
@@ -17,17 +17,17 @@
               h2.py-5 {{data.title}}
               span(v-html="data.subTitle")
               v-card-actions.pt-5
-                v-btn(color="amber" :width="$isMobile ? '' : '135'").ml-n2
+                v-btn(color="amber" width="135").ml-n2
                   strong.white--text {{data.btnTxt}}
       v-row(v-if="$isMobile" no-gutters)
-        v-col(cols="12" v-for="(data, key) in data.doctor" :key="key").text-center.bg-color-doctor.py-12
+        v-col(cols="12" v-for="(data, key) in data.doctor" :key="'doctor' + key").text-center.bg-color-doctor.py-12
           h2.white--text {{data.title}}
           img(:src="require(`~/assets/images/directory-doctor/mycure-sign-up-doctor-cms-blue-banner-mobile.png`)" width="100%").pb-5
           span.font-gray(v-html="data.subTitle")
           br
           v-btn(color="accent").mt-5
             strong {{data.btnTxt}}
-        v-col(cols="12" v-for="(data, key) in data.consult" :key="key").text-center.bg-color-px.py-12
+        v-col(cols="12" v-for="(data, key) in data.consult" :key="'patient' + key").text-center.bg-color-px.py-12
           h2 {{data.title}}
           img(:src="require(`~/assets/images/directory-doctor/mycure-sign-up-patient-portal-orange-banner-mobile.png`)" width="100%").pb-5
           span.font-gray(v-html="data.subTitle")
@@ -57,7 +57,7 @@ export default {
   width: 100vw;
 }
 .bg-color-px {
-  background-color: #fce4ec;
+  background-color: #ffebee;
   width: 100vw;
 }
 </style>
