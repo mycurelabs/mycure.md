69ef35225adaacefbea744ec8dc132e0dc0380ed
Gerald Canonizado <gerald35canonizado@gmail.com>
2020-07-01 11:57:17 -0800

    refactor: adjust alignment of enterprise 8th panel

--- components/enterprise/keep-employees/index.vue
+++ components/enterprise/keep-employees/index.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   v-container#corporate-clinics.py-12.panel-keep-employees
     v-row(justify="center" align="center")
-      v-col(cols="12" md="5" align-self="center" v-if="$isMobile").eightPanel-description
+      v-col(cols="12" md="5" align-self="center" v-if="$isMobile")
         p.font-16.mt-3.font-gray.primary--text {{eightPanel.subHeader}}
         h1.font-30.lh-title.pb-3.font-weight-light {{eightPanel.header}}
         p.text-justify.font-16.mt-3.font-gray {{eightPanel.description}}
@@ -9,14 +9,14 @@
         v-btn(text @click="onGetStarted").mt-5.ml-n4.get-started-btn
           strong.text-capitalize.primary--text {{eightPanel.btnTxt}}
           v-icon.primary--text {{eightPanel.btnIcon}}
-      v-col(cols="12" md="6" justify="center" align="center")
+      v-col(cols="12" md="6" justify="center" align="center").panel-image
         picture-source(
           customPath="enterprise/"
           :image="eightPanel.image"
           :imageAlt="eightPanel.header"
           imageFileExtension=".webp"
-        ).eightPanel-image
-      v-col(cols="12" md="5" align-self="center" v-if="!$isMobile").eightPanel-description
+        )
+      v-col(cols="12" md="5" align-self="center" v-if="!$isMobile")
         p.font-16.mt-3.font-gray.primary--text {{eightPanel.subHeader}}
         h1.font-30.lh-title.pb-3.font-weight-light {{eightPanel.header}}
         p.text-justify.font-16.mt-3.font-gray {{eightPanel.description}}
@@ -57,15 +57,14 @@ a {
   position: relative;
   z-index: 2;
 }
-@media screen and (device-width: 1024px) {
-  .eightPanel-image {
-    margin-left: -20%;
-    width: 100%;
+@media screen and (min-width: 1024px) {
+  .panel-image {
+    margin-right: 4%;
   }
 }
-@media screen and (min-width: 1366px) {
-  .eightPanel-description {
-    margin-left: 55px;
+@media screen and (min-width: 1920px) {
+  .panel-image {
+    margin-right: 5%;
   }
 }
 </style>
