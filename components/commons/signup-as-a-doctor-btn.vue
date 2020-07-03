10e3aaabe70dc36c9cde5b421bb643e8886778fb
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-07-04 03:02:49 +0800

    feat: create reusable signup as a doctor btn

---
+++ components/commons/signup-as-a-doctor-btn.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  v-btn(
+    v-bind="bindings"
+    :class="[...defaultClasses, ...extraClasses]"
+  ) A Doctor
+</template>
+
+<script>
+export default {
+  props: {
+    extraBindings: {
+      type: Object,
+      default: () => ({}),
+    },
+    extraClasses: {
+      type: Array,
+      default: () => ([]),
+    },
+  },
+  computed: {
+    href () {
+      return process.browser && `${window.location.origin}/signup/individual`;
+    },
+    defaultBindings () {
+      return {
+        color: '#8db738',
+        depressed: true,
+        href: this.href,
+      };
+    },
+    bindings () {
+      return {
+        ...this.defaultBindings,
+        ...this.extraBindings,
+      };
+    },
+    defaultClasses () {
+      return [
+        'text-none',
+        'white--text',
+      ];
+    },
+  },
+};
+</script>
