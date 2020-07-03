8bc923e7cd2df413dc6e582ad0dcf40ae963b148
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-07-04 03:03:03 +0800

    refactor: improve app bar

--- components/directory-doctor/app-bar.vue
+++ components/directory-doctor/app-bar.vue
@@ -1,52 +1,56 @@
 <template lang="pug">
-  v-app-bar(flat color="white")
-    nuxt-link(:to="{ name: 'index' }" title="MYCURE | Clinic Management System | Cloud EMR Philippines" id="toolbar-mycure-logo")
-      img(
-        height="45"
-        src="~/assets/images/mycure-header-logo.png"
-        to="/"
-      )
-    v-spacer
-    template(v-if="!$isMobile")
-      h4 Sign up as:
-      v-btn(
-        width="110"
-        color="#8db738"
-        elevation="0"
-        :href="doctorSignUpUrl"
-      ).text-none.letter-spacing-normal.white--text.ml-6.px-8 A Doctor
-      v-btn(
-        width="110"
-        color="#edba42"
-        elevation="0"
-        :href="patientSignUpUrl"
-      ).text-none.letter-spacing-normal.white--text.ml-6.px-8 A Patient
-    template(v-else)
-      v-menu(bottom left)
-        template(v-slot:activator="{ on, attrs }")
-          v-btn(icon v-bind="attrs" v-on="on")
-            v-icon mdi-dots-vertical
-        v-list
-          v-list-item
-            h5 Sign up as:
-          v-list-item
-            v-btn(
-              width="110"
-              color="#8db738"
-              elevation="0"
-              :href="doctorSignUpUrl"
-            ).text-none.letter-spacing-normal.white--text A Doctor
-          v-list-item
-            v-btn(
-              width="110"
-              color="#edba42"
-              elevation="0"
-              :href="patientSignUpUrl"
-            ).text-none.letter-spacing-normal.white--text A Patient
+  v-app-bar(
+    color="white"
+    elevate-on-scroll
+    fixed
+  )
+    generic-container
+      v-row(no-gutters align="center")
+        v-col.col-auto
+          nuxt-link(:to="{ name: 'index' }" title="MYCURE | Clinic Management System | Cloud EMR Philippines" id="toolbar-mycure-logo")
+            img(
+              height="45"
+              src="~/assets/images/mycure-header-logo.png"
+              to="/"
+            ).mt-2
+        v-col
+        v-col.col-auto
+          template(v-if="!$isMobile")
+            span.mr-1 #[b Sign up as:]
+            signup-as-a-doctor-btn(
+              :extra-classes="['mx-1']"
+            )
+            signup-as-a-patient-btn(
+              :extra-classes="['ml-1']"
+            )
+          template(v-else)
+            v-menu(bottom left)
+              template(v-slot:activator="{ on, attrs }")
+                v-btn(icon v-bind="attrs" v-on="on")
+                  v-icon mdi-menu
+              v-list(style="width: 300px")
+                v-list-item
+                  h5 Sign up as:
+                v-list-item
+                  signup-as-a-doctor-btn(
+                    :extra-bindings="{ block: true }"
+                  )
+                v-list-item
+                  signup-as-a-patient-btn(
+                    :extra-bindings="{ block: true }"
+                  )
 </template>
 
 <script>
+import GenericContainer from '~/components/commons/generic-container';
+import SignupAsADoctorBtn from '~/components/commons/signup-as-a-doctor-btn';
+import SignupAsAPatientBtn from '~/components/commons/signup-as-a-patient-btn';
 export default {
+  components: {
+    GenericContainer,
+    SignupAsADoctorBtn,
+    SignupAsAPatientBtn,
+  },
   props: {
     doctorSignUpUrl: {
       type: String,
