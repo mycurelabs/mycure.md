2bf5184e41992379a53621ef9ef7f670cf490631
Marc Almond Valleza <majvalleza@gmail.com>
2020-07-06 19:33:27 +0800

    refactor: update web navbar

--- components/commons/mc-navbar/web.vue
+++ components/commons/mc-navbar/web.vue
@@ -9,58 +9,33 @@
             v-toolbar(flat :color="appBarColor")
               nuxt-link(:to="{ name: 'index' }" title="MYCURE | Clinic Management System | Cloud EMR Philippines" id="toolbar-mycure-logo" @click.stop="handleMycureLogo").mr-3.mt-2
                 img(src="~/assets/images/MYCURE-virtual-clinic-healthcare-practice-online-logo.svg" width="140" alt="MYCURE logo")
-              v-spacer
               template(v-for="(item, key) in solutionsMenuItems")
-                v-menu(
-                  offset-y
-                  :left="item.position === 'left'"
-                  :right="item.position === 'right'"
-                  :nudge-right="item.position === 'right' ? '50': '0'"
-                  :value="item.position === 'right' ? professionalSubMenuOpen : patientSubMenuOpen"
-                  :max-width="item.position === 'right' ? '210' : '250'"
-                ).solutions-menu
+                v-btn(v-if="!item.subMenus" text @click="onNavLinkClick(item)")
+                  span.font-14.tab.text-none {{item.name}}
+                  span(v-if="item.new").ml-2.px-1.white--text.red.font-weight-bold.font-14.pill NEW
+                v-menu(offset-y v-else).solutions-menu
                   template(v-slot:activator="{ on }")
                     v-btn(
                       text
                       v-on="on"
-                      @click="handleSubMenuOpen(item)"
                     ).mx-1
                       span.font-14.tab.text-none {{item.name}}
                       v-icon(small) mdi-chevron-down
                   v-card
                     v-list
                       template(v-for="(subMenu, key) in item.subMenus")
-                        template(v-if="subMenu.underSubMenus === undefined")
-                          v-list-item(
-                            link
-                            dense
-                            @click="handleSubMenuClick(item, subMenu)"
-                          )
-                            span {{ subMenu.name }}
-                            span(v-show="subMenu.new").ml-2.px-1.white--text.red.font-weight-bold.font-14.pill NEW
-                        template(v-else)
-                          v-menu(offset-x)
-                            template(slot="activator" slot-scope="props")
-                              v-btn(
-                                text
-                                v-on="props.on"
-                                width="100%"
-                              ).font-16.d-block.d-flex.justify-space-between
-                                span.tab.text-none {{subMenu.name}}
-                                v-icon(small) mdi-chevron-right
-                            v-card
-                              v-list
-                                template(v-for="(underSubMenu, key) in subMenu.underSubMenus")
-                                  v-list-item(
-                                    link
-                                    dense
-                                    @click="handleSubMenuClick(subMenu, underSubMenu)"
-                                  ).px-4 {{ underSubMenu.name }}
+                        v-list-item(
+                          link
+                          dense
+                          @click="onNavLinkClick(subMenu)"
+                        )
+                          span {{ subMenu.name }}
+              v-spacer
               v-btn(
                 text
                 :to="{ name: loginURL }"
                 id="login-btn"
-                @click.stop="handleToolbarLinkClick('login-btn')"
+                @click.stop="onActionBtnClick('login-btn')"
               ).mr-2.ml-1
                 span.font-14.tab.text-none.font-weight-bold &nbsp;Login
               v-btn(
@@ -69,7 +44,7 @@
                 color="#98be58"
                 :to="currentRoute === 'doctors-clinics' ? { name: 'signup-individual' } : { name: 'signup-multispecialty' }"
                 id="start-free-btn"
-                @click.stop="handleToolbarLinkClick('start-free-btn')"
+                @click.stop="onActionBtnClick('start-free-btn')"
               )
                 strong.font-14.white--text.tab.text-none Get Started
               v-btn(
@@ -78,7 +53,7 @@
                 color="#98be58"
                 :to="{ name: 'signup-multispecialty' }"
                 id="enterprise-book-demo-btn"
-                @click.stop="handleToolbarLinkClick('enterprise-book-demo-btn')"
+                @click.stop="onActionBtnClick('enterprise-book-demo-btn')"
               )
                 strong.font-14.white--text.tab.text-none Book A Demo
               v-btn(
@@ -86,7 +61,7 @@
                 color="#98be58"
                 :to="{ name: 'signup-specialized' }"
                 id="specialized-signup-btn"
-                @click.stop="handleToolbarLinkClick('specialized-signup-btn')"
+                @click.stop="onActionBtnClick('specialized-signup-btn')"
               )
                 strong.font-14.white--text.tab.text-none Start 14-Day Trial
               v-btn(
@@ -94,7 +69,7 @@
                 v-else-if="currentRoute === 'fight-covid-19'"
                 color="#98be58"
                 id="fight-covid-19-get-started-btn"
-                @click.stop="handleToolbarLinkClick('fight-covid-19-get-started-btn')"
+                @click.stop="onActionBtnClick('fight-covid-19-get-started-btn')"
               )
                 strong.font-14.white--text.tab.text-none Get Started
               v-btn(
@@ -102,7 +77,8 @@
                 width="130"
                 color="#98be58"
                 id="get-started-btn"
-                @click.stop="handleToolbarLinkClick('get-started-btn')"
+                :to=" { name: 'signup-individual' }"
+                @click.stop="onActionBtnClick('get-started-btn')"
               )
                 strong.font-14.white--text.tab.text-none Get Started
 </template>
@@ -155,30 +131,14 @@ export default {
     },
   },
   methods: {
-    handleDisableSubMenus () {
-      this.patientSubMenuOpen = false;
-      this.professionalSubMenuOpen = false;
-    },
-    handleToolbarLinkClick (link) {
-      this.professionalSubMenuOpen = false;
-      this.$emit('toolbarLinkClick', link);
+    onActionBtnClick (actionBtn) {
+      this.$emit('actionBtnClick', actionBtn);
     },
     handleMycureLogo () {
-      this.professionalSubMenuOpen = false;
       this.$emit('logoClick');
     },
-    handleSubMenuClick (link, menu) {
-      this.professionalSubMenuOpen = false;
-      this.$emit('subMenuClick', { link, menu });
-    },
-    handleSubMenuOpen (item) {
-      if (item.position === 'right') {
-        this.professionalSubMenuOpen = true;
-        this.patientSubMenuOpen = false;
-      } else {
-        this.professionalSubMenuOpen = false;
-        this.patientSubMenuOpen = true;
-      }
+    onNavLinkClick (navLink) {
+      this.$emit('navLinkClick', navLink);
     },
   },
 };
