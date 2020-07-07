fa1f8be33d1b88c414127bc44be486887d9df395
Marc Almond Valleza <majvalleza@gmail.com>
2020-07-07 11:09:46 +0800

    refactor: add user based pricing card images

--- components/pricing/UserBased.vue
+++ components/pricing/UserBased.vue
@@ -6,20 +6,22 @@
     v-row
       v-col(v-for="(type, key) in pricingTypes" :key="key" cols="12" md="4")
         v-card(height="100%")
-          v-card-title
-            v-spacer
-            v-card-title-text.font-weight-bold {{ type.title }}
-            v-spacer
+          v-img(
+            :src="require(`~/assets/images/pricing/${type.image}.png`)"
+            :alt="type.title"
+          )
+            v-col(:class="{'white--text': key === 1}")
+              h2.font-weight-bold {{ type.title }}&nbsp;
+                v-chip(v-if="key === 1" color="warning" x-small).black--text BEST VALUE
+              strong.font-16 $
+              span.font-40.font-weight-medium {{ type.price }}
+              br
+              | per use monthly
+              br
+              strong.font-16 {{type.storage}}GB&nbsp;
+              span.font-16 Storage
           v-card-text
             v-container.content
-              v-col.text-center
-                strong.font-16 $
-                strong.font-30 {{ type.price }}
-                br
-                | per use monthly
-                br
-                br
-                span.font-16 {{type.storage}}GB Storage
               v-col.inclusions-container
                 v-row(
                   v-for="(inclusion, inclusionKey) in type.inclusions"
@@ -39,11 +41,16 @@
 </template>
 
 <script>
+import PictureSource from '~/components/commons/PictureSource';
 export default {
+  components: {
+    PictureSource,
+  },
   data () {
     this.pricingTypes = [
       {
         title: 'Essentials',
+        image: 'MYCURE-virtual-clinic-healthcare-practice-online-pricing-user-essentials',
         price: 0,
         storage: 1,
         inclusions: [
@@ -62,6 +69,7 @@ export default {
       },
       {
         title: 'Pro',
+        image: 'MYCURE-virtual-clinic-healthcare-practice-online-pricing-user-pro',
         price: 15,
         storage: 5,
         inclusions: [
@@ -79,6 +87,7 @@ export default {
       },
       {
         title: 'Team',
+        image: 'MYCURE-virtual-clinic-healthcare-practice-online-pricing-user-team',
         price: 20,
         storage: 10,
         inclusions: [
