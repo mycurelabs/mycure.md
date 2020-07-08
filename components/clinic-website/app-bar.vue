c0d498646c30ff4e13a0ef59c90e0c5a3a3ef8f8
Jason Jamora <jamorajj@gmail.com>
2020-07-08 08:38:37 +0800

    refactor: use generic container on appbar

--- components/clinic-website/app-bar.vue
+++ components/clinic-website/app-bar.vue
@@ -1,19 +1,29 @@
 <template lang="pug">
-  v-app-bar(flat color="white")
-    nuxt-link(:to="{ name: 'index' }" title="MYCURE | Clinic Management System | Cloud EMR Philippines" id="toolbar-mycure-logo")
-      img(
-        height="45"
-        src="~/assets/images/mycure-header-logo.png"
-        to="/"
-      )
-    v-spacer
-    book-appointment-btn(:outlined="false" btn-color="success")
+  v-app-bar(
+    color="white"
+    elevate-on-scroll
+    fixed
+  )
+    generic-container
+      v-row(no-gutters align="center")
+        v-col.col-auto
+          nuxt-link(:to="{ name: 'index' }" title="MYCURE | Clinic Management System | Cloud EMR Philippines" id="toolbar-mycure-logo")
+            img(
+              height="45"
+              src="~/assets/images/mycure-header-logo.png"
+              to="/"
+            )
+        v-spacer
+        v-col.col-auto
+          book-appointment-btn(:outlined="false" btn-color="success")
 </template>
 
 <script>
+import GenericContainer from '~/components/commons/generic-container';
 import BookAppointmentBtn from '~/components/commons/book-appointment-btn';
 export default {
   components: {
+    GenericContainer,
     BookAppointmentBtn,
   },
 };
