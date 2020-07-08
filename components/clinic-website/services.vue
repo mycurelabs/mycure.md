2f2921c7b9535e69ce7ec73c9fd04d7d48a6e126
Jason Jamora <jamorajj@gmail.com>
2020-07-08 13:36:02 +0800

    refactor: update services to be similar to doc site

--- components/clinic-website/services.vue
+++ components/clinic-website/services.vue
@@ -1,119 +1,40 @@
 <template lang="pug">
-  generic-container
-    v-row(justify="center" align="center")
-      h1 {{ headerText }}
-    v-row(v-if="!$isMobile" justify="center" align="center")
-      v-col(cols="12" md="4" align-self="start")
-        v-list(two-line).text-left
-          v-list-item(
-            v-for="(item, key) in servicesColumn1"
-            :key="key"
-          )
-            v-list-item-action
-              img(src="~/assets/images/mycure-web-bullet-check.png" alt="Check icon")
-            v-list-item-content
-              span {{ item }}
-      v-col(v-if="showTwoColumn || showThreeColumn" cols="12" md="4" align-self="start")
-        v-list(two-line).text-left
-          v-list-item(
-            v-for="(item, key) in servicesColumn2"
-            :key="key"
-          )
-            v-list-item-action
-              img(src="~/assets/images/mycure-web-bullet-check.png" alt="Check icon")
-            v-list-item-content
-              span {{ item }}
-      v-col(v-if="showThreeColumn" cols="12" md="4" align-self="start")
-        v-list(two-line).text-left
-          v-list-item(
-            v-for="(item, key) in servicesColumn3"
-            :key="key"
-          )
-            v-list-item-action
-              img(src="~/assets/images/mycure-web-bullet-check.png" alt="Check icon")
-            v-list-item-content
-              span {{ item }}
-    v-row(v-if="$isMobile" justify="start" align="center")
+  //- TODO: could also be refactored along with doctor website services?
+  generic-container.mb-10
+    v-row(justify="center")
+      v-col(cols="12").text-center
+        h1 We offer this healthcare services for you
+    v-row(justify="center").px-2
       v-col(cols="12")
-        v-list(two-line).text-left
-          v-list-item(
-            v-for="(item, key) in services"
-            :key="key"
-          )
-            v-list-item-action
-              img(src="~/assets/images/mycure-web-bullet-check.png" alt="Check icon")
-            v-list-item-content
-              span {{ item }}
+        v-row(justify="center")
+          template(v-if="services.length === 0")
+            div.text-center.pa-5
+              h3.grey--text #[i No services to show]
+          template(v-else v-for="(service, index) in services")
+            v-col(
+              cols="12"
+              sm="12"
+              md="4"
+            )
+              media(image-position="left")
+                template(slot="media-image")
+                  img(src="~/assets/images/mycure-web-bullet-check.png" width="25px")
+                template(slot="media-content")
+                  span {{ service }}
 </template>
 
 <script>
+import Media from '~/components/commons/media';
 import GenericContainer from '~/components/commons/generic-container';
 export default {
   components: {
+    Media,
     GenericContainer,
   },
   props: {
-    /**
-     * Array of services strings
-     * @type {Array}
-     */
     services: {
       type: Array,
       default: () => ([]),
-    },
-    /**
-     * String header text
-     * @type {String}
-     */
-    headerText: {
-      type: String,
-      default: '',
-    },
-  },
-  computed: {
-    showOneColumn () {
-      return this.services.length < 4;
-    },
-    showTwoColumn () {
-      return this.services.length < 7 && this.services.length > 3;
-    },
-    showThreeColumn () {
-      return this.services.length >= 7;
-    },
-    columnCount () {
-      return this.showOneColumn ? '4' : this.showTwoColumn ? '4' : '4';
-    },
-    columnOffsetCount () {
-      return this.showOneColumn ? '4' : this.showTwoColumn ? '2' : '';
-    },
-    servicesColumn1 () {
-      if (this.showOneColumn) {
-        return this.services;
-      }
-
-      if (this.showTwoColumn) {
-        return this.services.filter((v, i) => !(i % 2));
-      }
-
-      return this.services.filter((v, i) => !(i % 3));
-    },
-    servicesColumn2 () {
-      if (this.showOneColumn) {
-        return false;
-      }
-
-      if (this.showTwoColumn) {
-        return this.services.filter((v, i) => (i % 2));
-      }
-
-      return this.services.filter((v, i) => (i % 3 === 1));
-    },
-    servicesColumn3 () {
-      if (this.showThreeColumn) {
-        return this.services.filter((v, i) => (i % 3 > 1));
-      }
-
-      return false;
     },
   },
 };
