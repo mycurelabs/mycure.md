a2d79c5eb541c5ac2cee6bc95c2a60a16cac139d
Jason Jamora <jamorajj@gmail.com>
2020-07-09 18:33:08 +0800

    refactor: update item list mobile computed

--- components/clinic-website/doctor-item-list-mobile.vue
+++ components/clinic-website/doctor-item-list-mobile.vue
@@ -3,10 +3,10 @@
     v-card
       div.d-flex
         v-avatar(size="100" rounded).ma-4
-          img(v-lazy="require(`~/assets/images/doctor-website/${this.doctor.imageFile}`)")
+          img(v-lazy="picURL")
         div
-          v-card-title.headline.word-break {{ doctorName }}
-          v-card-subtitle.pb-2 {{ doctor.specialization }} | {{ doctor.experience }} Years
+          v-card-title.headline.word-break {{ fullNameWithSuffixes }}
+          v-card-subtitle.pb-2 {{ specialties[0] }} | {{ yearsOfExperience }} Years
           v-card-actions.ml-2
             v-btn(color="primary" elevation="0" width="110").letter-spacing-normal.text-none Book Now!
         div.ml-auto.mr-1
@@ -16,12 +16,11 @@
                 v-icon mdi-dots-vertical
             v-list
               v-list-item
-                v-btn(width="110" color="#fafcff" tile elevation="0" :class="{ 'mr-2': $isMobile }").letter-spacing-normal.text-none Availability
-              v-list-item
-                v-btn(width="110" color="#fafcff" tile elevation="0").letter-spacing-normal.text-none View Site
+                v-btn(width="110" color="#fafcff" tile elevation="0").letter-spacing-normal.text-none View Website
 </template>
 
 <script>
+import { formatName } from '~/utils/formats';
 export default {
   props: {
     /**
@@ -41,6 +40,61 @@ export default {
       }
       return name;
     },
+    doctorWebsite () {
+      const username = this.doctor?.doc_website; // eslint-disable-line
+      return process.browser && `${window.location.origin}/doctors/${username}`;
+    },
+    bookAppointmentUrl () {
+      const username = this.doctor?.doc_website; // eslint-disable-line
+      return `${process.env.PX_PORTAL_URL}/appointment/step-1?doctor=${username}`;
+    },
+    picURL () {
+      const sex = this.doctor?.sex;
+      if (sex === 'female') {
+        return this.doctor?.picURL || require('~/assets/images/doctor-website/doctor-website-profile-female.png');
+      }
+      return this.doctor?.picURL || require('~/assets/images/doctor-website/doctor-website-profile-male.png');
+    },
+    // NOTE: might use later, don't remove
+    // name () {
+    //   return this.doctor?.name;
+    // },
+    fullName () {
+      return formatName(this.doctor?.name || {}, 'firstName middleInitial lastName generationalSuffix');
+    },
+    fullNameWithSuffixes () {
+      return [
+        this.fullName,
+        ...this.professions,
+        this.doctor?.name.academicSuffix,
+        this.doctor?.name.professionalSuffix,
+      ].filter(Boolean).join(', ');
+    },
+    // NOTE: might use later, don't remove
+    // bio () {
+    //   return this.doctor?.doc_bio || ''; // eslint-disable-line
+    // },
+    specialties () {
+      return this.doctor?.doc_specialties || []; // eslint-disable-line
+    },
+    professions () {
+      return this.doctor?.doc_professions || []; // eslint-disable-line
+    },
+    practicingSince () {
+      return this.doctor?.doc_practicingSince; // eslint-disable-line
+    },
+    yearsOfExperience () {
+      const from = new Date(this.practicingSince).getFullYear();
+      const to = new Date().getFullYear();
+      return to - from;
+    },
+    // NOTE: might use later, don't remove
+    // services () {
+    //   return this.doctor?.doc_services; // eslint-disable-line
+    // },
+    // education () {
+    //   return this.doctor?.educations;
+    // },
   },
 };
 </script>
