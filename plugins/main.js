65b27894a2e13770b69b4167586fe72a346b907c
Marc Almond Valleza <majvalleza@gmail.com>
2020-07-11 15:10:56 +0800

    docs: add comment about regular screen mixin

--- plugins/main.js
+++ plugins/main.js
@@ -5,6 +5,7 @@ Vue.mixin({
     $isMobile () {
       return this.wXS || this.wSM;
     },
+    /* Used if you do not want changes to transcend to widescreens */
     $isRegularScreen () {
       return this.wMD || this.wLG;
     },
