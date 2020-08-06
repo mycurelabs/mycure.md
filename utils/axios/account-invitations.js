4d9a7966af066cd67ff5b5914476e798e1418564
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-08-06 14:29:20 +0800

    feat: create axios service for account invitation

---
+++ utils/axios/account-invitations.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { handleError } from './error-handler';
+
+// NOTE: not used
+export const getAccountInvitations = async (opts) => {
+  try {
+    const { data } = await axios({
+      method: 'get',
+      url: `${process.env.API_URL}/account-invitations/${opts.referralCode}`,
+    });
+  } catch (e) {
+    console.error(e);
+    throw handleError(e);
+  }
+};
