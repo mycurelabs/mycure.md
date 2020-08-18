a139bd1703615c756f36fb6147bc9e5f187d7401
gerald35canonizado <gerald35canonizado@gmail.com>
2020-08-18 08:38:40 +0800

    refactor: update home OG banner image
    resolves #785

--- pages/signup/individual/step-2.vue
+++ pages/signup/individual/step-2.vue
@@ -327,8 +327,7 @@ export default {
     return headMeta({
       title: 'Start Free EMR Doctor Practice Management System',
       description: 'Create a free MYCURE account today and become a techy doctor in minutes! Better operations, beautiful reports, bye paperworks!',
-      // - TODO: Replace with local if applicable
-      socialBanner: 'https://firebasestorage.googleapis.com/v0/b/mc-v4-prod.appspot.com/o/web-main-assets%2FMYCURE-Open-Graph-Images-Home.png?alt=media&token=61978d70-326f-4323-8e6a-17d7b9c9a53b',
+      socialBanner: require('~/assets/images/banners/MYCURE Open Graph Images -  Home.png'),
     });
   },
 };
