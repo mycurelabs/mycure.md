f1e2a47679960f89bfebee8e5df2522b9ce70af1
Jason Jamora <jamorajj@gmail.com>
2020-08-27 05:16:46 +0800

    refactor: use window object

--- plugins/amplitude.js
+++ plugins/amplitude.js
@@ -1,7 +1,6 @@
-import Vue from 'vue';
 const amplitude = require('amplitude-js');
 
 export default ({ app }) => {
-  Vue.prototype.$amplitude = amplitude.getInstance();
-  Vue.prototype.$amplitude.init(process.env.AMPLITUDE_API_KEY);
+  window.$amplitude = amplitude.getInstance();
+  window.$amplitude.init(process.env.AMPLITUDE_API_KEY);
 };
