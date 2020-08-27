7eac6813df9c24e4a9327b4d496f32e6724d2149
gerald35canonizado <gerald35canonizado@gmail.com>
2020-08-27 11:26:30 +0800

    refactor: add search panel

---
+++ components/clinic-website/search-and-sort.vue
@@ -0,0 +1,83 @@
+<template lang="pug">
+  generic-container
+    v-row(align="center" no-gutters)
+      v-col(cols="12" md="6" lg="7" align="start")
+        v-text-field(
+          v-model="searchObject.searchString"
+          prepend-inner-icon="mdi-magnify"
+          label="Search a doctor, specialization you need."
+          type="text"
+          background-color="yellow accent-1"
+          color="grey"
+          clearable
+          dense
+          hide-details
+          outlined
+        )
+      v-spacer
+      v-col(cols="12" md="3" align="end")
+        v-select(
+          v-model="searchObject.sortBy"
+          label="Sort By"
+          item-text="text"
+          dense
+          hide-details
+          outlined
+          return-object
+          :items="sortBy"
+        )
+</template>
+
+<script>
+import GenericContainer from '~/components/commons/generic-container';
+export default {
+  components: { GenericContainer },
+  props: {
+    searchString: {
+      type: String,
+      default: '',
+    },
+  },
+  data () {
+    this.sortBy = [
+      {
+        text: 'Vicinity',
+      },
+      {
+        text: 'Sort 1',
+      },
+      {
+        text: 'Sort 2',
+      },
+      {
+        text: 'Sort 3',
+      },
+      {
+        text: 'Sort 4',
+      },
+      {
+        text: 'Sort 5',
+      },
+    ];
+    return {
+      searchObject: {
+        searchString: '',
+        sortBy: {},
+      },
+    };
+  },
+  watch: {
+    'searchObject.sortBy': {
+      handler () {
+        this.onSearch();
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    onSearch () {
+      this.$emit('search', this.searchObject);
+    },
+  },
+};
+</script>
