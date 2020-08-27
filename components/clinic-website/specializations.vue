c5b2210cc1a0150a907fc4c975dcc9bd7b4523db
gerald35canonizado <gerald35canonizado@gmail.com>
2020-08-27 20:26:23 +0800

    refactor: add specialization and featured hospitals and clinics images

--- components/clinic-website/specializations.vue
+++ components/clinic-website/specializations.vue
@@ -1,14 +1,15 @@
 <template lang="pug">
   generic-container
     v-row
-      v-col(cols="12")
-        h1 Specializations (65)
-      v-col
-        v-card(v-for="(specialization, key) in specializations" :key="key")
-          v-card-text
-            img
-            p {{ specialization.title }}
+      v-col(cols="12").mb-5
+        h2 Specializations (65)
+      template(v-for="(specialization, key) in specializations")
+        v-card(:color="specialization.color" flat).ma-1
+          v-card-actions
+            img(src="~/assets/images/clinics-website/specialization-skin.png" width="30")
+            span.mx-3 {{ specialization.title }}
             strong {{ specialization.counts }}
+      h4.primary--text.mt-5 See All (35)
 </template>
 
 <script>
@@ -20,86 +21,103 @@ export default {
       {
         image: '',
         title: 'For my kids',
+        color: 'blue lighten-4',
         counts: '(124)',
       },
       {
         image: '',
         title: 'For my heart',
+        color: 'red lighten-4',
         counts: '(143)',
       },
       {
         image: '',
         title: 'For my family',
+        color: 'indigo lighten-4',
         counts: '(124)',
       },
       {
         image: '',
         title: 'For my liver',
+        color: 'green lighten-4',
         counts: '(143)',
       },
       {
         image: '',
         title: 'For my teeth',
+        color: 'light-blue lighten-4',
         counts: '(124)',
       },
       {
         image: '',
         title: 'For my lungs',
+        color: 'orange lighten-4',
         counts: '(143)',
       },
       {
         image: '',
         title: 'For mothers',
+        color: 'blue lighten-4',
         counts: '(124)',
       },
       {
         image: '',
         title: 'For my mental health',
+        color: 'red lighten-4',
         counts: '(143)',
       },
       {
         image: '',
         title: 'For my blood',
+        color: 'indigo lighten-4',
         counts: '(124)',
       },
       {
         image: '',
         title: 'For my nervous system',
+        color: 'green lighten-4',
         counts: '(143)',
       },
       {
         image: '',
         title: 'For my colon',
+        color: 'light-blue lighten-4',
         counts: '(124)',
       },
       {
         image: '',
         title: 'For my skin',
+        color: 'orange lighten-4',
         counts: '(143)',
       },
       {
         image: '',
         title: 'For my ears, nose and throat',
+        color: 'blue lighten-4',
         counts: '(124)',
       },
       {
         image: '',
         title: 'For my heart',
+        color: 'red lighten-4',
         counts: '(143)',
       },
       {
         image: '',
         title: 'For my stomach',
+        color: 'indigo lighten-4',
         counts: '(124)',
       },
       {
         image: '',
         title: 'For elderly care',
+        color: 'green lighten-4',
         counts: '(143)',
       },
       {
         image: '',
         title: 'For my hormones and metabolism',
+        color: 'light-blue lighten-4',
         counts: '(124)',
       },
     ];
