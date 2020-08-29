9d400e0951b1960f89ac83dab765599aaa96069e
gerald35canonizado <gerald35canonizado@gmail.com>
2020-08-29 18:36:17 +0800

    refactor: add services panel

---
+++ components/hospital-website/services.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div
+    h2 Services Offered
+</template>
+
+<script>
+export default {
+  props: {
+    // hospitalName: {
+    //   type: String,
+    //   default: '',
+    // },
+  },
+  data () {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
