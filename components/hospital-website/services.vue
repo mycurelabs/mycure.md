1e5565c7a6e3f04dfc7b92df53baa953fb0312c6
gerald35canonizado <gerald35canonizado@gmail.com>
2020-08-29 19:51:34 +0800

    refactor: update services panel

--- components/hospital-website/services.vue
+++ components/hospital-website/services.vue
@@ -1,17 +1,25 @@
 <template lang="pug">
   div
-    h2 Services Offered
+    h2.pb-6 Services Offered
+    div(v-for="(service, key) in services" :key="key").d-inline-flex
+      v-img(:src="require(`~/assets/images/${service.image}`)" width="35")
+      div.ml-3.mt-2
+        span {{ service.service }}
+    div.pt-6
+      strong.primary--text See All
 </template>
 
 <script>
 export default {
-  props: {
-    // hospitalName: {
-    //   type: String,
-    //   default: '',
-    // },
-  },
   data () {
+    this.services = [
+      { image: 'mycure-check.png', service: 'Newborn Jaundice' },
+      { image: 'mycure-check.png', service: 'Adolescent Medicine' },
+      { image: 'mycure-check.png', service: 'Chickenpox Treatment' },
+      { image: 'mycure-check.png', service: 'Physical Therapy' },
+      { image: 'mycure-check.png', service: 'ENT Service' },
+      { image: 'mycure-check.png', service: 'Weight Management' },
+    ];
     return {};
   },
 };
