40d0a6efdc1a06098e48e0695af0427c32d505db
gerald35canonizado <gerald35canonizado@gmail.com>
2020-08-29 18:26:20 +0800

    refactor: fetch hospital and clinics individually

--- utils/axios/clinics-website.js
+++ utils/axios/clinics-website.js
@@ -4,11 +4,31 @@ import { handleError } from './error-handler';
 
 export const getFeaturedHospitals = async (opts) => {
   try {
-    // const { limit } = opts || {};
     const { data } = await axios({
       method: 'get',
       url: `${process.env.API_URL}/organizations?type=cms`,
-      // url: `${process.env.API_URL}/personal-details?doc_website[$exists]=true&$limit=${limit}`,
+    });
+    if (data?.total === 0) {
+      return {
+        total: 0,
+        data: [],
+      };
+    }
+    return {
+      total: data?.total,
+      data: data?.data,
+    };
+  } catch (e) {
+    console.error(e);
+    throw handleError(e);
+  }
+};
+
+export const getFeaturedClinics = async (opts) => {
+  try {
+    const { data } = await axios({
+      method: 'get',
+      url: `${process.env.API_URL}/organizations?type=personal-clinic`,
     });
     if (data?.total === 0) {
       return {
@@ -29,39 +49,23 @@ export const getFeaturedHospitals = async (opts) => {
 export const getHospitalWebsite = async (opts) => {
   const { data } = await axios({
     method: 'get',
-    url: `${process.env.API_URL}/organizations?type=cms?name=${opts.username}`,
-  });
-  return data.data[0];
-};
-
-export const getHospitalProfile = async (opts) => {
-  const { data } = await axios({
-    method: 'GET',
-    url: `${process.env.API_URL}/organizations?createdBy=${opts.uid}`,
+    url: `${process.env.API_URL}/organizations?type=cms&id=${opts}`,
   });
   return data.data;
 };
 
-export const getFeaturedClinics = async (opts) => {
-  try {
-    // const { limit } = opts || {};
-    const { data } = await axios({
-      method: 'get',
-      // url: `${process.env.API_URL}/personal-details?doc_website[$exists]=true&$limit=${limit}`,
-      url: `${process.env.API_URL}/organizations?type=personal-clinic`,
-    });
-    if (data?.total === 0) {
-      return {
-        total: 0,
-        data: [],
-      };
-    }
-    return {
-      total: data?.total,
-      data: data?.data,
-    };
-  } catch (e) {
-    console.error(e);
-    throw handleError(e);
-  }
+export const getClinicWebsite = async (opts) => {
+  const { data } = await axios({
+    method: 'get',
+    url: `${process.env.API_URL}/organizations?type=personal-clinic&id=${opts}`,
+  });
+  return data.data;
+};
+
+export const getServices = async (opts) => {
+  const { data } = await axios({
+    method: 'GET',
+    url: `${process.env.API_URL}/services?facility=${opts}`,
+  });
+  return data.data;
 };
