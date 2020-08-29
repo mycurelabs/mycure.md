b6b196ea2aed13efba8cf68e2d5fb08a60957420
gerald35canonizado <gerald35canonizado@gmail.com>
2020-08-29 18:36:06 +0800

    refactor: add schedules panel

---
+++ components/hospital-website/schedules.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+  div
+    h2.pb-3 Clinic Schedule
+    template(v-if="hospitalSchedules && hospitalSchedules.length === 0")
+      i No schedules available
+    div(v-else v-for="(day, key) in hospitalSchedules" :key="key")
+      h3 {{ day.day | morph-capitalize }}
+      span.pl-2 {{ day.opening | morph-date-format('hh:mm A') }} - {{ day.closing | morph-date-format('hh:mm A') }}
+</template>
+
+<script>
+export default {
+  props: {
+    schedules: {
+      type: Array,
+      default: () => ([]),
+    },
+  },
+  data () {
+    this.days = [
+      { order: 1, day: 'mon' },
+      { order: 2, day: 'tue' },
+      { order: 3, day: 'wed' },
+      { order: 4, day: 'thu' },
+      { order: 5, day: 'fri' },
+      { order: 6, day: 'sat' },
+      { order: 7, day: 'sun' },
+    ];
+    return {
+      fullSchedules: [],
+    };
+  },
+  computed: {
+    hospitalSchedules () {
+      this.fullSchedules = this.schedules; // eslint-disable-line
+      const groupedSchedules = this.fullSchedules
+        .map((schedule) => {
+          const { order } = this.days.find(day => day.day === schedule.day);
+          return {
+            order,
+            ...schedule,
+          };
+        })
+        .sort((a, b) => a.day !== b.day ? a.order - b.order : a.opening - b.opening) || [];
+      return groupedSchedules;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
