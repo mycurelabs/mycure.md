a5fe2ae000daace49ce16d4474310afd6dde2194
gerald35canonizado <gerald35canonizado@gmail.com>
2020-08-29 18:29:49 +0800

    refactor: add hospital main infor panel

---
+++ components/hospital-website/info.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  div
+    h1 {{ hospitalName }}
+    p TeleMedicine Clinic | {{ address }}
+    v-icon(color="orange" size="20px").mb-4 mdi-star
+    p {{ completeAddress }}
+    div
+      v-btn(
+        color="primary"
+        width="160"
+        outlined
+        large
+        @click=""
+      ) #[b VIEW MAP]
+      v-btn(
+        color="primary"
+        width="160"
+        outlined
+        large
+        @click=""
+      ).ml-3 #[b CLINIC WEBSITE]
+</template>
+
+<script>
+export default {
+  props: {
+    hospitalName: {
+      type: String,
+      default: '',
+    },
+    address: {
+      type: String,
+      default: '',
+    },
+    completeAddress: {
+      type: String,
+      default: '',
+    },
+  },
+  data () {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
