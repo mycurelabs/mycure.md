afd865e7893560ab4bd3c08c9c7c8b0591c1ca41
gerald35canonizado <gerald35canonizado@gmail.com>
2020-08-29 18:29:09 +0800

    refactor: add about hospital panel

---
+++ components/hospital-website/about-us.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  div
+    div.text-center
+      v-avatar(size="100%")
+        img(:src="picURL")
+    div.text-justify
+      h2.py-5.primary--text About
+      p {{ description }}
+</template>
+
+<script>
+export default {
+  props: {
+    picURL: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+  },
+  data () {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
