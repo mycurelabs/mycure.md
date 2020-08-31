0b15c6c118a2f1df28452c61e17ea5b2fed267a2
gerald35canonizado <gerald35canonizado@gmail.com>
2020-08-31 11:40:12 +0800

    refactor: update services panel

--- components/hospital-website/services.vue
+++ components/hospital-website/services.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
   div
     h2.pb-6 Services Offered
-    div(v-for="(service, key) in services" :key="key").d-inline-flex
-      v-img(:src="require(`~/assets/images/${service.image}`)" width="35" contain)
-      div.ml-3.mt-2
-        span {{ service.service }}
+    div(v-for="(service, key) in services" :key="key")
+      div.d-inline-flex
+        v-img(:src="require(`~/assets/images/${service.image}`)" width="30" contain)
+        span.ml-3.mt-1 {{ service.title }}
     div.pt-6
       strong.primary--text See All
 </template>
@@ -13,12 +13,12 @@
 export default {
   data () {
     this.services = [
-      { image: 'mycure-check.png', service: 'Newborn Jaundice' },
-      { image: 'mycure-check.png', service: 'Adolescent Medicine' },
-      { image: 'mycure-check.png', service: 'Chickenpox Treatment' },
-      { image: 'mycure-check.png', service: 'Physical Therapy' },
-      { image: 'mycure-check.png', service: 'ENT Service' },
-      { image: 'mycure-check.png', service: 'Weight Management' },
+      { image: 'mycure-check.png', title: 'Newborn Jaundice' },
+      { image: 'mycure-check.png', title: 'Adolescent Medicine' },
+      { image: 'mycure-check.png', title: 'Chickenpox Treatment' },
+      { image: 'mycure-check.png', title: 'Physical Therapy' },
+      { image: 'mycure-check.png', title: 'ENT Service' },
+      { image: 'mycure-check.png', title: 'Weight Management' },
     ];
     return {};
   },
--- pages/hippocrates-directory/hospitals/index.vue
+++ pages/hippocrates-directory/hospitals/index.vue
@@ -12,16 +12,17 @@
           :completeAddress="completeAddress"
         )
         v-row.mt-3
-          v-col(cols="12" md="6")
+          v-col(cols="12" sm="6")
             services(style="background-color: #ececec; border-radius: 5px; min-height: 400px;").pa-3
-          v-col(cols="12" md="6")
+          v-col(cols="12" sm="6")
             schedules(:schedules="schedules" style="background-color: #ececec; border-radius: 5px; min-height: 250px;").pa-3
             v-col(cols="12" style="background-color: #ececec; border-radius: 5px; min-height: 126px;").mt-6
               consultations
 
-        template
-          h2 Testimonials
-          testimonials(:picURL="picURL" :testimonialDate="testimonialDate" :testimonialDescription="testimonialDescription")
+        v-row
+          v-col(cols="12")
+            h2 Testimonials
+            testimonials(:picURL="picURL" :testimonialDate="testimonialDate" :testimonialDescription="testimonialDescription")
 
     v-divider
     v-footer(color="white").mt-3
