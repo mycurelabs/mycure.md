fe79104ea68a9647949d9664962f88f5b6baf1c7
gerald35canonizado <gerald35canonizado@gmail.com>
2020-09-04 11:03:07 +0800

    refactor: apply default value when there is no testimonial

--- components/hippocrates-website/testimonials.vue
+++ components/hippocrates-website/testimonials.vue
@@ -2,16 +2,19 @@
   v-container
     v-row
       v-col(cols="12" style="background-color: #ececec; border-radius: 5px; min-height: 126px;").d-flex
-        div
-          v-avatar
-            img(:src="picURL")
-        div(style="width: 100%; margin-left: 12px;")
-          div.d-flex.pb-3
-            v-icon(color="orange") mdi-star
-            v-spacer
-            p.ma-0 {{ testimonialDate | morph-date-format('MMM DD, YYYY') }}
+        template(v-if="testimonialDescription !== ''")
           div
-            p.text-justify "{{ testimonialDescription }} Takimata dolores kasd elitr diam elitr. Aliquyam stet sed lorem sit et."
+            v-avatar
+              img(:src="picURL")
+          div(style="width: 100%; margin-left: 12px;")
+            div.d-flex.pb-3
+              v-icon(color="orange") mdi-star
+              v-spacer
+              p.ma-0 {{ testimonialDate | morph-date-format('MMM DD, YYYY') }}
+            div
+              p.text-justify {{ testimonialDescription }}
+        template(v-else)
+          i No testimonials available
 </template>
 
 <script>
@@ -32,6 +35,11 @@ export default {
   },
   data () {
     return {};
+  },
+  methods: {
+    check () {
+      return console.log(this.testimonialDescription);
+    },
   },
 };
 </script>
