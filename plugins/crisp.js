8367595ab88b09ca121a6b8951f26d11d51b644f
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-09-14 16:21:40 +0800

    refactor: fix late merge conflict

--- plugins/crisp.js
+++ plugins/crisp.js
@@ -1,13 +1,6 @@
 export default () => {
   window.$crisp = [];
-<<<<<<< HEAD
-  // PREVIOUS ID
-  // window.CRISP_WEBSITE_ID = '77efedac-1e3a-4bca-b12d-2a4ffc754baf';
-  window.CRISP_WEBSITE_ID = '8ec0faa8-cc7d-4aed-8ffa-e4816dff000b';
-
-=======
   window.CRISP_WEBSITE_ID = process.env.CRISP_WEBSITE_ID;
->>>>>>> cf8d85a0721d2d3171d16df41c1cf297b64c94e3
   (function () {
     const d = document;
     const s = d.createElement('script');
