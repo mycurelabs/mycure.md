3500ee8a28005d4a286625ed97e0c9ffa2e02173
Mar Jayson San Agustin <sanagustinjayson@gmail.com>
2020-09-29 15:14:23 +0800

    refactor: update chat-now card content

--- components/clinic-website/chat-now-card.vue
+++ components/clinic-website/chat-now-card.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
   v-card(color="primary").elevation-0
     v-card-text
-      p.white--text Not sure whom to consult? Chat with&nbsp;
+      p.white--text Not sure whom to consult?
+      p.white--text
         strong {{ hospitalName }}'s &nbsp;
-        | officers.
+        | has receptionists available via chat. Would you like to chat with them?.
       v-btn(
         color="white"
         target="_blank"
