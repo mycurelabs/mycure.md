a732555d2211b4b9786a427ff5b401bc55b11ad1
Mar Jayson San Agustin <sanagustinjayson@gmail.com>
2020-10-10 14:42:07 +0800

    feat: add lgu website healthcare providers

---
+++ components/lgu-website/healthcare-providers.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  div(v-if="doctors")
+    v-container.mt-5
+      v-row
+        v-col(cols="12").text-left
+          h2.grey--text.text--darken-2 Healthcare Providers
+      v-row.px-9.mt-5
+        template(v-for="doctor in doctors")
+          provider(:doctor="doctor")
+</template>
+
+<script>
+// components
+import Provider from './provier-item';
+
+export default {
+  components: {
+    Provider,
+  },
+  props: {
+    doctors: {
+      type: Array,
+      default: () => ([]),
+    },
+  },
+};
+</script>
