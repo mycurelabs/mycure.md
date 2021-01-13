ffb1fee4d371e031315934b4816d0690014502ec
Marc Almond Valleza <majvalleza@gmail.com>
2021-01-13 16:57:40 +0800

    docs: comment out previous referral code guard

--- components/signup-individual/step-1.vue
+++ components/signup-individual/step-1.vue
@@ -175,7 +175,6 @@ import EmailVerificationDialog from './email-verification-dialog';
 import {
   getCountries,
   getCountry,
-  getWaitlist,
   signupIndividual,
 } from '~/utils/axios';
 import {
@@ -313,37 +312,38 @@ export default {
 
         this.$refs.passwordRef.focus();
 
+        // - TODO: Totally remove if confirmed not needed in some way
         // ACCOUNT DATA RECEIVED AS PARAMETER
-        const accountData = this.$route.params.data;
+        // const accountData = this.$route.params.data;
 
         // CHECK IF THERE IS REFERRAL CODE AND ACCOUNT DATA
-        if (!accountData && getItem('account-invitation') === null) {
-          this.$nuxt.$router.push({ name: 'signup-individual-invite' });
-          return;
-        };
+        // if (!accountData && getItem('account-invitation') === null) {
+        //   this.$nuxt.$router.push({ name: 'signup-individual-invite' });
+        //   return;
+        // };
 
-        if (accountData) {
-          const phoneNumber = accountData.mobileNo && parsePhoneNumberFromString(accountData.mobileNo);
-          this.user.firstName = accountData.personalDetails?.name?.firstName;
-          this.user.lastName = accountData.personalDetails?.name?.lastName;
-          this.user.email = accountData.email;
-          this.user.mobileNo = phoneNumber?.nationalNumber;
-          this.user.doc_PRCLicenseNo = accountData.personalDetails?.doc_PRCLicenseNo; // eslint-disable-line
-          return;
-        }
+        // if (accountData) {
+        //   const phoneNumber = accountData.mobileNo && parsePhoneNumberFromString(accountData.mobileNo);
+        //   this.user.firstName = accountData.personalDetails?.name?.firstName;
+        //   this.user.lastName = accountData.personalDetails?.name?.lastName;
+        //   this.user.email = accountData.email;
+        //   this.user.mobileNo = phoneNumber?.nationalNumber;
+        //   this.user.doc_PRCLicenseNo = accountData.personalDetails?.doc_PRCLicenseNo; // eslint-disable-line
+        //   return;
+        // }
 
-        const accountInvitationData = getItem('account-invitation');
-        if (accountInvitationData) {
-          const accountInvitation = await getWaitlist({ referralCode: accountInvitationData.referralCode });
-          if (!accountInvitation) return;
+        // const accountInvitationData = getItem('account-invitation');
+        // if (accountInvitationData) {
+        //   const accountInvitation = await getWaitlist({ referralCode: accountInvitationData.referralCode });
+        //   if (!accountInvitation) return;
 
-          const phoneNumber = accountInvitation.mobileNo && parsePhoneNumberFromString(accountInvitation.mobileNo);
-          this.user.firstName = accountInvitation.personalDetails?.name?.firstName;
-          this.user.lastName = accountInvitation.personalDetails?.name?.lastName;
-          this.user.email = accountInvitation.email;
-          this.user.mobileNo = phoneNumber?.nationalNumber;
-          this.user.doc_PRCLicenseNo = accountInvitation.personalDetails?.doc_PRCLicenseNo; // eslint-disable-line
-        }
+        //   const phoneNumber = accountInvitation.mobileNo && parsePhoneNumberFromString(accountInvitation.mobileNo);
+        //   this.user.firstName = accountInvitation.personalDetails?.name?.firstName;
+        //   this.user.lastName = accountInvitation.personalDetails?.name?.lastName;
+        //   this.user.email = accountInvitation.email;
+        //   this.user.mobileNo = phoneNumber?.nationalNumber;
+        //   this.user.doc_PRCLicenseNo = accountInvitation.personalDetails?.doc_PRCLicenseNo; // eslint-disable-line
+        // }
 
         if (getItem('individual:step1:model') && this.pageType === 'signup-individual-step-1') {
           this.user = {
