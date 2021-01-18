715ce3e19ad5cb4d04e908a65cd3eed4463f7573
Mica Edrielle Jamito <edrielle1602@gmail.com>
2021-01-18 23:23:45 +0800

    refactor: revert old homepage and change new homepage to /patients-reserved

---
+++ pages/patients-reserved/index.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  div(v-if="!loading")
+    app-bar
+    usp.mb-10
+    how-it-works.mb-10
+    consult(v-if="!$isMobile").mb-10
+    services-section.mb-10
+    hipaa.mb-10
+    Cta.mb-10
+    my-footer
+</template>
+
+<script>
+import AppBar from '~/components/home/AppBar';
+import Usp from '~/components/home/Usp';
+import HowItWorks from '~/components/home/HowItWorks';
+import Consult from '~/components/home/Consult';
+import ServicesSection from '~/components/home/ServicesSection';
+import Hipaa from '~/components/home/Hipaa';
+import Cta from '~/components/home/Cta';
+import MyFooter from '~/components/home/MyFooter';
+export default {
+  layout: 'home',
+  components: {
+    AppBar,
+    Usp,
+    HowItWorks,
+    Consult,
+    ServicesSection,
+    Hipaa,
+    Cta,
+    MyFooter,
+  },
+  data () {
+    return {
+      loading: true,
+    };
+  },
+  mounted () {
+    this.loading = false;
+  },
+};
+</script>
