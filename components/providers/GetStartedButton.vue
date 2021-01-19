0bae54d06c3857f34c2f402a6b10298e0e2df7d0
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-01-19 21:32:04 +0800

    feat: create get started button

---
+++ components/providers/GetStartedButton.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  v-btn(
+    color="success"
+    rounded
+    :large="large"
+    :x-large="xLarge"
+    @click="getStarted"
+  ).text-none.font-weight-bold Get started for free
+</template>
+
+<script>
+export default {
+  props: {
+    large: Boolean,
+    xLarge: Boolean,
+  },
+  methods: {
+    getStarted () {
+      // TODO: get started
+    },
+  },
+};
+</script>
