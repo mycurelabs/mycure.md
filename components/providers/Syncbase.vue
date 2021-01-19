31883399f0c4837fb29768ac676fa6d2a65bc380
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-01-19 21:32:32 +0800

    feat: create syncbase panel

---
+++ components/providers/Syncbase.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  div(style="background-color: #BDDBE6")
+    v-container
+      v-row(align="center" justify="center").my-10
+        v-col(cols="12" md="6")
+          img(
+            width="100%"
+            src="../../assets/images/providers/mycure-syncbase-diagram-animate.gif"
+            style="border-radius: 10px;"
+          )
+        v-col(cols="12" md="6")
+          h1 MYCURE Health Suites work online and offline
+          p Work as if you have an in-house server with the convenience of the cloud. Create your medical records locally using multiple devices even if the internet is down! Once back online, it instantly syncs your data into the cloud.
+</template>
