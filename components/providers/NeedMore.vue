7d4513f1427d01b089c8d748ce1c4ca8cc228522
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-01-19 21:32:16 +0800

    feat: create need more panel

---
+++ components/providers/NeedMore.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div.need-more-container
+    v-row(align="center")
+      v-col.pa-10.grow
+        h1.white--text Need more?
+        p.white--text Enhance your experience with MYCURE's full suite of modules.
+      v-col.pa-10.shrink
+        v-btn(
+          x-large
+          rounded
+          color="primary"
+        ).font-weight-bold Learn more
+</template>
+
+<style scoped>
+.need-more-container {
+  background-color: #085B77;
+  border-radius: 10px;
+}
+</style>
