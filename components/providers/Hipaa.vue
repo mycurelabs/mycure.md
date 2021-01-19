21d19d68da5dad76140a1312fb617befdff75f93
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-01-19 21:32:11 +0800

    feat: create hipaa panel

---
+++ components/providers/Hipaa.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  v-container
+    v-row(align="center" justify="center").my-10
+      v-col(cols="12" md="6").text-center
+        img(
+          :width="$isMobile ? '100%' : '80%'"
+          src="../../assets/images/HIPAA Badge.png"
+        )
+      v-col(cols="12" md="6")
+        h1 MYCURE is HIPAA Compliant
+        p MYCURE demonstrates its commitment to industry-leading best practices for security and privacy of protected health information as required under HIPAA.
+        v-btn(
+          rounded
+          color="primary"
+        ).text-none Learn more
+</template>
