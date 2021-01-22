7b323d5b5f46630af8460c2cf8ef0955c6d91304
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-01-22 19:10:36 +0800

    chore: update test for Logo

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils';
-import Logo from '@/components/Logo.vue';
+import Money from '@/components/commons/Money.vue';
 
-describe('Logo', () => {
+describe('Money', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo);
+    const wrapper = mount(Money);
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
 });
