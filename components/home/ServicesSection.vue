5b49affea978a10873058049771b51c756f6adc4
Mica Edrielle Jamito <edrielle1602@gmail.com>
2021-01-26 21:38:19 +0800

    refactor: applied changes based on website QA from biz

--- components/home/ServicesSection.vue
+++ components/home/ServicesSection.vue
@@ -3,7 +3,7 @@
     v-row(justify="center" align="center").text-center
       v-col(cols="12")
         h1.font-40.mb-4 Services
-      template(v-for="service in services")
+      template(v-if="!$isMobile" v-for="service in services")
         v-col(cols="12" md="5").d-flex.justify-center
             v-card(width="325")
               img(
@@ -13,6 +13,19 @@
               )
               v-card-title #[b.ma-auto {{service.title}}]
               v-card-subtitle {{ service.description }}
+      template(v-else)
+        v-row.service-card
+          v-col(cols="4")
+            img(
+              :src="service.image"
+              width="80"
+              alt="services"
+            )
+          v-col(cols="6").text-left
+            h1.font-18 {{service.title}}
+            p.font-16.font-gray {{ service.description }}
+          v-col.my-auto
+            v-icon mdi-chevron-right
 </template>
 
 <script>
@@ -45,3 +58,8 @@ export default {
   },
 };
 </script>
+<style scoped>
+.service-card {
+  border-bottom: 1px solid;
+}
+</style>
--- components/home/Usp.vue
+++ components/home/Usp.vue
@@ -2,40 +2,62 @@
   div(:class="{'main-container' : !$isMobile, 'mobile-container' : $isMobile}")
     v-container
       v-row(align="center" justify="center")
-        v-col(cols="12")
-          search-bar(v-if="!$isMobile").mt-8
-        v-col.mt-8
+        v-col(cols="12").mt-12.text-center.pb-0
+          search-bar
+        v-col(:class="{ 'text-center pt-0' : $isMobile }").mt-md-8
           h1(:class="{ 'font-72' : !$isMobile }") Instantly Book #[br] Healthcare #[br] Services
           v-btn(
             large
             depressed
             color="success"
             rounded
+            @click="openPxPortalSignUp"
           ).mt-2 #[b Get Started]
+      div(v-if="!$isMobile").icon.d-flex.justify-center
+        v-btn(
+          large
+          icon
+          @click="scrollToHowItWorks"
+        )
+          v-icon(large) mdi-chevron-down
 </template>
 
 <script>
+import VueScrollTo from 'vue-scrollto';
 import SearchBar from '~/components/commons/SearchBar';
 export default {
   components: {
     SearchBar,
+  },
+  computed: {
+    pxPortalSignUp () {
+      return `${process.env.PX_PORTAL_URL}/signup`;
+    },
+  },
+  methods: {
+    scrollToHowItWorks () {
+      VueScrollTo.scrollTo('#howItWorks', 500, { offset: -100, easing: 'ease' });
+    },
+    openPxPortalSignUp () {
+      window.open(this.pxPortalSignUp);
+    },
   },
 };
 </script>
 
 <style scoped>
 .main-container {
-  background-image: url('../../assets/images/home/mycure-homepage-cover-01-b.png');
-  background-size: cover;
+  background-image: url('../../assets/images/home/MYCURE-For-Patients-Homepage-USP.png');
+  background-size: contain;
   background-position: center;
   width: 100%;
   height: 100vh;
 }
 .mobile-container{
   background-image: url('../../assets/images/home/mycure-homepage-cover-01-mobile.png');
+  background-size: cover;
   background-position: center;
   width: 100%;
-  height: 100vh;
 }
 .font-72 {
   font-size: 72px;
