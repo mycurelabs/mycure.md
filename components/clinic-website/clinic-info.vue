9ab377e0083aecac39ae2f83b34bee6c5cde13cc
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-01-27 18:57:38 +0800

    refactor: rename component

---
+++ components/clinic-website/clinic-info.vue
@@ -0,0 +1,67 @@
+<template lang="pug">
+  div.container
+    v-row(justify="center" align="center")
+      v-col(cols="12" sm="12").text-center
+        v-avatar(size="130")
+          img(:src="picURL")
+      v-col(cols="12").text-center
+        h1 {{ hospitalName }}
+        p.font-14 {{ completeAddress }}
+      v-col(cols="10")
+        v-icon(medium) mdi-phone
+        span.ml-2 #[b {{ contactNumber }}]
+    v-row(justify="center" align="center")
+      v-col(
+        cols="12"
+        :class="{ 'text-left': !$isMobile, 'text-center': $isMobile }"
+      )
+        h3.font-21 About
+        p.text-justify.font-14 {{ description }}
+</template>
+
+<script>
+export default {
+  props: {
+    hospitalName: {
+      type: String,
+      default: '',
+    },
+    address: {
+      type: String,
+      default: '',
+    },
+    completeAddress: {
+      type: String,
+      default: '',
+    },
+    picURL: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+    contactNumber: {
+      type: String,
+      default: '',
+    },
+  },
+  data () {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  /* background: rgba(0, 153, 204, 0.2); */
+  /* width:448px; */
+}
+h1{
+  font-size: 27px;
+}
+p {
+  color: #4D4D4D;
+}
+</style>
