f9dd6749fcf7d2660e6aa06f335ba6e6f21c260d
Marc Almond Valleza <majvalleza@gmail.com>
2021-02-02 16:35:29 +0800

    feat: initial setup of service item

---
+++ components/clinic-website/services/service-item.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  div
+    v-row
+      v-col(cols="12" md="8")
+        h3 {{ title }}
+        span Mon - Fri
+          | (
+          b 7:00 AM to 3:00 PM
+          | )
+        br
+        br
+        span Coverages:
+        br
+        v-avatar(size="30" color="secondary")
+        v-avatar(size="30" color="secondary")
+      v-col(cols="12" md="2").text-center
+        v-chip(color="success" small) AVAILABLE
+      v-col(cols="12" md="2")
+        h2 PHP 200
+        br
+        br
+        v-btn(color="success" rounded).text-none Book Now
+    v-row
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => ({}),
+    },
+    isDoctor: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    title () {
+      console.log('item', this.item);
+      if (this.isDoctor) {
+        return this.item?.doctorName;
+      }
+      return '';
+    },
+  },
+};
+</script>
--- components/clinic-website/services/tabs.vue
+++ components/clinic-website/services/tabs.vue
@@ -1,21 +1,49 @@
 <template lang="pug">
-  v-card(flat)
-    v-toolbar(flat)
-      v-tabs
-        v-tab #[b Laboratory]
-        v-tab #[b Imaging]
-        v-tab #[b PE Packages]
-        v-tab #[b Doctors]
-        v-tab #[b Procedures]
+  v-card(color="#f0f0f0" flat)
+    v-toolbar(color="#f0f0f0" flat)
+      v-tabs(v-model="activeServiceType")
+        v-tab(href="#lab") #[b Laboratory]
+        v-tab(href="#imaging") #[b Imaging]
+        v-tab(href="#pe") #[b PE Packages]
+        v-tab(href="#doctors" ) #[b Doctors]
+        v-tab(href="#procedure") #[b Procedures]
     v-card-text
+      template(v-for="item in items")
+        service-item(
+          :item="item"
+          :is-doctor="activeServiceType === 'doctors'"
+        )
+        v-divider
 </template>
 
 <script>
+import ServiceItem from './service-item';
 export default {
+  components: {
+    ServiceItem,
+  },
+  props: {
+    value: {
+      type: String,
+      default: null,
+    },
+    items: {
+      type: Array,
+      default: () => ([]),
+    },
+  },
   data () {
-    return {
-      activeTab: null,
-    };
+    return {};
+  },
+  computed: {
+    activeServiceType: {
+      get () {
+        return this.value;
+      },
+      set (val) {
+        this.$emit('input', val);
+      },
+    },
   },
 };
 </script>
