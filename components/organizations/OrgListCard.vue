ea0e4ae9b5208b0077f8a2a034f089fea55ec17a
Marc Almond Valleza <majvalleza@gmail.com>
2021-03-05 02:03:51 +0800

    refactor: improve org list card ui

--- components/organizations/OrgListCard.vue
+++ components/organizations/OrgListCard.vue
@@ -1,64 +1,58 @@
 <template lang="pug">
   v-card(flat color="#fafafa" height="100%").orgs-card
-    div.d-flex
-      v-col.shrink
-        img(
-          :src="picURL"
-          alt="Services"
-          :width="!$isMobile ? 146 : 80"
-        )
-      v-col.grow
-        strong.font-18 {{ organization.name }}
-        div
-          template(v-if="!isDescriptionExpanded && organization.description")
-            v-clamp(:max-lines="2" autoresize) {{ organization.description }}
-            a(@click="isDescriptionExpanded = true").primary--text See more
-          template(v-else-if="isDescriptionExpanded")
-            p {{ organization.description }}
-            a(@click="isDescriptionExpanded = false").primary--text Collapse
-          div(v-if="organization !== undefined").mt-4
-            div.d-flex
-              v-icon(color="primary").mr-2.mb-auto mdi-map-marker
-              p(:class="{ 'font-italic': !organization.address }") {{ address }}
-            div.d-flex
-              v-icon(color="primary)").mr-2.mb-auto mdi-phone
-              p(v-if="organization.phone").font-weight-bold {{ organization.phone }}
-              i(v-else) No phone number available
-            div.d-flex
-              v-icon(color="primary").mr-2.mb-auto mdi-calendar-today
-              div(v-if="fullSchedules.length")
-                div(v-if="!scheduleExpanded")
-                  span.text-capitalize {{ formatTodaySchedule(schedulesToday) }}
-                    a(v-if="fullSchedules.length > 1" @click="scheduleExpanded = true").primary--text &nbsp; View More
-                div(v-else)
-                  div(v-for="(schedule, key) in groupedSchedules" :key="key")
-                    span.text-capitalize {{ formatIndividualSchedule(schedule) }}
-                    br
-                  a(@click="scheduleExpanded = false").primary--text View Less
-                br
-                br
-              template(v-else)
-                i No schedules available
-                br
-    v-row(v-if="!$isMobile && !readOnly && hasWebsite")
-      v-col(cols="12").text-center
-        v-btn(
-          color="primary"
-          @click="openFacility"
-          target="_blank"
-          rel="noopener noreferrer"
-          rounded
-        ) #[b Visit Facility]
-    v-row(justify="center"  v-if="$isMobile && !readOnly && hasWebsite")
-      v-col(cols="12")
-        v-btn(
-          color="primary"
-          @click="openFacility"
-          target="_blank"
-          rel="noopener noreferrer"
-          rounded
-          block
-          ) #[b Visit Facility]
+    v-card-text.py-0.pl-0
+      div.d-flex
+        v-col.shrink
+          img(
+            :src="picURL"
+            alt="Services"
+            :width="!$isMobile ? 146 : 80"
+          )
+        v-col.grow
+          strong.font-18 {{ organization.name }}
+          div
+            template(v-if="!isDescriptionExpanded && organization.description")
+              v-clamp(:max-lines="2" autoresize) {{ organization.description }}
+              a(@click="isDescriptionExpanded = true").primary--text See more
+            template(v-else-if="isDescriptionExpanded")
+              p {{ organization.description }}
+              a(@click="isDescriptionExpanded = false").primary--text Collapse
+            div(v-if="organization !== undefined").mt-4
+              div.d-flex
+                v-icon(color="primary").mr-2.mb-auto mdi-map-marker
+                p(:class="{ 'font-italic': !organization.address }") {{ address }}
+              div.d-flex
+                v-icon(color="primary)").mr-2.mb-auto mdi-phone
+                p(v-if="organization.phone").font-weight-bold {{ organization.phone }}
+                i(v-else) No phone number available
+              div.d-flex
+                v-icon(color="primary").mr-2.mb-auto mdi-calendar-today
+                div(v-if="fullSchedules.length")
+                  div(v-if="!scheduleExpanded")
+                    span.text-capitalize {{ formatTodaySchedule(schedulesToday) }}
+                      a(v-if="fullSchedules.length > 1" @click="scheduleExpanded = true").primary--text &nbsp; View More
+                  div(v-else)
+                    div(v-for="(schedule, key) in groupedSchedules" :key="key")
+                      span.text-capitalize {{ formatIndividualSchedule(schedule) }}
+                      br
+                    a(@click="scheduleExpanded = false").primary--text View Less
+                  br
+                  br
+                template(v-else)
+                  i No schedules available
+                  br
+      v-row(v-if="!readOnly" justify="end")
+        v-col(cols="12" md="4")
+          v-btn(
+            color="primary"
+            target="_blank"
+            rel="noopener noreferrer"
+            rounded
+            block
+            :disabled="!hasWebsite"
+            @click="openFacility"
+          ).text-none.elevation-0
+            b {{ hasWebsite ? 'Visit Facility' : 'Unavailable' }}
 </template>
 
 <script>
