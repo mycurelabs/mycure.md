242edb73920674143bc24da68022c80614cc4cbf
Marc Almond Valleza <majvalleza@gmail.com>
2021-03-09 02:29:05 +0800

    refactor: update pxp usp

--- pages/pxp/index.vue
+++ pages/pxp/index.vue
@@ -1,57 +1,28 @@
 <template lang="pug">
   div(v-if="!loading")
     v-content
-      //- USP
-      v-container
-        v-row(justify="center" align="center").mt-10.mb-12
-          v-col(cols="12" md="6" :class="{ 'text-center' : $isMobile }")
-            h1.primary--text Find the right healthcare services near you
-            p Diagnostic clinics, hospitals, and full service clinics in Metro Manila are within your reach whenever you need them.
-            v-btn(
-              depressed
-              color="primary"
-              rounded
-              large
-              dense
-              @click="openPxPortal(pxPortal)"
-            ).text-none #[b Get Started Free]
-          v-col(cols="8" md="6" :class="{ 'order-first' : $isMobile }")
-            v-img(
-              :src="require('~/assets/images/patients/usp-patients.png')"
-              width="100%"
-              alt="patients-usp"
-            )
+      usp
       services(:fixed-search-bar="false")
 </template>
 
 <script>
-import AppBar from '~/components/home/AppBar';
 import Services from '~/components/services';
+import Usp from '~/components/pxp/Usp';
 import headMeta from '~/utils/head-meta';
 
 export default {
   layout: 'home',
   components: {
-    AppBar,
     Services,
+    Usp,
   },
   data () {
     return {
       loading: true,
     };
   },
-  computed: {
-    pxPortal () {
-      return `${process.env.PX_PORTAL_URL}/`;
-    },
-  },
   mounted () {
     this.loading = false;
-  },
-  methods: {
-    openPxPortal (location) {
-      window.open(location);
-    },
   },
   head () {
     return headMeta({
