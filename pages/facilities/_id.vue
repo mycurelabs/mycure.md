ca9e3947035b906fbaf8579e3f300f3836d643a6
Marc Almond Valleza <majvalleza@gmail.com>
2021-03-12 00:56:25 +0800

    refactor: update tabs pagination

--- pages/facilities/_id.vue
+++ pages/facilities/_id.vue
@@ -37,6 +37,7 @@
             hide-tabs
             show-back-button
             :items="listItems"
+            :items-total="itemsTotal"
             :organization="orgId"
             :loading="loading.list"
             :has-next-page="hasNextPage"
@@ -44,8 +45,7 @@
             :is-preview-mode="isPreviewMode"
             :service-types="serviceTypes"
             @back="mobileServicesListView = false"
-            @next="refetchListItems(activeTab, page + 1)"
-            @previous="refetchListItems(activeTab, page - 1)"
+            @paginate="refetchListItems(activeTab, $event)"
           )
         v-col(cols="12" md="3")
           clinic-info(
@@ -73,6 +73,7 @@
             v-if="!searchResultsMode && !$isMobile"
             v-model="activeTab"
             :items="listItems"
+            :items-total="itemsTotal"
             :organization="orgId"
             :loading="loading.list"
             :has-next-page="hasNextPage"
@@ -80,8 +81,7 @@
             :is-preview-mode="isPreviewMode"
             :service-types="serviceTypes"
             :has-doctors="hasDoctors"
-            @next="refetchListItems(activeTab, page + 1)"
-            @previous="refetchListItems(activeTab, page - 1)"
+            @paginate="refetchListItems(activeTab, $event)"
           )
           services-search-results(
             v-else-if="searchResultsMode"
@@ -249,7 +249,7 @@ export default {
         dayName: 'Sunday',
       },
     ];
-    this.itemsLimit = 7;
+    this.itemsLimit = 10;
     return {
       // UI State
       loading: {
