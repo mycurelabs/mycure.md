e58692d8e1480f43dfd35251da5367e6aeb7a654
Marc Almond Valleza <majvalleza@gmail.com>
2021-04-05 12:24:34 +0800

    refactor: transfer website layouts

---
+++ layouts/doctor-website.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  v-app#app
+    app-bar(v-if="!loading")
+    nuxt
+    app-footer(v-if="!loading")
+</template>
+
+<script>
+import AppBar from '~/components/doctor-website/app-bar';
+import AppFooter from '~/components/doctor-website/app-footer';
+export default {
+  components: {
+    AppBar,
+    AppFooter,
+  },
+  data () {
+    return {
+      loading: true,
+    };
+  },
+  mounted () {
+    this.loading = false;
+  },
+};
+</script>
---
+++ layouts/signin.vue
@@ -0,0 +1,92 @@
+<template lang="pug">
+  v-app.sign-in-page
+    v-layout(:class="contentClasses" fill-height fluid)
+      v-row(align="center" justify="center" :class="[contentClasses, footerClasses]").footer-bg
+        nuxt
+    v-row(align="center" justify="center").footer
+      v-col(cols="12").mt-2.text-center
+        span.white--text Don't have MYCURE yet?
+        nuxt-link(:to="{ name: 'signup-health-facilities' , params: { scrollHealthSuites: true}}").router-link.primary--text &nbsp;&nbsp;Get your account here.
+        v-divider(dark).mt-5.edge-divider
+      v-row(align="center" justify="center").mt-n2.text-center
+        v-col(cols="12" md="6").pl-12.pr-12
+          span.white--text Copyright &copy; 2016 - {{new Date().getFullYear()}} MYCURE Inc. All Rights Reserved.
+        v-col(cols="12" md="6" :class="footerPaddingClasses")
+          a(@click.stop="goToTerms") Terms of Use
+          span.white--text &nbsp;&nbsp;|&nbsp;&nbsp;
+          a(@click.stop="goToPrivacy") Privacy Policy
+</template>
+
+<script>
+import dayOrNight from '../utils/day-or-night';
+export default {
+  data () {
+    return {
+      dayOrNight: '',
+    };
+  },
+  computed: {
+    contentClasses () {
+      return [this.dayOrNight === 'night' ? 'night-sky' : 'white'];
+    },
+    footerClasses () {
+      return [this.dayOrNight === 'night' ? 'night-sky' : 'white', this.dayOrNight === 'night' ? 'night-bg' : 'day-bg'];
+    },
+    footerPaddingClasses () {
+      return [this.$isMobile ? 'pb-6' : ''];
+    },
+  },
+  mounted () {
+    this.dayOrNight = dayOrNight();
+    this.$vuetify.theme.dark = this.dayOrNight === 'night';
+  },
+  methods: {
+    goToTerms () {
+      const routeData = this.$nuxt.$router.resolve({ name: 'terms' });
+      const changeRoute = window.open(routeData.href, '_blank');
+      changeRoute.opener = null;
+      changeRoute.rel = 'noopener noreferrer';
+    },
+    goToPrivacy () {
+      const routeData = this.$nuxt.$router.resolve({ name: 'privacy-policy' });
+      const changeRoute = window.open(routeData.href, '_blank');
+      changeRoute.opener = null;
+      changeRoute.rel = 'noopener noreferrer';
+    },
+  },
+};
+</script>
+
+<style scoped>
+.footer-bg {
+  background-repeat: repeat-x;
+  background-position: bottom center;
+  padding-bottom: 100px;
+}
+.day-bg {
+  background-image: url('../assets/images/sign-in/mycure-final-cta-background.png');
+  background-size: auto 18%;
+}
+.night-bg {
+  background-image: url('../assets/images/mycure-onboarding-background-dark-mode.png');
+  background-size: auto 18%;
+}
+.night-sky {
+  background-color: rgb(28,28,28);
+  background-color: -moz-linear-gradient(180deg, rgba(28,28,28,1) 0%, rgba(60,60,60,1) 50%);
+  background-color: -webkit-linear-gradient(180deg, rgba(28,28,28,1) 0%, rgba(60,60,60,1) 50%);
+  background-color: linear-gradient(180deg, rgba(28,28,28,1) 0%, rgba(60,60,60,1) 50%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#1c191c",endColorstr="#3c3c3c",GradientType=1);
+}
+.footer {
+  background-color: #343a40 !important;
+}
+.router-link {
+  text-decoration: none;
+}
+@media screen and (min-width: 768px) {
+  .footer-bg {
+    padding-bottom: unset;
+  }
+}
+</style>
