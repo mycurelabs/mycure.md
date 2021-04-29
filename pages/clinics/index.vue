7e3246f8c548805b6897228bbafd53764384b670
Marc Almond Valleza <majvalleza@gmail.com>
2021-04-29 14:22:26 +0800

    fix: fix icons

--- pages/clinics/index.vue
+++ pages/clinics/index.vue
@@ -140,7 +140,7 @@ export default {
       },
       {
         title: 'Skin & Aesthetics',
-        icon: 'Multispecialty',
+        icon: 'Skin',
         iconExtension: '.png',
       },
       {
