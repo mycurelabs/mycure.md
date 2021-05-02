5cbce20287f47f41f5dd85cf8215bed7b0614958
Marc Almond Valleza <majvalleza@gmail.com>
2021-05-03 04:41:34 +0800

    refactor: change head position

--- pages/our-story/index.vue
+++ pages/our-story/index.vue
@@ -38,6 +38,14 @@ export default {
       loading: true,
     };
   },
+  head () {
+    return headMeta({
+      title: 'MYCURE - Our Story | Caring the Extra Mile',
+      description: 'Caring the extra mile is our mantra. MYCURE is committed to saving lives by building advanced apps for patients, physicians, and other healthcare practitioners.',
+      // - TODO: Replace with local if applicable
+      socialBanner: 'https://firebasestorage.googleapis.com/v0/b/mc-v4-prod.appspot.com/o/web-main-assets%2FMYCURE-Open-Graph-Images-Our-Story.png?alt=media&token=de5c4443-0b36-41ee-93e6-3fac9aae0b1d',
+    });
+  },
   watch: {
     $isMobile: {
       handler (val) {
@@ -48,14 +56,6 @@ export default {
   mounted () {
     this.isMobile = this.$isMobile;
     this.loading = false;
-  },
-  head () {
-    return headMeta({
-      title: 'MYCURE - Our Story | Caring the Extra Mile',
-      description: 'Caring the extra mile is our mantra. MYCURE is committed to saving lives by building advanced apps for patients, physicians, and other healthcare practitioners.',
-      // - TODO: Replace with local if applicable
-      socialBanner: 'https://firebasestorage.googleapis.com/v0/b/mc-v4-prod.appspot.com/o/web-main-assets%2FMYCURE-Open-Graph-Images-Our-Story.png?alt=media&token=de5c4443-0b36-41ee-93e6-3fac9aae0b1d',
-    });
   },
 };
 </script>
