d6eacc5b02ef6ba3ddbf6f67c6ce8a1400caded2
Marc Almond Valleza <majvalleza@gmail.com>
2021-05-07 23:33:06 +0800

    refactor: update think long term panel

--- components/commons/panels/ThinkLongTerm.vue
+++ components/commons/panels/ThinkLongTerm.vue
@@ -4,41 +4,68 @@
       v-col(cols="12" md="6").text-center
         strong.font-xs.primary--text Why MYCURE?
         h1(:class="panelHeaderClasses") Think Long-Term
-    generic-media-panel(
-      v-for="(panel, key) in panels"
-      :key="key"
-      offset-cols-right="1"
-      :header="panel.header"
-      :descriptions="panel.descriptions"
-      :header-classes="headerClasses"
-      :description-classes="descriptionClasses"
-      :dense='$isMobile'
-      v-bind="getPanelBindings(key)"
-      extension-exclusive
-      custom-image-path="commons/"
-      file-extension=".png"
-      :web-image="panel.image"
-    )
+    v-row(justify="center")
+      v-col(
+        cols="12"
+        md="4"
+        v-for="(panel, key) in panels"
+        :key="key"
+      )
+        v-card(flat color="#fafafa" height="100%")
+          v-card-text
+            div.text-center
+              picture-source(
+                extension-exclusive
+                custom-path="commons/"
+                image-file-extension=".png"
+                :image="panel.image"
+                :image-alt="panel.header"
+                image-width="90%"
+              )
+            br
+            br
+            h2(:class="headerClasses") {{ panel.header }}
+            br
+            p(:class="descriptionClasses") {{ panel.descriptions }}
+    //- generic-media-panel(
+    //-   v-for="(panel, key) in panels"
+    //-   :key="key"
+    //-   offset-cols-right="1"
+    //-   :header="panel.header"
+    //-   :descriptions="panel.descriptions"
+    //-   :header-classes="headerClasses"
+    //-   :description-classes="descriptionClasses"
+    //-   :dense='$isMobile'
+    //-   v-bind="getPanelBindings(key)"
+    //-   extension-exclusive
+    //-   custom-image-path="commons/"
+    //-   file-extension=".png"
+    //-   :web-image="panel.image"
+    //- )
 </template>
 
 <script>
 import classBinder from '~/utils/class-binder';
 import GenericMediaPanel from '~/components/commons/generic-media-panel';
+import PictureSource from '~/components/commons/PictureSource';
 export default {
   components: {
     GenericMediaPanel,
+    PictureSource,
   },
   data () {
     this.panels = [
       {
         header: 'MYCURE is not just your ordinary medical app.',
-        descriptions: ['It’s a platform built for the healthcare ecosystem. You can connect and share files with clinics, diagnostic centers, hospitals, and other physicians within the MYCURE network. MYCURE has open APIs to give you more flexibility in integrating with other systems.'],
-        image: 'Preventing Failures',
+        descriptions: 'It’s a platform built for the healthcare ecosystem. You can connect and share files with clinics, diagnostic centers, hospitals, and other physicians within the MYCURE network. MYCURE has open APIs to give you more flexibility in integrating with other systems.',
+        image: 'Prevent Failures',
+        imageWidth: '80%',
       },
       {
         header: 'As your practice grows, your tech tools need to upgrade too.',
-        descriptions: ['The great thing about starting with MYCURE is that you only need to upgrade once you need to. We’re here to make your practice so much easier to manage because you are a crucial part of healthcare. Together, let’s make this world a healthier place.'],
-        image: 'Achieving Success',
+        descriptions: 'The great thing about starting with MYCURE is that you only need to upgrade once you need to. We’re here to make your practice so much easier to manage because you are a crucial part of healthcare. Together, let’s make this world a healthier place.',
+        image: 'Success',
+        imageWidth: '80%',
       },
     ];
     return {};
@@ -53,10 +80,12 @@ export default {
     headerClasses () {
       const headerClasses = [
         classBinder(this, {
-          mobile: ['font-s', 'text-center'],
+          mobile: ['font-s'],
           regular: ['font-m'],
         }),
         'lh-title',
+        'text-justify',
+        'primary--text',
       ];
       return headerClasses;
     },
@@ -68,6 +97,7 @@ export default {
         }),
         'font-open-sans',
         'font-gray',
+        'text-justify',
       ];
       return descriptionClasses;
     },
