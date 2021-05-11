30b6d2d3658b11b1e2ff44ba0d202a951de3ff2a
Marc Almond Valleza <majvalleza@gmail.com>
2021-05-11 18:01:05 +0800

    refactor: add email verification dialog

---
+++ components/signup/EmailVerificationDialog.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  v-dialog(v-model="model" width="400" persistent)
+    v-card.pt-3
+      v-card-text.text-center
+        img(
+          src="~/assets/images/mycure-sign-up-success-pop-up-image.png"
+          alt="Email"
+        )
+        h1.font-40 Sign Up Success!
+        p.subheading A verification link has been sent to
+          br
+          b {{ email ? email : 'your email.' }}
+      v-card-text.text-center
+        v-btn(
+          large
+          bottom
+          color="primary"
+          @click="$emit('confirm')"
+        ).text-none.font-weight-bold Okay!
+</template>
+
+<script>
+export default {
+  props: {
+    value: null, // eslint-disable-line
+    email: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    model: {
+      get () {
+        return this.value;
+      },
+      set (val) {
+        this.$emit('input', val);
+      },
+    },
+  },
+};
+</script>
