512f9344c47fd5d5f62fa428bab0a5f7b052d52b
Joff Tiquez <jofftiquez@gmail.com>
2021-05-21 19:12:55 +0800

    feat: add new page for pricing

---
+++ pages/signup/health-facilities/pricing.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  h1 pricing
+</template>
+
+<script>
+import { SUBSCRIPTION_MAPPINGS } from '~/constants/subscription';
+import {
+  DOCTORS_PRICING,
+  CLINICS_PRICING,
+  DIAGNOSTICS_PRICING,
+} from '~/constants/pricing';
+export default {
+  layout: 'user',
+  data () {
+    this.subscriptionMappings = SUBSCRIPTION_MAPPINGS;
+    this.doctorsPricing = DOCTORS_PRICING;
+    this.clinicsPricing = CLINICS_PRICING;
+    this.diagnotics = DIAGNOSTICS_PRICING;
+    return {};
+  },
+};
+</script>
