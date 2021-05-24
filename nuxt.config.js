ec54903332f413f677c77b82205c179ca2d99f8d
Marc Almond Valleza <majvalleza@gmail.com>
2021-05-24 14:09:27 +0800

    chore: change vuetify breakpoint values

--- nuxt.config.js
+++ nuxt.config.js
@@ -97,28 +97,8 @@ export default {
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
-    theme: {
-      themes: {
-        light: {
-          primary: '#0099cc',
-          accent: '#7fad33',
-          secondary: '#424242',
-          info: '#0174BB',
-          warning: '#FFC107',
-          error: '#f75a5f',
-          success: '#7fad33',
-        },
-        dark: {
-          primary: '#0099cc',
-          accent: '#7fad33',
-          secondary: '#424242',
-          info: '#2196F3',
-          warning: '#FFC107',
-          error: '#f75a5f',
-          success: '#7fad33',
-        },
-      },
-    },
+    treeShake: true,
+    optionsPath: './vuetify.options.js'
   },
 
   googleAnalytics: {
