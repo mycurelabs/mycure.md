56b8bb44da4cd5c63e5ee178a3e8ddbf35327177
Joff Tiquez <jofftiquez@gmail.com>
2021-05-25 17:09:31 +0800

    refactor: update subscription packages

--- services/subscription-packages.js
+++ services/subscription-packages.js
@@ -1,24 +1,24 @@
 import sdk from '@mycure/sdk-js';
 import { getCountry } from '~/utils/axios';
 
-const generatePackageIds = (type, tiers, currencies = ['usd', 'php'], intervals = ['month', 'year']) => {
-  const ids = [`package_${type}_lite`];
-  tiers?.forEach(tier => {
-    for (let cI = 0; cI < currencies.length; cI++) {
-      for (let iI = 0; iI < intervals.length; iI++) {
-        ids.push(`package_${type}_${tier}_${currencies[cI]}_${intervals[iI]}ly`);
-      }
-    }
-  });
-  return ids;
-};
+// const generatePackageIds = (type, tiers, currencies = ['usd', 'php'], intervals = ['month', 'year']) => {
+//   const ids = [`package_${type}_lite`];
+//   tiers?.forEach(tier => {
+//     for (let cI = 0; cI < currencies.length; cI++) {
+//       for (let iI = 0; iI < intervals.length; iI++) {
+//         ids.push(`package_${type}_${tier}_${currencies[cI]}_${intervals[iI]}ly`); // eslint_disable_line
+//       }
+//     }
+//   });
+//   return ids;
+// };
 
-const BOOKING_PACKAGE_IDS = [
-  'package_booking_free',
-];
-const DOCTOR_PACKAGE_IDS = generatePackageIds('physicians', ['premium']);
-const CLINIC_PACKAGE_IDS = generatePackageIds('outpatients', ['premium', 'platinum', 'enterprise']);
-const DIAGNOSTIC_PACKAGE_IDS = generatePackageIds('diagnostics', ['premium', 'platinum', 'enterprise']);
+// const BOOKING_PACKAGE_IDS = [
+//   'package_booking_free',
+// ];
+// const DOCTOR_PACKAGE_IDS = generatePackageIds('physicians', ['premium']);
+// const CLINIC_PACKAGE_IDS = generatePackageIds('outpatients', ['premium', 'platinum', 'enterprise']);
+// const DIAGNOSTIC_PACKAGE_IDS = generatePackageIds('diagnostics', ['premium', 'platinum', 'enterprise']);
 
 export const state = () => ({
   subscriptionPackages: [],
@@ -26,16 +26,22 @@ export const state = () => ({
 
 export const getSubscriptionPackages = async ({ types }) => {
   const country = await getCountry();
-  console.warn(country);
-  const { items } = await sdk.service('subscription/packages').find({ organizationType: 'facility', organizationTypes: types });
-  // if (!items.length) {
-  //   return {
-
-  //   }
-  // }
-  const doctorPackages = items?.filter(pkg => DOCTOR_PACKAGE_IDS.includes(pkg.id));
-  return {
-    doctorPackages,
-  };
+  console.warn('country', country);
+  const currency = country?.country_currency === 'PH' ? 'php' : 'usd';
+  console.warn(currency);
+  const { items } = await sdk.service('subscription/packages').find({
+    organizationType: 'facility',
+    organizationTypes: types,
+    $or: [
+      { currency },
+      { tags: 'lite' },
+    ],
+  });
+  return items;
 };
 
+export const getSubscriptionPackagesPricing = async ({ types }) => {
+  const packages = await getSubscriptionPackages({ types });
+  // TODO: Map this packages
+  return packages;
+};
