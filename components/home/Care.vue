48b4014ae5cdd3c6e316ece10fe12abf45d9ad49
Marc Almond Valleza <majvalleza@gmail.com>
2021-05-28 17:18:13 +0800

    refactor: update care panel animation

--- components/home/Care.vue
+++ components/home/Care.vue
@@ -5,14 +5,18 @@
           v-col(cols="12" md="10" xl="12").text-center.pa-0
             h1(:class="headerClasses").font-weight-semibold.primary--text Caring for people who care for people
             p(:class="descriptionClasses").grey--text.font-open-sans MYCURE is dedicated to help healthcare providers save more lives each day.
-            v-row(justify="center")
+            v-row(
+              justify="center"
+            ).stat-container
               v-col(
                 v-for="(stat, key) in stats"
                 :key="key"
                 cols="12"
                 md="4"
+                v-observe-visibility="{ callback: onVisibilityChange, intersection: { threshold: 1 } }"
               ).text-center
-                h1(:class="{ 'font-xl': $isWideScreen, 'font-l': $isRegularScreen, 'font-m' : $isMobile }").primary--text.font-weight-semibold {{ stat.amount }}
+                h1(:class="{ 'font-xl': $isWideScreen, 'font-l': $isRegularScreen, 'font-m' : $isMobile }").primary--text.font-weight-semibold {{ statData[stat.amountKey].toLocaleString() }}
+                  span(v-if="statData[stat.amountKey] > 0") +
                 h3(:class="{ 'font-m': $isWideScreen, 'font-s' : $isRegularScreen, 'font-xs' : $isMobile }").font-weight-regular.grey--text {{ stat.title }}
 </template>
 
@@ -24,21 +28,29 @@ export default {
     GenericPanel,
   },
   data () {
-    this.stats = [
-      {
-        title: 'Medical Records',
-        amount: '2,700,000+',
+    return {
+      statData: {
+        'medical-records': 0,
+        lives: 0,
+        providers: 0,
       },
-      {
-        title: 'Lives Saved',
-        amount: '1,400,000+',
-      },
-      {
-        title: 'Partner Providers',
-        amount: '2,000+',
-      },
-    ];
-    return {};
+      stats: [
+        {
+          title: 'Medical Records',
+          amountKey: 'medical-records',
+        },
+        {
+          title: 'Lives Saved',
+          amountKey: 'lives',
+        },
+        {
+          title: 'Partner Providers',
+          amountKey: 'providers',
+        },
+      ],
+      isVisible: false,
+      hasAnimated: false,
+    };
   },
   computed: {
     headerClasses () {
@@ -54,6 +66,26 @@ export default {
         regular: ['font-s', 'mb-12'],
         wide: ['font-m', 'mb-12'],
       });
+    },
+  },
+  watch: {
+    isVisible (val) {
+      if (!val || this.hasAnimated) return;
+      this.$anime({
+        targets: this.statData,
+        'medical-records': 2700000,
+        lives: 1400000,
+        providers: 2000,
+        easing: 'linear',
+        round: 1,
+        duration: 500,
+      });
+      this.hasAnimated = true;
+    },
+  },
+  methods: {
+    onVisibilityChange (isVisible) {
+      this.isVisible = isVisible;
     },
   },
 };
