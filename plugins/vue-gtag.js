61dffc907278bb98159c4dfcede1992757fba049
Marc Almond Valleza <majvalleza@gmail.com>
2021-06-03 00:56:43 +0800

    fix: fix gtag implementation

--- plugins/vue-gtag.js
+++ plugins/vue-gtag.js
@@ -1,24 +1,13 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import VueGtag from 'vue-gtag';
 
-const router = new VueRouter({
-  routes: [
-    { name: 'Home', path: '/' },
-    { name: 'Doctors Clinics', path: '/doctors-clinics' },
-    { name: 'Outpatient Clinics', path: '/clinics' },
-    { name: 'Diagnostic Centers', path: '/diagnostics' },
-    { name: 'Telehealth', path: '/telehealth' },
-    { name: 'Booking', path: '/booking' },
-    { name: 'Signup', path: '/signup/health-facilities' },
-  ],
-});
-
-Vue.use(VueGtag, {
-  config: {
-    id: process.env.GA_ID,
-    params: {
-      send_page_view: false,
+export default ({ app }) => {
+  Vue.use(VueGtag, {
+    config: {
+      id: process.env.GA_ID,
+      params: {
+        send_page_view: false,
+      },
     },
-  },
-}, router);
+  }, app.router);
+};
