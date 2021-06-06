7d5fd3fb543eca3024018452b4b9402b0033d865
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-06-06 18:50:20 +0800

    refactor: update sentry plugin

--- plugins/sentry.js
+++ plugins/sentry.js
@@ -2,18 +2,16 @@ import Vue from 'vue';
 import * as Sentry from '@sentry/vue';
 import { Integrations } from '@sentry/tracing';
 
-export default () => {
-  Sentry.init({
-    Vue,
-    dsn: 'https://58567a0b813e4a6b8d82f7d81a65546f@o595042.ingest.sentry.io/5748409',
-    integrations: [new Integrations.BrowserTracing()],
+Sentry.init({
+  Vue,
+  dsn: 'https://58567a0b813e4a6b8d82f7d81a65546f@o595042.ingest.sentry.io/5748409',
+  integrations: [new Integrations.BrowserTracing()],
 
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
 
-    // Our config
-    environment: process.env.ENV,
-  });
-};
+  // Our config
+  environment: process.env.ENV,
+});
