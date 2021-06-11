9d0f9398ed93e126bfd1f7dd7f3efd11cc48173f
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-06-11 16:24:03 +0800

    refactor: temp page for booking page

--- pages/booking/index.vue
+++ pages/booking/index.vue
@@ -2,61 +2,227 @@
   v-container(v-if="!loading" fluid).white
     //- 1st panel
     usp(
-      description="Bring in more patients using a powerful healthcare service booking and management software. It's free, secure, and easy to use."
-      slotted-title
-      image="Booking USP"
-      custom-image-path="booking/"
+      has-custom-background
+      background-image="Doctor Landing Page"
+      title="Easy Booking System to Grow Your Practice"
+      meta-title="MYCURE Booking"
+      image="Doc USP"
+      image-width="115%"
+      custom-image-path="doctors-clinics/"
+      image-align="right"
+      btn-text="Get Started Free"
+      parse-title
+      :description="uspDescription"
+      :parse-title-fields="['Grow ']"
+      :media-column-bindings="{ cols: 12, md: 6, offsetMd: 1, xl: 6}"
+      :content-column-bindings="{ cols: 12, md: 5 }"
     )
-      template(slot="title")
-        h1(:class="[{'text-center': $isMobile}, ...headerClasses]").font-weight-bold Grow your #[br]
-          vue-typer(
-            :text="['Practice', 'Clinic', 'Diagnostics', 'Hospital']"
-            :repeat="Infinity"
-            :erase-delay="100"
-            erase-style="backspace"
-          ).typer
-          span #[br] Safely
     //- 2nd panel
-    div.mx-n3.grey-bg
-      start-easy(:header-classes="headerClasses")
-    //- 3rd - 5th panel
-    features(
-      :header-classes="headerClasses"
-      :description-classes="descriptionClasses"
-    )
-    //- 6th panel
-    plans.mb-n3
+    div.grey-bg.mx-n3
+      //- HOW IT WORKS
+    //- 3rd to 5th panels
+    //- generic-media-panel(
+    //-   v-for="(content, key) in contents"
+    //-   :key="key"
+    //-   :content="content"
+    //-   :title-classes="headerClasses"
+    //-   hide-btn
+    //- )
+    //-6th panel
+    //- generic-media-panel(
+    //-   content-right
+    //-   :content="sixthPanel"
+    //-   :title-classes="headerClasses"
+    //-   hide-btn
+    //- )
+    //-   //- Check list
+    //-   template(slot="additional-content")
+    //-     div.mb-10
+    //-       v-row(
+    //-         v-for="(item, i) in sixthPanel.list"
+    //-         :key="item"
+    //-         dense
+    //-       )
+    //-         v-col(cols="2" sm="1" md="1").pr-2.pt-2
+    //-           img(width="20" src="~/assets/images/mycure-check.png" alt="Check icon")
+    //-         v-col(cols="7" sm="5" md="7")
+    //-           span(:class="descriptionClasses") {{ item }}
+    //-     div(:class="{ 'text-center': $isMobile }")
+    //-       signup-button(
+    //-         depressed
+    //-         rounded
+    //-         :x-large="$isWideScreen"
+    //-         :large="!$isWideScreen"
+    //-         color="success"
+    //-       ).text-none.font-s
+    //-         span Get Started Free
+    //-         v-icon(small right) mdi-arrow-right
+    //- 7th panel
+    div.grey-bg.mx-n3
+      //- mycure-csi
+    //- 8th panel
+    div.blue-bg.mx-n3
+      //- generic-media-panel(
+      //-   :content="eightPanel"
+      //-   :title-classes="[...headerClasses, 'white--text']"
+      //-   :contentClasses="eightPanelContentClasses"
+      //-   hide-btn
+      //- )
+      //-   template(slot="additional-content")
+      //-     v-row(
+      //-       v-for="(item, i) in eightPanel.list"
+      //-       :key="i"
+      //-       dense
+      //-     )
+      //-       v-col(cols="2" sm="1" md="1").pr-2.pt-2
+      //-         v-icon(color="white") mdi-checkbox-marked-circle
+      //-       v-col(cols="10" sm="5" md="11")
+      //-         span(:class="eightPanelContentClasses") {{ item }}
+    //- 9th panel
+    //- think-long-term
+    v-divider.divider
+    //- 10th panel
+    //- pricing(
+    //-   type="doctor"
+    //-   title="Start free and only pay as you grow"
+    //-   :column-bindings="{ cols: '12', md: '4', xl: '3'}"
+    //- )
+    //- 11th panel
+    //- call-to-action(:fluid="!$isMobile")
 </template>
 
 <script>
-// - utils
-import classBinder from '~/utils/class-binder';
+// utils
 import headMeta from '~/utils/head-meta';
-// - components
+import classBinder from '~/utils/class-binder';
+// constants
+import { DOCTORS_PRICING } from '~/constants/pricing';
+// components
+import CallToAction from '~/components/commons/panels/CallToAction';
+import Features from '~/components/commons/panels/Features';
+import GenericMediaPanel from '~/components/generic/GenericMediaPanel';
+import MycureCsi from '~/components/commons/panels/MycureCsi';
+import Pricing from '~/components/commons/panels/Pricing';
+import ThinkLongTerm from '~/components/commons/panels/ThinkLongTerm';
 import Usp from '~/components/commons/panels/SevenWondersUsp';
-import StartEasy from '~/components/booking/StartEasy';
-import Features from '~/components/booking/Features';
-import Syncbase from '~/components/booking/Syncbase';
-import Plans from '~/components/booking/Plans';
+import SignupButton from '~/components/commons/SignupButton';
 
 export default {
   components: {
-    Usp,
-    StartEasy,
+    CallToAction,
     Features,
-    Syncbase,
-    Plans,
+    GenericMediaPanel,
+    MycureCsi,
+    Pricing,
+    ThinkLongTerm,
+    Usp,
+    SignupButton,
   },
   data () {
+    // Panel content
+    this.uspDescription = 'Let your patients book and schedule appointments efficiently.';
+    this.featuresDescription = 'Use the tools that work best for you. Everything you need is here. It’s FREE!';
+    this.contents = [
+      {
+        title: 'Lightning-fast prescriptions',
+        description: 'Create and print prescriptions and other medical forms in 10 seconds or less.',
+        contentAlign: 'left',
+        imageBindings: {
+          customPath: 'doctors-clinics/',
+          image: 'Lightning fast.png',
+          extensionExclusive: true,
+          mobileImage: 'Lightning fast mobile.png',
+        },
+      },
+      {
+        title: 'Going digital means better medical history',
+        description: 'Imagine looking back at your charts from 5 or 10 years ago on your mobile devices with a quick search. How convenient? S-U-P-E-R.',
+        contentAlign: 'right',
+        imageBindings: {
+          customPath: 'doctors-clinics/',
+          image: 'Going digital.png',
+          mobileImage: 'Going digital mobile.png',
+          extensionExclusive: true,
+        },
+      },
+      {
+        title: 'Help patients anywhere',
+        description: 'Reach out to more people who need your expertise without getting limited by time or location.',
+        contentAlign: 'left',
+        imageBindings: {
+          customPath: 'features/',
+          image: 'MYCURE-virtual-clinic-healthcare-practice-online-features-C-telehealth.webp',
+        },
+      },
+    ];
+    this.features = [
+      {
+        title: 'Digital Records',
+        icon: 'Digital Records',
+        iconExtension: '.png',
+      },
+      {
+        title: 'Telehealth',
+        icon: 'Telehealth',
+        iconExtension: '.png',
+      },
+      {
+        title: 'Daily Reports',
+        icon: 'Daily Reports',
+        iconExtension: '.png',
+      },
+      {
+        title: 'Professional Website',
+        icon: 'Professional Website',
+        iconExtension: '.png',
+      },
+      {
+        title: 'Appointment Booking',
+        icon: 'Appointment Booking',
+        iconExtension: '.png',
+      },
+    ];
+    this.sixthPanel = {
+      title: 'Expand your reach',
+      description: 'Opt in to MYCURE ONE, a global online directory of modern healthcare practitioners and facilities',
+      list: [
+        'Patients can easily find you',
+        'Get more organized appointments',
+        'Comes with a Professional Website',
+      ],
+      contentAlign: 'right',
+      imageBindings: {
+        image: 'Expand your reach.png',
+        customPath: 'commons/',
+        extensionExclusive: true,
+      },
+    };
+    this.eightPanel = {
+      title: 'Practice as a Group',
+      description: 'Easily coordinate with other physicians in your group practice and centralize your medical records in one comprehensive workspace.',
+      imageBindings: {
+        image: 'Practice.png',
+        customPath: 'doctors-clinics/',
+        extensionExclusive: true,
+      },
+      list: [
+        'Collated Medical Records',
+        'Optimized Patient Queuing',
+        'Group Clinic Chatbox',
+        'Shared Secretary Account',
+        'Booking Website',
+      ],
+    };
+    this.pricingDetails = DOCTORS_PRICING;
     return {
       loading: true,
     };
   },
   head () {
     return headMeta({
-      title: 'MYCURE Healthcare Service Booking Management Software',
-      description: 'MYCURE helps you bring in more patients using a powerful healthcare service booking and management software. It’s free, secure, and easy to use.',
-      socialBanner: require('~/assets/images/banners/OG Booking.png'),
+      title: 'MYCURE EMR Practice Management System for Doctors',
+      description: 'MYCURE organizes your daily tasks to make your practice more simple, secure, and efficient.',
+      socialBanner: require('~/assets/images/banners/OG Doc.png'),
     });
   },
   computed: {
@@ -67,8 +233,9 @@ export default {
           regular: ['font-l'],
           wide: ['font-xl'],
         }),
-        'font-weight-semibold',
         'lh-title',
+        'primary--text',
+        'font-weight-semibold',
       ];
       return headerClasses;
     },
@@ -84,9 +251,19 @@ export default {
       ];
       return descriptionClasses;
     },
+    eightPanelContentClasses () {
+      return [
+        classBinder(this, {
+          mobile: ['font-xs'],
+          regular: ['font-s'],
+          wide: ['font-m'],
+        }),
+        'font-open-sans',
+        'white--text',
+      ];
+    },
   },
   mounted () {
-    window.$crisp.push(['safe', true]);
     this.loading = false;
   },
 };
@@ -96,21 +273,11 @@ export default {
 .grey-bg {
   background-color: #fafafa;
 }
-
-.vue-typer >>> .custom.char.typed  {
-  color: #04B1E7;
+.blue-bg {
+  background-color: #0099cc;
 }
-
-.vue-typer >>> .custom.caret {
-  width: 5px;
-  background-color: #FFFFFF;
-}
-
-.vue-typer >>> .custom.caret.typing {
-  background-color: #04B1E7;
-}
-
-.syncbase-container {
-  background-color: #BDDBE6
+.divider {
+  margin-right: 40% !important;
+  margin-left: 40% !important;
 }
 </style>
