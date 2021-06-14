f9aee002bd06fdd2e077dda06f855ba00946d4f6
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-06-14 12:29:03 +0800

    style: fix indent

--- components/commons/panels/Features.vue
+++ components/commons/panels/Features.vue
@@ -2,30 +2,30 @@
   v-container
     v-row(justify="center" align="center")
       generic-panel(:row-bindings="{ justify: 'center' }")
-          v-col(cols="12" :md="titleColSize").text-center
-            strong(v-if="metaTitle" :class="metaTitleClasses").primary--text {{ metaTitle }}
-            h2(:class="titleClasses").lh-title.font-weight-semibold {{ title }}
-          v-col(cols="12" :md="contentColSize").text-center.py-3
-            div(:class="descriptionClasses").font-open-sans.font-gray
-              slot(name="description")
-                p {{ description }}
-          v-col(cols="12" :md="iconContainerColSize")
-            v-row(justify="center")
-              v-col(:cols="iconColSizeMobile" :md="iconColSize" v-for="(item, key) in items" :key="key").text-center
-                picture-source(
-                  v-if="item.icon"
-                  :extension-exclusive="extensionExclusive"
-                  :custom-path="imageDir"
-                  :image="item.icon"
-                  :image-alt="item.title"
-                  :image-file-extension="item.iconExtension || '.png'"
-                  :image-width="!$isMobile ? imageWidth : imageWidthMobile"
-                )
-                br
-                h3(:class="itemTextClasses").font-open-sans.font-gray {{ item.title }}
-                p(v-if="item.description" :class="itemTextClasses") {{ item.description }}
-                nuxt-link(v-if="!hideLearnMore && item.route" :to="{ name: item.route }").primary--text.font-weight-bold.learnLink Learn more
-          slot(name="additional-content")
+        v-col(cols="12" :md="titleColSize").text-center
+          strong(v-if="metaTitle" :class="metaTitleClasses").primary--text {{ metaTitle }}
+          h2(:class="titleClasses").lh-title.font-weight-semibold {{ title }}
+        v-col(cols="12" :md="contentColSize").text-center.py-3
+          div(:class="descriptionClasses").font-open-sans.font-gray
+            slot(name="description")
+              p {{ description }}
+        v-col(cols="12" :md="iconContainerColSize")
+          v-row(justify="center")
+            v-col(:cols="iconColSizeMobile" :md="iconColSize" v-for="(item, key) in items" :key="key").text-center
+              picture-source(
+                v-if="item.icon"
+                :extension-exclusive="extensionExclusive"
+                :custom-path="imageDir"
+                :image="item.icon"
+                :image-alt="item.title"
+                :image-file-extension="item.iconExtension || '.png'"
+                :image-width="!$isMobile ? imageWidth : imageWidthMobile"
+              )
+              br
+              h3(:class="itemTextClasses").font-open-sans.font-gray {{ item.title }}
+              p(v-if="item.description" :class="itemTextClasses") {{ item.description }}
+              nuxt-link(v-if="!hideLearnMore && item.route" :to="{ name: item.route }").primary--text.font-weight-bold.learnLink Learn more
+        slot(name="additional-content")
 </template>
 
 <script>
