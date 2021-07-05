b1e31b1830a7332bfde8858687a0727806611389
Marc Almond Valleza <majvalleza@gmail.com>
2021-07-05 17:15:21 +0800

    refactor: update clinic website footer

--- components/clinic-website/AppFooter.vue
+++ components/clinic-website/AppFooter.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
   div
-    v-footer(v-if="!$isMobile" dark color="#343A40").pa-5.pb-10
-        v-row.d-flex
+    v-footer(dark color="#343A40").pa-5.pb-16
+        v-row(:class="{'flex-column text-center': $isMobile}").d-md-flex
           v-col.font-14
-            div.d-flex
+            div(:class="{'text-center justify-center': $isMobile}").d-flex
               p.ml-5.mt-1 #[b Powered by]
               img(
                 height="30"
@@ -11,48 +11,26 @@
                 alt="MYCURE"
                 @click="$nuxt.$router.push({ name: 'index' })"
               ).ml-2
-            p.ml-5 Copyright &#169;{{new Date().getFullYear()}} All Rights Reserved.
-          v-col.d-flex
-            nuxt-link(to="/terms").font-14 Terms of Use
-            | &nbsp;|&nbsp;
-            nuxt-link(to="/privacy-policy").font-14 Privacy Policy
-            | &nbsp;|&nbsp;
+            p(v-if="!$isMobile").ml-5 Copyright &#169;{{new Date().getFullYear()}} All Rights Reserved.
+          v-col(:class="{'flex-column text-center': $isMobile}").d-md-flex
+            nuxt-link(to="/terms").font-14.white--text Terms of Use
+            span(v-if="!$isMobile") &nbsp;|&nbsp;
+            br(v-else)
+            nuxt-link(to="/privacy-policy").font-14.white--text Privacy Policy
+            span(v-if="!$isMobile") &nbsp;|&nbsp;
+            br(v-else)
             a(
               :href="feedbackLink"
               target="_blank"
               rel="noopener noreferrer"
-            ).font-14 Send us your feedback
-          v-col.d-flex.text-center
+            ).font-14.white--text Send us your feedback
+            template(v-if="$isMobile")
+              br
+              p.ml-5.mt-5 Copyright &#169;{{new Date().getFullYear()}} All Rights Reserved.
+          v-col(v-if="!$isMobile").d-flex.text-center
             nuxt-link(to="/directory/results").font-14 See more Health Facilities
             | &nbsp;|&nbsp;
             nuxt-link(to="/signup/health-facilities").font-14 Create my own Health Facility Website
-    v-footer(v-else dark color="#343A40" :class="{'pb-mobile': $isMobile}").pa-5
-      v-row(justify="center" align="center")
-        v-col(cols="12" align="center")
-          div.d-flex.justify-center.white--text
-            nuxt-link(to="/terms").white--text.font-14 Terms of Use
-            | &nbsp;&nbsp;|&nbsp;&nbsp;
-            nuxt-link(to="/privacy-policy").white--text.font-14 Privacy Policy
-            | &nbsp;&nbsp;|&nbsp;&nbsp;
-            a(
-              :href="feedbackLink"
-              target="_blank"
-              rel="noopener noreferrer"
-            ).white--text.font-14 Send us your feedback
-        v-col(cols="10" align="center")
-          div.d-flex.white--text
-            nuxt-link(to="/directory/results").white--text.font-14 See more Health Facilities
-            | &nbsp;&nbsp;|&nbsp;&nbsp;
-            nuxt-link(to="/signup/health-facilities").white--text.font-14 Create my own Health Facility Website
-        v-col(cols="10" align="center")
-          div.d-flex.justify-center
-            img(
-              height="26"
-              src="~/assets/images/MYCURE-logo-white.png"
-              alt="MYCURE"
-              @click="$nuxt.$router.push({ name: 'index' })"
-            )
-            p.white--text.font-14.ml-4.mt-1 &#169;{{new Date().getFullYear()}} All Rights Reserved.
 </template>
 
 <script>
