16d6a837787813c964a5c9b9eef564e0347e6775
markus <elija.lindo@eee.upd.edu.ph>
2021-09-17 18:07:54 +0800

    refactore: remove console log

--- utils/in-ph.js
+++ utils/in-ph.js
@@ -2,7 +2,6 @@ import { getCountry } from '~/utils/axios';
 
 export default async () => {
   const country = await getCountry() || {};
-  console.log(country);
   const code = country.country_code;
   if (!code || code === 'PH') return true;
   return false;
