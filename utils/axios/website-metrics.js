3d4d88057a746587b65685e78a3d9af27bcb8a12
Marc Almond Valleza <majvalleza@gmail.com>
2021-10-08 15:53:00 +0800

    refactor: update website metrics

--- utils/axios/website-metrics.js
+++ utils/axios/website-metrics.js
@@ -17,6 +17,12 @@ export const fetchWebsiteMetrics = async () => {
       url: `${process.env.API_URL}/metrics/metrics?name=new_facilities_total&$aggregate[sum]=1`,
     });
 
+    // Sub data
+    const { data: doctorsData } = await axios({
+      method: 'GET',
+      url: `${process.env.API_URL}/metrics/metrics?name=new_users_total&labels.is_doctor=true&$aggregate[sum]=1`,
+    });
+
     // const medicalRecordsData = await sdk.service('metrics/metrics').find({
     //   name: 'new_medical_records_total',
     //   $aggregate: { sum: 1 },
@@ -31,9 +37,10 @@ export const fetchWebsiteMetrics = async () => {
     //   $aggregate: { sum: 1 },
     // });
     return {
-      medicalRecordsData: medicalRecordsData[0]?.data[0]?.value || 0,
-      patientsData: patientsData[0]?.data[0]?.value || 0,
-      providersData: providersData[0]?.data[0]?.value || 0,
+      // Patients will be added to records as well
+      medicalRecordsData: (medicalRecordsData[0]?.data[0]?.value || 0) + (patientsData[0]?.data[0]?.value || 0),
+      patientsData: (patientsData[0]?.data[0]?.value || 0),
+      providersData: (providersData[0]?.data[0]?.value || 0) + (doctorsData[0]?.data[0]?.value || 0),
       // medicalRecordsData: medicalRecordsData[0]?.value || 0,
       // patientsData: patientsData[0]?.value || 0,
       // providersData: providersData[0]?.value || 0,
