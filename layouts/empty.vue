bec1da8d361ec0c558b2dff18bddab416631c592
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-10-21 21:28:07 +0800

    refactor: add style to empty layout

--- layouts/empty.vue
+++ layouts/empty.vue
@@ -2,3 +2,18 @@
   v-app
     nuxt
 </template>
+
+<style>
+#app {
+  max-width: 100%;
+  overflow-x: hidden;
+  font-family: 'Poppins', sans-serif, 'Arial';
+}
+/* --OTHER CSS-- */
+.lh-title {
+  line-height: 1.25em !important
+}
+.pre-white-space {
+  white-space: pre;
+}
+</style>
