d94bfa1919defc7cd0029b3850c78bbb0a79be4d
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-10-21 21:28:36 +0800

    feat: create new pages for pxp payment success and error

---
+++ pages/payment/pxp/error.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  v-container
+    v-row
+      v-col.text-center
+        label
+          v-icon(style="font-size: 75px; color: red;") mdi-close
+        h1.mb-5.text--black Payment Error
+        p There was an error!
+        p You may now close this window.
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  mounted () {
+    this.close();
+  },
+  methods: {
+    close () {
+      window.close();
+    },
+  },
+};
+</script>
---
+++ pages/payment/pxp/success.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  v-container
+    v-row
+      v-col.text-center
+        label
+          v-icon(style="font-size: 75px; color: green;") mdi-check
+        h1.mb-5.text--black Payment Success
+        p You may now close this window.
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  mounted () {
+    this.close();
+  },
+  methods: {
+    close () {
+      window.close();
+    },
+  },
+};
+</script>
