e787092b8d52181c6c1dbc0ad1140e1adc9a89d3
markus <lindo.elija@gmail.com>
2021-11-19 16:04:01 +0800

    refactor: import icons for router

--- components/home/Router.vue
+++ components/home/Router.vue
@@ -10,10 +10,10 @@
                   h2.mc-h2 Join the Next Generation of Healthcare IT Solutions: MYCURE
               p.mc-b2 Integrated healthcare solutions for your practice shouldn’t be expensive or complicated. MYCURE’s mission is to create an affordable yet effective patient records system. Featuring a variety of tools, you can customize your experience to fit the needs of your practice perfectly.
         v-col(cols="12" md="4" v-for="(page, key) in pages" :key="key")
-          v-card(style="background-color: #fafafa;" height="100%").rounded-xl.pa-5
+          v-card(style="background-color: #fafafa;" height="100%").rounded-lg.pa-5
             v-row(align="start")
               //- v-skeleton-loader(type="image" width="30%" height="50%").ma-3.
-              v-icon(large).blue-grad.ma-3 {{ page.icon }}
+              v-icon(large color="#0099CC").ma-3 {{ page.icon }}
               v-col
                 p.mc-h4 {{ page.text }}
                 p.mc-b4 {{ page.description }}
@@ -42,48 +42,55 @@
 </template>
 
 <script>
+import { mdiDoctor, mdiStethoscope, mdiClipboardListOutline, mdiCalendarClock, mdiLaptop, mdiWifiCheck } from '@mdi/js';
 import PictureSource from '~/components/commons/PictureSource';
 import GenericPanel from '~/components/generic/GenericPanel';
 export default {
   components: {
     PictureSource,
     GenericPanel,
+    mdiDoctor,
+    mdiStethoscope,
+    mdiClipboardListOutline,
+    mdiCalendarClock,
+    mdiLaptop,
+    mdiWifiCheck,
   },
   data () {
     this.pages = [
       {
         text: 'Physicians',
-        icon: 'mdi-doctor',
+        icon: mdiDoctor,
         route: 'doctors-clinics',
         description: 'Explore a simpler way to create electronic medical records, prescriptions, and diagnostic test orders.',
       },
       {
         text: 'Outpatient Clinics',
-        icon: 'mdi-stethoscope',
+        icon: mdiStethoscope,
         route: 'clinics',
         description: 'Experience streamlined workflows and real-time reports for optimized operational efficiency.',
       },
       {
         text: 'Diagnostics',
-        icon: 'mdi-clipboard-list-outline',
+        icon: mdiClipboardListOutline,
         route: 'diagnostics',
         description: 'Embrace a simplified diagnostic test process from specimen collection to online test results.',
       },
       {
         text: 'Easy Scheduling',
-        icon: 'mdi-calendar-clock',
+        icon: mdiCalendarClock,
         route: 'booking',
         description: 'Let your patients schedule appointments with ease and cut down on long waiting times.',
       },
       {
         text: 'Telehealth',
-        icon: 'mdi-laptop',
+        icon: mdiLaptop,
         route: 'telehealth',
         description: 'Try a safe and secure platform to build your virtual practice through video and audio calls.',
       },
       {
         text: 'Work Offline',
-        icon: 'mdi-wifi-check',
+        icon: mdiWifiCheck,
         route: 'syncbase',
         description: 'Enjoy the benefits of using a cloud-based medical software even with little to no internet connection.',
       },
@@ -130,10 +137,10 @@ export default {
   background-color: #0099cc;
 }
 .blue-grad {
-  background: linear-gradient(180deg, #0099CC 0%, #00397B 100%);
-  -webkit-background-clip: text;
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
+  background: linear-gradient(180deg, #0099CC 0%, #00397B 100%) !important;
+  -webkit-background-clip: text !important;
+  background-clip: text !important;
+  -webkit-text-fill-color: transparent !important;
 }
 
 .product-btn {
