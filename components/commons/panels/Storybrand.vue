0bdb2d8860905f2b7119933903c6b39c43e6ac14
markus <lindo.elija@gmail.com>
2021-11-19 18:09:17 +0800

    refactor: apply mobile improvements

--- components/commons/panels/Storybrand.vue
+++ components/commons/panels/Storybrand.vue
@@ -3,21 +3,22 @@
     v-row(justify="center").pa-5
       v-card(:width="!$isMobile ? '80%' : '100%'" elevation="5" :class="{'px-0': $isMobile}").rounded-xl.pa-10.my-16
         v-card-text(:class="$isMobile ? 'px-0' : ''")
-          v-row(justify="center")
-            v-col(cols="10" align="center")
-              span.mc-h2.black--text {{ title }}
-            v-col(cols="12").px-10
-              div(v-for="paragraph in content")
-                p(:class="$isWideScreen ? 'mb-8' : 'mb-5'").mc-b2.font-gray.font-open-sans {{ paragraph }}
-            v-col(cols="12" align="end")
-              signup-button(
-                depressed
-                color="primary"
-                class="rounded-lg"
-                :width="!$isWideScreen ? '228px' : '300'"
-                :height="!$isWideScreen ? '59px' : '73.68'"
-              ).text-none
-                span.generic-button-text Get Started Now
+          v-col
+            v-row(justify="center")
+              v-col(cols="10" align="center")
+                span.mc-h2.black--text {{ title }}
+              v-col(cols="12").px-10
+                div(v-for="paragraph in content")
+                  p(:class="$isWideScreen ? 'mb-8' : 'mb-5'").mc-b2.font-gray.font-open-sans {{ paragraph }}
+              v-col(cols="12" align="end" :class="{'text-center': $isMobile}")
+                signup-button(
+                  depressed
+                  color="primary"
+                  class="rounded-lg"
+                  :width="!$isWideScreen ? '228px' : '300'"
+                  :height="!$isWideScreen ? '59px' : '73.68'"
+                ).text-none
+                  span.generic-button-text Get Started Now
 
             //- v-col(cols="12" :class="{'px-0': $isMobile}" align="center")
             //-   signup-button(
