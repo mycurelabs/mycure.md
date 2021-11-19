654a4c4611508487a85165dcd72b2328e44e7f6c
markus <lindo.elija@gmail.com>
2021-11-19 18:43:03 +0800

    refactor: solve firefox alignment issue

--- components/commons/panels/Storybrand.vue
+++ components/commons/panels/Storybrand.vue
@@ -3,14 +3,14 @@
     v-row(justify="center").pa-5
       v-card(:width="!$isMobile ? '80%' : '100%'" elevation="5" :class="{'px-0': $isMobile}").rounded-xl.pa-10.my-16
         v-card-text(:class="$isMobile ? 'px-0' : ''")
-          v-col
+          v-col.pa-0
             v-row(justify="center")
               v-col(cols="10" align="center")
                 span.mc-h2.black--text {{ title }}
               v-col(cols="12").px-10
                 div(v-for="paragraph in content")
                   p(:class="$isWideScreen ? 'mb-8' : 'mb-5'").mc-b2.font-gray.font-open-sans {{ paragraph }}
-              v-col(cols="12" align="end" :class="{'text-center': $isMobile}")
+              v-col(cols="12" :class="$isMobile ? 'text-center' : 'text-right'")
                 signup-button(
                   depressed
                   color="primary"
