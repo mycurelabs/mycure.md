20a1d6972503f69e054e1e91f8c7f43cc4a3ea0c
Marc Almond Valleza <majvalleza@gmail.com>
2021-12-09 17:32:26 +0800

    refactor: update telehealth icon sizes

--- components/telehealth/VirtualClinic.vue
+++ components/telehealth/VirtualClinic.vue
@@ -32,7 +32,7 @@
             v-row(justify="center")
               v-col(align="center" :cols="$isMobile ? '12' : '2'").pb-0
                 div.text-center.pt-1
-                  img(v-lazy="require(`~/assets/images/telehealth/${data.headerIcon}`)" :width="$isWideScreen ? '80px' : '50px'" :height="$isWideScreen ? '80px' : '50px'" color="white")
+                  img(v-lazy="require(`~/assets/images/telehealth/${data.headerIcon}`)" :width="$isWideScreen ? '80px' : '50px'" :height="$isWideScreen ? '60px' : '35px'" color="white")
               v-col
                 h1.mc-h3.white--text {{ data.header }}
                 br
