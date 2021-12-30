36da0d1d8eb75abec487531787c2cda09bf1c963
markus <lindo.elija@gmail.com>
2021-12-30 15:33:55 +0800

    refactor: add padding top to dialog

--- components/doctor-website/ChooseAppointment.vue
+++ components/doctor-website/ChooseAppointment.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   v-dialog(v-model="dialog" width="600" height="800" persistent)
-    v-card
+    v-card.pt-4
       v-toolbar(flat)
         v-spacer
         h2.font-weight-bold.title--text.mc-h4 How can {{ isClinic ? 'we' : 'I' }} help you?
