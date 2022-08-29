ce103fc85316e448a66da71548fb614a0555a00b
Joff Tiquez <jofftiquez@gmail.com>
2022-08-29 20:43:11 +0800

    refactor: create layout page

---
+++ layouts/RegistrationLayout.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+v-app#app
+  v-container(fluid style="height: 100vh")
+    v-row(style="height: 100vh" align="center" justify="center")
+      v-col(
+        v-if="!$isMobile"
+        style="height: 100vh"
+        cols="12"
+        md="6"
+      ).primary
+      v-col(cols="12" md="6" lg="6")
+        nuxt
+</template>
