b02a7742197a9bb3313ab19fea1f2cb8e16b0bb8
Joff Tiquez <jofftiquez@gmail.com>
2022-09-01 18:37:17 +0800

    refactor: add OTP field

--- pages/register/otp.vue
+++ pages/register/otp.vue
@@ -1,9 +1,33 @@
 <template lang="pug">
-h1 OTP
+v-row(justify="center").red
+  v-col(cols="11" md="6").text-center
+    div.d-flex.text-center.justify-center.my-15
+      v-otp-input(
+        ref="otpInput"
+        separator=""
+        input-classes="otp-input"
+        :class="{ 'mobile-otp': $isMobile }"
+        :is-input-num="true"
+        :num-inputs="6"
+        :should-auto-focus="true"
+        @on-change="otp = $event"
+      ).green
 </template>
 
 <script>
 export default {
   layout: 'RegistrationLayout',
+  data () {
+    return {
+      otp: null,
+    };
+  },
 };
 </script>
+
+<style scoped>
+.mobile-otp >>> .otp-input {
+  margin-left: 4px;
+  margin-right: 4px;
+}
+</style>
