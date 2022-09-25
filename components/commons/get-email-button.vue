7023aaaff42543ea85e6e362e4a38b038f2703b7
Joff Tiquez <jofftiquez@gmail.com>
2022-09-26 03:49:11 +0800

    feat: create get email button

---
+++ components/commons/get-email-button.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+v-text-field(
+  background-color="white"
+  placeholder="Email Address"
+  color="primary"
+  outlined
+  hide-details
+).elevation-2
+  template(slot="append")
+    v-btn(
+      style="margin-top: -11px; margin-right: -6px;"
+      depressed
+      large
+      :color="buttonColor"
+    ).text-none Get Started
+</template>
+
+<script>
+export default {
+  props: {
+    buttonColor: {
+      type: String,
+      default: 'primary',
+    },
+  },
+};
+</script>
