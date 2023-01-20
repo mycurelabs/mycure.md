feda9642e98145bfd58c85701721a3d439c84d80
Joff Tiquez <jofftiquez@gmail.com>
2023-01-20 15:37:30 +0800

    refactor: add log

--- pages/signup/index.vue
+++ pages/signup/index.vue
@@ -6,6 +6,7 @@
 export default {
   mounted () {
     const queryParams = this.$route.query || {};
+    console.warn('process.env.SIGNUP_URL', process.env.SIGNUP_URL);
     const url = new URL(process.env.SIGNUP_URL || 'https://myaccount.mycure.md');
     const params = new URLSearchParams(url.search);
     for (const key in queryParams) {
