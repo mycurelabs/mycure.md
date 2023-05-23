3e631dc3953c93065128fa8cebad9a951de7bbf5
Joff Tiquez <jofftiquez@gmail.com>
2023-05-24 00:53:30 +0800

    🎨 style(tailwind.css): update font family and text styles The font family has been updated to Inter, which is a more modern and versatile font. The text styles have been updated to use tailwind classes instead of custom classes. The button class has been updated to use normal-case instead of uppercase. The title, subtitle, and button classes have been updated to use more appropriate font sizes.
    🎨 style(tailwind.config.js): add daisyui plugin and theme
    The daisyui plugin has been added to the tailwind.config.js file. A new theme called 'mycure' has been added to the daisyui themes array. This theme includes custom colors for primary, secondary, accent, neutral, base-100, info, success, warning, and error.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './node_modules/flowbite.{js,ts}',
-  ],
+  content: ['./node_modules/flowbite.{js,ts}'],
   theme: {
     extend: {
       colors: {
@@ -12,15 +10,33 @@ module.exports = {
         warning: '#FFAC2A',
       },
       fontFamily: {
-        primary: ['Montserrat'],
-        secondary: ['Hind Siliguri'],
-        general: ['Merriweather'],
+        primary: ['Inter'],
       },
     },
+  },
+  daisyui: {
+    base: false,
+    themes: [
+      {
+        mycure: {
+          primary: '#0099cc',
+          secondary: '#424242',
+          accent: '#82B1FF',
+          neutral: '#191D24',
+          'base-100': '#2A303C',
+          info: '#2196F3',
+          success: '#7fad33',
+          warning: '#FFC107',
+          error: '#f75a5f',
+        },
+      },
+      'light',
+    ],
   },
   plugins: [
     '@tailwindcss/forms',
     '@tailwindcss/container-queries',
-    require('flowbite/plugin'),
+    require('@tailwindcss/typography'),
+    require('daisyui'),
   ],
 };
