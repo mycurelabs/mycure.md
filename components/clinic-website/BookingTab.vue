d5af67ab8f3a9204a4fe32aeb62f97a4714c19c1
Joff Tiquez <jofftiquez@gmail.com>
2024-04-18 16:48:14 +0800

    ✨ (BookingTab.vue): Add new BookingTab component for clinic website The new BookingTab component is added to enhance the user experience on the clinic website. It provides a list of calendar events grouped by type, and allows users to select and view details of each event. This component also integrates with the booking service to fetch and display the calendar events. The addition of this component will make it easier for users to find and book appointments.

---
+++ components/clinic-website/BookingTab.vue
@@ -0,0 +1,114 @@
+<template lang="pug">
+  v-row.transparent
+    v-col(cols="12" md="4").pt-0
+      v-list(flat)
+        template(v-for="(calendarEvents, labelName) in calendarEventsByType")
+          v-subheader.primary--text.font-weight-bold {{ labelName | start-case }}
+          template(v-for="(calendarEvent, index) in calendarEvents")
+            v-list-item(:key="calendarEvent.id" v-ripple @click="openEvent(calendarEvent)" color="primary")
+              v-list-item-content
+                v-list-item-title {{ calendarEvent.title }}
+                v-list-item-subtitle {{ calendarEvent.description }}
+    v-col(cols="12" md="8" style="overflow: auto;").white
+      div(
+        style="width: 100%; height: auto; overflow: scroll; padding-right: 10px;"
+      )#calcom-mounting-point
+      //- pre {{ bookingEmbedUrl }}
+      //- pre {{ calLink }}
+      //- pre {{ JSON.stringify(calendar, null, 2) }}
+      //- pre {{ JSON.stringify(selectedAppointmentType, null, 2) }}
+</template>
+
+<script>
+import { groupBy, startCase } from 'lodash';
+import { getCalendar, listCalendarEvents } from '@/services/booking';
+export default {
+  props: {
+    clinic: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  filters: {
+    startCase,
+  },
+  data () {
+    return {
+      calendar: {},
+      calendarEvents: [],
+      selectedAppointmentType: null,
+      calLink: '',
+    };
+  },
+  computed: {
+    clinicId () {
+      return this.clinic?.id || '';
+    },
+    clinicName () {
+      return this.clinic?.name || '';
+    },
+    picUrl () {
+      return this.clinic?.picURL || '';
+    },
+    description () {
+      return this.clinic?.description || '';
+    },
+    insurers () {
+      return this.clinic?.insurers || [];
+    },
+    bookingPublicPageUrl () {
+      return this.calendar?.integration?.calcom?.publicPageUrl || '';
+    },
+    bookingEmbedUrl () {
+      return this.calendar?.integration?.calcom?.embedUrl || '';
+    },
+    calendarEventsByType () {
+      return groupBy(this.calendarEvents, 'metadata.appointmentType');
+    },
+    calendarEventsAppointmentTypes () {
+      const appointmentTypes = Object.keys(this.calendarEventsByType);
+      return appointmentTypes.map((appointmentType) => {
+        return {
+          value: appointmentType,
+          label: startCase(appointmentType) + 's',
+        };
+      });
+    },
+  },
+  watch: {
+    selectedAppointmentType (newValue) {
+      console.warn('selectedAppointmentType', newValue);
+      this.calLink = `${this.calendar?.id}/${newValue?.id}`;
+      this.mountCalcom();
+    },
+  },
+  async mounted () {
+    await this.init();
+    this.selectedAppointmentType = this.calendarEvents?.[0];
+    this.$calcom('init', { origin: 'https://cal.parmazip.com' });
+  },
+  methods: {
+    async init () {
+      this.calendar = await getCalendar({ organizationId: this.clinicId });
+      this.calendarEvents = await listCalendarEvents({ calendarId: this.calendar.id });
+    },
+    openEvent (event) {
+      console.warn('openEvent', event);
+      this.selectedAppointmentType = event;
+    },
+    mountCalcom () {
+      try {
+        console.warn('mountCalcom', this.calLink);
+        globalThis.document.getElementById('calcom-mounting-point').innerHTML = '';
+        this.$calcom('inline', {
+          elementOrSelector: '#calcom-mounting-point',
+          // calLink: 'rick/get-rick-rolled',
+          calLink: this.calLink,
+        });
+      } catch (e) {
+        console.error('Error mounting calcom', e);
+      }
+    },
+  },
+};
+</script>
