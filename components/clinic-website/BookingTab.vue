d912037bb54eb714b086a1c11cee7d014c0374e6
Joff Tiquez <jofftiquez@gmail.com>
2024-09-08 23:11:43 +0800

    refactor: Update Calcom embed URL and add conditional rendering for booking tab based on calendar events availability

--- components/clinic-website/BookingTab.vue
+++ components/clinic-website/BookingTab.vue
@@ -1,28 +1,95 @@
 <template lang="pug">
-  v-row.transparent
-    v-col(cols="12" md="4").pt-0
-      v-list(flat)
-        template(v-for="(calendarEvents, labelName) in calendarEventsByType")
-          v-subheader.primary--text.font-weight-bold {{ labelName | start-case }}
-          template(v-for="(calendarEvent, index) in calendarEvents")
-            v-list-item(:key="calendarEvent.id" v-ripple @click="openEvent(calendarEvent)" color="primary")
-              v-list-item-content
-                v-list-item-title {{ calendarEvent.title }}
-                v-list-item-subtitle {{ calendarEvent.description }}
-    v-col(cols="12" md="8" style="overflow: auto;").white
-      div(
-        style="width: 100%; height: auto; overflow: scroll; padding-right: 10px;"
-      )#calcom-mounting-point
-      //- pre bookingEmbedUrl -  {{ bookingEmbedUrl }}
-      //- pre calLink - {{ calLink }}
-      //- pre calendar object - {{ JSON.stringify(calendar, null, 2) }}
-      //- pre appoint type object - {{ JSON.stringify(selectedAppointmentType, null, 2) }}
+  div
+    v-dialog(v-model="successDialog" persistent max-width="500")
+      v-card
+        v-card-text(style="padding: 40px;").text-center
+          h1(style="font-size: 50px") 🎉
+          br
+          h2 Booking successful!
+          br
+          p.subheading Your #[b {{ successfulBookingEventTypeName }}] booking with #[b {{ successfulBookingClinicName }}] on #[b {{ successfulBookingDate }} ] from #[b {{ successfulBookingStartTime }} ] to #[b {{ successfulBookingEndTime }}] has been successfully set! 🎉
+          br
+          v-btn(
+            color="primary"
+            @click="closeDialog"
+          ) Let's go!
+
+    v-row.transparent
+      v-col(cols="12" md="4").pt-0
+        div(v-if="currentUser")
+          //- p You are logged in as:
+          div(
+            style="display: flex; align-items: center; border: 1px solid lightgrey; border-radius: 4px; background-color: white; gap: 10px; padding: 10px; width: 100%;"
+          ).mb-4
+
+            div(style="display: flex; align-items: center;")
+              v-avatar(color="primary" size="50")
+                img(
+                  v-if="currentUser.picURL"
+                  :src="currentUser.picURL"
+                )
+                v-icon(v-else dark) {{ mdiAccount }}
+
+            div(style="width: 100%;")
+              h3.mc-h4.title--text {{ currentUser.name.firstName }} {{ currentUser.name.lastName }}
+              span.mc-b2 {{ currentUser.email }}
+
+            div(style="display: flex; justify-content: end; align-items: center; width: 100%;")
+              v-btn(color="primary" depressed @click="onSignout") Log out
+
+        div(style="border: 1px solid lightgrey; border-radius: 4px;")
+          v-list(flat style="border-radius: 4px;")
+            template(v-for="(calendarEvents, labelName) in calendarEventsByType")
+              v-subheader.primary--text.font-weight-bold {{ labelName | start-case }}
+              template(v-for="(calendarEvent, index) in calendarEvents")
+                v-list-item(:key="calendarEvent.id" v-ripple @click="openEvent(calendarEvent)" color="primary")
+                  v-list-item-content
+                    v-list-item-title {{ calendarEvent.title }}
+                    v-list-item-subtitle {{ calendarEvent.description }}
+      v-col(cols="12" md="8" style="overflow: auto;").white
+        template(v-if="currentUser")
+          div(
+            style="width: 100%; height: auto; overflow: scroll; padding-right: 10px;"
+          )#calcom-mounting-point
+        template(v-else)
+          v-row(justify="center" align="center" style="height: 100%")
+            v-col.text-center
+              p.black--text You need an account to book an appointment.
+              p.black--text Login or create an account #[a(:href="pxpRedirectURL") here].
 </template>
 
 <script>
+import {
+  mdiAccount,
+} from '@mdi/js';
 import { groupBy, startCase } from 'lodash';
 import { getCalendar, listCalendarEvents } from '@/services/booking';
+import { getCurrentUser, signout } from '@/services/auth';
+
+function formatDate (date, format) {
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+  const year = date.getFullYear();
+
+  return format
+    .replace('dd', day)
+    .replace('MM', month)
+    .replace('yyyy', year);
+};
+
+function formatTime (date, format) {
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  return format
+    .replace('hh', hours)
+    .replace('mm', minutes)
+    .replace('a', hours >= 12 ? 'PM' : 'AM');
+}
+
 export default {
+  components: {
+    mdiAccount,
+  },
   props: {
     clinic: {
       type: Object,
@@ -38,9 +105,30 @@ export default {
       calendarEvents: [],
       selectedAppointmentType: null,
       calLink: '',
+      successDialog: false,
+      successfulBookingData: {},
+      currentUser: null,
     };
   },
   computed: {
+    successfulBookingEventTypeName () {
+      return this.successfulBookingData?.eventType?.title;
+    },
+    successfulBookingClinicName () {
+      return this.successfulBookingData?.organizer?.name;
+    },
+    successfulBookingDate () {
+      const startTime = this.successfulBookingData?.booking?.startTime;
+      return formatDate(new Date(startTime), 'dd/MM/yyyy');
+    },
+    successfulBookingStartTime () {
+      const startTime = this.successfulBookingData?.booking?.startTime;
+      return formatTime(new Date(startTime), 'hh:mm a');
+    },
+    successfulBookingEndTime () {
+      const endTime = this.successfulBookingData?.booking?.endTime;
+      return formatTime(new Date(endTime), 'hh:mm a');
+    },
     clinicId () {
       return this.clinic?.id || '';
     },
@@ -78,6 +166,10 @@ export default {
         };
       });
     },
+    pxpRedirectURL () {
+      const pxpUrl = process.env.PXP_URL;
+      return `${pxpUrl}?redirect=${window.location.href}`;
+    },
   },
   watch: {
     selectedAppointmentType (newValue) {
@@ -92,6 +184,7 @@ export default {
   },
   methods: {
     async init () {
+      this.currentUser = await getCurrentUser();
       this.calendar = await getCalendar({ organizationId: this.clinicId });
       this.calendarEvents = await listCalendarEvents({ calendarId: this.calendar.id });
       this.$emit('loaded', this.calendarEvents);
@@ -100,18 +193,47 @@ export default {
       console.warn('openEvent', event);
       this.selectedAppointmentType = event;
     },
-    mountCalcom () {
+    async mountCalcom () {
       try {
+        if (!this.currentUser) return;
         console.warn('mountCalcom', this.calLink);
         globalThis.document.getElementById('calcom-mounting-point').innerHTML = '';
-        this.$calcom('inline', {
+        await this.$calcom('inline', {
           elementOrSelector: '#calcom-mounting-point',
           // calLink: 'rick/get-rick-rolled',
           calLink: this.calLink,
+          config: {
+            'subject-identifier': this.currentUser.id,
+            subjectId: this.currentUser.id,
+            firstName: this.currentUser.name.firstName,
+            lastName: this.currentUser.name.lastName,
+            email: this.currentUser.email,
+            sex: this.currentUser.sex,
+          },
+        });
+        this.$calcom('on', {
+          action: 'bookingSuccessful',
+          callback: this.onBookingSuccessful,
         });
       } catch (e) {
         console.error('Error mounting calcom', e);
       }
+    },
+    unmountCalcom () {
+      globalThis.document.getElementById('calcom-mounting-point').innerHTML = '';
+    },
+    closeDialog () {
+      this.successDialog = false;
+    },
+    onBookingSuccessful (event) {
+      this.successfulBookingData = event?.detail?.data || {};
+      this.successDialog = true;
+      this.mountCalcom();
+    },
+    onSignout () {
+      signout();
+      this.currentUser = null;
+      this.unmountCalcom();
     },
   },
 };
